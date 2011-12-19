5d7a4356e16dc09798009dcf6bc0f5e2b423c869
Yida Zhang <hitzhangyd@gmail.com>
2011-12-18 23:19:07 -0500

    add horizon dir and change occmap idx

--- Lib/Util/Matlab/plot_freespace.m
+++ Lib/Util/Matlab/plot_freespace.m
@@ -2,5 +2,6 @@ function [] = plot_freespace( free, scale )
 % TODO: Show freespace boundary in labelB
 hold on;
 plot(free.x,free.y,'m--','LineWidth',2);
+plot(free.hDirX,free.hDirY+free.h,'r--','LineWidth',2);
 hold off;
 end

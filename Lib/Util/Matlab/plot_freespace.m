9a45a0bcbd03ebb06805d49e1a4f4c1c7519da1f
Yida Zhang <hitzhangyd@gmail.com>
2012-01-15 16:21:46 -0500

    matlab show tilted horizon line

--- Lib/Util/Matlab/plot_freespace.m
+++ Lib/Util/Matlab/plot_freespace.m
@@ -1,7 +1,14 @@
 function [] = plot_freespace( free, scale )
 % TODO: Show freespace boundary in labelB
 hold on;
-plot(free.x,free.y,'m--','LineWidth',2);
-plot(free.hDirX,free.hDirY+free.h,'r--','LineWidth',2);
+if (scale == 4)
+    X = free.Bx;
+    Y = free.By;
+else
+    X = free.Ax;
+    Y = free.Ay;
+end
+plot(X,Y,'m--','LineWidth',2);
+%plot(free.hDirX,free.hDirY+free.h,'r--','LineWidth',2);
 hold off;
 end

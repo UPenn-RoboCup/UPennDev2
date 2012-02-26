0eed76904b50ea57bf04923cc72085387427b2de
yida@betty <darwin@betty.localdomain>
2012-02-25 19:43:27 -0500

    standarize send_yuyv2 and remove ip dependency

--- Lib/Platform/OP/Comm/luaOPMonitorCommWired.cc
+++ Lib/Platform/OP/Comm/luaOPMonitorCommWired.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <deque>
 #include "string.h"
@@ -9,6 +10,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <assert.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -24,23 +27,13 @@ extern "C"
 #include <vector>
 #include <stdint.h>
 
-// Set the IP address for where to listen
-//#define IP "255.255.255.255"
-//#define IP "172.18.255.255"
-#define IP "192.168.123.255" // OP wired
-//#define IP "192.168.1.255" // OP, wireless
-//#define IP "192.168.255.255"
-//#define IP "158.130.103.255" // AirPennNet-Guest specific
-//#define IP "158.130.104.255" //AIRPENNET
-//#define IP "10.66.68.255"
-//#define IP "192.168.118.255" // OP, Istanbul
-
 #define PORT 111111
 #define MDELAY 2
 #define TTL 16
 #define MAX_LENGTH 160000 //Size for sending 640*480 yuyv data without resampling
 
 const int maxQueueSize = 6;
+static std::string IP;
 
 static std::deque<std::string> recvQueue;
 static int send_fd, recv_fd;
@@ -55,13 +48,22 @@ void mexExit(void)
 }
 */
 
+static int lua_darwinopcomm_init(lua_State *L) {
+	const char *ip = luaL_checkstring(L, 1);
+	IP = ip;
+ 	return 1;
+}
+
 static int lua_darwinopcomm_update(lua_State *L) {
   static sockaddr_in source_addr;
   static char data[MAX_LENGTH];
 
+	// Check whether initiated
+  assert(IP.empty()!=1);	
+
   static bool init = false;
   if (!init) {
-    struct hostent *hostptr = gethostbyname(IP);
+    struct hostent *hostptr = gethostbyname(IP.c_str());
     if (hostptr == NULL) {
       printf("Could not get hostname\n");
       return -1;
@@ -175,7 +177,11 @@ static int lua_darwinopcomm_send(lua_State *L) {
 
   // TODO: implement send command
   const char *data = luaL_checkstring(L, 1);
-  std::string dataStr(data);
+	std::string header;
+  std::string dataStr;
+	std::string contents(data);
+  header.push_back(11);
+	dataStr = header + contents;
   int ret = send(send_fd, dataStr.c_str(), dataStr.size(), 0);
     
   lua_pushinteger(L, ret);
@@ -311,38 +317,53 @@ static int lua_darwinopcomm_send_yuyv2(lua_State *L) {
   }
   int m = luaL_checkint(L, 2);    //Width (in macro pixel)
   int n = luaL_checkint(L, 3);    //Height
-  int robotid = luaL_checkint(L, 4);    //robot ID
-
+	int teamid = luaL_checkint(L,4);	// team ID
+  int playerid = luaL_checkint(L, 5);	// player ID
 //I had to divide the image into sections and send one by one
 
-  int division = luaL_checkint(L, 5);    //How many division?
-  int section = luaL_checkint(L, 6);	//Which section?    
+  int division = luaL_checkint(L, 6);    //How many division?
+  int section = luaL_checkint(L, 7);	//Which section?    
 
   yuyv=yuyv+ m*(n/division)*section;   //Skip unused section
 
   dataStrY.push_back(18);	//Partitioned yuyv data Y
-  dataStrY.push_back(robotid);	//robot ID
+  dataStrY.push_back(m/256); //High bit of width
+  dataStrY.push_back(m%256); //Low bit of width
+  dataStrY.push_back(n/256); //High bit of height
+  dataStrY.push_back(n%256); //Low bit of height
+	dataStrY.push_back(teamid);	// team ID
+  dataStrY.push_back(playerid);	// player ID
   dataStrY.push_back(division);	//division
   dataStrY.push_back(section);	//section
 
   dataStrU.push_back(19);	//Partitioned yuyv data U
-  dataStrU.push_back(robotid);	//robot ID
+  dataStrU.push_back(m/256); //High bit of width
+  dataStrU.push_back(m%256); //Low bit of width
+  dataStrU.push_back(n/256); //High bit of height
+  dataStrU.push_back(n%256); //Low bit of height
+	dataStrU.push_back(teamid);	// team ID
+  dataStrU.push_back(playerid);	// player ID
   dataStrU.push_back(division);	//division
   dataStrU.push_back(section);	//section
 
   dataStrV.push_back(20);	//Partitioned yuyv data V
-  dataStrV.push_back(robotid);	//robot ID
+  dataStrV.push_back(m/256); //High bit of width
+  dataStrV.push_back(m%256); //Low bit of width
+  dataStrV.push_back(n/256); //High bit of height
+  dataStrV.push_back(n%256); //Low bit of height
+  dataStrV.push_back(teamid);	//team ID
+	dataStrV.push_back(playerid);	// player ID
   dataStrV.push_back(division);	//division
   dataStrV.push_back(section);	//section
 
 
   for (int j = 0; j < n/division; j++) {
         for (int i = 0; i < m; i++) {
-		char indexY= (*yuyv & 0xFC000000) >> 26;
-		char indexU= (*yuyv & 0x0000FC00) >> 10;
-		char indexV= (*yuyv & 0xFC0000FC) >> 2;
-		dataStrY.push_back(indexY);
-		dataStrU.push_back(indexU);
+					char indexY= (*yuyv & 0xFC000000) >> 26;
+					char indexU= (*yuyv & 0x0000FC00) >> 10;
+					char indexV= (*yuyv & 0xFC0000FC) >> 2;
+					dataStrY.push_back(indexY);
+					dataStrU.push_back(indexU);
 	        dataStrV.push_back(indexV);
 	        yuyv++;
         }
@@ -379,6 +400,7 @@ static int lua_darwinopcomm_send_particle(lua_State *L) {
 
 
 static const struct luaL_reg OPMonitorCommWired_lib [] = {
+  {"init", lua_darwinopcomm_init},
   {"size", lua_darwinopcomm_size},
   {"receive", lua_darwinopcomm_receive},
   {"send", lua_darwinopcomm_send},

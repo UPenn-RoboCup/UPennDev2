634e0e5b4207f6a46201e0ee1bcdf1d529e45c81
SJ <seungjoon.yi@gmail.com>
2012-03-13 03:16:40 +0900

    WebotsController/start.lua

--- WebotsController/start.lua
+++ WebotsController/start.lua
@@ -5,9 +5,9 @@ print("\nStarting Webots Lua controller...");
 playerID = os.getenv('PLAYER_ID') + 0;
 teamID = os.getenv('TEAM_ID') + 0;
 
-dofile("Player/player.lua");
+--dofile("Player/player.lua");
 -- Run test_vision
-dofile("Player/Test/test_vision_webots_op.lua");
---dofile("Player/Test/test_walk_webots_op.lua");
+--dofile("Player/Test/test_vision_webots_op.lua");
+dofile("Player/Test/test_walk_webots_op.lua");
 --dofile("Player/Test/test_punch_webots_op.lua");
 

032000faeb8052cfa77a027ad46310966c198a38
Yida Zhang <hitzhangyd@gmail.com>
2012-03-15 13:56:00 -0400

    remove temp files

--- Lib/Util/Matlab/gauss_distribution.m
+++
@@ -1,9 +0,0 @@
-function f = gauss_distribution(x, mu, s)
-p1 = -.5 * ((x - mu)/s) .^ 2;
-p2 = (s * sqrt(2*pi));
-f = exp(p1) ./ p2; 
-
-a = -100; b = 100;
-x = a + (b-a) * rand(1, 500);
-m = (a + b)/2;
-s = 30; 

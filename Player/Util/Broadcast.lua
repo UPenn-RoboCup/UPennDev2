08c46cff21f7f0d1fee5cfe46ea462c1681eac3a
yida@betty <darwin@betty.localdomain>
2012-03-15 14:30:18 -0400

    working data sending server end

--- Player/Util/Broadcast.lua
+++ Player/Util/Broadcast.lua
@@ -15,6 +15,10 @@ require('serialization');
 require('ImageProc')
 require('Config');
 
+--sendShm = {'wcm','vcm','gcm'}
+sendShm = { wcmshm=wcm, gcmshm=gcm, vcmshm=vcm }
+itemReject = 'yuyv,labelA,labelB'
+
 -- Initiate Sending Address
 Comm.init(Config.dev.ip_wired,111111);
 print('Sending to',Config.dev.ip_wired);
@@ -84,72 +88,27 @@ function sendImg()
 
 end
 
-function update_new(enable)
-	if enable == 0 then return; end
-        local g = wcm.get_goal()
---        util.ptable( g );
---        util.ptable( g.attack );
-	for key,value in pairs(wcm.shared) do 
-	--	if (string.find(key,'get')) then
-			print('\nget_',key);
-                util.ptable( wcm['get_'..key]() )
-	--	end
-
-	end	
-end
-
 function update(enable)
-  if enable==0 then return; end
-  
-  send = {};
-
-  send.robot = {};
---[[
-  local robotpose = wcm.get_robot_pose();
-  send.robot.pose = {x=robotpose[1], y=robotpose[2], a=robotpose[3]};
---]]
-  send.robot.pose = wcm.get_pose();
-
-  send.ball = {};
-  send.ball.detect = vcm.get_ball_detect();
-  local ballcentroid = vcm.get_ball_centroid();
-  send.ball.centroid = {x=ballcentroid[1], y=ballcentroid[2]};
-  send.ball.axisMajor = vcm.get_ball_axisMajor();
-  send.ball.axisMinor = vcm.get_ball_axisMinor();
-  local ballxy = wcm.get_ball_xy();
-  send.ball.x = ballxy[1];
-  send.ball.y = ballxy[2];
-  send.ball.t = wcm.get_ball_t();
-
-  send.goal = {};
-  send.goal.detect = vcm.get_goal_detect();
-  send.goal.color = vcm.get_goal_color();
-  send.goal.type = vcm.get_goal_type();
-  local goalv1 = vcm.get_goal_v1();
-  send.goal.v1 = {x=goalv1[1], y=goalv1[2], z=goalv1[3], scale=goalv1[4]};
-  local goalv2 = vcm.get_goal_v2();
-  send.goal.v2 = {x=goalv2[1], y=goalv2[2], z=goalv2[3], scale=goalv2[4]};
-  local bb1 = vcm.get_goal_postBoundingBox1();
-  send.goal.postBoundingBox1 = {x1=bb1[1], x2=bb1[2], y1=bb1[3], y2=bb1[4]};
-  local bb2 = vcm.get_goal_postBoundingBox2();
-  send.goal.postBoundingBox2 = {x1=bb2[1], x2=bb2[2], y1=bb2[3], y2=bb2[4]};
-
-  send.time = unix.time();
-
-  send.team = {};
-  send.team.number = gcm.get_team_number();
-  send.team.player_id = gcm.get_team_player_id();
-  send.team.color = gcm.get_team_color();
-  send.team.role = gcm.get_team_role();
-  send.team.attackBearing = wcm.get_attack_bearing();
-  send.team.penalty = gcm.get_game_penalty( gcm.get_team_player_id() );
-
+	if enable == 0 then return; end
+	
+	send = {};	
+	for shmHandlerkey,shmHandler in pairs(sendShm) do
+		send[shmHandlerkey] = {};
+		for sharedkey,sharedvalue in pairs(shmHandler.shared) do
+			send[shmHandlerkey][sharedkey] = {};
+			for itemkey,itemvalue in pairs(shmHandler.shared[sharedkey]) do
+				if string.find(itemReject, itemkey) == nil then
+					send[shmHandlerkey][sharedkey][itemkey] = shmHandler['get_'..sharedkey..'_'..itemkey]();
+				end
+			end
+		end
+	
+	end
   Comm.send(serialization.serialize(send));
-  
+
 end
 
 function update_img( enable, imagecount )
-  local division = 4; -- for image sending part by part
   if(enable==2) then
 --    sendB();
 --    sendImg(); -- half of sub image

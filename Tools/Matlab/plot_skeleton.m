a680b34ed0c2d175d9221220c3ad7cd518e2abb5
Stephen McGill <smcgill3@seas.upenn.edu>
2012-03-23 10:00:27 -0400

    Adding an absolute to local rotation function.  Recentering at waist.

--- Tools/Matlab/plot_skeleton.m
+++ Tools/Matlab/plot_skeleton.m
@@ -7,7 +7,7 @@ end
 run_once = 0;
 
 jointNames = { ...
-    'Head', 'Neck', 'Torso', 'Waist', ...
+    'Waist', 'Torso', 'Head', 'Neck', ...
     'CollarL','ShoulderL', 'ElbowL', 'WristL', 'HandL', 'FingerL', ...
     'CollarR','ShoulderR', 'ElbowR', 'WristR', 'HandR', 'FingerR', ...
     'HipL', 'KneeL', 'AnkleL', 'FootL', ...
@@ -22,6 +22,7 @@ positions = zeros(nJoints,3);
 rots = zeros(3,3,nJoints);
 confs = zeros(nJoints,2);
 axis_angles = zeros(nJoints,4);
+axis_angles_loc = zeros(nJoints,4);
 scale = 10;
 
 figure(1);clf;
@@ -36,15 +37,20 @@ while(1)
         axis_angles(j,:) = vrrotmat2vec(joint.rot);
         confs(j,:) = joint.confidence;
     end
-    positions = positions - repmat(positions(3,:),nJoints,1);
-    axis_angles2 = axis_angles(:,1:3) .* repmat(axis_angles(:,4),1,3);
+    [ local_rots ] = abs2local_rot( rots );
+    for j=1:nJoints
+        axis_angles_loc(j,:) = vrrotmat2vec(local_rots(:,:,j));
+    end
+    positions = positions - repmat(positions(1,:),nJoints,1); % Center at waist
+    %axis_angles2 = axis_angles(:,1:3) .* repmat(axis_angles(:,4),1,3);
+    axis_angles2 = axis_angles_loc(:,1:3) .* repmat(axis_angles_loc(:,4),1,3) .* repmat(confs(:,2)~=0,1,3);
     clf;
     plot3( positions(:,1), positions(:,2), positions(:,3), 'o', ...
         'MarkerEdgeColor','k', 'MarkerFaceColor',[.49 1 .63], 'MarkerSize',10 );
     hold on;
     quiver3(positions(:,1), positions(:,2), positions(:,3), axis_angles2(:,1),axis_angles2(:,2),axis_angles2(:,3),'b-','LineWidth',3 );
     view(0,90);
-    axis([-1000 1000 -1300 500 -1000 1000]);
+    axis([-1000 1000 -1300 700 -1000 1000]);
     %plot( positions(:,1), positions(:,2), 'o', ...
     %    'MarkerEdgeColor','k', 'MarkerFaceColor',[.49 1 .63], 'MarkerSize',10 );
     

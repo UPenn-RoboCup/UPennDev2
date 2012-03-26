f9da74bcf9ed13c1b8335c3b2e21af6db18f17ed
SJ <seungjoon.yi@gmail.com>
2012-03-26 17:53:18 -0400

    Added test_main_webots to test game FSM behaviors

--- Player/Test/test_vision_webots.lua
+++ Player/Test/test_vision_webots.lua
@@ -39,14 +39,9 @@ require('vector')
 
 require('BodyFSM');
 require('HeadFSM');
-require('getch')
 require('Motion');
 require('walk');
 require('HeadTransform')
---[[
-require('Broadcast')
-require('Comm')
---]]
 require('Speak')
 require('vcm')
 require('Vision')
@@ -68,194 +63,125 @@ if (string.find(Config.platform.name,'Webots')) then
   webots = true;
 end
 
-getch.enableblock(1);
---unix.usleep(1E6*1.0);
-
 -- initialize state machines
 HeadFSM.entry();
 Motion.entry();
-
 World.entry();
---Team.entry();
 Vision.entry();
 OccupancyMap.entry();
 
-Body.set_head_hardness({0.4,0.4});
 HeadFSM.sm:set_state('headScan');
-
---BodyFSM.sm:set_state('bodySearch');
---Motion.sm:set_state('stance');
---walk.entry();
---Motion.sm:set_state('sit');
-
+Body.set_head_hardness({0.4,0.4});
 controller.wb_robot_keyboard_enable(100);
+
 -- main loop
+headsm_running=0;
+bodysm_running=0;
+
 count = 0;
-vcmcount=0;
-local t0=Body.get_time();
-local last_update_time=t0;
-local headangle=vector.new({0,10*math.pi/180});
-local headsm_running=0;
-local bodysm_running=0;
-local last_vision_update_time=t0;
+t0=Body.get_time();
+last_update_time=t0;
+last_vision_update_time=t0;
+
+headangle=vector.new({0,10*math.pi/180});
 targetvel=vector.zeros(3);
-t_update=2;
-
 vision_update_interval = 0.04; --25fps update
-
-
---Motion.fall_check=0;
---Motion.fall_check=1;
-broadcast_enable=0;
-ballcount,visioncount,imagecount=0,0,0;
-hires_broadcast=0;
-
-cameraparamcount=1;
-broadcast_count=0;
-buttontime=0;
-
-function print_debug()
-  local t = Body.get_time();
-  local ball = wcm.get_ball();
-  local goal = wcm.get_goal_attack();
-  local robot = wcm.get_pose();
---  local goalRelative = util.pose_relative(goal, {robot.x, robot.y, robot.a})
---  util.ptable(goalRelative);
-  --print(string.format("Goal: (%f, %f) seen %.1f sec ago",goal.attack, t-goal.t));
---  local occr = wcm.get_occmap_r();
---  print(unpack(occr));
---  local block = vcm.get_freespace_block();
---  print(block);
-  print(string.format("Ball: (%f, %f) seen %.1f sec ago", ball.x, ball.y, t-ball.t));
-end
 
 function process_keyinput()
   local str = controller.wb_robot_keyboard_get_key();
   if str>0 then
     byte = str;
-	-- Webots only return captal letter number
-	if byte>=65 and byte<=90 then
-		byte = byte + 32;
-	end
---[[
-local str=getch.get();
-  if #str>0 then
-	local byte=string.byte(str,1);
---]]
-	-- Move the head around
-	if byte==string.byte("w") then
-		headsm_running=0;
-		headangle[2]=headangle[2]-5*math.pi/180;
-	elseif byte==string.byte("a") then
-		headangle[1]=headangle[1]+5*math.pi/180;
-		headsm_running=0;
-	elseif byte==string.byte("s") then	
-		headangle[1],headangle[2]=0,0;
-		headsm_running=0;
-	elseif byte==string.byte("d") then
-		headangle[1]=headangle[1]-5*math.pi/180;
-		headsm_running=0;
-	elseif byte==string.byte("x") then	
-		headangle[2]=headangle[2]+5*math.pi/180;
-		headsm_running=0;
-	elseif byte==string.byte("e") then	
-		headangle[2]=headangle[2]-1*math.pi/180;
-		headsm_running=0;
-	elseif byte==string.byte("c") then	
-		headangle[2]=headangle[2]+1*math.pi/180;
-		headsm_running=0;
-  elseif byte==string.byte("v") then
+    -- Webots only return captal letter number
+    if byte>=65 and byte<=90 then
+      byte = byte + 32;
+    end
+    --Turn the head around
 
+    if byte==string.byte("w") then
+      headsm_running=0;
+      headangle[2]=headangle[2]-5*math.pi/180;
+    elseif byte==string.byte("a") then
+      headangle[1]=headangle[1]+5*math.pi/180;
+      headsm_running=0;
+    elseif byte==string.byte("s") then	
+      headangle[1],headangle[2]=0,0;
+      headsm_running=0;
+    elseif byte==string.byte("d") then
+      headangle[1]=headangle[1]-5*math.pi/180;
+      headsm_running=0;
+    elseif byte==string.byte("x") then	
+      headangle[2]=headangle[2]+5*math.pi/180;
+      headsm_running=0;
+    elseif byte==string.byte("e") then	
+      headangle[2]=headangle[2]-1*math.pi/180;
+      headsm_running=0;
+    elseif byte==string.byte("c") then	
+      headangle[2]=headangle[2]+1*math.pi/180;
+      headsm_running=0;
 
-
---    HeadFSM.sm:set_state('headFigure8');
-
---[[
-  --Broadcast selection
-  elseif byte==string.byte("g") then
-    local mymod = 2;
-    mymod = 4;
-    broadcast_enable=(broadcast_enable+1)%mymod;
-		print("Broadcast:", broadcast_enable);
---]]
   -- Walk velocity setting
-	elseif byte==string.byte("i") then	targetvel[1]=targetvel[1]+0.02;
-	elseif byte==string.byte("j") then	targetvel[3]=targetvel[3]+0.1;
-	elseif byte==string.byte("k") then	targetvel[1],targetvel[2],targetvel[3]=0,0,0;
-	elseif byte==string.byte("l") then	targetvel[3]=targetvel[3]-0.1;
-	elseif byte==string.byte(",") then	targetvel[1]=targetvel[1]-0.02;
-	elseif byte==string.byte("h") then	targetvel[2]=targetvel[2]+0.02;
-	elseif byte==string.byte(";") then	targetvel[2]=targetvel[2]-0.02;
+    elseif byte==string.byte("i") then	targetvel[1]=targetvel[1]+0.02;
+    elseif byte==string.byte("j") then	targetvel[3]=targetvel[3]+0.1;
+    elseif byte==string.byte("k") then	targetvel[1],targetvel[2],targetvel[3]=0,0,0;
+    elseif byte==string.byte("l") then	targetvel[3]=targetvel[3]-0.1;
+    elseif byte==string.byte(",") then	targetvel[1]=targetvel[1]-0.02;
+    elseif byte==string.byte("h") then	targetvel[2]=targetvel[2]+0.02;
+    elseif byte==string.byte(";") then	targetvel[2]=targetvel[2]-0.02;
+
   -- HeadFSM setting
     elseif byte==string.byte("`") then
-		headsm_running = 1-headsm_running;
-		if (headsm_running == 1) then
-		  HeadFSM.sm:set_state('headSweep');
-		end
-	elseif byte==string.byte("1") then	
-		headsm_running = 1-headsm_running;
-    if( headsm_running==1 ) then
-  		HeadFSM.sm:set_state('headScan');
-    end
-	elseif byte==string.byte("2") then
-    headsm_running = 0; -- Turn off the head state machine
-    -- HeadTransform
-    local ball = wcm.get_ball();
-    local trackZ = Config.vision.ball_diameter; -- Look a little above the ground
-    -- TODO: Nao needs to add the camera select
-    headangle = vector.zeros(2);
-    headangle[1],headangle[2] = HeadTransform.ikineCam(ball.x, ball.y, trackZ);
-    print("Head Angles for looking directly at the ball", unpack(headangle*180/math.pi));
+      headsm_running = 1-headsm_running;
+      if (headsm_running == 1) then
+        HeadFSM.sm:set_state('headSweep');
+      end
+    elseif byte==string.byte("1") then	
+      headsm_running = 1-headsm_running;
+      if( headsm_running==1 ) then
+        HeadFSM.sm:set_state('headScan');
+      end
+    elseif byte==string.byte("2") then
+      headsm_running = 0; -- Turn off the head state machine
+      -- HeadTransform
+      local ball = wcm.get_ball();
+      local trackZ = Config.vision.ball_diameter; -- Look a little above the ground
+      -- TODO: Nao needs to add the camera select
+      headangle = vector.zeros(2);
+      headangle[1],headangle[2] = HeadTransform.ikineCam(ball.x, ball.y, trackZ);
+      print("Head Angles for looking directly at the ball", unpack(headangle*180/math.pi));
 
-	elseif byte==string.byte("3") then	
-		if Config.game.robotID==9 then
-			local ball = vcm.ball;
-			pickup.throw=0;
-			Motion.event("pickup");
-		else
-			kick.set_kick("kickForwardLeft");
-			Motion.event("kick");
-		end
-	elseif byte==string.byte("4") then	
-		if Config.game.robotID==9 then
-			pickup.throw=1;
-			Motion.event("pickup");
-		else
-			kick.set_kick("kickForwardRight");
-			Motion.event("kick");
-		end
+    elseif byte==string.byte("3") then	
+      kick.set_kick("kickForwardLeft");
+      Motion.event("kick");
+    elseif byte==string.byte("4") then	
+      kick.set_kick("kickForwardRight");
+      Motion.event("kick");
 
+   elseif byte==string.byte("5") then	--Turn on body SM
+     headsm_running=1;
+     bodysm_running=1;
+     BodyFSM.sm:set_state('bodySearch');   
+     HeadFSM.sm:set_state('headScan');
+   elseif byte==string.byte("6") then	--Kick head SM
+     headsm_running=1;
+     HeadFSM.sm:set_state('headKick');
+   elseif byte==string.byte("7") then	Motion.event("sit");
+   elseif byte==string.byte("8") then	
+     if walk.active then walk.stop();end
+     Motion.event("standup");
+     bodysm_running=0;
+   elseif byte==string.byte("9") then	
+     Motion.event("walk");
+     walk.start();
+   end
 
-	elseif byte==string.byte("5") then	--Turn on body SM
-		headsm_running=1;
-		bodysm_running=1;
-   	        BodyFSM.sm:set_state('bodySearch');   
-		HeadFSM.sm:set_state('headScan');
-	elseif byte==string.byte("6") then	--Kick head SM
-		headsm_running=1;
-		HeadFSM.sm:set_state('headKick');
-	elseif byte==string.byte("7") then	Motion.event("sit");
-	elseif byte==string.byte("8") then	
-		if walk.active then 
-			walk.stopAlign();
-		end
-		Motion.event("standup");
-	        bodysm_running=0;
-		
-	elseif byte==string.byte("9") then	
-		Motion.event("walk");
-		walk.start();
-	end
+   walk.set_velocity(unpack(targetvel));
 
-	walk.set_velocity(unpack(targetvel));
-	
-	-- Apply manul control head angle
-	if headsm_running == 0 then
-		Body.set_head_command(headangle);
-	end
-	print("Headangle:", headangle[2]*180/math.pi)
-  end
-
+   -- Apply manul control head angle
+   if headsm_running == 0 then
+     Body.set_head_command(headangle);
+     print("Headangle:", headangle[2]*180/math.pi)
+   end
+ end
 end
 
 function update()
@@ -277,12 +203,10 @@ function update()
   if imageProcessed then 
     World.update_vision();
     OccupancyMap.update();
---    Team.update();
   end
    
   -- Update the relevant engines
   Body.update();
-
   Motion.update();
 
   -- Update the HeadFSM if it is running
@@ -297,33 +221,9 @@ function update()
   
   -- Get a keypress
   process_keyinput();
---[[
-  -- Send updates over the network
-  Broadcast.update(broadcast_enable);
---]]
 end
 
-local tDelay=0.002*1E6;
-local ncount = 100;
-local tUpdate = Body.get_time();
 while 1 do
-  count = count + 1;
-  
   update();
-
-  -- Show FPS
-  local t = Body.get_time();
-  if(count==ncount) then
-    local fps = ncount/(t-tUpdate);
-    tUpdate = t;
-    count = 1;
-    print(fps.." FPS")
-    -- Print the debug information
-    --print_debug();
-  end
-
-  --Wait until dcm has done reading/writing
---  unix.usleep(tDelay);
-
 end
 

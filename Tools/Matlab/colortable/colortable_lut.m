6311ba02ed337e23eaf1028b9ed31acde7d946cd
SJ <seungjoon.yi@gmail.com>
2012-03-27 20:21:53 -0400

    Added online Label view mode for colortable

--- Tools/Matlab/colortable/colortable_lut.m
+++ Tools/Matlab/colortable/colortable_lut.m
@@ -10,12 +10,18 @@ if (nargin < 1)
   score_min = 0.1;
 end
 
+
+%For real-time label view, we store smeared score to another matrix
+%And generate lut from that matrix
+
 % initialize LUT array
-nlut = size(COLORTABLE.score, 1);
+%nlut = size(COLORTABLE.score, 1);
+nlut = size(COLORTABLE.score_smeared, 1);
 lut = zeros(nlut, 1, 'uint8');
 
 % find maximum score across colors
-[ymax, imax] = max(COLORTABLE.score, [], 2);
+%[ymax, imax] = max(COLORTABLE.score, [], 2);
+[ymax, imax] = max(COLORTABLE.score_smeared, [], 2);
 
 % YUV color indices that have a score above the min threshold
 ivalid = (ymax > score_min);

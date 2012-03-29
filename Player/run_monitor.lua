0c3f2d776f8f275841b762f74083a0adff56502c
SJ@Scarface <seungjoon.yi@gmail.com>
2012-03-29 14:05:28 -0400

    Added yuyv downsampling for monitor

--- Player/run_monitor.lua
+++ Player/run_monitor.lua
@@ -45,10 +45,6 @@ local tUpdate = t0;
 local maxFPS = 10;
 local imgFPS = 5;
 
-local maxFPS = 20;
-local imgFPS = 20;
-
-
 local maxPeriod = 1.0 / maxFPS;
 local imgRate = math.max( math.floor( maxFPS / imgFPS ), 1);
 
@@ -83,8 +79,11 @@ while true do
   update();
   -- Get time after sending packets
   tloop = unix.time() - tstart;  -- Sleep in order to get the right FPS
-  if (tloop < 0.1) then
-    unix.usleep((0.1 - tloop)*(1E6));
+
+--print("tloop:",tloop)
+
+  if (tloop < maxPeriod) then
+    unix.usleep((maxPeriod - tloop)*(1E6));
   end
 
   -- Display our FPS and broadcast level

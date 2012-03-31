307eb3135f50120a988ce20ee4f49151a95d7f12
SJ@Scarface <seungjoon.yi@gmail.com>
2012-03-31 14:54:46 -0400

    Consolidated and parameterized detection codes

--- Player/Config/Vision/Config_Nao_Vision.lua
+++ Player/Config/Vision/Config_Nao_Vision.lua
@@ -1,6 +1,5 @@
 module(..., package.seeall);
 
-require('vector')
 
 -- Vision Parameters
 
@@ -12,11 +11,11 @@ color.field = 8;
 color.white = 16;
 
 vision = {};
+vision.ballColor = color.orange;
+vision.goal1Color = color.yellow;
+vision.goal2Color = color.cyan;
 vision.maxFPS = 30;
 vision.scaleB = 4;
-vision.ball_diameter = 0.065;
-vision.ball_height_max = -0.20;
-vision.yellow_goal_count_thres = 150;
 
 -- use this to enable line detection
 vision.enable_line_detection = 1;
@@ -24,8 +23,6 @@ vision.enable_line_detection = 1;
 vision.enable_spot_detection = 0;
 -- use this to enable midfield landmark detection
 vision.enable_midfield_landmark_detection = 0;
--- Enable the Velocity filter
-vision.enable_velocity_detection = 0;
 -- use this to enable copying images to shm (for colortables, testing)
 vision.copy_image_to_shm = 1;
 -- use this to enable storing all images
@@ -34,15 +31,46 @@ vision.store_all_images = 1;
 vision.store_goal_detections = 0;
 -- use this to enable storing images where the ball was detected
 vision.store_ball_detections = 0;
--- use this to enable ground check 
-vision.check_for_ground = 1;
-
--- use this to substitute goal check with blue/yellow ball check
+-- Subsample image?
+vision.subsampling = 1;
+--Use yellow/cyan Ball instead of posts 
 vision.use_point_goal = 0;
 
--- use this to use top post to determine the type of post
-vision.use_centerpost = 1;
+--Vision parameter values
+--For 320*240 labelA
+vision.ball={};
+vision.ball.diameter = 0.065;
+vision.ball.th_min_color = 2;
+vision.ball.th_min_color2 = 2;
+vision.ball.th_min_fill_rate = 0.35;
+vision.ball.th_height_max  = 0.20;
+vision.ball.th_ground_boundingbox = {-15,15,0,10};
+vision.ball.th_min_green1 = 100;
+vision.ball.th_min_green2 = 40;
 
-vision.ballColor = color.orange;
-vision.goal1Color = color.yellow;
-vision.goal2Color = color.cyan;
+vision.ball.check_for_ground = 1;
+
+
+--Vision check values
+--For 320*240 labelA 
+vision.goal={};
+vision.goal.th_min_color_count=25;
+vision.goal.th_nPostB = 3;
+vision.goal.th_min_areaB = 10;
+vision.goal.th_min_orientation = 60*math.pi/180;
+vision.goal.th_min_fill_extent=0.35;
+vision.goal.th_aspect_ratio={2.5,15};
+vision.goal.th_edge_margin= 2;
+vision.goal.th_bottom_boundingbox=0.9;
+vision.goal.th_ground_boundingbox={-7,7,-7,5}; 
+vision.goal.th_min_green_ratio = 0.2;
+vision.goal.th_min_bad_color_ratio = 0.1;
+vision.goal.th_goal_separation = {0.35,3.0}; --for OP
+vision.goal.th_min_area_unknown_post = 40;
+
+-- use this to substitute goal check with blue/yellow ball check
+vision.goal.cut_top_post = 0;
+vision.goal.use_centerpost = 1;
+vision.goal.check_for_ground = 1;
+
+

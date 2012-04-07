336c14c18ee7909b8e36e61fed886d352f9c8002
SJ <seungjoon.yi@gmail.com>
2012-04-07 16:11:55 -0400

    Tested master with Nao

--- Player/Config/Walk/Config_Nao_Walk_Blimp_Room.lua
+++ Player/Config/Walk/Config_Nao_Walk_Blimp_Room.lua
@@ -73,9 +73,9 @@ walk.walkKickVel = {0.03, 0.08} --step / kick / follow
 walk.walkKickSupportMod = {{-0.03,0},{-0.03,0}}
 walk.walkKickHeightFactor = 3.0;
 
-walk.sideKickVel1 = {0.04,0.04};
-walk.sideKickVel2 = {0.09,0.05};
-walk.sideKickVel3 = {0.09,-0.02};
+walk.sideKickVel1 = {0.04,0.04,0};
+walk.sideKickVel2 = {0.09,0.05,0};
+walk.sideKickVel3 = {0.09,-0.02,0};
 walk.sideKickSupportMod = {{0,0},{0,0}};
 walk.tStepSideKick = 0.70;
 

a08bfeb61d8efc37bbd2922d51efa25abdc76e08
yida@betty <darwin@betty.localdomain>
2012-04-09 05:59:52 -0400

    Added realtime servo calibration using shm

--- Player/Config/calibration.lua
+++ Player/Config/calibration.lua
@@ -51,21 +51,21 @@ cal["betty"].headPitchComp = 0.005;
 
 cal["linus"].servoBias={3,1,2,1,1,-3,-8,3,-13,-4,1,-5};
 cal["linus"].footXComp = 0.00;
-cal["linus"].footYComp = 0.002,
-cal["linus"].kickXComp = -0.005,
-cal["linus"].headPitchComp = 0.005,
+cal["linus"].footYComp = 0.002;
+cal["linus"].kickXComp = -0.005;
+cal["linus"].headPitchComp = 0.005;
 
-cal["lucy"].servoBias={1,-28,-207,108,-26,-15,-43,-51,110,35,84,-29},
-cal["lucy"].footXComp = 0.002,
-cal["lucy"].footYComp = 0.002,
-cal["lucy"].kickXComp = -0.005,
-cal["lucy"].headPitchComp = 0.00,
+cal["lucy"].servoBias={1,-28,-207,108,-26,-15,-43,-51,110,35,84,-29};
+cal["lucy"].footXComp = 0.002;
+cal["lucy"].footYComp = 0.002;
+cal["lucy"].kickXComp = -0.005;
+cal["lucy"].headPitchComp = 0.00;
 
-cal["scarface"].servoBias={0,0,0,0,0,0,0,0,0,-9,-4,0},
-cal["scarface"].footXComp = -0.005,
-cal["scarface"].footYComp = 0.00,
-cal["scarface"].kickXComp = -0.005,
-cal["scarface"].headPitchComp = 0.00,
+cal["scarface"].servoBias={0,0,0,0,0,0,0,0,0,-9,-4,0};
+cal["scarface"].footXComp = -0.005;
+cal["scarface"].footYComp = 0.00;
+cal["scarface"].kickXComp = -0.005;
+cal["scarface"].headPitchComp = 0.00;
 
 ------------------------------------------------------------
 

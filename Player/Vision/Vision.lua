59acb1d865cf98065559e1e8f6182658426a5894
SJ@Scarface <seungjoon.yi@gmail.com>
2012-04-11 14:44:19 -0400

    Fixed monitor issues

--- Player/Vision/Vision.lua
+++ Player/Vision/Vision.lua
@@ -142,8 +142,12 @@ function update()
   else
     return false; 
   end
+
+--SJ: Camera image keeps changing
+--So copy it here to shm, and use it for all vision process
+  vcm.set_image_yuyv(camera.image);
   vcm.refresh_debug_message();
-    
+
   -- Add timer measurements
   count = count + 1;
 
@@ -159,7 +163,7 @@ function update()
   if(webots) then
     labelA.data = Camera.get_labelA( carray.pointer(camera.lut) );
   else
-    labelA.data  = ImageProc.yuyv_to_label(camera.image,
+    labelA.data  = ImageProc.yuyv_to_label(vcm.get_image_yuyv(),
                                           carray.pointer(camera.lut),
                                           camera.width/2,
                                           camera.height);
@@ -201,19 +205,24 @@ function update_shm(status)
             and vcm.get_debug_store_ball_detections() == 1)
         or ((goalCyan.detect == 1 or goalYellow.detect == 1) 
             and vcm.get_debug_store_goal_detections() == 1)) then
-      vcm.set_image_labelA(labelA.data);
-      vcm.set_image_labelB(labelB.data);
-      vcm.set_image_yuyv(camera.image);
 
-      --Store downsampled yuyv for monitoring
-      if subsampling>0 then
-        vcm.set_image_yuyv2(ImageProc.subsample_yuyv2yuyv(
-  	  camera.image,camera.width/2, camera.height,2));
-      end
-      if subsampling2>0 then --1/4 sized image, for OP
-        vcm.set_image_yuyv3(ImageProc.subsample_yuyv2yuyv(
-  	  camera.image,camera.width/2, camera.height,4));
-      end
+        vcm.set_image_labelA(labelA.data);
+        vcm.set_image_labelB(labelB.data);
+--        vcm.set_image_yuyv(camera.image);
+        --Store downsampled yuyv for monitoring
+
+        if subsampling>0 then
+          vcm.set_image_yuyv2(ImageProc.subsample_yuyv2yuyv(
+	  vcm.get_image_yuyv(),
+--  	  camera.image,
+	  camera.width/2, camera.height,2));
+        end
+        if subsampling2>0 then --1/4 sized image, for OP
+          vcm.set_image_yuyv3(ImageProc.subsample_yuyv2yuyv(
+--  	  camera.image,
+	  vcm.get_image_yuyv(),
+	  camera.width/2, camera.height,4));
+        end
 
     end
   end

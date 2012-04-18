0e01f089c8599fe4d37987d8304953c35ed0dbc8
Stephen McGill <smcgill3@seas.upenn.edu>
2012-04-17 20:37:50 -0400

    New colortable for OP with only green post

--- Player/Config/Vision/Config_OP_Camera_Grasp.lua
+++ Player/Config/Vision/Config_OP_Camera_Grasp.lua
@@ -54,5 +54,6 @@ camera.param[5] = {key='white balance temperature', val={1000}};
 camera.param[6] = {key='sharpness',     val={0}};
 camera.param[7] = {key='exposure (absolute)',      val={1005}};
 
-camera.lut_file = 'lut_low_contrast_pink_n_green.raw';
+--camera.lut_file = 'lut_low_contrast_pink_n_green.raw';
+camera.lut_file = 'lut_Grasp_Greenonly.raw';
 

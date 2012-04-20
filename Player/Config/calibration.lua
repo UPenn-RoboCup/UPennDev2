fa00137c83d94f825b1c2e6675654c5ca992a010
DARwIn <darwin@linus.(none)>
2012-04-20 09:49:59 -0400

    goal detection fix and new lut file

--- Player/Config/calibration.lua
+++ Player/Config/calibration.lua
@@ -116,3 +116,8 @@ cal["lucy"].kickXComp=0.005;
 cal["lucy"].servoBias={19,3,4,20,19,10,-15,11,-20,0,6,6,};
 cal["lucy"].footXComp=0.006;
 cal["lucy"].kickXComp=0.005;
+
+-- Updated date: Fri Apr 20 00:00:29 2012
+cal["linus"].servoBias={3,1,2,1,1,-3,-8,3,-13,-4,1,-5,};
+cal["linus"].footXComp=-0.003;
+cal["linus"].kickXComp=0.000;

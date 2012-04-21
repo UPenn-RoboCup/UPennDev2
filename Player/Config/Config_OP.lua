b35ca12ce27f2028343f0df6c7ad3877713c4fba
Yida Zhang <hitzhangyd@gmail.com>
2012-04-21 12:42:31 -0400

    enable line detection

--- Player/Config/Config_OP.lua
+++ Player/Config/Config_OP.lua
@@ -80,7 +80,7 @@ fsm.head = {'GeneralPlayer'};
 fsm.body = {'GeneralPlayer'};
 
 --Behavior flags, should be defined in FSM Configs but can be overrided here
-fsm.enable_obstacle_detection = 0;
+fsm.enable_obstacle_detection = 1;
 fsm.kickoff_wait_enable = 0;
 --fsm.playMode = 3; --1 for demo, 2 for orbit, 3 for direct approach
 --fsm.enable_walkkick = 1;

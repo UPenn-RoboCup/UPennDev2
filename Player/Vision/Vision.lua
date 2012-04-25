77a190e2c4e1b24e0f0585f23dd78c1111e3402d
SJ@AW <seungjoon.yi@gmail.com>
2012-04-24 22:22:12 -0400

    Added FOV check for ball detect emulation

--- Player/Vision/Vision.lua
+++ Player/Vision/Vision.lua
@@ -178,6 +178,7 @@ end
 function update()
   --If we are only using gps info, skip whole vision update 	
   if use_gps_only>0 then
+    update_gps_only();
     return true;
   end
 
@@ -186,7 +187,6 @@ function update()
 
   -- get image from camera
   camera.image = Camera.get_image();
-
 
   local status = Camera.get_camera_status();
   if status.count ~= lastImageCount then
@@ -247,6 +247,67 @@ function update()
   return true;
 end
 
+function check_side(v,v1,v2)
+  --find the angle from the vector v-v1 to vector v-v2
+  local vel1 = {v1[1]-v[1],v1[2]-v[2]};
+  local vel2 = {v2[1]-v[1],v2[2]-v[2]};
+  angle1 = math.atan2(vel1[2],vel1[1]);
+  angle2 = math.atan2(vel2[2],vel2[1]);
+  return util.mod_angle(angle1-angle2);
+end
+
+function update_gps_only()
+  --We are now using ground truth robot and ball pose data
+  headAngles = Body.get_head_position();
+  --TODO: camera select
+--  HeadTransform.update(status.select, headAngles);
+  HeadTransform.update(0, headAngles);
+  
+  --update FOV
+  update_shm_fov()
+
+  --Get GPS coordinate of robot and ball
+  gps_pose = wcm.get_robot_gpspose();
+  ballGlobal=wcm.get_robot_gps_ball();  
+  
+  --Check whether ball is inside FOV
+  ballLocal = util.pose_relative(ballGlobal,gps_pose);
+ 
+  --Get the coordinates of FOV boundary
+  local v_TL = vcm.get_image_fovTL();
+  local v_TR = vcm.get_image_fovTR();
+  local v_BL = vcm.get_image_fovBL();
+  local v_BR = vcm.get_image_fovBR();
+
+--[[
+print("BallLocal:",unpack(ballLocal))
+print("V_TL:",unpack(v_TL))
+print("V_TR:",unpack(v_TR))
+print("V_BL:",unpack(v_BL))
+print("V_BR:",unpack(v_BR))
+print("Check 1:",
+   check_side(v_TL, ballLocal, v_TR));
+print("Check 2:",
+     check_side(v_TL, v_BL, ballLocal) );
+print("Check 3:",
+     check_side(v_BR, v_TR, ballLocal) );
+print("Check 4:",
+     check_side(v_BL, v_BR, ballLocal) );
+--]]
+
+  --Check whether ball is within FOV boundary 
+  if check_side(v_TR, v_TL, ballLocal) < 0 and
+     check_side(v_TL, v_BL, ballLocal) < 0 and
+     check_side(v_BR, v_TR, ballLocal) < 0 and
+     check_side(v_BL, v_BR, ballLocal) < 0 then
+    vcm.set_ball_detect(1);
+  else
+    vcm.set_ball_detect(0);
+  end
+
+
+end
+
 function update_shm(status)
   -- Update the shared memory
   -- Shared memory size argument is in number of bytes
@@ -295,11 +356,11 @@ end
 function update_shm_fov()
   --This function projects the boundary of current labeled image
 
-  local fovC={camera.width/2,camera.height/2};
-  local fovBL={0,camera.height};
-  local fovBR={camera.width,camera.height};
+  local fovC={Config.camera.width/2,Config.camera.height/2};
+  local fovBL={0,Config.camera.height};
+  local fovBR={Config.camera.width,Config.camera.height};
   local fovTL={0,0};
-  local fovTR={camera.width,0};
+  local fovTR={Config.camera.width,0};
 
   vcm.set_image_fovC(vector.slice(HeadTransform.projectGround(
  	  HeadTransform.coordinatesA(fovC,0.1)),1,2));

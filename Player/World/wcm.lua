af7173470661a86c590da31239da896a39740337
Yida Zhang <hitzhangyd@gmail.com>
2012-05-01 04:09:10 -0400

    remove old occupancy map

--- Player/World/wcm.lua
+++ Player/World/wcm.lua
@@ -5,8 +5,6 @@ require('util');
 require('vector');
 require('Config');
 
-enable_occmap = Config.vision.enable_freespace_detection or 0;
-
 -- shared properties
 shared = {};
 shsize = {};
@@ -89,12 +87,6 @@ shared.teamdata.landmark=vector.zeros(10);
 shared.teamdata.landmarkv1=vector.zeros(10);
 shared.teamdata.landmarkv2=vector.zeros(10);
 
-
-if (enable_occmap == 1) then
-	shared.occmap = {};
-	shared.occmap.t = vector.zeros(Config.occmap.div);
-	shared.occmap.r = vector.zeros(Config.occmap.div);
-end
 
 util.init_shm_segment(getfenv(), _NAME, shared, shsize);
 

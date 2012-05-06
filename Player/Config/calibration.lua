23ca7f348a86fad0a31ec4d409649293a3d167fd
Yida@scarface <yida@seas.upenn.edu>
2012-05-06 11:07:28 -0400

    Number of fixes

--- Player/Config/calibration.lua
+++ Player/Config/calibration.lua
@@ -52,39 +52,10 @@ cal["felix"]={
   footXComp = 0,
   footYComp = 0,
   kickXComp = 0,
-  headPitchComp = 0,
+  headPitchComp = 4*math.pi/180;
   armBias={0,0,0,0,0,0},
-  pid = 1,
+  pid = 1, --NEW FIRMWARE
 };
-
-----------------------------------------------------------------
--------------------------------------------------------------
---Default values (may not be needed)
-
-cal["betty"].servoBias={0,0,2,-6,-1,0,0,0,-3,-1,10,0};
-cal["betty"].footXComp = 0.010;
-cal["betty"].footYComp = 0.002;
-cal["betty"].kickXComp = 0.005;
-cal["betty"].headPitchComp = 0.005;
-
-cal["linus"].servoBias={3,1,2,1,1,-3,-8,3,-13,-4,1,-5};
-cal["linus"].footXComp = 0.00;
-cal["linus"].footYComp = 0.002;
-cal["linus"].kickXComp = -0.005;
-cal["linus"].headPitchComp = 0.005;
-
-cal["lucy"].servoBias={1,-28,-207,108,-26,-15,-43,-51,110,35,84,-29};
-cal["lucy"].footXComp = 0.002;
-cal["lucy"].footYComp = 0.002;
-cal["lucy"].kickXComp = -0.005;
-cal["lucy"].headPitchComp = 0.00;
-cal["lucy"].armBias = vector.new({0,-15,0,0,-3,0}) * math.pi/180;
-
-cal["scarface"].servoBias={0,0,0,0,0,0,0,0,0,-9,-4,0};
-cal["scarface"].footXComp = -0.005;
-cal["scarface"].footYComp = 0.00;
-cal["scarface"].kickXComp = -0.005;
-cal["scarface"].headPitchComp = 0.00;
 
 ------------------------------------------------------------
 --Auto-appended calibration settings
@@ -183,3 +154,4 @@ cal["felix"].kickXComp=0.005;
 cal["felix"].servoBias={11,1,0,0,0,0,-9,10,0,0,-6,15,};
 cal["felix"].footXComp=0.001;
 cal["felix"].kickXComp=0.005;
+

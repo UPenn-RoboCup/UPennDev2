b15c1d26d8320e7b2b5be4fd60383b034ad43770
Yida@hook <yida@seas>
2012-05-07 00:30:10 +0000

    enable both color goal for VT field

--- Player/Config/World/Config_Nao_World.lua
+++ Player/Config/World/Config_Nao_World.lua
@@ -54,7 +54,7 @@ world.Lcorner[17]={0.6,0};
 world.Lcorner[18]={-0.6,0};
 
 --same-colored goalposts
-world.use_same_colored_goal=1;
+world.use_same_colored_goal=0;
 
 --should we use new triangulation?
 world.use_new_goalposts=1;

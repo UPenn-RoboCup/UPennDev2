2a8eb5003f474c5b58d6f7cafdbff4832bcdc302
Jordan Brindza <jbrindza@gmail.com>
2012-05-11 11:35:31 -0400

    added header file for cam_util

--- Lib/Platform/NaoV4/Camera/cam_util.cc
+++ Lib/Platform/NaoV4/Camera/cam_util.cc
@@ -1,28 +1,4 @@
-#include <stdio.h>
-#include <errno.h>
-#include <string>
-#include <string.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <pthread.h>
-
-#include <map>
-#include <string>
-#include <vector>
-
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/mman.h> 
-
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <linux/videodev2.h>
-
-typedef unsigned char uint8;
-typedef unsigned int uint32;
-
+#include "cam_util.h"
 
 // initializes the camera at the given dev path
 //  return the file descriptor on success and -1 on error

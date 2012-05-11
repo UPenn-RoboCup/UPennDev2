f5c99b48af6157d0d711ff4f18b71fd27d0608a2
Jordan Brindza <jbrindza@gmail.com>
2012-05-11 12:17:42 -0400

    updated cam_util with grab frame and test to use new cam util functions

--- Lib/Platform/NaoV4/Camera/cam_util.h
+++ Lib/Platform/NaoV4/Camera/cam_util.h
@@ -34,7 +34,7 @@ int init_camera(const char *dev, int width, int height);
 
 // initialize memory map for image buffer
 //  return number of buffers on success and -1 on error
-int init_mmap(int fd, struct v4l2_buffer **v4l2buffers, uint32 **imbuffers);
+int init_mmap(int fd, struct v4l2_buffer **v4l2buffers, uint32 **imbuffers, int nbufDesired);
 
 
 // iterate over all possible camera parameters and print any 
@@ -50,5 +50,21 @@ int set_camera_param(int fd, int id, int value);
 // starts the actual camera stream
 //  return 0 on success and -1 on failure
 int start_stream(int fd);
+
+
+// gets the next frame from the camera if one is available
+//  fd - camera file descriptor
+//  v4l2buffers - array of v4l2 buffer structs for this camera stream
+//  currV4l2Buf - pointer to the current v4l2 buffer
+//  nextV4l2Buf - pointer to the next v4l2 buffer
+//  nextV4l2Buf - pointer to the next v4l2 buffer
+//  nbuf - total number of v4l2 buffers
+//  enqueued - flag indicating if a buffer is currently queued
+//  ibuf - index of the current v4l2 buffer 
+//  nframe - counter for total number of frames 
+//
+//  return 0 on success and -1 on failure
+//
+int grab_frame(int fd, struct v4l2_buffer *v4l2buffers, struct v4l2_buffer *currV4l2Buf, v4l2_buffer *nextV4l2Buf, int nbuf, int &enqueued, int &ibuf, int &nframe);
 
 #endif

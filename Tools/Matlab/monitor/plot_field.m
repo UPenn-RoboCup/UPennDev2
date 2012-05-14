3357068c193e38e88085dda71671f3dd3d96632c
Yida Zhang <hitzhangyd@gmail.com>
2012-05-13 23:00:27 -0400

    fix lua comment in matlab code

--- Tools/Matlab/monitor/plot_field.m
+++ Tools/Matlab/monitor/plot_field.m
@@ -3,7 +3,7 @@ function h = plot_field(handle,type)
 %  cla( handle );
 
   if type==0 % Kidsize
---[[
+%{  
     fieldX = [-3.00  3.00 3.00 -3.00 -3.00];
     fieldY = [-2.00 -2.00 2.00  2.00 -2.00];
     goalX = [3.00 (3.00+0.40) (3.00+0.40) 3.00];
@@ -13,7 +13,7 @@ function h = plot_field(handle,type)
     spotX=1.2;
     circleR = .625;
     fieldB=[-3.5 3.5 -2.5 2.5];
---]]
+%}  
 
 %TEMPORARY VALUES FOR EXPO
     fieldX = [-2.7  2.7 2.7 -2.7 -2.7];

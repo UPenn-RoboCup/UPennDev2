f4ab6f9bbcb541796f434457d10d0ca947beea72
aditya <adityasr@seas.upenn.edu>
2012-05-21 21:33:46 -0400

    Updated color table L512

--- Player/Config/Vision/Config_OP_Camera_L512.lua
+++ Player/Config/Vision/Config_OP_Camera_L512.lua
@@ -32,5 +32,5 @@ camera.param[5] = {key='white balance temperature', val={80}};
 camera.param[6] = {key='sharpness',     val={107}};
 camera.param[7] = {key='exposure (absolute)',      val={941}};
 
-camera.lut_file = 'lut_L512_05_18_time_23_33.raw';
+camera.lut_file = 'lut_L512_05_21_time_21_30.raw';
 

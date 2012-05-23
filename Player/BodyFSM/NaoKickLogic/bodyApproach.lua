6f3f8d2f944525f9d03249ab3756b5c7edc55e4e
Spencer Lee <lesp@seas.upenn.edu>
2012-05-23 13:17:04 -0400

    Moved kick logic to NaoKickLogic folder in BodyFSM. Restored NaoPlayer to a copy of USOpen2012 NaoPlayer.

--- Player/BodyFSM/NaoKickLogic/bodyApproach.lua
+++ Player/BodyFSM/NaoKickLogic/bodyApproach.lua
@@ -33,6 +33,9 @@ rFar = 0.45;
 -- alignment
 thAlign = 15.0*math.pi/180.0;
 
+-- goalpost distance threshold
+pNear = Config.fsm.bodyApproach.pNear or 0.3;
+pFar = Config.fsm.bodyApproach.pFar or 1.0;
 
 function entry()
   print(_NAME.." entry");
@@ -49,6 +52,18 @@ function update()
   --print('ball: '..ball.x..', '..ball.y);
   --print('ballR '..ballR);
 
+  -- get attack goalpost positions and goal angle
+  posts = {wcm.get_goal_attack_post1(), wcm.get_goal_attack_post2()}
+
+  -- calculate the relative distance to each post, find closest
+  pose = wcm.get_pose();
+  p1Relative = util.pose_relative({posts[1][1], posts[1][2], 0}, {pose.x, pose.y, pose.a});
+  p2Relative = util.pose_relative({posts[2][1], posts[2][2], 0}, {pose.x, pose.y, pose.a});
+  p1Dist = math.sqrt(p1Relative[1]^2 + p1Relative[2]^2);
+  p2Dist = math.sqrt(p2Relative[1]^2 + p2Relative[2]^2);
+  pClosest = math.min(p1Dist, p2Dist);
+  pFarthest = math.max(p1Dist, p2Dist);
+
   -- calculate walk velocity based on ball position
   vStep = vector.new({0,0,0});
   vStep[1] = .5*(ball.x - xTarget);
@@ -58,7 +73,6 @@ function update()
 
   ballA = math.atan2(ball.y - math.max(math.min(ball.y, 0.05), -0.05), math.max(ball.x+0.10, 0.10));
   vStep[3] = 0.5*ballA;
-
   walk.set_velocity(vStep[1],vStep[2],vStep[3]);
 
   attackBearing, daPost = wcm.get_attack_bearing();
@@ -82,8 +96,13 @@ function update()
   end
   if ((ball.x < xKick) and (math.abs(ball.y) < yKickMax) and
       (math.abs(ball.y) > yKickMin)) then
-    print('kick');
-    return "kick";
+    if ((pClosest > pNear) and (pClosest < pFar)) then
+      print('kick');   
+      return "kick";
+    else
+      print("My distance is ",pClosest,"\n");
+      return "walkKick";
+    end
   end
 end
 

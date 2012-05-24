3cb125d755a88a07d3b39ade97045663f64d1fb2
Yida Zhang <yida@seas.upenn.edu>
2012-05-24 18:45:03 -0400

    add time decay and finish coding vision update

--- Lib/OccMap/OccMap.cpp
+++ Lib/OccMap/OccMap.cpp
@@ -4,7 +4,9 @@
 #include <cassert>
 #include <ctime>
 #include <iostream>
+#include <iomanip>
 #include <cmath>
+#include <algorithm>
 
 
 OccMap::OccMap()
@@ -29,15 +31,27 @@ OccMap::~OccMap() {
 
 }
 
-int OccMap::reset_size(int size, int robot_x, int robot_y) {
+inline double OccMap::range_check(double num) {
+  if (num > 10) return 10; // log likelihood 0.9999
+  if (num < -10) return -10; // log likelihood 0.0001
+}
+
+int OccMap::reset_size(int size, int robot_x, int robot_y, double time) {
   map_size = size;
   resolution = map_size_metric / map_size;
   grid_num = map_size * map_size;
   rx = robot_x;
   ry = robot_y;
   max_dis = sqrt(rx * rx + ry * ry);
-  grid.resize(grid_num);
+  if (grid.size() < grid_num) 
+    grid.resize(grid_num);
   randomize_map();
+
+  // reset map init time
+  if (grid_updated_time.size() < grid_num)
+    grid_updated_time.resize(grid_num);
+  for (int i = 0; i < grid_num; i++)
+    grid_updated_time[i] = time;
   return 1;
 }
 
@@ -57,6 +71,12 @@ int OccMap::get_map(vector<double> & map) {
   return 1;
 }
 
+int OccMap::get_map_updated_time(vector<double> & updated_time) {
+  for (int i = 0; i < grid_updated_time.size(); i++)
+    updated_time.push_back(grid_updated_time[i]);
+  return 1;
+}
+
 int& OccMap::get_robot_pos_x(void) {
   return rx;
 }
@@ -65,9 +85,21 @@ int& OccMap::get_robot_pos_y(void) {
   return ry;
 }
 
-int OccMap::vision_update(double *free_bound, double *free_bound_type, int width) {
-//  cout << free_bound[width-1] << ' ' << free_bound[2*width-1] << endl;
-//  cout << free_bound_type[width-1] << endl;
+int OccMap::time_decay(double time) {
+  double decay_coef = 0.5;
+  for (int i = 0; i < grid_num; i++) {
+    grid[i] = range_check(exp(-decay_coef * 
+                            (time - grid_updated_time[i])) *grid[i]);
+  }
+  return 1;
+}
+
+int OccMap::vision_update(double *free_bound, double *free_bound_type,
+    int width, double time) {
+  cout << setprecision(15) << "last_updated_time " << grid_updated_time[0] << 
+          " last time " << time - grid_updated_time[0] << endl;
+  grid_updated_time[0] = time;
+/*
   if (gau_a.size() < width) {
       gau_a.resize(width);  
       cout << "resize container for Gaussians Coef a" << endl;
@@ -100,16 +132,36 @@ int OccMap::vision_update(double *free_bound, double *free_bound_type, int width
     gau_c[i] = 0.5 * pow(sin(gau_theta[i]) / var_x, 2) +
                 0.5 * pow(cos(gau_theta[i]) / var_y, 2);
   }
+  // Update grid
+  double grid_p = 0, grid_p_max = 0; // likelihood of one point on grid
+  double i_metric = 0, j_metric = 0; // grid point with metric position
+  for (int i = 0; i < map_size; i++)
+    for (int j = 0; j < map_size; j++) {
+      // (1, 2) on grid -> (0.02, 0.04) on real OccMap
+      i_metric = i * resolution;
+      j_metric = j * resolution; 
+      // Go through all observation to decide the max likelihood
+      for (int k = 0; k < width; k++) {
+        ob_x = free_bound[i];
+        ob_y = free_bound[i + width];
+        grid_p = 1 / exp(gau_a[k] * pow(i_metric - ob_x, 2) + 
+                        2 * gau_b[k] * (i_metric - ob_x) * (j_metric - ob_y) + 
+                        gau_c[k] * pow(j_metric - ob_y, 2));
+        grid_p_max = max(grid_p_max, grid_p);
+      }
+      // update log likelihood
+      grid[j * map_size + i] += log(grid_p / (1 - grid_p));
+    }
+*/
   return 1;
 }
 
 
-int OccMap::odometry_update(const double odomX, const double odomY, const double odomA) {
-//  cout << "Odom Change: " << endl;
+int OccMap::odometry_update(const double odomX, const double odomY,
+    const double odomA) {
   odom_x += odomX / resolution;
   odom_y += odomY / resolution;
   odom_a += odomA;
-//  cout << odom_x << ' ' << odom_y << ' ' << odom_a << endl;
   if ((odom_x > 1) || (odom_y > 1) || (odom_a * max_dis > 1)) {
     double nx = 0; // new point in occmap coordinate x
     double ny = 0; // new point in occmap coordinate y
@@ -121,17 +173,14 @@ int OccMap::odometry_update(const double odomX, const double odomY, const double
     randomize_map();
     for (int i = 0; i < map_size; i++)
       for (int j = 0; j < map_size; j++) {
-       // cout << rx - i - odom_x << ' ' << ry - j - odom_y << endl;
         nx = ca * (i - rx - odom_x) - sa * (ry - j - odom_y);
         ny = sa * (i - rx - odom_x) + ca * (ry - j - odom_y);
         ni = round(nx + rx);
         nj = round(ry - ny);
         if ((ni >= 0) and (ni < map_size) and (nj >= 0) and (nj < map_size))
           grid[nj * map_size + ni] = grid_temp[j * map_size + i];
-      //  cout << i << ' ' << j << ' ' << ni << ' ' << ' ' << nj << endl;
       }
 
-//    cout << "Update Odometry: " << odom_x << ' ' << odom_y << ' ' << odom_a*max_dis << endl;
     odom_x = 0;
     odom_y = 0;
     odom_a = 0;
--- Lib/OccMap/luaOccMap.cc
+++ Lib/OccMap/luaOccMap.cc
@@ -12,6 +12,7 @@ extern "C" {
 
 #include "OccMap.h"
 #include <iostream>
+#include <iomanip>
 
 OccMap map;
 
@@ -25,7 +26,8 @@ static int lua_occmap_init_map(lua_State *L) {
   int map_size = luaL_checkint(L, 1);
   int robot_x = luaL_checkint(L, 2);
   int robot_y = luaL_checkint(L, 3);
-  map.reset_size(map_size, robot_x, robot_y);
+  double time = luaL_checknumber(L, 4);
+  map.reset_size(map_size, robot_x, robot_y, time);
   return 1;
 }
 
@@ -44,7 +46,9 @@ static int lua_occmap_vision_update(lua_State *L) {
   }
   double *free_bound_type = (double *) lua_touserdata(L, 2);
   int width = luaL_checkint(L, 3);
-  map.vision_update(free_bound, free_bound_type, width);
+  double time = luaL_checknumber(L, 4);
+//  cout << setprecision(15) << time << endl;
+  map.vision_update(free_bound, free_bound_type, width, time);
   return 1;
 }
 
@@ -52,6 +56,13 @@ static int lua_occmap_retrieve_map(lua_State *L) {
   vector<double> cur_map;
   map.get_map(cur_map);
 	lua_pushlightuserdata(L, &cur_map[0]);
+  return 1;
+}
+
+static int lua_occmap_retrieve_map_updated_time(lua_State *L) {
+  vector<double> updated_time;
+  map.get_map_updated_time(updated_time);
+	lua_pushlightuserdata(L, &updated_time[0]);
   return 1;
 }
 
@@ -69,13 +80,30 @@ static int lua_occmap_retrieve_data(lua_State *L) {
   return 1;
 }
 
+static int lua_occmap_empty_userdata(lua_State *L) {
+  int size = luaL_checkint(L, 1);
+  vector<double> empty;
+  empty.resize(size);
+  lua_pushlightuserdata(L, &empty[0]);
+  return 1;
+}
+
+static int lua_occmap_time_decay(lua_State *L) {
+  double time = luaL_checknumber(L, 1);
+  map.time_decay(time);
+  return 1;
+}
+
 static const struct luaL_reg OccMap_lib [] = {
   {"init", lua_occmap_init_map},
   {"reset", lua_occmap_reset},
+  {"time_decay", lua_occmap_time_decay},
   {"retrieve_map", lua_occmap_retrieve_map},
+  {"retrieve_map_updated_time", lua_occmap_retrieve_map_updated_time},
   {"retrieve_data", lua_occmap_retrieve_data},
   {"vision_update", lua_occmap_vision_update},
   {"odometry_update", lua_occmap_odometry_update},
+  {"empty_userdata", lua_occmap_empty_userdata},
   {NULL, NULL}
 };
 

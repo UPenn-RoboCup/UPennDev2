57daadfb9bafcec3978713e2ee3b6a51e0017fcf
SJ@AW <seungjoon.yi@gmail.com>
2012-05-29 09:21:51 -0400

    Enabled diving for goalie

--- Player/BodyFSM/GeneralPlayer/bodyAnticipate.lua
+++ Player/BodyFSM/GeneralPlayer/bodyAnticipate.lua
@@ -25,6 +25,8 @@ tFollowDelay = Config.fsm.bodyKick.tFollowDelay;
 rClose = Config.fsm.bodyAnticipate.rClose or 1.0;
 thFar = Config.fsm.bodyAnticipate.thFar or 0.25;
 
+goalie_dive = Config.goalie_dive or 0;
+
 function entry()
   print(_NAME.." entry");
   t0 = Body.get_time();
@@ -46,24 +48,51 @@ function update()
   homeRelative = util.pose_relative(homePosition, {pose.x, pose.y, pose.a});
   rHomeRelative = math.sqrt(homeRelative[1]^2 + homeRelative[2]^2);
 
+--TODO: Diving handling 
+
+  if goalie_dive > 0 then
+
+    tStartDelay = 1.0;
+--Penalty mark dist is 1.8m from goal line
+    rCloseDive = 2.0; 
+    rMinDive = 0.7;
+    ball_velocity_th = -0.5;
+
+    if t-t0>tStartDelay and t-ball.t<0.1 then
+      --Tracking the ball in ready position. Stop off head movement
+      --Body.set_head_hardness(0);
+      ballR=math.sqrt(ball.x^2+ball.y^2);
+      if ball.vx<ball_velocity_th and 
+	ballR<rCloseDive and
+	ballR>rMinDive then
+        t0=t;
+        py = ball.y - (ball.vy/ball.vx) * ball.x;
+        print("Ball velocity:",ball.vx,ball.vy);
+        print("Projected y pos:",py);
+        if py>0.07 then 
+          dive.set_dive("diveLeft");
+        elseif py<-0.07 then
+          dive.set_dive("diveRight");
+        else
+          dive.set_dive("diveCenter");
+        end
+        Motion.event("dive");
+        return "dive";
+      end
+    end
+  end
+
   if ballR<rClose and t-ball.t<0.1 then
     Motion.event("walk");
     return "ballClose";
   end
-
---[[
-  if ball.t<0.1 and ball.vx<-0.5 then
-    dive.set_dive("diveLeft");
-    Motion.event("dive");
-    return "dive";
-  end
---]]
-
   -- Check if out of position
   if( rHomeRelative>math.sqrt(thFar[1]^2+thFar[2]^2) ) then
     Motion.event("walk");
     return 'position';
   end
+
+
 
   if (t - t0 > timeout) then
     return "timeout";

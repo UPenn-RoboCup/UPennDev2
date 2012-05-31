f362ca2657c6c241929f55afe6cee0b24cb32c2a
Jordan Brindza <jbrindza@gmail.com>
2012-05-31 19:23:10 -0400

    added initial working tone detector

--- Lib/Platform/NaoV4/Sound/sound_params.h
+++ Lib/Platform/NaoV4/Sound/sound_params.h
@@ -1,22 +1,29 @@
 #ifndef __SOUND_PARAMS_H__
 #define __SOUND_PARAMS_H__
 
+// number of channels (stereo)
+static const int NCHANNEL = 2;
+// sampling rate
+static const int SAMPLING_RATE = 16000;
+
 // 1 sample = 2 bytes
 static const int BYTES_PER_SAMPLE = 2;
 // 1 frame = 2 samples = 1 left and 1 right value
 static const int SAMPLES_PER_FRAME = 2;
 // 1 period = x frames
-// NOTE: the nao driver will not allow any other period size that 341
+// NOTE: the nao driver will not allow any other period size that 341 (per channel)
 static const int FRAMES_PER_PERIOD = 341;
+// number of total frames
+static const int NFRAMES = FRAMES_PER_PERIOD;
 // number of samples per period (number of elements in each audio sequence)
-//static const int NSAMPLE = (FRAMES_PER_PERIOD*SAMPLES_PER_FRAME);
+static const int ASAMPLE = (FRAMES_PER_PERIOD*SAMPLES_PER_FRAME);
 // the nao cross compiler does not like the other one
-static const int NSAMPLE = 341*2;
+//static const int NSAMPLE = 341*2;
 
 
-// number of channels (stereo)
-static const int NCHANNEL = 2;
-// sampling rate
-static const int SAMPLING_RATE = 16000;
+// number of frames in audio segment we want to process
+static const int PFRAMES = 512;
+// size of audio segment we want to process
+static const int PSAMPLE = (PFRAMES*SAMPLES_PER_FRAME);
 
 #endif

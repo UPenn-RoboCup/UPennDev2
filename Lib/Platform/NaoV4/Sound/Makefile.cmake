f5518f641e3f40d75a16c49bd9d2cf9ef4d528ea
Jordan Brindza <jbrindza@gmail.com>
2012-05-31 19:24:08 -0400

    added makefiles for nao sound

---
+++ Lib/Platform/NaoV4/Sound/Makefile.cmake
@@ -0,0 +1,26 @@
+CWD= $(shell pwd)
+CTC= $(NaoqiCTC)
+
+all: naoqi 
+
+naoqi:
+ifeq ($(CTC),)
+	@echo Cross compilitation tool not specified. \
+				Please download the ctc tool from \
+				the aldebaran website and follow the \
+				instructions to configure it for your system \
+        && make -f Makefile.local
+else
+	rm -rf build
+	mkdir build
+	cd build && cmake -DCMAKE_TOOLCHAIN_FILE="$(CTC)/toolchain-atom.cmake" .. \
+		&& make && cd $(CWD) && scp build/sound_comm nao@192.168.0.107:
+
+endif
+
+clean:
+ifeq ($(CTC),)
+	make -f Makefile.local clean	
+else
+	rm -rf build
+endif

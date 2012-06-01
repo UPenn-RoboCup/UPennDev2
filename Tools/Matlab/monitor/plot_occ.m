2b96ceef83b7a2be4990cdd113440dae844d819d
Yida Zhang <hitzhangyd@gmail.com>
2012-06-01 16:44:26 -0400

    add prob value specific display

--- Tools/Matlab/monitor/plot_occ.m
+++ Tools/Matlab/monitor/plot_occ.m
@@ -12,8 +12,9 @@ function plot_occ(occ)
   occ_col = robot_pos(2) - occ_col;
 
   hold on;
-  plot(occ_row(find(occ_v >= 0.9)), occ_col(find(occ_v >= 0.9)), '*');
-  plot(occ_row(find(occ_v > 0.8 & occ_v < 0.9)), occ_col(find(occ_v > 0.8 & occ_v < 0.9)), 'o');
+  plot(occ_row(find(occ_v >= 0.95)), occ_col(find(occ_v >= 0.95)), '*');
+  plot(occ_row(find(occ_v > 0.8 & occ_v < 0.95)), ...
+        occ_col(find(occ_v > 0.8 & occ_v < 0.95)), 'o');
   % draw robot body
   rectangle('Position', [-0.06, -0.04, 0.12, 0.07], ...
             'Curvature',[0.8, 0.4],...

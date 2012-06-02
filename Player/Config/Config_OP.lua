890e3db45f7b9980649594be800111741303b725
SJ@Scarface <seungjoon.yi@gmail.com>
2012-06-02 16:25:52 -0400

    New vision params for day

--- Player/Config/Config_OP.lua
+++ Player/Config/Config_OP.lua
@@ -20,7 +20,8 @@ loadconfig('Kick/Config_OP_Kick')
 loadconfig('Vision/Config_OP_Vision')
 --Location Specific Camera Parameters--
 --loadconfig('Vision/Config_OP_Camera_VT')
-loadconfig('Vision/Config_OP_Camera_L512')
+--loadconfig('Vision/Config_OP_Camera_L512')
+loadconfig('Vision/Config_OP_Camera_L512_Day')
 
 -- Device Interface Libraries
 dev = {};

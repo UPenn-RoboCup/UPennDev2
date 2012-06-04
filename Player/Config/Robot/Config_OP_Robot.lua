9df122485474aa429fbaf74af4226776b9d10bad
DARwIn <darwin@linus.(none)>
2012-06-04 02:24:53 -0400

    Goalie Parameter change to keep standing

--- Player/Config/Robot/Config_OP_Robot.lua
+++ Player/Config/Robot/Config_OP_Robot.lua
@@ -11,7 +11,13 @@ stance.bodyHeightSit = 0.18;
 stance.qLArmSit = math.pi/180*vector.new({140,8,-40});
 stance.qRArmSit = math.pi/180*vector.new({140,-8,-40});
 stance.dpLimitSit=vector.new({.03,.01,.06,.1,.3,.3});
-stance.bodyHeightDive= 0.25;
+--stance.bodyHeightDive= 0.25;
+--stance.bodyTiltDive= 0;
+
+stance.bodyHeightDive= 0.295;
+stance.bodyTiltDive= 20*math.pi/180;
+
+
 stance.bodyTiltStance=20*math.pi/180; --bodyInitial bodyTilt, 0 for webots
 stance.dpLimitStance=vector.new({.04, .03, .07, .4, .4, .4});
 

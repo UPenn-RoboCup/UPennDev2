bb06f2354def7fc88bfc6f2a129a3d4ec507d257
Yida Zhang <yida@seas.upenn.edu>
2012-06-06 20:18:58 -0400

    add map shift

--- Lib/OccMap/test_const_exp.cpp
+++ Lib/OccMap/test_const_exp.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 int main() {
+  int rem = 50 % 50;
+  int quo = (50 - rem ) / 50;
+  cout << rem << ' ' << quo << endl;
   double var_x = 0.015;
   double var_y = 0.035;
   double gau_theta = M_PI/4;

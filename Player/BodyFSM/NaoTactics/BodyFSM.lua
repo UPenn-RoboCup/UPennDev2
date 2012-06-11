63a240ffd98cf86de265311a03c9d587035daa6d
Spencer Lee <lesp@seas.upenn.edu>
2012-06-11 12:30:54 -0400

    Restored BodyFSM to original form.
    -Replaced all instances of 'bodyAttack' to 'bodyPosition'

--- Player/BodyFSM/NaoTactics/BodyFSM.lua
+++ Player/BodyFSM/NaoTactics/BodyFSM.lua
@@ -13,7 +13,7 @@ require('bodyApproach')
 require('bodyKick')
 require('bodyOrbit')
 require('bodyGotoCenter')
-require('bodyAttack')
+require('bodyPosition')
 require('bodyObstacle')
 require('bodyObstacleAvoid')
 require('bodyWalkKick')
@@ -27,54 +27,54 @@ sm:add_state(bodyApproach);
 sm:add_state(bodyKick);
 sm:add_state(bodyOrbit);
 sm:add_state(bodyGotoCenter);
-sm:add_state(bodyAttack);
+sm:add_state(bodyPosition);
 sm:add_state(bodyObstacle);
 sm:add_state(bodyObstacleAvoid);
 sm:add_state(bodyWalkKick);
 
-sm:set_transition(bodyStart, 'done', bodyAttack);
+sm:set_transition(bodyStart, 'done', bodyPosition);
 
-sm:set_transition(bodyAttack, 'timeout', bodyAttack);
-sm:set_transition(bodyAttack, 'ballLost', bodySearch);
-sm:set_transition(bodyAttack, 'ballAlign', bodyOrbit);
-sm:set_transition(bodyAttack, 'approach', bodyApproach);
-sm:set_transition(bodyAttack, 'obstacle', bodyObstacle);
+sm:set_transition(bodyPosition, 'timeout', bodyPosition);
+sm:set_transition(bodyPosition, 'ballLost', bodySearch);
+sm:set_transition(bodyPosition, 'ballAlign', bodyOrbit);
+sm:set_transition(bodyPosition, 'approach', bodyApproach);
+sm:set_transition(bodyPosition, 'obstacle', bodyObstacle);
 
-sm:set_transition(bodyObstacle, 'clear', bodyAttack);
+sm:set_transition(bodyObstacle, 'clear', bodyPosition);
 sm:set_transition(bodyObstacle, 'timeout', bodyObstacleAvoid);
 
-sm:set_transition(bodyObstacleAvoid, 'clear', bodyAttack);
-sm:set_transition(bodyObstacleAvoid, 'timeout', bodyAttack);
+sm:set_transition(bodyObstacleAvoid, 'clear', bodyPosition);
+sm:set_transition(bodyObstacleAvoid, 'timeout', bodyPosition);
 
-sm:set_transition(bodySearch, 'ball', bodyAttack);
+sm:set_transition(bodySearch, 'ball', bodyPosition);
 sm:set_transition(bodySearch, 'timeout', bodyGotoCenter);
 
-sm:set_transition(bodyGotoCenter, 'ballFound', bodyAttack);
+sm:set_transition(bodyGotoCenter, 'ballFound', bodyPosition);
 sm:set_transition(bodyGotoCenter, 'done', bodySearch);
 sm:set_transition(bodyGotoCenter, 'timeout', bodySearch);
 
-sm:set_transition(bodyOrbit, 'timeout', bodyAttack);
+sm:set_transition(bodyOrbit, 'timeout', bodyPosition);
 sm:set_transition(bodyOrbit, 'ballLost', bodySearch);
-sm:set_transition(bodyOrbit, 'ballFar', bodyAttack);
+sm:set_transition(bodyOrbit, 'ballFar', bodyPosition);
 sm:set_transition(bodyOrbit, 'done', bodyApproach);
 
-sm:set_transition(bodyApproach, 'ballFar', bodyAttack);
+sm:set_transition(bodyApproach, 'ballFar', bodyPosition);
 sm:set_transition(bodyApproach, 'ballAlign', bodyOrbit);
 sm:set_transition(bodyApproach, 'ballLost', bodySearch);
-sm:set_transition(bodyApproach, 'timeout', bodyAttack);
+sm:set_transition(bodyApproach, 'timeout', bodyPosition);
 sm:set_transition(bodyApproach, 'kick', bodyKick);
 sm:set_transition(bodyApproach, 'walkKick', bodyWalkKick);
 
-sm:set_transition(bodyKick, 'timeout', bodyAttack);
-sm:set_transition(bodyKick, 'done', bodyAttack);
+sm:set_transition(bodyKick, 'timeout', bodyPosition);
+sm:set_transition(bodyKick, 'done', bodyPosition);
 
-sm:set_transition(bodyWalkKick, 'timeout', bodyAttack);
-sm:set_transition(bodyWalkKick, 'done', bodyAttack);
+sm:set_transition(bodyWalkKick, 'timeout', bodyPosition);
+sm:set_transition(bodyWalkKick, 'done', bodyPosition);
 
-sm:set_transition(bodyAttack, 'fall', bodyAttack);
-sm:set_transition(bodyApproach, 'fall', bodyAttack);
-sm:set_transition(bodyKick, 'fall', bodyAttack);
-sm:set_transition(bodyWalkKick, 'fall', bodyAttack);
+sm:set_transition(bodyPosition, 'fall', bodyPosition);
+sm:set_transition(bodyApproach, 'fall', bodyPosition);
+sm:set_transition(bodyKick, 'fall', bodyPosition);
+sm:set_transition(bodyWalkKick, 'fall', bodyPosition);
 
 -- set state debug handle to shared memory settor
 sm:set_state_debug_handle(gcm.set_fsm_body_state);

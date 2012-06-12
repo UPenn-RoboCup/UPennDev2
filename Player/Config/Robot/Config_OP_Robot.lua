9b585cbc1894009727691a857b98bf2dbc648893
Yida@scarface <yida@seas.upenn.edu>
2012-06-11 22:12:58 -0400

    Tested diving params

--- Player/Config/Robot/Config_OP_Robot.lua
+++ Player/Config/Robot/Config_OP_Robot.lua
@@ -12,11 +12,16 @@ stance.qLArmSit = math.pi/180*vector.new({140,8,-40});
 stance.qRArmSit = math.pi/180*vector.new({140,-8,-40});
 stance.dpLimitSit=vector.new({.03,.01,.06,.1,.3,.3});
 
---stance.bodyHeightDive= 0.25;
---stance.bodyTiltDive= 0;
-stance.bodyHeightDive= 0.295;
-stance.bodyTiltDive= 20*math.pi/180;
-stance.dpLimitDive=vector.new({.06,.06,.06,.9,.9,.9});
+--This makes correct sideways dive
+stance.bodyHeightDive= 0.25;
+stance.bodyTiltDive= 0;
+
+--stance.bodyHeightDive= 0.28;
+
+
+--stance.bodyHeightDive= 0.295;
+--stance.bodyTiltDive= 20*math.pi/180;
+stance.dpLimitDive=vector.new({.06,.06,.09,.9,.9,.9});
 
 stance.bodyTiltStance=20*math.pi/180; --bodyInitial bodyTilt, 0 for webots
 stance.dpLimitStance=vector.new({.04, .03, .07, .4, .4, .4});

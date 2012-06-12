337a3caca1db97c454f8cec91899ab25e4b60246
SJ@AW <seungjoon.yi@gmail.com>
2012-06-12 17:57:38 -0400

    Still fixing goalie

--- Player/Config/Config_WebotsOP.lua
+++ Player/Config/Config_WebotsOP.lua
@@ -141,8 +141,8 @@ head.bodyTilt = 0;
 --km.kick_left = 'km_NSLOP_StandupFromFront2.lua';
 
 --Shutdown Vision and use ground truth gps info only
---use_gps_only = 0;
-use_gps_only = 1;
+use_gps_only = 0;
+--use_gps_only = 1;
 
 goalie_dive = 2; --1 for arm only, 2 for actual diving
 goalie_dive_waittime = 6.0; --How long does goalie lie down?
--- Player/World/Velocity.lua
+++ Player/World/Velocity.lua
@@ -13,6 +13,8 @@ noball_count = 1;
 ball_count = 0;
 --If ball is not seen for this # of frames, remove ball memory 
 noball_threshold = 3; 
+--How many succeding ball observations is needed before updating?
+--We need at least two observation to update velocity
 
 ball_threshold = 2;
 
@@ -29,15 +31,13 @@ function entry()
 end
 
 function update(newx,newy)
-  --We need at least two observation to update velocity
   t=Body.get_time();
   ball_count = ball_count + 1;
   ballOldR = math.sqrt(oldx^2+oldy^2);
   ballR = math.sqrt(newx^2+newy^2);
   ballA = math.atan2(newy,newx);
 
-  --Lower gamma for distant ball
-  gamma = 0.3 * math.min(1,math.max(0,(1-(ballR-2.0)/1.3)));
+  gamma = 0.7;
 
   --Lower gamma if head not locked on at the ball
   locked_on = wcm.get_ball_locked_on();
@@ -49,12 +49,15 @@ function update(newx,newy)
   if t>tLast and ball_count>=ball_threshold then
       tPassed=t-tLast;
 
+--[[
       --Filter ball distance
       thR = math.max(0,(ballR-1.0)*0.1);
       newR = oldR;
       if math.abs(oldR - ballR)>thR then
-        newR = ballR;
       end
+--]]
+
+      newR = ballR;
       newA = ballA;
 
       nfvx = (newx-oldx)/tPassed; 
@@ -66,43 +69,45 @@ function update(newx,newy)
       vxCurrent = (filteredx-oldx)/tPassed;
       vyCurrent = (filteredy-oldy)/tPassed;
 
-      --Update velocity 
-      vx=(1-gamma)*discount*vx + gamma*vxCurrent;
-      vy=(1-gamma)*discount*vy + gamma*vyCurrent;
-      vxOld = vxCurrent;
-      vyOld = vyCurrent;
+      vMagCurrent = math.sqrt(vxCurrent^2+vyCurrent^2);
 
-      --Update position
-      oldx=filteredx;
-      oldy=filteredy;
-      oldA=newA;
-      oldR=newR; 
+      if vMagCurrent < 4.0 and newR < 2.0 then --Kill outlier
+        --Update velocity 
+        vx=(1-gamma)*discount*vx + gamma*vxCurrent;
+        vy=(1-gamma)*discount*vy + gamma*vyCurrent;
+        vxOld = vxCurrent;
+        vyOld = vyCurrent;
 
-      if math.sqrt(nfvx^2+nfvy^2)>0.3 and nfvx<-0.3 then
-         print(string.format(
+        --Update last ball position
+        oldx=filteredx;
+        oldy=filteredy;
+        oldA=newA;
+        oldR=newR; 
+        tLast=t;
+--[[
+        if math.sqrt(nfvx^2+nfvy^2)>0.3 and nfvx<-0.3 then
+           print(string.format(
 "%d Ball xy:%.2f %.2f vc: %.2f %.2f vcf:%.2f %.2f v:%.2f %.2f",
-	ball_count,newx,newy,nfvx,nfvy,vxCurrent,vyCurrent,vx,vy));
+  	  ball_count,newx,newy,nfvx,nfvy,vxCurrent,vyCurrent,vx,vy));
+        end
+--]]
+      else
+	ball_count=0;
+	--This should be a outlier
+	vx=vx*discount;
+	vy=vy*discount;
       end
-
-
   else 
-      --Ball first seen, don't update velocity
-      vx=0;vy=0;
-
-      --Update position
-      oldx=newx;
-      oldy=newy;
-      oldA=ballA;
-      oldR=ballR; 
+     --Ball first seen, don't update velocity
+     vx=0;vy=0;
+     --Update position
+     oldx=newx;
+     oldy=newy;
+     oldA=ballA;
+     oldR=ballR; 
+     tLast=t;
+     noball_count=0;
   end
-
---  if ball_count<15 and vx<-0.5 then
-
-
---  end
-
-  tLast=t;
-  noball_count=0;
 end
 
 function update_noball()
@@ -113,8 +118,8 @@ function update_noball()
     print("Velocity resetted")
     vx=0;vy=0;
   else
-   vx=discount*gamma*vx;
-   vy=discount*gamma*vy;
+   vx=gamma*vx;
+   vy=gamma*vy;
   end
 end
 

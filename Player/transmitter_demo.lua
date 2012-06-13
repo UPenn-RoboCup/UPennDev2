5f7ec1de82f2b34539a80d416e00b94cb05f12e1
Jordan Brindza <jbrindza@gmail.com>
2012-06-13 14:09:12 -0400

    added keyboard control to transmitter demo

--- Player/transmitter_demo.lua
+++ Player/transmitter_demo.lua
@@ -2,8 +2,10 @@ dofile('init.lua')
 require('unix')
 require('Body')
 require('SoundComm')
+require('getch');
+getch.enableblock(1);
 
-SoundComm.set_transmitter_volume(50)
+SoundComm.set_transmitter_volume(100)
 
 symbols = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '#', '*', 'A', 'B', 'C', 'D'};
 
@@ -14,18 +16,26 @@ SoundComm.pause_receiver();
 
 -- continuous
 continuous = false;
+period = 1;
 lastTx = unix.time();
 
 while (1) do
+  
+   local cin = getch.get();
+   local byte = nil;
+   if (#cin > 0) then
+      byte = string.byte(cin,1);
+   end
 
    if (continuous) then
-      if (Body.get_sensor_bumperLeft()[1] == 1 
-            and Body.get_sensro_bumperRight()[1] == 1
+      if (( (Body.get_sensor_bumperLeft()[1] == 1 and Body.get_sensor_bumperRight()[1] == 1)
+               or (byte and byte == string.byte(' ')))
             and unix.time() - lastPress > 1) then
+         print('chest button mode');
          continuous = false;
          lastPress = unix.time();
       else
-         if (unix.time() - lastTx > 2) then
+         if (unix.time() - lastTx > period) then
             -- pick random tone symbol
             ind = math.random(#symbols);
             symbol = symbols[ind];
@@ -47,8 +57,11 @@ while (1) do
         SoundComm.play_pnsequence(symbol);
 
         lastPress = unix.time();
-     elseif (Body.get_sensor_bumperLeft()[1] == 1 and Body.get_sensro_bumperRight()[1] == 1) then
+     elseif (((Body.get_sensor_bumperLeft()[1] == 1 and Body.get_sensor_bumperRight()[1] == 1)
+               or (byte and byte == string.byte(' ')))
+               and unix.time() - lastPress > 1) then
         -- enable continuous transmittion
+        print('continuous mode');
         continuous = true;
         lastPress = unix.time();
      end

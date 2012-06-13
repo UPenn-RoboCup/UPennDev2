cff5970526297dac02d0357db0a751b4d02a3a2b
Yida@linus <yida@seas.upenn.edu>
2012-06-13 14:46:13 -0400

    Sally rebiased with new walking sidekick plus tested head angle w.r.t ball position for stationary kick -- Looks good (with padding)

--- Player/Config/calibration.lua
+++ Player/Config/calibration.lua
@@ -652,3 +652,8 @@ cal["sally"].kickXComp=0.000;
 cal["hokie"].servoBias={0,-5,-17,-13,-1,-20,0,2,34,-17,-20,-3,};
 cal["hokie"].footXComp=0.002;
 cal["hokie"].kickXComp=0.000;
+
+-- Updated date: Wed Jun 13 14:44:35 2012
+cal["sally"].servoBias={-33,-22,-6,0,53,-15,0,0,-4,0,0,0,};
+cal["sally"].footXComp=-0.001;
+cal["sally"].kickXComp=0.000;

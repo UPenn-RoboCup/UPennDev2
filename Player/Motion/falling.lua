608f5d7dac147f0eb18bff1fa10bcbe3fd7d5e4f
SJ@Scarface <seungjoon.yi@gmail.com>
2012-06-15 21:56:40 -0400

    Fixed front getup

--- Player/Motion/falling.lua
+++ Player/Motion/falling.lua
@@ -14,20 +14,20 @@ function entry()
   print(_NAME.." entry");
 
   -- relax all the joints while falling
---  Body.set_body_hardness(0);
+  Body.set_body_hardness(0);
 
+--[[
   --Ukemi motion (safe fall)
   local imuAngleY = Body.get_sensor_imuAngle(2);
   if (imuAngleY > 0) then --Front falling 
 print("UKEMI FRONT")
-
     Body.set_larm_hardness({0.6,0,0.6});
     Body.set_rarm_hardness({0.6,0,0.6});
     Body.set_larm_command(qLArmFront);
     Body.set_rarm_command(qRArmFront);
-
   else
   end
+--]]
 
   t0 = Body.get_time();
   Body.set_syncread_enable(1); --OP specific

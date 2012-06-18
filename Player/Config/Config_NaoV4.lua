b5ac925124c73557c9be07b2a164c8e0a0d8a23d
yizhe <yizhe@seas.upenn.edu>
2012-06-18 12:49:06 -0400

    colortable for Mexico, June 18th

--- Player/Config/Config_NaoV4.lua
+++ Player/Config/Config_NaoV4.lua
@@ -17,10 +17,10 @@ end
   
 param = {}
 param.world = 'World/Config_Nao_World'
-param.walk = 'Walk/Config_NaoV4_Walk_L512' 
+param.walk = 'Walk/Config_NaoV4_Walk_FastStable' 
 param.kick = 'Kick/Config_Nao_Kick'
 param.vision = 'Vision/Config_NaoV4_Vision'
-param.camera = 'Vision/Config_NaoV4_Camera_Levine512night'
+param.camera = 'Vision/Config_NaoV4_Camera_Mexico618'
 param.fsm = 'FSM/Config_NaoV4_FSM'
 
 loadconfig(param.world)

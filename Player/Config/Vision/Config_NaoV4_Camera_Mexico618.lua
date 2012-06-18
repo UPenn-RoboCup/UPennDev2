b775614988bc2bf5dd06fdcabb98aebb98c985a9
yizhe <yizhe@seas.upenn.edu>
2012-06-18 19:25:38 -0400

    new colortable June 18th

--- Player/Config/Vision/Config_NaoV4_Camera_Mexico618.lua
+++ Player/Config/Vision/Config_NaoV4_Camera_Mexico618.lua
@@ -25,9 +25,9 @@ camera.param[2] = {key='Saturation'     , val={200, 240}};
 -- Hue will automatically change to 0 if set to a number between -5 and 5, but cannot be set by other numbers
 camera.param[3] = {key='Hue'            , val={0  , 0}};
 
-camera.param[4] = {key='Exposure'       , val={55 , 65}};
+camera.param[4] = {key='Exposure'       , val={55 , 80}};
 -- Gain should be set between 32 and 255
-camera.param[5] = {key='Gain'           , val={80 , 110}};
+camera.param[5] = {key='Gain'           , val={80 , 120}};
 -- Sharpness should be set between 0 and 7
 camera.param[6] = {key='Sharpness'      , val={2  , 2}};
 

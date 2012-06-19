fa149ec2e57cddafa5c2abda88f8f710d9e80a53
Yida@Jiminy <yida@seas>
2012-06-19 13:45:54 -0400

    bias jiminy head angle and tested

--- Player/Config/calibration.lua
+++ Player/Config/calibration.lua
@@ -72,7 +72,7 @@ cal["jiminy"]={
   footXComp = 0,
   footYComp = 0,
   kickXComp = 0,
-  headPitchComp = 0;
+  headPitchComp = 4*math.pi/180;
   armBias={0,12*math.pi/180,0,0,-6*math.pi/180,0},
   pid = 1, --NEW FIRMWARE
 };

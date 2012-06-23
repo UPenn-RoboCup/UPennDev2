3cdfbde5c42ec2cef5cf07ad9c3945934a561b6e
Aditya <aditya@aditya-laptop.(none)>
2012-06-22 20:37:30 -0500

    Switched color table for field B

--- Player/Config/Config_OP.lua
+++ Player/Config/Config_OP.lua
@@ -41,9 +41,9 @@ loadconfig('Vision/Config_OP_Vision')
 --loadconfig('Vision/Config_OP_Camera_RC12_day0')
 
 --loadconfig('Vision/Config_OP_Camera_RC12_day1_8AM')
-loadconfig('Vision/Config_OP_Camera_RC12_FieldD')
 --loadconfig('Vision/Config_OP_Camera_RC12_FieldD')
---loadconfig('Vision/Config_OP_Camera_RC12_FieldB')
+--loadconfig('Vision/Config_OP_Camera_RC12_FieldD')
+loadconfig('Vision/Config_OP_Camera_RC12_FieldB')
 
 --loadconfig('Vision/Config_OP_Camera_RC12_day1_8AM')
 --loadconfig('Vision/Config_OP_Camera_RC12_FieldD')

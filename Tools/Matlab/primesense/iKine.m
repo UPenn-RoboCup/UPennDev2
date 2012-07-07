6f5d27222628f4b9903c75591d171b2b81821aab
Stephen McGill <smcgill3@seas.upenn.edu>
2012-07-06 21:40:09 -0400

    Add burst mode for sending team messages.

--- Tools/Matlab/primesense/iKine.m
+++ Tools/Matlab/primesense/iKine.m
@@ -1,8 +1,8 @@
 function theta = iKine(coord)
 
 % OP arm lengths
-a_u = .5;
-a_l = .5;
+a_u = .06;
+a_l = .129;
 theta = [nan nan nan];
 
 % Assume it is reachable
@@ -13,16 +13,19 @@ z = coord(3);
 c = sqrt(x^2+y^2+z^2);
 
 if(c>(a_u+a_l) || y<0 )
-    %disp('Unreachable!')
+    disp('Unreachable!')
     return;
 end
 
 %c^2 = a_u^2+a_l^2-2*a_u*a_l*cos(theta(3))
+%tmp = (a_u^2+a_l^2-c^2)/(2*a_u*a_l)
+%acoss = acos( (a_u^2+a_l^2-c^2)/(2*a_u*a_l) )
 theta(3) = pi - real( acos( (a_u^2+a_l^2-c^2)/(2*a_u*a_l) ) );
 %NOTE: pi - stuff is due to my convention
 
 % Trouble below
 %y = (a_u+a_l*cos(theta(3)))*sin(theta(2));
+%stuff = y/(a_u+a_l*cos( theta(3) ))
 theta(2) = real( asin( y/(a_u+a_l*cos( theta(3) )) ) );
 
 % Here is where we are
@@ -31,7 +34,7 @@ x_0 = a_l*sin(theta(3));
 
 % We want to get to x,z
 if(isreal([x z x_0 z_0]))
-    theta(1) = atan2(z,x) - atan2(z_0,x_0);
+    theta(1) = real(atan2(z,x) - atan2(z_0,x_0));
 end
 
 for i=1:3
--- Tools/Matlab/primesense/test_kine.m
+++ Tools/Matlab/primesense/test_kine.m
@@ -1,31 +1,34 @@
 %% Test the iKine and fKine within the ranges
-a_u = .5;
-a_l = .5;
+a_u = .06;
+a_l = .129;
+tot_len = a_u+a_l;
 all_diffs = [];
 bad = 0;
 good = 0;
 % y must be positive
-for y=0:.1:1
-    for x=-1:.1:1
-        for z=-1:.1:1
-            theta = iKine( [x y z] );
-            coords = fKine( theta );
-            diff = [x y z] - coords;
-            if( ~isnan(coords) )
-                all_diffs = [all_diffs ; diff];
-                c = sqrt(x^2+y^2+z^2);
-                if( ~isreal(coords) || ~isreal(theta) )
+for y=0:tot_len/10:tot_len
+    for x=-tot_len:tot_len/10:tot_len
+        for z=-tot_len:tot_len/10:tot_len
+            c = sqrt(x^2+y^2+z^2);
+            if( c<=(a_u+a_l) )
+                theta = iKine2( [x y z] );
+                coords = fKine( theta );
+                diff = [x y z] - coords;
+                if( ~isnan(coords) )
+                    all_diffs = [all_diffs ; diff];
+                end
+                
+                if( any(isnan(coords)) || ~isreal(coords) || ~isreal(theta) || sum(diff.^2)>0.05 )
                     bad = bad+1;
                     initc = [x y z]
-                    theta
+                    %theta
                     %t_1 = (c^2-a_u^2-a_l^2)/(-2*a_u*a_l)
                     %t_2 = a_u+a_l*cos(theta(3))
                     coords
                 else
                     good = good+1;
                 end
-            else
-                %disp('unreachable')
+                
             end
         end
     end

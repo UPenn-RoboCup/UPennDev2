d30996a397103ea89886ae86ff388cf66cce554e
Yida Zhang <yida@seas.upenn.edu>
2012-07-07 12:08:49 -0400

    all main scripts use init module to init package path

--- Player/listen_team_monitor.lua
+++ Player/listen_team_monitor.lua
@@ -1,21 +1,5 @@
-module(... or '', package.seeall)
-
-
--- Add the required paths
-cwd = '.';
-
-uname = io.popen('uname -s')
-system = uname:read()
-
-package.cpath = cwd.."/Lib/?.so;"..package.cpath;
-
-package.path = cwd.."/Util/?.lua;"..package.path;
-package.path = cwd.."/Config/?.lua;"..package.path;
-package.path = cwd.."/Lib/?.lua;"..package.path;
-package.path = cwd.."/Dev/?.lua;"..package.path;
-package.path = cwd.."/World/?.lua;"..package.path;
-package.path = cwd.."/Vision/?.lua;"..package.path;
-package.path = cwd.."/Motion/?.lua;"..package.path; 
+cwd = os.getenv('PWD')
+require('init')
 
 require 'unix'
 require ('Config')

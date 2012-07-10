3d73cdac208a4ea6e9dfa336b69033bf372e9a88
Yida Zhang <yida@seas.upenn.edu>
2012-07-09 22:12:16 -0400

    randomize obstacle position

--- WebotsProj/worlds/randomObs.lua
+++ WebotsProj/worlds/randomObs.lua
@@ -20,6 +20,7 @@ end
 fs:write(header)
 for cnt = 1, obs_num do
   fs:write('DEF OBSTACLE'..cnt..' cylinder {\n')
+  fs:write('\ttranslation '..obs.x[cnt]..' 0.25 '..obs.y[cnt]..'\n')
   fs:write('\theight 0.5\n')
   fs:write('}\n\n');
 end

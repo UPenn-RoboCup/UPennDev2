f5ee17f3bbf5fed65e735f40b63349eba7e94176
Yida Zhang <yida@seas.upenn.edu>
2012-07-15 19:05:09 -0400

    add bodyApproachGoal state for robot keep walking to attack goal and return done to stop robot

---
+++ Player/BodyFSM/ObstacleChallenge/bodyApproachGoal.lua
@@ -0,0 +1,48 @@
+module(..., package.seeall);
+
+require('Body')
+require('walk')
+require('vector')
+require('Config')
+require('wcm')
+require('mcm')
+
+t0 = 0;
+direction = 1;
+timeout = 5
+tLost = 0.5;
+
+function entry()
+  print(_NAME.." entry");
+
+  t0 = Body.get_time();
+
+end
+
+function update()
+  local t = Body.get_time();
+
+  attackBearing = wcm.get_attack_bearing();
+  if attackBearing ~= nil then
+    vx = 0.05;
+    vy = 0;
+    va = 0.2 * attackBearing;
+    walk.set_velocity(vx, vy, va);
+  end
+
+--  if ( t - wcm.get_goal_t() > tLost) then
+--    return 'GoalLost'
+--  end
+  pose = wcm.get_robot_pose();
+  if pose[1] > 3 then
+    return "done"
+  end
+
+  if (t - t0 > timeout) then
+    return "timeout";
+  end
+end
+
+function exit()
+  mcm.set_walk_isSearching(0);
+end

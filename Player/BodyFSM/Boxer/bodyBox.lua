f936a974315b3c5e454f20e4fdab5ce1d9ff67f4
Stephen McGill <smcgill3@seas.upenn.edu>
2012-07-20 11:49:14 -0400

    Making the arm state machine

--- Player/BodyFSM/Boxer/bodyBox.lua
+++ Player/BodyFSM/Boxer/bodyBox.lua
@@ -1,7 +1,7 @@
 module(..., package.seeall);
 
 require('Body')
-require('wcm')
+require('boxercm')
 require('walk')
 require('vector')
 
@@ -19,22 +19,23 @@ function update()
   local t = Body.get_time();
 
   -- Check if there is a punch activated
-  local punch = 0;
+  local punch = boxercm.get_body_punch();
+  local velocity = boxercm.get_body_velocity();
 
-  if( move==0 ) then --switch stance
-    stance_dir = math.floor(math.random()*2)+1;
-    if(math.floor(math.random()*5)==0) then
-      walk.switch_stance( 0 );
-    else
-      walk.switch_stance( stance_dir );
+  if( punch[1]==1 and punch[2]==0 ) then -- left arm punch
+    --Check the stance
+    if( walk.stance==1 ) then --left
     end
-  else
-    punch_type = math.floor(math.random()*2)+1;
-    walk.doPunch(punch_type);
+  elseif( punch[1]==0 and punch[2]==1 ) then -- right arm punch
+  elseif( punch[1]==1 and punch[2]==1 ) then -- both arm punch (pushaway)
+  else -- No punch
+  end
+
+  if( primecm.get_body_enabled() == 0 ) then
+    return "lost";
   end
 
 end
 
 function exit()
-  print('Exiting rand box!')
 end

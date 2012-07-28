f5611527e25f418dfcb20e246caddaec8b7da8a9
Yida Zhang <yida@seas.upenn.edu>
2012-07-28 16:26:01 -0400

    cmake linux lua include path problem fixed

--- Lib/toolchain.cmake
+++ Lib/toolchain.cmake
@@ -1,13 +1,2 @@
-set(CMAKE_CXX_COMPILER "/usr/bin/g++")
-
-IF ( APPLE )
-	MESSAGE( STATUS "Configure for APPLE" )
-  set(CMAKE_CXX_FLAGS "-O2 -fomit-frame-pointer -mtune=native" CACHE STRING "" FORCE)
-  set(CMAKE_C_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING "" FORCE)
-ENDIF( APPLE )
-
-IF ( UNIX )
-	MESSAGE( STATUS "Configure for UNIX" )
-ENDIF( UNIX )
 
 

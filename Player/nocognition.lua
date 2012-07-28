326b4eb6f19edd356e2d767e315dde8ec317eac6
Stephen McGill <smcgill3@seas.upenn.edu>
2012-07-28 18:28:17 -0400

    Adding start script for boxer with the 60fps nocognition mode (i.e. no camera updates)

---
+++ Player/nocognition.lua
@@ -0,0 +1,104 @@
+module(... or "",package.seeall)
+cwd = os.getenv('PWD')
+require('init')
+
+require('unix')
+require('vcm')
+require('gcm')
+require('wcm')
+require('mcm')
+require('Body')
+require('Vision')
+require('World')
+require('Detection') 
+require('OccupancyMap') 
+comm_inited = false;
+vcm.set_camera_teambroadcast(0);
+vcm.set_camera_broadcast(0);
+--Now vcm.get_camera_teambroadcast() determines 
+--Whether we use wired monitoring comm or wireless team comm
+
+count = 0;
+nProcessedImages = 0;
+tUpdate = unix.time();
+
+if (string.find(Config.platform.name,'Webots')) then
+  webots = true;
+end
+
+function broadcast()
+  broadcast_enable = vcm.get_camera_broadcast();
+  if broadcast_enable>0 then
+    if broadcast_enable==1 then 
+      --Mode 1, send 1/4 resolution, labeB, all info
+      imgRate = 1; --30fps
+    elseif broadcast_enable==2 then 
+      --Mode 2, send 1/2 resolution, labeA, labelB, all info
+      imgRate = 2; --15fps
+    else
+      --Mode 3, send 1/2 resolution, info for logging
+      imgRate = 1; --30fps
+    end
+    -- Always send non-image data
+    Broadcast.update(broadcast_enable);
+    -- Send image data every so often
+    if nProcessedImages % imgRate ==0 then
+      Broadcast.update_img(broadcast_enable);    
+    end
+    --Reset this flag at every broadcast
+    --To prevent monitor running during actual game
+    vcm.set_camera_broadcast(0);
+  end
+end
+
+function entry()
+  World.entry();
+  Vision.entry();
+  OccupancyMap.entry();
+end
+
+function update()
+  count = count + 1;
+  tstart = unix.time();
+
+  World.update_odometry();
+ 
+  if not comm_inited and 
+    (vcm.get_camera_broadcast()>0 or
+     vcm.get_camera_teambroadcast()>0) then
+    if vcm.get_camera_teambroadcast()>0 then 
+      require('Team');
+      require('GameControl');
+      Team.entry();
+      GameControl.entry();
+      print("Starting to send wireless team message..");
+    else
+      require('Broadcast');
+      print("Starting to send wired monitor message..");
+      require('Receive');
+      print("Starting to wired message..");
+    end
+    comm_inited = true;
+  end
+
+  if comm_inited then
+    if vcm.get_camera_teambroadcast()>0 then 
+      GameControl.update();
+      Team.update();
+    else
+      broadcast();
+      Receive.update();
+    end
+  end
+end
+
+-- exit 
+function exit()
+  if vcm.get_camera_teambroadcast()>0 then 
+    Team.exit();
+    GameControl.exit();
+  end
+  Vision.exit();
+  World.exit();
+end
+

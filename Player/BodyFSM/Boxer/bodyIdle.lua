9d08ef70a0b45859d7abee8345abfb6489d01b25
Stephen McGill <smcgill3@seas.upenn.edu>
2012-07-28 20:51:22 -0400

    Toggle via buttons the stabilization for the walk engine

--- Player/BodyFSM/Boxer/bodyIdle.lua
+++ Player/BodyFSM/Boxer/bodyIdle.lua
@@ -2,21 +2,45 @@ module(..., package.seeall);
 
 require('Body')
 require('Motion')
+require 'walk'
+require 'Speak'
+require 'gcm'
 
 t0 = 0;
 
+playerID = Config.game.playerID;
+
 function entry()
+  buttonPressed = 0;
   print(_NAME..' entry');
   t0 = Body.get_time();
-  
+
   Motion.event("sit");
---  Motion.sm:set_state('resit');
---  Motion.sm:set_state('standstill');
+  --  Motion.sm:set_state('resit');
+  --  Motion.sm:set_state('standstill');
 end
 
 function update()
   Motion.event("sit");
   t = Body.get_time();
+
+  -- Toggle stabilization when idle and penalized
+  if( gcm.in_penalty() ) then
+    if( Body.get_change_role()==0 ) then
+      -- toggle when button is released
+      if buttonPressed == 1 then
+        if(walk.no_stabilize) then
+          Speak.talk('Enabling stabilization!')
+        else
+          Speak.talk('Disabling stabilization!')
+        end
+        walk.no_stabilize = not walk.no_stabilize;
+        buttonPressed = 0;
+      end
+    else
+      buttonPressed = 1;
+    end
+  end
 end
 
 function exit()

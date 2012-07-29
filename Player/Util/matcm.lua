f6fffbd571955e7adff8e4d0a0267ead4c18dba1
larry <yida@seas.upenn.edu>
2012-07-29 13:05:22 -0400

    add matlab control key

--- Player/Util/matcm.lua
+++ Player/Util/matcm.lua
@@ -10,6 +10,7 @@ shsize = {}
 
 shared.control = {}
 shared.control.lut_updated = vector.zeros(1);
+shared.control.key = vector.zeros(1);
 
 util.init_shm_segment(getfenv(), _NAME, shared, shsize);
 

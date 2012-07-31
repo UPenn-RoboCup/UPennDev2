6a44b1926acb4e5df36c37a11643cf8c626086f5
Stephen McGill <smcgill3@seas.upenn.edu>
2012-07-31 11:04:09 -0400

    Fixing the switching using a simple fix

--- Player/run_prime.lua
+++ Player/run_prime.lua
@@ -113,88 +113,92 @@ while( not logs or count<n_logs ) do
       end
       center[pl] = pos[1]; -- only left/right
     end
-  end
 
-  switch = false;
-  if( center[1]>0 ) then
-    switch = true;
-  end
+    switch = false;
+    if( center[1]>0 ) then
+      switch = true;
+    end
 
-  -- What if we lost a player?
-  -- FOR NOW: just using persitent center array
+    -- What if we lost a player?
+    -- FOR NOW: just using persitent center array
 
-  -- Check each player
-  -- Is each player active?
-  for i,v in ipairs(pc[playerID].jointNames) do
-    if( logs ) then
-      for pl=1,nPlayers do
-        log = inp_logs[pl];
-        pos = { log[count].x[i],log[count].y[i],log[count].z[i] };
-        confidence = { log[count].posconf[i],log[count].rotconf[i] };
-        if( switch and nPlayers==2) then
-          primecm = pc[3-pl];
-        else
-          primecm = pc[pl];
+    -- Check each player
+    -- Is each player active?
+    for i,v in ipairs(pc[playerID].jointNames) do
+      if( logs ) then
+        for pl=1,nPlayers do
+          log = inp_logs[pl];
+          pos = { log[count].x[i],log[count].y[i],log[count].z[i] };
+          confidence = { log[count].posconf[i],log[count].rotconf[i] };
+          if( switch and nPlayers==2) then
+            primecm = pc[3-pl];
+          else
+            primecm = pc[pl];
+          end
+          primecm['set_position_'..v]( pos );
+          primecm['set_confidence_'..v]( confidence );
+          primecm.set_skeleton_found( 1 );
+          primecm.set_skeleton_timestamp( timestamp-timestamp0 );
         end
-        primecm['set_position_'..v]( pos );
-        primecm['set_confidence_'..v]( confidence );
-        primecm.set_skeleton_found( 1 );
-        primecm.set_skeleton_timestamp( timestamp-timestamp0 );
-      end
-    else
-      -- Multiple players
-      for pl=1,nPlayers do
-        pos, rot, confidence, active = primesense.get_jointtables(pl,i);
-        pos = vector.new(pos)/1000;
-        primecm = pc[pl];
-        primecm['set_position_'..v]( pos );
-        primecm['set_orientation_'..v]( rot );
-        primecm['set_confidence_'..v]( confidence );
-        primecm.set_skeleton_found( active );
-        primecm.set_skeleton_timestamp( timestamp-timestamp0 );
+      else
+        -- Multiple players
+        for pl=1,nPlayers do
+          pos, rot, confidence, active = primesense.get_jointtables(pl,i);
+          pos = vector.new(pos)/1000;
+          if( switch ) then
+            primecm = pc[3-pl];
+          else
+            primecm = pc[pl];
+          end
+          io:flush()
+          primecm['set_position_'..v]( pos );
+          primecm['set_orientation_'..v]( rot );
+          primecm['set_confidence_'..v]( confidence );
+          primecm.set_skeleton_found( active );
+          primecm.set_skeleton_timestamp( timestamp-timestamp0 );
+        end
       end
     end
   end
-end
 
--- Update the Team information
-if( net ) then
-  Team.update()
-end
-
---[[
--- Done running
-local byte = get_key_byte();
-if( byte==string.byte('e') ) then
-primecm.set_skeleton_enabled( 1-primecm.get_skeleton_enabled() );
-end
-if(byte==string.byte('q')) then
-break;
-end
---]]
-
--- Timing
-if( init and not logs ) then
-  if( count % 60==0 ) then
-    local fps = 60 / (unix.time()-(t_count or 0))
-    t_count = unix.time();
-    print('FPS: ',fps)
-    print('Time',timestamp-timestamp0 );
-    print('User 1: ', pc[1]['get_skeleton_found']());
-    print('User 2: ', pc[2]['get_skeleton_found']());
-    print('Switched? ',switch)
-    print();
+  -- Update the Team information
+  if( net ) then
+    Team.update()
   end
-end
-if(logs and count<n_logs) then
-  if( count % 60==0 ) then
-    print('Count:',count,'Time:',timestamp );    
+
+  --[[
+  -- Done running
+  local byte = get_key_byte();
+  if( byte==string.byte('e') ) then
+  primecm.set_skeleton_enabled( 1-primecm.get_skeleton_enabled() );
   end
-  local timestamp_next = log[count+1].t;
-  local twait = timestamp_next-timestamp;
-  local t_loop = unix.time() - t_start;
-  unix.usleep( 1e6*math.max(twait-t_loop,0) );
-end
+  if(byte==string.byte('q')) then
+  break;
+  end
+  --]]
+
+  -- Timing
+  if( init and not logs ) then
+    if( count % 60==0 ) then
+      local fps = 60 / (unix.time()-(t_count or 0))
+      t_count = unix.time();
+      print('FPS: ',fps)
+      print('Time',timestamp-timestamp0 );
+      print('User 1: ', pc[1]['get_skeleton_found']());
+      print('User 2: ', pc[2]['get_skeleton_found']());
+      print('Switched? ',switch)
+      print();
+    end
+  end
+  if(logs and count<n_logs) then
+    if( count % 60==0 ) then
+      print('Count:',count,'Time:',timestamp );    
+    end
+    local timestamp_next = log[count+1].t;
+    local twait = timestamp_next-timestamp;
+    local t_loop = unix.time() - t_start;
+    unix.usleep( 1e6*math.max(twait-t_loop,0) );
+  end
 
 end
 -- After done playing, reset the skeleton found variable so no more movement

7430889b1fe65dca034fd36f576b78a9814460de
Yida@linus <yida@seas.upenn.edu>
2012-08-11 21:49:17 -0400

    add working lut for l512 obs testing and headback state for linear interpolation

--- Player/HeadFSM/ObstacleChallenge/headBack.lua
+++ Player/HeadFSM/ObstacleChallenge/headBack.lua
@@ -15,7 +15,7 @@ yawMag = 10*math.pi/180;
 yaw0 = 0*math.pi/180;
 
 tScan = 3.0;
-timeout = tScan * 32;
+timeout = tScan;
 
 t0 = 0;
 direction = 1;
@@ -28,12 +28,12 @@ function entry()
   -- start scan in ball's last known direction
   t0 = Body.get_time();
   ball = wcm.get_ball();
-  timeout = tScan * 2;
+--  timeout = tScan * 2;
 
   yaw_0, pitch_0 = HeadTransform.ikineCam(ball.x, ball.y,0);
   local currentYaw = Body.get_head_position()[1];
-  local InitHeadAngle = Body.get_head_position();
-  print(InitHeadAngle[1] * 180 / math.pi, InitHeadAngle[2] * 180 / math.pi);
+  InitHeadAngle = Body.get_head_position();
+--  print(InitHeadAngle[1] * 180 / math.pi, InitHeadAngle[2] * 180 / math.pi);
 
   if currentYaw>0 then
     direction = 1;
@@ -60,30 +60,11 @@ function update()
   local ph = (t-t0)/tScan;
   ph = ph - math.floor(ph);
 
-  if ph<0.25 then --phase 0 to 0.25
---    yaw=yawMag*(ph*4)* direction;
-      pitch=pitchMag*(ph*4)*direction;
---    pitch=pitch0+pitchMag*pitchDir;
-      yaw= yaw0 + yawMag*yawDir;
-  elseif ph<0.75 then --phase 0.25 to 0.75
---    yaw=yawMag*(1-(ph-0.25)*4)* direction;
-      pitch=pitchMag*(1-(ph-0.25)*4)* direction;
---  pitch=pitch0-pitchMag*pitchDir;
-      yaw = yaw0 - yawMag * yawDir;
-  else --phase 0.75 to 1
-      pitch=pitchMag*(-1+(ph-0.75)*4)* direction;
---    yaw=yawMag*(-1+(ph-0.75)*4)* direction;
---    pitch=pitch0+pitchMag*pitchDir;
-      yaw = yaw0 + yawMag * yawDir;
-  end
+  yaw = InitHeadAngle[1] - ph * InitHeadAngle[1];
+  pitch = InitHeadAngle[2] - ph * InitHeadAngle[2];
+--  print(ph, yaw * 180 / math.pi, pitch * 180/ math.pi);
 
-  Body.set_head_command({yaw0, pitch-pitchBias+pitch0});
-
-  ocm.set_vision_update(1);
---  if vcm.get_freespace_allBlocked() == 1 then
---    print('blocked view')
---    return 'blocked'
---  end
+  Body.set_head_command({yaw, pitch});
 
   if (t - t0 > timeout) then
     return 'timeout';

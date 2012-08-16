8f718d41772e60a0615ee042805a95921a5ffd2d
Spencer Lee <lesp@seas.upenn.edu>
2012-08-16 16:17:16 -0400

    Modified Makefile for nao, modified Config
    -Makefile: removed setup_occmap
    -config_Naov4.lua: Disabled espeak, changed camera config to a file that exists

--- Player/Config/Config_NaoV4.lua
+++ Player/Config/Config_NaoV4.lua
@@ -31,7 +31,7 @@ param.world = 'World/Config_Nao_World'
 param.walk = 'Walk/Config_NaoV4_Walk_FastStable' 
 param.kick = 'Kick/Config_Nao_Kick'
 param.vision = 'Vision/Config_NaoV4_Vision'
-param.camera = 'Vision/Config_NaoV4_Camera_Mexico621_fieldC'
+param.camera = 'Vision/Config_NaoV4_Camera_Levine512'
 param.fsm = 'FSM/Config_NaoV4_FSM'
 
 loadconfig(param.world)
@@ -57,7 +57,7 @@ dev.walk = 'Walk/NaoV4Walk';
 dev.kick = 'NewKick';
 
 --Speak enable
-speakenable = 1;
+speakenable = 0;
 
 
 -- FSM Parameters

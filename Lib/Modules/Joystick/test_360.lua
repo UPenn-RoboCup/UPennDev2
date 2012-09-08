4daa15427da44827f1e2379484e6142c342b68c9
Stephen McGill <smcgill3@seas.upenn.edu>
2012-09-07 22:25:18 -0400

    Working xbox360 module with libusb-1.0

--- Lib/Modules/Joystick/test_360.lua
+++ Lib/Modules/Joystick/test_360.lua
@@ -3,6 +3,12 @@ require 'unix'
 
 xbox360.open()
 
-unix.usleep(5e6)
+for i=1,100 do
+	local buttons = xbox360.button()
+	local axes = xbox360.axis()
+	print('Buttons:', unpack(buttons) )
+	print('Axes:', unpack(axes) )
+	unix.usleep(1e5)
+end
 
 xbox360.close()

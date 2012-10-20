42a22768e250d6eb88e353fc5bdf9432ebdf5284
SJ <seungjoon.yi@gmail.com>
2012-10-19 22:05:05 -0400

    Add some ICRA code

--- Tools/Matlab/primesense/Skeleton.m
+++ Tools/Matlab/primesense/Skeleton.m
@@ -25,7 +25,7 @@ jointNames = { ...
     };
 nJoints = numel(jointNames);
 
-nPlayers = 2;
+nPlayers =1; %2;
 if( run_from_log )
     nPlayers = 1;
 end
@@ -43,7 +43,7 @@ end
 if( ~isfield( players{1},'sk' ) && ~run_from_log )
     disp('Recreating SHM block access...')
     startup;
-    team = 0;
+    team = 18;
     for pl=1:nPlayers
         players{pl}.sk = shm_primesense(team,pl);
     end

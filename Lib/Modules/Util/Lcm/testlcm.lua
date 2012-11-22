53093efad596c92bbe8f01df774addbc80e2259b
Yida Zhang <hitzhangyd@gmail.com>
2012-11-21 19:40:35 -0800

    add sample lcm type and swig interface

--- Lib/Modules/Util/Lcm/testlcm.lua
+++ Lib/Modules/Util/Lcm/testlcm.lua
@@ -1,8 +1,12 @@
-local lcm = require('lualcm')
+lcm = require('lualcm')
+require('lcm_rpc_t')
 
-for k, v in pairs(lcm) do
-  if type(v) == 'function' then
-    print(k)
-  end
-end
+local rpc_request = {
+  process_id = 124,
+  request_id = 456,
+  eval_string = "walk.stop()"
+}
+rpc_request.nbyyes = #rpc_request.eval_string
 
+rpc_client = lcm.lcm_create(NULL)
+lcm_rpc_t.lcm_rpc_t_publish(rpc_client, "EXAMPLE", rpc_request)

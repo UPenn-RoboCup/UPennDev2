dbc281b6b472daa872f20fcb4023e7ce357fab0f
Yida Zhang <hitzhangyd@gmail.com>
2012-11-27 12:23:09 -0500

    change to use lcm as the module name

--- Lib/Modules/Util/Lcm/testlcm.lua
+++ Lib/Modules/Util/Lcm/testlcm.lua
@@ -1,6 +1,7 @@
-lcm = require('lualcm')
-require('lcm_rpc_t')
+require('lcm')
+--require('lcm_rpc_t')
 
+--[[
 local rpc_request = {
   process_id = 124,
   request_id = 456,
@@ -10,3 +11,8 @@ rpc_request.nbyyes = #rpc_request.eval_string
 
 rpc_client = lcm.lcm_create(NULL)
 lcm_rpc_t.lcm_rpc_t_publish(rpc_client, "EXAMPLE", rpc_request)
+--]]
+
+for k,v in pairs(lcm) do
+  print(k,v)
+end

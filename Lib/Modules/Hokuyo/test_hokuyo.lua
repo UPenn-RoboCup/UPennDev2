e0efffbcabaa14400da25384aef55f3413fc7704
Yida Zhang <hitzhangyd@gmail.com>
2012-11-30 19:42:20 -0500

    add lua_signal to handle SIGINT and SIGTERM for proper hokuyo shutdown

--- Lib/Modules/Hokuyo/test_hokuyo.lua
+++ Lib/Modules/Hokuyo/test_hokuyo.lua
@@ -1,1 +1,13 @@
 require('Hokuyo')
+require('signal')
+
+function ShutDownFN()
+  print("Proper shutdown")
+  Hokuyo.shutdown()
+  os.exit(1);
+end
+
+while (true) do
+  signal.signal("SIGINT", ShutDownFN);
+  signal.signal("SIGTERM", ShutDownFN);
+end

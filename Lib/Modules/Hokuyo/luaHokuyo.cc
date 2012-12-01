1f0684aee3bc1989fe8a7f526a88a2f269be0e58
Yida Zhang <hitzhangyd@gmail.com>
2012-11-30 19:42:20 -0500

    add lua_signal to handle SIGINT and SIGTERM for proper hokuyo shutdown

--- Lib/Modules/Hokuyo/luaHokuyo.cc
+++ Lib/Modules/Hokuyo/luaHokuyo.cc
@@ -1,11 +1,20 @@
 #include "luaHokuyo.h"
 
+#include <iostream>
+
+static int lua_hokuyo_shutdown(lua_State *L) {
+  std::cout << "Proper Shutdown Hokuyo" << std::endl;
+  return 1;
+}
+
 static const struct luaL_reg hokuyo_lib [] = {
+  {"shutdown", lua_hokuyo_shutdown}, 
   {NULL, NULL}
 };
 
 int luaopen_Hokuyo(lua_State *L) {
   luaL_register(L, "Hokuyo", hokuyo_lib);
+
   return 1;
 }
 

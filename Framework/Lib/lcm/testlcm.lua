0be15a0aed4542442bb821494bb89d44c5b7e7e6
Stephen McGill <smcgill3@seas.upenn.edu>
2012-12-04 12:32:44 -0500

    Fixing build for test files

--- Framework/Lib/lcm/testlcm.lua
+++ Framework/Lib/lcm/testlcm.lua
@@ -13,16 +13,14 @@ rpc_client = lcm.lcm_create(NULL)
 lcm_rpc_t.lcm_rpc_t_publish(rpc_client, "EXAMPLE", rpc_request)
 --]]
 
---[[
 for k,v in pairs(lcm) do
   if type(v) == 'function' then
     print(k, v)
   end
 end
---]]
 
 -- Set multicast TTL to 1 to limit packets published to local network
-lc = lcm.lcm_create('udpm://239.255.76.67:7667?ttl=1')
+lc = lcm.new('udpm://239.255.76.67:7667?ttl=1')
 if (lc == 0) then
   print("lcm create error");
   return 1;
@@ -30,11 +28,11 @@ else
   print(type(lc))
 end
 
---[[
+
 while (true) do
-  lcm.lcm_handle(lc);
+  lcm.handle(lc);
 end
---]]
+
 
 lcm.lcm_destroy(lc)
 

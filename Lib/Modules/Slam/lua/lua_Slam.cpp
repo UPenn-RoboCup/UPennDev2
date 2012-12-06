cfd96ec13b1320bcbd2bbf69800c7854e099c768
Yida Zhang <hitzhangyd@gmail.com>
2012-12-06 11:04:05 -0500

    add lua wrapper for SubsambleDistance

--- Lib/Modules/Slam/lua/lua_Slam.cpp
+++ Lib/Modules/Slam/lua/lua_Slam.cpp
@@ -14,28 +14,15 @@ extern "C" {
 #include "lua_getMapCellsFromRay.h"
 #include "lua_HoughTransform.h"
 #include "lua_ScanMatch2D.h"
+#include "lua_SubsambleDistance.h"
 
-static int lua_test(lua_State *L) {
-  lua_createtable(L, 0, 1);
-  lua_pushstring(L, "counter");
-  lua_pushinteger(L, 5467);
-  lua_settable(L, -3);
-
-  lua_createtable(L, 0, 1);
-  lua_pushstring(L, "ter");
-  lua_pushinteger(L, 5467);
-  lua_settable(L, -3);
- 
-  return 2;
-}
 
 static const struct luaL_reg Slam_lib [] = {
-  {"test", lua_test},
   {"binStats", lua_binStats},
   {"getMapCellsFromRay", lua_getMapCellsFromRay},
   {"HoughTransform", lua_HoughTransform},
   {"ScanMatch2D", lua_ScanMatch2D},
-//  {"SubsambleDistance", lua_SubsambleDistance},
+  {"SubsambleDistance", lua_SubsambleDistance},
   {NULL, NULL}
 };
 

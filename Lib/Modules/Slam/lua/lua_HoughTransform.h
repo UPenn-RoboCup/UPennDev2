d8519cc05610944a05ff9777b29603dc1be105bc
Yida Zhang <hitzhangyd@gmail.com>
2012-12-05 19:15:43 -0500

    add more header for individual slam function

---
+++ Lib/Modules/Slam/lua/lua_HoughTransform.h
@@ -0,0 +1,6 @@
+#undef __lua_HoughTransform_H
+#define  __lua_HoughTransform_H
+
+int lua_HoughTransform(lua_State *L);
+
+#endif
---
+++ Lib/Modules/Slam/lua/test_slam.lua
@@ -0,0 +1,5 @@
+require('Slam');
+
+for k,v in pairs(Slam) do
+  print(k,v);
+end

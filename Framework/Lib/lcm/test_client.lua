a4c7bc9621928275015c6dc9bbbfb15e1f629b61
Mike Hopkins <michael-hopkins@outlook.com>
2012-12-07 00:54:15 -0500

    change rpc eval_string to nul terminated string, lcm variable arrays have a bug

--- Framework/Lib/lcm/test_client.lua
+++ Framework/Lib/lcm/test_client.lua
@@ -4,10 +4,9 @@ require('lcm_rpc_request_t')
 local msg = {
   client_id = "uuid",
   request_id = 456,
-  eval_string = "walk.stop()",
+  eval_string = "walk.stop",
   synchronous = true
 }
-msg.eval_nbytes = #msg.eval_string
 
 rpc_client = lcm.new()
 rpc_client:rpc_request_t_publish("EXAMPLE", msg)

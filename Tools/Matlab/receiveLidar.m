519e5b285ec31d1ddf2559ce20c193966d00e98c
Yida@humanoids <yida@seas.upenn.edu>
2012-12-06 21:44:25 -0500

    merge hcm with rcm and test receiveLiar example with lightuserdata range output

--- Tools/Matlab/receiveLidar.m
+++ Tools/Matlab/receiveLidar.m
@@ -1,7 +1,8 @@
-robot = shm_robot(0,1);
+robot = shm_robot(18,1);
 
 while(1)
-      plot(robot.hokuyo.get_ranges(), '.');
+      range = typecast(robot.lidar.get_ranges() ,'SINGLE');
+      plot(range, '.');
       drawnow;
       %fprintf(1,'.');
 end

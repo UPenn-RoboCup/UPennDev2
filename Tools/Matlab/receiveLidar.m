f405964748cea1174d41e71ad71ad721d3d5aa5c
UPenn Humanoids <upenn.humanoids@gmail.com>
2012-12-16 12:45:32 -0500

    Fixing the client machine for receiving data

--- Tools/Matlab/receiveLidar.m
+++ Tools/Matlab/receiveLidar.m
@@ -1,8 +1,13 @@
-robot = shm_robot(18,1);
+%robot = shm_robot(1,1);
 
 while(1)
       range = typecast(robot.lidar.get_ranges() ,'SINGLE');
+      t = robot.lidar.get_timestamp();
+      c = robot.lidar.get_counter();
+      fprintf(1,'Time: %.4f, Count: %d\n',t,c);
+      
       plot(range, '.');
       drawnow;
       %fprintf(1,'.');
+      pause(1/40);
 end

75d6fa2dacd1c366ecc96bdc17b365f2a8bc3287
Mike Hopkins <michael-hopkins@outlook.com>
2013-01-05 18:03:38 -0500

    add stepMJT parameter files

--- Run/Test/test_step.lua
+++ Run/Test/test_step.lua
@@ -1,25 +1,38 @@
 dofile('../include.lua')
 
 require('Body')
+require('Proprioception')
 require('Transform')
 require('Kinematics')
 require('stepMJT')
 require('vector')
 require('dcm')
 
---local INIT_STANCE = true
 local INIT_STANCE = false
+local PARAMETER_MODULE = 'parameters_stepMJT_WebotsASH_0'
+local SLOW_MO = true
 
--- initialize step parameters (coordinates relative to l_foot frame)
-----------------------------------------------------------------------
+-- load parameters
+--------------------------------------------------------------------------------
 
+local success, parameters = pcall(require, PARAMETER_MODULE)
+if (not success) then
+  print('Could not load parameter module', PARAMETER_MODULE)
+  return
+end
+
+-- init controller
+--------------------------------------------------------------------------------
 Body.entry()
+Proprioception.entry()
+step:set_parameters(parameters)
 step:entry()
 
 local q_start = step:get_joint_start_position()
 local v_torso = step:get_torso_start_velocity()
 
 -- update stance
+--------------------------------------------------------------------------------
 if (INIT_STANCE) then
   print('init stance')
   Body.set_simulator_torso_frame(Transform.pose6D{0, 0, 100})
@@ -34,14 +47,21 @@ if (INIT_STANCE) then
 end
 
 -- update step
+--------------------------------------------------------------------------------
 print('update step')
-step:reset()
 Body.set_simulator_torso_twist({v_torso[1], v_torso[2], v_torso[3], 0, 0, 0})
+step:reset()
 while (true) do
   Body.update()
+  Proprioception.update()
   step:update()
-  unix.usleep(1e4)
+  if (SLOW_MO) then
+    unix.usleep(1e4)
+  end
 end
 
+-- exit
+--------------------------------------------------------------------------------
 step:exit()
+Proprioception.exit()
 Body.exit()

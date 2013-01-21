86fe0ce411a8a709a30d4dd2949872607329c0ec
yizhe <yizhe@seas.upenn.edu>
2013-01-21 15:26:14 -0500

    new vision configuration for Grasp Lab

--- Player/Config/Vision/Config_NaoV4_Camera_Levine512.lua
+++ Player/Config/Vision/Config_NaoV4_Camera_Levine512.lua
@@ -18,15 +18,15 @@ camera.focal_base = 640; -- image width used in focal length calculation
 
 camera.param = {};
 -- Contrast should be set between 17 and 64
-camera.param[1] = {key='Contrast'       , val={20 , 20}};
+camera.param[1] = {key='Contrast'       , val={25 , 25}};
 
-camera.param[2] = {key='Saturation'     , val={180, 180}};
+camera.param[2] = {key='Saturation'     , val={200, 200}};
 -- Hue will automatically change to 0 if set to a number between -5 and 5, but cannot be set by other numbers
 camera.param[3] = {key='Hue'            , val={0  , 0}};
 
-camera.param[4] = {key='Exposure'       , val={43 , 43}};
+camera.param[4] = {key='Exposure'       , val={65 , 80}};
 -- Gain should be set between 32 and 255
-camera.param[5] = {key='Gain'           , val={70 , 70}};
+camera.param[5] = {key='Gain'           , val={65 , 80}};
 -- Sharpness should be set between 0 and 7
 camera.param[6] = {key='Sharpness'      , val={2  , 2}};
 

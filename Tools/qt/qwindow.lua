4294d68989ea7bfb55d7ad55961e2875acc3fc0e
Yida Zhang <yida@seas.upenn.edu>
2013-01-21 15:34:22 -0500

    Qt Window and Menu testing - add testing for QMainWindow, QAction, QMenu, QToolBar, QStatusBar

--- Tools/qt/qwindow.lua
+++ Tools/qt/qwindow.lua
@@ -7,13 +7,15 @@ require 'qtgui'
 app = QApplication(1 + select('#', ...), {arg[0], ...})
 app.__gc = app.delete -- take ownership of object
 
-for k,v in pairs(QFrame) do
-  print(k, v)
+local onplus = function(self)
+  local val = QString.toInt(QLabel.text(self))
+  val = val + 1;
+  QLabel.setText(self, QString.number(val))
 end
 
 local new_window = function(...)
-  width = 800;
-  height = 600;
+  width = 800
+  height = 600
   
   local this = QWidget.new(...)
   
@@ -22,23 +24,33 @@ local new_window = function(...)
   button:setGeometry(0, 0, 75, 30);
   button:connect("2clicked()", app, "1quit()")
   
-  local frame1 = QFrame(this, ...);
+  local frame1 = QFrame(this, ...)
   frame1:setFrameStyle(1)
   frame1:setCursor('SizeAllCursor')
   --
-  local frame2 = QFrame(this, ...);
+  local frame2 = QFrame(this, ...)
   frame2:setFrameStyle(1)
   frame2:setCursor('WaitCursor')
   --
-  local frame3 = QFrame(this, ...);
+  local frame3 = QFrame(this, ...)
   frame3:setFrameStyle(1)
   frame3:setCursor('PointingHandCursor')
-  
+
+  local label = QLabel(this, ...)
+  label:setGeometry(190, 80, 20, 30)
+  label:setText(QString.number(40))
+  label:__addmethod("OnPlus()", onplus)
+  local plusbutton = QPushButton("+", this)
+  plusbutton:connect('2clicked()', label, '1OnPlus()');
+
+
   layout = QGridLayout.new()
   layout:addWidget(button, 0, 0)
   layout:addWidget(frame1, 0, 1)
   layout:addWidget(frame2, 1, 0)
   layout:addWidget(frame3, 0, 2)
+  layout:addWidget(label, 1, 1)
+  layout:addWidget(plusbutton, 1, 2)
   this:setLayout(layout)
   
   return this

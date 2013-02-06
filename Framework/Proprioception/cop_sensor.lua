a65657e2c409c81beb25172d5a4a5a515853c71f
Mike Hopkins <michael-hopkins@outlook.com>
2013-02-05 23:54:43 -0500

    add contact_sensor and cop_sensor to proprioception

--- Framework/Proprioception/cop_sensor.lua
+++ Framework/Proprioception/cop_sensor.lua
@@ -1,60 +1,48 @@
 require('dcm')
+require('pcm')
 require('Config')
 
 ---------------------------------------------------------------------------
--- cop_zmp : utilities for CoP / ZMP estimation
+-- cop_sensor : estimates CoP from foot poses and wrenches
 ---------------------------------------------------------------------------
 
-cop_zmp = {}
-
-local l_foot_cop_transform = Config.mechanics.l_foot.force_torque_transform
-local r_foot_cop_transform = Config.mechanics.r_foot.force_torque_transform
-local l_foot_cop_offset = l_foot_cop_transform:get_pose6D()
-local r_foot_cop_offset = r_foot_cop_transform:get_pose6D()
+cop_sensor = {}
 
 local function sign(x)
   return x < 0 and -1 or 1
 end
 
-function cop_zmp.estimate_l_foot_cop()
-  -- returns CoP relative to left foot frame
-  -- (considering ground reaction forces acting on left foot only)
-  local ft = dcm:get_force_torque('l_foot')
-  local pressure = ft[3]
+local function compute_foot_cop(foot_wrench)
+  -- compute local CoP relative to foot frame
+  local w = foot_wrench
+  local fx, fy, fz = w[1], w[2], w[3]
+  local tx, ty, tz = w[4], w[5], w[6]
 
-  if (math.abs(pressure) < 1e-10) then
-    pressure = sign(pressure)*1e-10
-  end
-
+  local pressure
   local cop = {}
-  cop[1] = -ft[5]/pressure + l_foot_cop_offset[1]
-  cop[2] = ft[4]/pressure + l_foot_cop_offset[2]
-  cop[3] = 0
+  if (math.abs(fz) < 1e-10) then
+    cop[1] = 0
+    cop[2] = 0
+    cop[3] = 0
+    pressure = sign(fz)*1e-10
+  else
+    cop[1] = -ty/fz
+    cop[2] = tx/fz
+    cop[3] = 0
+    pressure = fz
+  end
   return cop, pressure
 end
 
-function cop_zmp.estimate_r_foot_cop()
-  -- returns CoP relative to right foot frame
-  -- (considering ground reaction forces acting on right foot only)
-  local ft = dcm:get_force_torque('r_foot')
-  local pressure = ft[3]
-
-  if (math.abs(pressure) < 1e-10) then
-    pressure = sign(pressure)*1e-10
-  end
-
-  local cop = {}
-  cop[1] = -ft[5]/pressure + r_foot_cop_offset[1]
-  cop[2] = ft[4]/pressure + r_foot_cop_offset[2]
-  cop[3] = 0
-  return cop, pressure
+function cop_sensor.entry()
 end
 
-function cop_zmp.estimate_cop(l_foot_position, r_foot_position)
-  -- returns CoP relative to torso frame
-  -- (considering ground reaction forces acting on left and right foot)
-  local l_foot_cop, l_foot_pressure = cop_zmp.estimate_l_foot_cop()
-  local r_foot_cop, r_foot_pressure = cop_zmp.estimate_r_foot_cop()
+function cop_sensor.update()
+  -- compute net CoP relative to base frame
+  local l_foot_pose = pcm:get_l_foot_pose()
+  local r_foot_pose = pcm:get_r_foot_pose()
+  local l_foot_cop, l_foot_pressure = compute_foot_cop(pcm:get_l_foot_wrench())
+  local r_foot_cop, r_foot_pressure = compute_foot_cop(pcm:get_r_foot_wrench())
   local pressure = l_foot_pressure + r_foot_pressure
 
   if (math.abs(pressure) < 1e-10) then
@@ -62,12 +50,22 @@ function cop_zmp.estimate_cop(l_foot_position, r_foot_position)
   end
 
   local cop = {}
-  cop[1] = (l_foot_position[1] + l_foot_cop[1])*l_foot_pressure/pressure
-         + (r_foot_position[1] + r_foot_cop[1])*r_foot_pressure/pressure
-  cop[2] = (l_foot_position[2] + l_foot_cop[2])*l_foot_pressure/pressure
-         + (r_foot_position[2] + r_foot_cop[2])*r_foot_pressure/pressure
+  cop[1] = (l_foot_pose[1] + l_foot_cop[1])*l_foot_pressure/pressure
+         + (r_foot_pose[1] + r_foot_cop[1])*r_foot_pressure/pressure
+  cop[2] = (l_foot_pose[2] + l_foot_cop[2])*l_foot_pressure/pressure
+         + (r_foot_pose[2] + r_foot_cop[2])*r_foot_pressure/pressure
   cop[3] = 0
-  return cop, pressure
+
+  -- update pcm
+  pcm:set_cop(cop)
+  pcm:set_cop_pressure(pressure)
+  pcm:set_l_foot_cop(l_foot_cop)
+  pcm:set_l_foot_cop_pressure(l_foot_cop_pressure)
+  pcm:set_r_foot_cop(r_foot_cop)
+  pcm:set_r_foot_cop_pressure(r_foot_cop_pressure)
 end
 
-return cop_zmp
+function cop_sensor.exit()
+end
+
+return cop_sensor

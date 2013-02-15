8d6b95526a391d220836db0cee0728a2f731baef
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2013-02-14 21:01:33 -0500

    First-pass update of VRep Platform.lua

--- Platform/VRepASH/Platform.lua
+++ Platform/VRepASH/Platform.lua
@@ -11,31 +11,24 @@ local function limit(x, min, max)
   return math.min(math.max(x, min), max)
 end
 
+local function sign(x)
+  if x > 0 then
+    return 1
+  elseif x < 0 then
+    return -1
+  else
+    return 0
+  end
+end
+
 -- Setup
 ---------------------------------------------------------------------------
 
 local N_JOINT = 12
-local joint = Config.joint
-local simulator_iterations = 2
-
--- servo controller parameters
-local max_force = 100
-local max_stiffness = 20000
-local max_damping = 0  -- damping disabled due to ODE instability
-local max_velocity = 7
-local max_acceleration = 70
-local velocity_p_gain = 0.25*vector.ones(N_JOINT)
-
-local joint_ff_force = vector.zeros(N_JOINT)
-local joint_p_force = vector.zeros(N_JOINT)
-local joint_d_force = vector.zeros(N_JOINT)
-
 local handles = {} -- vrep handles
-local time_step = nil
-local torso_twist_updated = 0
-local torso_twist = vector.zeros(6)
+local time_step_ms = nil
 
-local servoNames = { -- webots servo names
+local servoNames = { -- vrep servo names
   'l_hip_yaw', 'l_hip_roll', 'l_hip_pitch',
   'l_knee_pitch', 'l_ankle_pitch', 'l_ankle_roll',
   'r_hip_yaw', 'r_hip_roll', 'r_hip_pitch',
@@ -46,35 +39,23 @@ local servoNames = { -- webots servo names
 ----------------------------------------------------------------
 
 local function initialize_devices()
-  -- intialize webots devices
-  handles.robot = webots.wb_supervisor_node_get_from_def('Ash')
-  handles.robot_translation = webots.wb_supervisor_node_get_field(
-    handles.robot, "translation")
-  handles.robot_rotation = webots.wb_supervisor_node_get_field(
-    handles.robot, "rotation")
-  handles.gyro = webots.wb_robot_get_device('gyro')
-  webots.wb_gyro_enable(handles.gyro, time_step)
-  handles.accel = webots.wb_robot_get_device('accelerometer')
-  webots.wb_accelerometer_enable(handles.accel, time_step)
-  handles.physics_receiver = webots.wb_robot_get_device('physics_receiver')
-  webots.wb_receiver_enable(handles.physics_receiver, time_step)
-  handles.physics_emitter = webots.wb_robot_get_device('physics_emitter')
+  -- intialize vrep devices
+  handles.robot = simGetObjectHandle('Ash')
+  handles.gyro = simTubeOpen(0, 'gyroData#')
+  handles.accel = simTubeOpen(0, 'accelerometerData#')
 
   handles.servo = {}
   for i = 1,N_JOINT do
-    handles.servo[i] = webots.wb_robot_get_device(servoNames[i])
-    webots.wb_servo_enable_position(handles.servo[i], time_step)
-    webots.wb_servo_enable_motor_force_feedback(handles.servo[i], time_step)
-    webots.wb_servo_set_control_p(handles.servo[i], 100)
-    webots.wb_servo_set_position(handles.servo[i], 1/0)
-    webots.wb_servo_set_velocity(handles.servo[i], 0)
-    webots.wb_servo_set_motor_force(handles.servo[i], 1e-15)
-    webots.wb_servo_set_acceleration(handles.servo[i], max_acceleration)
+    handles.servo[i] = simGetObjectHandle(servoNames[i])
+    -- make joint motion cyclic (unbounded)
+    simSetJointInterval(handles.servo[i], true, nil)
+    simSetJointTargetVelocity(handles.servo[i], 0)
+    simSetJointForce(handles.servo[i], 0)
   end
 end
 
 local function update_actuators()
-  -- update webots actuator values
+  -- update vrep actuator values
   local joint_enable = dcm:get_joint_enable()
   local joint_force_desired = dcm:get_joint_force()
   local joint_position_desired = dcm:get_joint_position()
@@ -83,104 +64,47 @@ local function update_actuators()
   local joint_velocity_actual = dcm:get_joint_velocity_sensor()
   local joint_stiffness = dcm:get_joint_stiffness()
   local joint_damping = dcm:get_joint_damping()
-  local position_error = vector.zeros(N_JOINT)
-  local velocity_error = vector.zeros(N_JOINT)
-
-  -- calculate joint forces
-  for i = 1,N_JOINT do
-    if (joint_enable[i] == 0) then
-      -- zero forces
-      joint_ff_force[i] = 0
-      joint_d_force[i] = 0
-      joint_p_force[i] = 0
-      webots.wb_servo_set_force(handles.servo[i], 0)
-    else
-      -- calculate feedforward force
-      joint_ff_force[i] = joint_force_desired[i]
-      joint_ff_force[i] = limit(joint_ff_force[i], -max_force, max_force)
-      -- calculate spring force
-      position_error[i] = joint_position_desired[i] - joint_position_actual[i]
-      joint_stiffness[i] = limit(joint_stiffness[i], 0, 1)
-      joint_p_force[i] = joint_stiffness[i]*max_stiffness*position_error[i]
-      joint_p_force[i] = limit(joint_p_force[i], -max_force, max_force)
-      -- calculate damping force
-      velocity_error[i] = joint_velocity_desired[i] - joint_velocity_actual[i]
-      joint_damping[i] = limit(joint_damping[i], 0, 1)
-      joint_d_force[i] = joint_damping[i]*max_damping*velocity_error[i]
-      joint_d_force[i] = limit(joint_d_force[i], -max_force, max_force)
-    end
-  end
 
   -- update spring force using motor velocity controller
   for i = 1,N_JOINT do
-    local max_servo_force = math.abs(joint_p_force[i])
-    local servo_velocity = velocity_p_gain[i]*position_error[i]*(1000/time_step)
-    servo_velocity = limit(servo_velocity, -max_velocity, max_velocity)
-    webots.wb_servo_set_motor_force(handles.servo[i], max_servo_force)
-    webots.wb_servo_set_velocity(handles.servo[i], servo_velocity)
+    local max_servo_force = math.abs(joint_force_desired[i])
+    simSetJointForce(handles.servo[i], max_servo_force)
+    simSetJointTargetVelocity(handles.servo[i], joint_velocity_desired[i])
   end
-
-  local buffer = cbuffer.new((N_JOINT + 7)*8)
-
-  -- update feedforward and damping forces using physics plugin
-  for i = 1,N_JOINT do
-    local servo_force = joint_ff_force[i] + joint_d_force[i]
-    servo_force =  limit(servo_force, -max_force, max_force)
-    buffer:set('double', servo_force, (i-1)*8)
-  end
-
-  -- update torso twist using physics plugin
-  buffer:set('double', torso_twist_updated, (N_JOINT)*8)
-  for i = 1,6 do
-    buffer:set('double', torso_twist[i], (N_JOINT + i)*8)
-  end
-  torso_twist_updated = 0
-
-  webots.wb_emitter_send(handles.physics_emitter, tostring(buffer))
 end
 
 local function update_sensors()
-  -- update webots sensor values
+  -- update VRep sensor values
   local joint_enable = dcm:get_joint_enable()
   for i = 1,N_JOINT do
     if (joint_enable[i] == 0) then
       dcm:set_joint_force_sensor(0, i)
     else
-      dcm:set_joint_force_sensor(webots.wb_servo_get_motor_force_feedback(
-          handles.servo[i]) + joint_ff_force[i], i)
+      dcm:set_joint_force_sensor(
+        simJointGetForce(handles.servo[i]), i)
     end
+    
     dcm:set_joint_position_sensor(
-        webots.wb_servo_get_position(handles.servo[i]), i)
+        simGetJointPosition(handles.servo[i]), i)
   end
 
   -- update imu readings
-  local t = {}
-  local orientation = webots.wb_supervisor_node_get_orientation(handles.robot)
-  t[1] = vector.new({orientation[1], orientation[2], orientation[3], 0})
-  t[2] = vector.new({orientation[4], orientation[5], orientation[6], 0})
-  t[3] = vector.new({orientation[7], orientation[8], orientation[9], 0})
-  t[4] = vector.new({0, 0, 0, 1});
-  local euler_angles = Transform.get_euler(t)
-  dcm:set_ahrs(webots.wb_gyro_get_values(handles.gyro), 'gyro')
-  dcm:set_ahrs(webots.wb_accelerometer_get_values(handles.accel), 'accel')
+  local euler_angles = simGetObjectOrientation(handles.robot)
   dcm:set_ahrs(euler_angles, 'euler')
+  
+  local data = simTubeRead(handles.gyro)
+  if (data) then
+    dcm:set_ahrs(simUnpackFloats(data), 'gyro')
+  end
+  
+  data = simTubeRead(handles.accel)
+  if (data) then
+    dcm:set_ahrs(simUnpackFloats(data), 'accel')
+  end
 
-  -- update force-torque readings and joint velocities using physics plugin
-  local buffer = cbuffer.new(webots.wb_receiver_get_data(handles.physics_receiver))
-  local joint_velocity = {}
-  for i = 1,N_JOINT do
-    joint_velocity[i] = buffer:get('double', (i-1)*8)
-  end
-  dcm:set_joint_velocity_sensor(joint_velocity)
-  local l_fts = {}
-  local r_fts = {}
-  for i = 1,6 do
-    l_fts[i] = buffer:get('double', (i-1)*8 + N_JOINT*8)
-    r_fts[i] = buffer:get('double', (i-1)*8 + N_JOINT*8 + 48)
-  end
-  dcm:set_force_torque(l_fts, 'l_foot')
-  dcm:set_force_torque(r_fts, 'r_foot')
-  webots.wb_receiver_next_packet(handles.physics_receiver)
+  -- update force-torque readings
+  -- dcm:set_force_torque(l_fts, 'l_foot')
+  -- dcm:set_force_torque(r_fts, 'r_foot')
 end
 
 -- User interface
@@ -193,39 +117,36 @@ function Platform.set_time_step(t)
 end
 
 function Platform.get_time_step()
-  return time_step*simulator_iterations/1000
+  return time_step_ms/1000
 end
 
 function Platform.get_update_rate()
-  return 1000/(time_step*simulator_iterations)
+  return 1000/time_step_ms
 end
 
 function Platform.reset_simulator()
-  webots.wb_supervisor_simulation_revert()
+  simStopSimulation()
+  simStartSimulation()
 end
 
 function Platform.reset_simulator_physics()
-  webots.wb_supervisor_simulation_physics_reset()
+  simResetDynamicObject(handles.robot)
 end
 
 function Platform.set_simulator_torso_frame(frame)
   local pose = frame:get_pose6D()
-  webots.wb_supervisor_field_set_sf_vec3f(handles.robot_translation,
-    {pose[1], pose[2], pose[3]})
-  webots.wb_supervisor_field_set_sf_rotation(handles.robot_rotation,
-    {0, 0, 1, pose[4]})
+  -- -1 means set absolute position/orientation
+  simSetObjectPosition(handles.robot, -1, {pose[1], pose[2], pose[3]})
+  simSetObjectOrientation(handles.robot, -1, {pose[4], pose[5], pose[6]})
 end
 
 function Platform.set_simulator_torso_twist(twist)
-  torso_twist = twist
-  torso_twist_updated = 1
 end
 
 function Platform.entry()
-  -- initialize webots devices
-  webots.wb_robot_init()
-  time_step = simGetSimulationTimeStep()
+  -- initialize vrep devices
   initialize_devices()
+  time_step_ms = simGetSimulationTimeStep()
 
   -- initialize shared memory
   dcm:set_joint_enable(1, 'all')
@@ -243,13 +164,8 @@ function Platform.entry()
 end
 
 function Platform.update()
-  for i = 1, simulator_iterations do
-    update_actuators()
-    if (webots.wb_robot_step(time_step) < 0) then
-      os.exit()
-    end
-    update_sensors()
-  end
+  update_actuators()
+  update_sensors()
 end
 
 function Platform.exit()

1ee462d8a65101fc9f2116b13e7edadc91de12ab
Mike Hopkins <michael-hopkins@outlook.com>
2013-02-19 00:12:27 -0500

    add string parameter support to Motion api

--- Framework/Motion/MotionState.lua
+++ Framework/Motion/MotionState.lua
@@ -16,7 +16,6 @@ local joint = Config.joint
 function MotionState.new(name)
   local o = {_NAME = name}
   o.dcm = dcm.new_access_point()
-  o.running = false
   o.parameters = {}
   return setmetatable(o, MotionState)
 end
@@ -89,14 +88,8 @@ function MotionState:get_joint_access(index)
   return self.dcm:get_joint_write_access(index)
 end
 
-function MotionState:is_running()
-  -- return true if state is executing 
-  return self.running 
-end
-
 function MotionState:entry()
   -- default entry
-  self.running = true
 end
 
 function MotionState:update()
@@ -105,7 +98,6 @@ end
 
 function MotionState:exit()
   -- default exit
-  self.running = false
 end
 
 return MotionState

d30a6a873e534afaeffef28b4af463b5eb1e98a7
Stephen McGill <smcgill3@seas.upenn.edu>
2013-02-24 19:00:44 -0500

    Fixing the Ardu IMU reader

--- Player/Log/log_ardu_imu.lua
+++ Player/Log/log_ardu_imu.lua
@@ -1,55 +1,78 @@
-local pwd = os.getenv('PWD')
-package.cpath = pwd..'/../qt/lib/qt/?.so;'..package.cpath
-package.path = pwd..'/../qt/lib/ffi/?.lua;'..package.path
+-- Include the proper directories
+cwd = '.';
+package.cpath = cwd.."/../Lib/?.so;"..package.cpath;
+package.path = cwd.."/../Util/?.lua;"..package.path;
+package.path = cwd..'/../Util/ffi/?.lua;'..package.path
 
 --require 'include'
 local ffi = require 'ffi'
 local Serial = require('Serial');
 local unix = require('unix');
 
-fps = 150;
-seconds = 20
-nframes = fps*seconds;
-
-dev = '/dev/ttyUSB0';
+--dev = '/dev/ttyUSB0';
+dev = '/dev/tty.usbserial-A700eEMV'
 baud = 115200;
 s1 = Serial.connect(dev, baud);
 
-packetID = -1;
-function ReceivePacket() 
-  buf, buftype, bufsize = Serial.read(36, 5000);
-  return buf, bufsize
-end
-
--- Create files
-function cdata2string(cdata, len)
-  str = '';
-  for i = 0, len - 1 do
-    if cdata[i] == 40 or cdata[i] == 41 then cdata[i] = 32 end
-    str = str..string.format('%c', cdata[i])
+function ReceivePacket( nbytes ) 
+  local buf, buftype, bufsize = Serial.read( nbytes, 5000 );
+  local ts = unix.time();
+  -- Process the packet
+  local str = ffi.string( buf, bufsize );
+  -- Apply the straggler
+  if( straggle ) then
+    str = straggle..str;
+    straggle = nil;
   end
-  return str
+  -- Check if a newline is present
+  local newline_idx = str:find( '\n' );
+  if( newline_idx and newline_idx==nstr ) then
+    log_str = str:sub(1,#str-1);
+  elseif newline_idx then
+    straggle = string.sub( str, newline_idx+1 );
+    log_str =  str:sub( 1, newline_idx-1 );
+  else
+    straggle = str;
+  end
+  return log_str, ts;
 end
 
--- Log files
-file = io.open('imu', 'w')
-tfile = io.open('time', 'w')
+function flush_initial()
+  local buf, buftype, bufsize = Serial.read( 100, 100 );
+  unix.usleep(2e6);
+  local buf, buftype, bufsize = Serial.read( 5000, 100 );
+  ReceivePacket( 36 ); 
+  print( 'Flushed', bufsize );
+end
 
-for fr=1,nframes do
-  t1 = unix.time();
-  packet, size = ReceivePacket();
-  if size > 0 then
-    data = ffi.cast('uint8_t*', packet)
-    str = cdata2string(data, size)
-    if str[-1] ~= '*' then -- Last character delimiter
-      packet, size = ReceivePacket();
-      data = ffi.cast('uint8_t*', packet)
-      str = str..cdata2string(data, size)
-    end
-    tfile:write(unix.time()) tfile:write('\n')
-    file:write(str)
+function record()
+  -- Make sure we are writing to a file
+  if not imu_file then
+    imu_file = io.open('imu_'..logfile, 'w')
+  end
+  -- Receive data
+  imu_data_str, ts = ReceivePacket( 36 );
+  -- Write data
+  if imu_data_str and #imu_data_str>0 then
+    print( ts.." "..imu_data_str )
+    imu_file:write( ts.." "..imu_data_str.."\n" )
+    counter = counter+1;
+  end
+  
+  -- Close file at certain intervals
+  if counter%150==0 then
+    counter = 0;
+    logfile = logfile+1;
+    imu_file:close()
+    imu_file = nil;
   end
 end
 
-file:close();
-tfile:close();
+-- Run the Recording
+flush_initial()
+counter = 0;
+logfile = 1;
+while true do
+  record()
+--  return;
+end

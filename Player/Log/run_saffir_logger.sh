f9916a00e3d09ca2c492026666bbc056f1268e53
Yida@Betty <yida@seas.upenn.edu>
2013-02-26 10:34:56 -0500

    Modify the loggers

--- Player/Log/run_saffir_logger.sh
+++ Player/Log/run_saffir_logger.sh
@@ -9,3 +9,5 @@ echo "Start IMU recording"
 screen -dm -L -S log_imu $LUA log_ardu_imu.lua
 echo "Start LIDAR record"
 screen $SCREEN_FLAGS log_lidar $LUA log_lidar.lua
+echo "Start FLIR record"
+screen $SCREEN_FLAGS log_flir ./mnt/shadwell/log_flir

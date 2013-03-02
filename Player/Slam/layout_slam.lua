c8cccd8790a81144c5a2e31f37615038116cb0a8
Stephen McGill <smcgill3@seas.upenn.edu>
2013-03-02 13:43:36 -0500

    Forgot to add simple_ipc

--- Player/Slam/layout_slam.lua
+++ Player/Slam/layout_slam.lua
@@ -20,6 +20,7 @@ qimage = QImage(sz[2],sz[1],QImage.Format.Format_RGB32)
 omap_channel = simple_ipc.setup_subscriber('omap');
 slam_map = torch.ByteTensor():ones(sz[1],sz[2])*128
 slam_map_cdata = torch.data( slam_map )
+timestamp_cdata = ffi.new('double[1]',0);
 local tmp1 = slam_map:select(2, 1); -- smooth stride!
 local tmp2 = slam_map:select(1, 1); -- smooth stride!
 print('Strd',slam_map:stride(1), slam_map:stride(2) )
@@ -53,7 +54,12 @@ Widget = function(...)
 
   -- Set up the timer event
   function this:timerEvent(e)
-	  local n_bytes_recv = omap_channel:receive( slam_map_cdata, sz[1]*sz[2] );
+	  local n_bytes_recv, has_more = omap_channel:receive( slam_map_cdata, sz[1]*sz[2] );
+		if has_more then 
+			local nbytes, has_more = omap_channel:receive(timestamp_cdata)
+		else
+			print('No omap timestamp received!')
+		end
 	  --print('Received '..n_bytes_recv.." bytes")
     drawTensor(this)
 	-- Only redraws every other timer event??? why?
@@ -64,13 +70,12 @@ end
 
 function drawTensor( widget )
 	-- Get the pointer to the Torch data
-	local t0 = unix.time()
 	for line_no = 1,sz[1] do
 		local map_line = slam_map:select(1, line_no); -- smooth stride!
 		tutil.tensor2qimage( map_line, qimage:scanLine(line_no-1) )
 	end
-	local t1 = unix.time()
-	--print('Time to copy: ',t1-t0)
-pixmapitem:setPixmap( QPixmap.fromImage(qimage) )
+	local timestamp = timestamp_cdata[0]
+	print('Map timestamp',timestamp)
+	pixmapitem:setPixmap( QPixmap.fromImage(qimage) )
 
 end

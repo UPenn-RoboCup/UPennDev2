dbebd1027fccc0880427591b07111168436afe07
Stephen McGill <smcgill3@seas.upenn.edu>
2013-03-01 21:16:01 -0500

    Moving around test functions

--- Player/Test/ipc/test_zmq_reply.lua
+++ Player/Test/ipc/test_zmq_reply.lua
@@ -1,10 +1,17 @@
-local ffi = require( "ffi" )
-local zmq = require( "zmq" )
+-- Include the proper directories
+cwd = '.';
+package.path = cwd.."/../../Util/?.lua;"..package.path;
+
+-- Require the ffi modules
+local ffi = require 'ffi'
+local zmq = require 'ffi/zmq'
+
+-- Setup the millisecond sleep function
 ffi.cdef[[
 int poll(struct pollfd *fds, unsigned long nfds, int timeout);
 ]]
-function sleep(s)
-    ffi.C.poll(nil, 0, s*1000)
+local function msleep(ms)
+  ffi.C.poll(nil, 0, ms)
 end
 
 content = {}
@@ -26,38 +33,38 @@ local size, size1, size2 = ffi.sizeof(buf), ffi.sizeof(buf1), ffi.sizeof(buf2)
 local connection = { }
 
 local function zmq_error()
-   local errno = zmq.zmq_errno()
-   local strerror = ffi.string( zmq.zmq_strerror( errno ) )
-   error( "ZMQ ERROR: " .. errno .. ": " .. strerror )
+  local errno = zmq.zmq_errno()
+  local strerror = ffi.string( zmq.zmq_strerror( errno ) )
+  error( "ZMQ ERROR: " .. errno .. ": " .. strerror )
 end
 
 
 function connection.send( self, buf, len )
-   local r = send( self.handle, buf, len, 0 )
-   if r ~= len then zmq_error() end
-   return r
+  local r = send( self.handle, buf, len, 0 )
+  if r ~= len then zmq_error() end
+  return r
 end
 
 function connection.recv( self, buf, len )
-   local r = recv( self.handle, buf, len, 0 )
-   if r ~= len then zmq_error() end
-   return r
+  local r = recv( self.handle, buf, len, 0 )
+  if r ~= len then zmq_error() end
+  return r
 end
 
 function new_connection( handle )
-   local c = {}
-   c.send = connection.send
-   c.recv = connection.recv
-   c.handle = handle
-   return c
+  local c = {}
+  c.send = connection.send
+  c.recv = connection.recv
+  c.handle = handle
+  return c
 end
 
 local function bounce( sb )
-   local r2, r3
-   r2 = sb:recv( buf1, size1 )
-   r3 = sb:send( buf1, size1 )
-   assert( r2 == size1 )
-   assert( r3 == size1 )
+  local r2, r3
+  r2 = sb:recv( buf1, size1 )
+  r3 = sb:send( buf1, size1 )
+  assert( r2 == size1 )
+  assert( r3 == size1 )
 end
 
 -- Init the context
@@ -76,9 +83,9 @@ assert (rc == 0);
 local sb = new_connection( sb )
 
 for i=1,5 do
-	print('Sending msg...')
-	bounce( sb )
-  sleep(1)
+  print('Sending msg...')
+  bounce( sb )
+  msleep(1000)
 end
 
 -- Debug

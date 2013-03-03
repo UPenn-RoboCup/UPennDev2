b731f10aef3e71388bea039943357b14dcbd72f1
Mike Hopkins <michael-hopkins@outlook.com>
2013-03-03 17:34:24 -0500

    remove dictionary call from test_rpc_client

--- Run/Test/test_rpc_client.lua
+++ Run/Test/test_rpc_client.lua
@@ -10,10 +10,6 @@ client = rpc.client.new('tcp://localhost:5555', context)
 client:set_timeout(0.5)
 assert(client:connect(1))
 
-for k in pairs(client:get_dictionary()) do 
-  print(k)
-end
-
 status = client:call('io.stderr:write', 'HELLO REMOTE WORLD')
 status, result = client:call('my_remote_function', 0)
 

1ec7f1306a15ee0ea19a6f37f07457fb7350ff88
Stephen McGill <smcgill3@seas.upenn.edu>
2013-03-05 13:59:56 -0500

    More documentation

--- Run/Test/test_simple_subscribe.lua
+++ Run/Test/test_simple_subscribe.lua
@@ -1,7 +1,7 @@
 dofile('../include.lua')
 local ffi = require 'ffi'
 local simple_ipc = require 'simple_ipc'
-inter_pc = false
+inter_pc = true
 if inter_pc then
   test_channel = simple_ipc.setup_subscriber(5555);
 else
@@ -9,6 +9,6 @@ else
 end
 local buffer_ct = ffi.new('double[1]',0);
 while true do
-	test_channel:receive( buffer_ct, 1 )
-	print(buffer_ct[0])
+	test_channel:receive( buffer_ct, ffi.sizeof('double') )
+	print('Received',buffer_ct[0])
 end

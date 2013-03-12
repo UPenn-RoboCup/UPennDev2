221eee7424a2ac39dc465e6f81a9b1782fb63da3
Yida Zhang <hitzhangyd@gmail.com>
2013-03-12 08:26:41 -0400

    Finalize encoder parser - add torch - add torch support for matfile parser

--- Tools/magic/convertEncoder.lua
+++ Tools/magic/convertEncoder.lua
@@ -1,13 +1,30 @@
 require 'include'
+require 'common'
+
+local serialization = require 'serialization'
 
 local mat = require 'mat'
 
 filePath = '../../../bus/proj4/'
-fileName = filePath..'Encoders20.mat'
 
-content = mat.load(fileName, true)
---content = mat.load('aa.mat', true)
+local EncoderFileList = assert(io.popen('/bin/ls '..filePath..'Encoder2*'))
+local EncoderFileNum = 0
+for line in EncoderFileList:lines() do
+  EncoderFileNum = EncoderFileNum + 1
+end
 
-for k, v in pairs(content[1]) do
-  print(k, v)
+for i = 1, 4 do
+  local fileName = filePath..'Encoders2'..i..'.mat'
+  local content = mat.load(fileName, false)
+  local counts = content[1].Encoders.counts['']
+  local ts = content[1].Encoders.ts['']
+  local Encorder = {}
+  for i = 1, counts:size(2) do
+    local encorder = {['num1'] = counts[1][i], ['num2'] = counts[2][i],
+                      ['num3'] = counts[3][i], ['num4'] = counts[4][i],
+                      ['timestamp'] = ts[1][i]}
+    Encorder[#Encorder + 1] = encorder
+  end
+  
+  saveData(Encorder, 'Encoder2'..i, filePath)
 end

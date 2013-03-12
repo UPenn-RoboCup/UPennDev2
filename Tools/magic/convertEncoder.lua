d124d9c9cc13192ab13af198be42604b89b8efa6
Yida Zhang <hitzhangyd@gmail.com>
2013-03-12 07:36:01 -0400

    Add struct cell support for matfile reader

--- Tools/magic/convertEncoder.lua
+++ Tools/magic/convertEncoder.lua
@@ -6,6 +6,7 @@ filePath = '../../../bus/proj4/'
 fileName = filePath..'Encoders20.mat'
 
 content = mat.load(fileName, true)
+--content = mat.load('aa.mat', true)
 
 for k, v in pairs(content[1]) do
   print(k, v)

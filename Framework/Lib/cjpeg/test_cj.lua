2ebb683a0999a54c03d05185f2a81016df7b4afa
Stephen McGill <smcgill3@seas.upenn.edu>
2013-03-13 00:22:47 -0400

    move cjpeg to right folder.  Add grayscale

--- Framework/Lib/cjpeg/test_cj.lua
+++ Framework/Lib/cjpeg/test_cj.lua
@@ -22,7 +22,7 @@ for k=1,nbytes,ch do
   img[k+1] = 0;
   img[k+2] = 255;
   -- Green halfway through
-  if k>3*w*h/2 then
+  if k>nbytes/2 then
     img[k] = 255;
     img[k+1] = 255;
     img[k+2] = 0;
@@ -33,5 +33,34 @@ img_jpeg = cjpeg.compress( carray.pointer(img), w, h )
 print(type(img_jpeg),'Compression Ratio:', #img_jpeg, #img_jpeg/nbytes )
 
 f = io.open('img.jpeg','w')
+n = f:write( img_jpeg )
+f:close()
+
+
+-- gray
+ch = 1;
+nbytes = w*h*ch;
+print('Filling a '..w..' by '..h..' image',ch..' channels.')
+
+if using_luajit then
+  ffi = require 'ffi'
+  img = ffi.new('uint8_t[?]', w*h*ch, 0)
+else
+  require 'carray'
+  img = carray.new('c',w*h*ch)
+end
+
+for k=1,nbytes,ch do
+  if k>nbytes/2 then
+    img[k] = 255;
+  else
+    img[k] = 0;
+  end
+end
+
+img_jpeg = cjpeg.compress( carray.pointer(img), w, h, 1 )--gray
+print(type(img_jpeg),'Compression Ratio:', #img_jpeg, #img_jpeg/nbytes )
+
+f = io.open('img_gray.jpeg','w')
 n = f:write( img_jpeg )
 f:close()

44d008c97cf725dba26f83f1c279b8846fe47152
newtonj <newtonj@vt.edu>
2013-03-13 17:57:44 -0400

    moves all motor updates into loops, adds additional type mappings to settings file

--- Platform/robotis_arm/Comms/dynamixel_settings.h
+++ Platform/robotis_arm/Comms/dynamixel_settings.h
@@ -11,17 +11,13 @@ enum DXL_TYPE {
 
 //mapping of dynamixel motor ids to DCM values
 const int dynamixel_id[N_JOINT] = {
-  4, 6, 8, 10, 12, 14,
-  3, 5, 7, 9, 11, 13,
-  16, 18, 20,
-  15, 17, 19
+  1, 2, 3, 4, 5, 6,
+  7, 8, 9
 };
 
 //types corresponding to each DCM mapping
 const DXL_TYPE dynamixel_series[N_JOINT] = {
   NX_DXL, NX_DXL, NX_DXL, NX_DXL, NX_DXL, NX_DXL,
-  NX_DXL, NX_DXL, NX_DXL, NX_DXL, NX_DXL, NX_DXL,
-  MX_DXL, MX_DXL, MX_DXL, 
   MX_DXL, MX_DXL, MX_DXL
 };
 

9bade686e9943eaead52a998e1c74b29da9fbd8b
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-03-16 19:17:04 -0400

    Reducing the amount of stuff.

--- Player/Interaction/run_ws.js
+++ Player/Interaction/run_ws.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var app = express();
-var http = require('http')
-app.use(express.static(__dirname + '/public'));
-
-var server = http.createServer(app);
-server.listen(8080);
+// Check for channels to listen on
+if( process.argv.length <= 2 ) {
+  console.log('No channels listening!');
+  return;
+}
+var channels = [];
+// Publish someting
+process.argv.forEach(function (val, index, array) {
+  if( index>1 ) {
+//    console.log(index + ': ' + val);
+    console.log('Listening to ' + val);
+    channels.push( val );
+  }
+});
 
 // Require the appropriate modules
 var mp = require('msgpack');
@@ -17,34 +24,31 @@ var counter = 0;
 
 // Send data to clients at a set interval
 // For now, this is 15fps
-var fps = 15;
+var fps = 5;
 setInterval(  function(){
   counter++;
 }, 1000/fps);
 
 // Listen to IPC sensor messages
 var zmq_img = zmq.socket('sub');
-zmq_img.connect('ipc:///tmp/img');
+zmq_img.connect('ipc:///tmp/'+channels[0]);
 zmq_img.subscribe('');
-console.log('IPC | Connected to img');
+console.log('ZeroMQ IPC | Connected to '+channels[0]);
 // Process lidar
-var last_img_cntr = counter;
+var last_cntr = counter;
 zmq_img.on('message', function(msg){
-  console.log('IPC | Got img message!')
-  console.log( msg.readFloatLE(0) )
-  var more = zmq_img.getsockopt( zmq.ZMQ_RCVMORE );
-  console.log( more );
+  // console.log('ZeroMQ IPC | Got'+channels[0]+' message!')
   if( counter>last_img_cntr ) {
     for(var s=0;s<wskts.length;s++) {
       wskts[s].send(msg,{binary:true},function(){
       });
     }
-    last_img_cntr = counter;
+    last_cntr = counter;
   }
 });
 
 // Set up a Websocket server on 9001
-var wss = new WebSocketServer({server: server});
+var wss = new WebSocketServer({port: 9000});
 // Listen to binary websockets
 wss.on('connection', function(ws) {
   console.log('A client is Connnected!');
@@ -55,5 +59,4 @@ wss.on('connection', function(ws) {
   wskts.push(ws)
 //  ws.send('something');
 });
-
 

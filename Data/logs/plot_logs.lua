a0aa946212db3326eb1f96613f66ccb4502b37cb
Mike Hopkins <michael-hopkins@outlook.com>
2013-03-15 23:10:09 -0400

    add plot_logs.lua for plotting log files in /Data

---
+++ Data/logs/plot_logs.lua
@@ -0,0 +1,18 @@
+package.path = '../../Framework/Util/?.lua;'..package.path
+
+require('gnuplot')
+
+if (not arg[1]) then
+  print('usage : lua plot_logs.lua [file1] [file2] ...')
+end
+
+for i = 1, #arg do
+  local data = {}
+  for line in io.lines(arg[i]) do
+    data[#data + 1] = tonumber(line)
+  end
+  gnuplot.figure()
+  gnuplot.plot(data, '~')
+  gnuplot.title(arg[i])
+  gnuplot.grid(true)
+end

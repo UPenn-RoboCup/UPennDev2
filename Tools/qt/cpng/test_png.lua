f3db840b7791e0a58cb94e7dae5ede1b4171213f
Yida Zhang <hitzhangyd@gmail.com>
2013-03-17 18:25:31 -0400

    add more method for png userdata

--- Tools/qt/cpng/test_png.lua
+++ Tools/qt/cpng/test_png.lua
@@ -9,7 +9,17 @@ t0 = unix.time()
 local p = cpng.new('small.png')
 print(unix.time() - t0)
 
+print('width  '..p:width())
+print('height  '..p:height())
+print('stride  '..p:stride())
+print('color type  '..p:color_type())
+print('bit depth  '..p:bit_depth())
+print('data lenght  '..p:__len())
+
+print(p[1920]) -- start from 1
+print(p[1]) -- start from 1
+
 t0 = unix.time()
 p:write('iii.png')
-print(unix.time() - t0)
-print(p)
+--print(unix.time() - t0)
+--print(p)

27ce4d3f031dc1459d0b34e89c0f47c536a90a7b
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-03-23 01:52:36 -0400

    Fixing kinect bit shift issue

--- Run/Test/Cognition/test_kinect.lua
+++ Run/Test/Cognition/test_kinect.lua
@@ -4,29 +4,35 @@ local simple_ipc = require 'simple_ipc'
 local kinect_channel = simple_ipc.setup_publisher('kinect');
 
 require 'Kinect'
-print( "Opening the Kinect..." )
---k = Kinect.open()
---k = Kinect.open('/Users/stephen/Desktop/test_log_kinect2','record')
-k = Kinect.open('/Users/stephen/Desktop/test_log_kinect2')
+local k = Kinect.open()
+--local k = Kinect.open('/Users/stephen/Desktop/test_log_kinect2','record')
+--local k = Kinect.open('/Users/stephen/Desktop/test_log_kinect2')
 print( "Kinect Open!", k )
-
+if not k then
+  return
+end
+-- zoom of 2 to 5 are good.  else not supported
+print("Current zoom:",Kinect.zoom(0))
+print('Setting zoom to',Kinect.zoom(4))
 --for n=1,10 do
+counter = 0;
 while true do
+  counter = counter+1;
   local s = Kinect.update()
   --print( "Kinect Stream: ", s )
   local d = Kinect.retrieve( s )
+  --local res = Kinect.zoom( (math.floor(counter/60)%6) +0 );
   --print( "Kinect Data: ", d )
   local z = nil;
   if s==0 then -- depth
     z = cjpeg.compress( d, 320, 240, 1 )
-    --print('Depth:', #z, #z/(320*240))
+--    print('JDepth:', #z, #z/(320*240))
     kinect_channel:send( {'d', z} );
+--    kinect_channel:send( 'a'..z ); -- MATLAB
   elseif s==1 then --color
     z = cjpeg.compress( d, 320, 240 )
-    --print('Color: ', #z, #z/(320*240*3))
+    --print('JColor: ', #z, #z/(320*240*3))
     kinect_channel:send( {'c', z} );
   end
 end
 Kinect.shutdown()
-
--- TODO: how to shutdown the zeromq ipc?

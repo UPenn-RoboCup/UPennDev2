8d24e4c2f7b1e126127380da43ab6aa679b53c18
Michael Hopkins <michael-hopkins@outlook.com>
2013-03-23 19:34:32 -0400

    tune gazebo walk parameters, increase pid gains

--- Data/gazebo_ash_lowerbody/parameters_walk_osc.lua
+++ Data/gazebo_ash_lowerbody/parameters_walk_osc.lua
@@ -1,15 +1,15 @@
 return {
-  ["z_swing_amplitude"] = 0.002,
-  ["zmp_shift_ratio"] = 0.5,
-  ["ankle_roll_fb_slope"] = 0,
+  ["y_swing_amplitude"] = 0.043,
   ["ankle_pitch_fb_slope"] = 0,
-  ["knee_pitch_fb"] = -0,
-  ["ankle_pitch_fb"] = 0.08,
-  ["y_offset_ratio"] = 0.3,
-  ["y_swing_amplitude"] = 0.048,
-  ["hip_roll_fb"] = 0,
+  ["ankle_pitch_fb"] = 0.1,
+  ["zmp_shift_ratio"] = 0.4,
+  ["z_swing_amplitude"] = 0.002,
+  ["step_amplitude"] = 0.034,
+  ["ankle_roll_fb"] = 0.1,
   ["dsp_ratio"] = 0.25,
-  ["ankle_roll_fb"] = 0.08,
-  ["period_time"] = 1.36,
-  ["step_amplitude"] = 0.034
+  ["y_offset_ratio"] = 0.3,
+  ["hip_roll_fb"] = 0,
+  ["ankle_roll_fb_slope"] = 0,
+  ["knee_pitch_fb"] = 0,
+  ["period_time"] = 1.36
 }

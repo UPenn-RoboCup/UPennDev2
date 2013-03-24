be7b11c80feef673f1fd6bd5fa393e6fd9429ee3
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-03-24 15:13:47 -0400

    fix a memory leak

--- Player/Interaction/listen_kinect.js
+++ Player/Interaction/listen_kinect.js
@@ -8,16 +8,18 @@ var c_ctx;
 var dCanvas;
 var d_ctx;
 // Image elements
-var myjpeg;
-var mypng;
-var jpeg_url_name;
 var c_imgdata;
 var d_imgdata;
 var c_img = new Image;
+c_img.alt="color";
 var d_img = new Image;
+d_img.alt="depth";
+var URL = window.URL || window.webkitURL;
 // Update the cloud colors
 c_img.onload = function(){
   c_ctx.drawImage( c_img, 0, 0 );
+  URL.revokeObjectURL( this.src );
+  this.src = '';
   c_imgdata = c_ctx.getImageData(0, 0, 320, 240);
   if(use_cloud == 1){
     update_cloud_image( new Uint8Array(c_imgdata.data.buffer) );
@@ -25,56 +27,28 @@ c_img.onload = function(){
   if(use_mesh == 1){
     update_mesh_image( new Uint8Array(c_imgdata.data.buffer) );
   }
-  // Revoke the Object URL
-  if(window.webkitURL!==undefined) {
-    window.webkitURL.revokeObjectURL( this.src );
-  } else {
-    window.URL.revokeObjectURL( this.src );
-  }
 }; //c onload
 // Update the cloud depths    
 d_img.onload = function(e){
   d_ctx.drawImage( d_img, 0, 0 );
+  URL.revokeObjectURL( this.src );
+  this.src = '';
   d_imgdata = d_ctx.getImageData(0, 0, 320, 240);
   if(use_cloud == 1){
     update_cloud_depth( new Uint8ClampedArray(d_imgdata.data.buffer) );
-  }
-  if(use_mesh == 1){
+  } else if(use_mesh == 1){
     update_mesh_depth( new Uint8ClampedArray(d_imgdata.data.buffer) );
-  }
-  // Revoke the Object URL
-  if(window.webkitURL!==undefined) {
-    window.webkitURL.revokeObjectURL( this.src );
-  } else {
-    window.URL.revokeObjectURL( this.src );
   }
 }; //d onload
 
 // Process Websocket messages
 var process_kmsg = function (event) {
   mymeta = new Uint8Array(event.data, event.data.byteLength-2, 2 );
-  //var mydata = new Uint8Array(event.data, 2, event.data.byteLength-2 );
   // Act on color vs. depth data
   if( mymeta[0] == 13 ){
-    myjpeg = new Blob( [event.data], { type: "image\/jpeg" } );
-    //myjpeg = new Blob( [mydata], { type: "image\/jpeg" } );
-    c_img.alt="color"
-    // Load the image
-    if(window.webkitURL!==undefined) {
-      c_img.src = window.webkitURL.createObjectURL( myjpeg );
-    } else {
-      c_img.src = window.URL.createObjectURL( myjpeg );
-    }
+    c_img.src = URL.createObjectURL( new Blob( [event.data], { type: "image\/jpeg" } ) );
   } else {
-    mypng = new Blob( [event.data], { type: "image\/jpeg" } );
-    //mypng = new Blob( [mydata], { type: "image\/jpeg" } );
-    d_img.alt="depth"
-    // Load the image
-    if(window.webkitURL!==undefined) {
-      d_img.src = window.webkitURL.createObjectURL( mypng );
-    } else {
-      d_img.src = window.URL.createObjectURL( mypng );
-    }
+    d_img.src = URL.createObjectURL( new Blob( [event.data], { type: "image\/jpeg" } ) );
   }
 };
 
@@ -96,6 +70,5 @@ var init_kinect = function(){
   ws_k = new WebSocket('ws://' + host + ':9002');
   ws_k.binaryType = "arraybuffer"; //"blob"
   ws_k.onmessage = process_kmsg;
-  
 }
 document.addEventListener( "DOMContentLoaded", init_kinect );

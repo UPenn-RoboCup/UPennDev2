dcf0a24ad3eefe26ff037ab52aa9e62d47a2ad55
Yida Zhang <hitzhangyd@gmail.com>
2013-03-24 01:55:40 -0400

    Add MessagePack module and more test on simple_ipc

--- Tools/qt/testipc.lua
+++ Tools/qt/testipc.lua
@@ -6,8 +6,31 @@ package.path = pwd..'/../../Player/Util/?.lua;'..package.path
 local simple_ipc = require 'simple_ipc'
 local mp = require 'MessagePack'
 local imu_channel = simple_ipc.setup_subscriber('imu');
+local util = require 'util'
+local unix = require 'unix'
 
-while (1) do
+imu_channel.callback = function()
   local imu_data, has_more = imu_channel:receive();
-  print(imu_data)
+--  print(mp.unpack(imu_data)..' '..unix.time())
+  t = mp.unpack(imu_data)
+  util.ptable(t)
+--  print(unix.time())
+  print(has_more)
+  while (has_more) do
+    imu_data, has_more = imu_channel:receive();
+--    print('1 '..unix.time())
+  end
+--  print(mp.unpack(imu_data)..' '..unix.time())
+end
+local wait_channels = { imu_channel }
+local channel_poll = simple_ipc.wait_on_channels( wait_channels );
+
+local channel_timeout = 1e3;
+--channel_timeout = 0;
+while (1) do
+  channel_poll:poll(channel_timeout)
+--  local imu_data, has_more = imu_channel:receive();
+----  print(imu_data..' '..unix.time())
+--  print(mp.unpack(imu_data)..' '..unix.time())
+--  print(unix.time())
 end

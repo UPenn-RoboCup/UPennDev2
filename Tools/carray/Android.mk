6a9bdd4e6405ae2f541263631ec5157bf4437bb2
Yida Zhang <hitzhangyd@gmail.com>
2013-03-24 00:05:21 -0400

    remove Tools/carray after merge

--- Tools/carray/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-# carray module to manipulate C arrays in lua
-include $(CLEAR_VARS)
-LOCAL_MODULE := carray
-LOCAL_SRC_FILES := luacarray.cpp
-LOCAL_SHARED_LIBRARIES := lua-activity
-include $(BUILD_SHARED_LIBRARY)

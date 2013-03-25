51118a46cd542e1df04c378783dfc3a059bce11b
Yida Zhang <yida@seas.upenn.edu>
2013-03-25 19:12:59 -0400

    Fix matlab char class pack and unpack

--- Tools/msgpack/testmp.m
+++ Tools/msgpack/testmp.m
@@ -1,14 +1,17 @@
 clear all;
-filename = 'msg';
+%filename = 'msg';
+%
+%fid = fopen(filename);
+%data = fread( fid , '*uint8');
+%
+%fclose(fid);
+%
+%msgpack('unpack', data)
 
-fid = fopen(filename);
-data = fread( fid , '*uint8');
-
-fclose(fid);
-%tline = fileread(filename);
-% for i = 1: numel(data)
-% %  fprintf('%u ', tline(i));
-%   fprintf('%u ', data(i));
-% end
-
-msgpack('unpack', data)
+%msg = msgpack('pack', [1,432,43.4]);
+msg = msgpack('pack', 'fafafaeew23f');
+msgpack('unpack', msg)
+%filename = 'msg1';
+%fid = fopen(filename, 'w');
+%data = fwrite(fid, msg, '*uint8');
+%fclose(fid);

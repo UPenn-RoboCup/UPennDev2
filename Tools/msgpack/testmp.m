efcbd0a611e28490582394a7eeb3383da484fb45
Yida Zhang <yida@seas.upenn.edu>
2013-03-25 11:20:33 -0400

    add matlab pack example

--- Tools/msgpack/testmp.m
+++ Tools/msgpack/testmp.m
@@ -2,14 +2,13 @@ clear all;
 filename = 'msg';
 
 fid = fopen(filename);
-data = fread( fid , '*int8');
-whos
+data = fread( fid , '*uint8');
 
-fclose(fid)
+fclose(fid);
 %tline = fileread(filename);
-%for i = 1: numel(tline)
-%  fprintf('%u ', tline(i));
-%  fprintf('%u ', data(i));
-%end
+% for i = 1: numel(data)
+% %  fprintf('%u ', tline(i));
+%   fprintf('%u ', data(i));
+% end
 
 msgpack('unpack', data);

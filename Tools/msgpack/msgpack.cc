0c9aae54b17dd09f285a3955ada5bc2a0a1627b5
Yida Zhang <yida@seas.upenn.edu>
2013-03-25 11:20:33 -0400

    add matlab pack example

--- Tools/msgpack/msgpack.cc
+++ Tools/msgpack/msgpack.cc
@@ -15,40 +15,39 @@ void mex_unpack(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   std::cout << "unpack "<< std::endl;
   const char *str = (const char*)mxGetPr(prhs[0]);
   size_t size = mxGetM(prhs[0]) * mxGetN(prhs[0]);
-//  for (int i = 0; i < size - 1;  i++)
-//    std::cout << (uint8_t)str[i] << ' ';
-//
-//  /* creates buffer and serializer instance. */
-//  msgpack_sbuffer* buffer = msgpack_sbuffer_new();
-//  msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
-// 
-//  /* serializes ["Hello", "MessagePack"]. */
-//  msgpack_pack_array(pk, 2);
-//  msgpack_pack_raw(pk, 5);
-//  msgpack_pack_raw_body(pk, "Hello", 5);
-//  msgpack_pack_raw(pk, 11);
-//  msgpack_pack_raw_body(pk, "MessagePack", 11);
-// 
-//  /* deserializes it. */
+
+  /* deserializes it. */
   msgpack_unpacked msg;
   msgpack_unpacked_init(&msg);
-//  bool success = msgpack_unpack_next(&msg, buffer->data, buffer->size, NULL);
   bool success = msgpack_unpack_next(&msg, str, size, NULL);
 // 
-//  /* prints the deserialized object. */
+  /* prints the deserialized object. */
   msgpack_object obj = msg.data;
   msgpack_object_print(stdout, obj);  /*=> ["Hello", "MessagePack"] */
   printf("%d", obj.type);
-// 
-//  /* cleaning */
-//  msgpack_sbuffer_free(buffer);
-//  msgpack_packer_free(pk);
-
 }
 
 void mex_pack(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) 
 {
   std::cout << "pack "<< std::endl;
+  /* creates buffer and serializer instance. */
+  msgpack_sbuffer* buffer = msgpack_sbuffer_new();
+  msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
+ 
+  /* serializes ["Hello", "MessagePack"]. */
+  msgpack_pack_array(pk, 2);
+  msgpack_pack_raw(pk, 5);
+  msgpack_pack_raw_body(pk, "Hello", 5);
+  msgpack_pack_raw(pk, 11);
+  msgpack_pack_raw_body(pk, "MessagePack", 11);
+
+  plhs[0] = mxCreateNumericMatrix(1, buffer->size, mxUINT8_CLASS, mxREAL);
+  memcpy(mxGetPr(plhs[0]), buffer->data, buffer->size * sizeof(uint8_t));
+
+  /* cleaning */
+  msgpack_sbuffer_free(buffer);
+  msgpack_packer_free(pk);
+
 }
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])

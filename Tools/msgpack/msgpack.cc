9b38c82a6b9b9ba3deb35f118c0fc2d586d74b4e
Yida Zhang <yida@seas.upenn.edu>
2013-03-25 10:51:18 -0400

    working initial msgpack for matlab

--- Tools/msgpack/msgpack.cc
+++ Tools/msgpack/msgpack.cc
@@ -3,7 +3,7 @@
 #include <map>
 #include <vector>
 #include <stdio.h>
-
+#include <unistd.h>
 #include <msgpack.h>
 
 #include "mex.h"
@@ -13,16 +13,36 @@ using namespace std;
 void mex_unpack(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) 
 {
   std::cout << "unpack "<< std::endl;
-  const char *str = mxArrayToString(prhs[0]);
-  size_t size = mxGetScalar(prhs[1]);
-  std::cout << str << std::endl;
-  std::cout << size << std::endl;
+  const char *str = (const char*)mxGetPr(prhs[0]);
+  size_t size = mxGetM(prhs[0]) * mxGetN(prhs[0]);
+//  for (int i = 0; i < size - 1;  i++)
+//    std::cout << (uint8_t)str[i] << ' ';
+//
+//  /* creates buffer and serializer instance. */
+//  msgpack_sbuffer* buffer = msgpack_sbuffer_new();
+//  msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
+// 
+//  /* serializes ["Hello", "MessagePack"]. */
+//  msgpack_pack_array(pk, 2);
+//  msgpack_pack_raw(pk, 5);
+//  msgpack_pack_raw_body(pk, "Hello", 5);
+//  msgpack_pack_raw(pk, 11);
+//  msgpack_pack_raw_body(pk, "MessagePack", 11);
+// 
+//  /* deserializes it. */
   msgpack_unpacked msg;
   msgpack_unpacked_init(&msg);
+//  bool success = msgpack_unpack_next(&msg, buffer->data, buffer->size, NULL);
   bool success = msgpack_unpack_next(&msg, str, size, NULL);
-
+// 
+//  /* prints the deserialized object. */
   msgpack_object obj = msg.data;
-  printf("%d\n", obj.type);
+  msgpack_object_print(stdout, obj);  /*=> ["Hello", "MessagePack"] */
+  printf("%d", obj.type);
+// 
+//  /* cleaning */
+//  msgpack_sbuffer_free(buffer);
+//  msgpack_packer_free(pk);
 
 }
 
--- Tools/msgpack/testmp.m
+++ Tools/msgpack/testmp.m
@@ -1,5 +1,15 @@
-
+clear all;
 filename = 'msg';
-tline = fileread(filename);
 
-msgpack('unpack', tline, size(tline, 2));
+fid = fopen(filename);
+data = fread( fid , '*int8');
+whos
+
+fclose(fid)
+%tline = fileread(filename);
+%for i = 1: numel(tline)
+%  fprintf('%u ', tline(i));
+%  fprintf('%u ', data(i));
+%end
+
+msgpack('unpack', data);

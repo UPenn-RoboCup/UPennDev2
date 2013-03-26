b7d8a13e11ef51c33501250938db286dd3b01008
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-03-26 11:40:58 -0400

    Finishing the Polling

--- Tools/zeromq/testzmq.m
+++ Tools/zeromq/testzmq.m
@@ -4,9 +4,10 @@ zmq('send',s1,'hello world!')
 s2 = zmq('publish','debug')
 zmq('send',s2,'hey-o')
 s3 = zmq('subscribe','test')
+s4 = zmq('subscribe','test2')
 %r = zmq('receive',s3)
-r = [];
-while numel(r)<1
-  r = zmq('poll',100);
+data = [];
+while numel(data)<1
+  [data,idx] = zmq('poll',100);
 end
-char(r')
+char( data{1}' )

d2120ebbf4b9004e82593480cb51386ed3afcecd
Mike Hopkins <michael-hopkins@outlook.com>
2013-03-26 23:22:50 -0400

    increase default gazebo pid gains

--- Config/gazebo_ash/Config_platform.lua
+++ Config/gazebo_ash/Config_platform.lua
@@ -6,9 +6,9 @@ comms_manager = {
   -- # joints controlled by comms_manager
   joint_max = 29,
   -- impedance controller settings
-  p_gain_constant = 1000,
-  i_gain_constant = 1000,
-  d_gain_constant = 1000,
+  p_gain_constant = 1600,
+  i_gain_constant = 1600,
+  d_gain_constant = 1600,
   d_break_freq = 30,
   -- initial controller gains
   p_gain_default = 0.5,

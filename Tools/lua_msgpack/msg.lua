092a332b43d22d8d635af39c190f6c4c140ed755
Yida Zhang <hitzhangyd@gmail.com>
2013-03-29 19:44:55 -0400

    first lua msgpack wrapper from c msgpack

---
+++ Tools/lua_msgpack/msg.lua
@@ -0,0 +1,17 @@
+package.cpath = '../../Player/Lib/?.so;'..package.cpath
+require 'msgpack'
+require 'carray'
+
+msgpack.pack(432.543)
+msgpack.pack(true)
+msgpack.pack('hellp world')
+
+local t = {}
+t.ast = 'fsfa'
+t.brg = 32423
+t[1] = 3445
+msgpack.pack(t)
+
+local udata = carray.byte('this is a test')
+msgpack.pack(udata)
+msgpack.pack(udata:pointer())

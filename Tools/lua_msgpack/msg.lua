ec0f6d32119f4fbfff7d656282309c48ee7cc28d
Yida Zhang <hitzhangyd@gmail.com>
2013-03-29 21:17:23 -0400

    Add unpack functions and use MessagePack to generate exmaples"

--- Tools/lua_msgpack/msg.lua
+++ Tools/lua_msgpack/msg.lua
@@ -1,23 +1,50 @@
 package.cpath = '../../Player/Lib/?.so;'..package.cpath
+package.path = '../../Player/Util/?.lua;'..package.path
+
 require 'msgpack'
 require 'carray'
+local mp = require 'MessagePack'
 
-msgpack.pack(432.543)
-msgpack.pack(true)
-msgpack.pack('hellp world')
+str = msgpack.pack(a)
+strmp = mp.pack(a)
+msgpack.unpack(strmp)
+print(#str)
+
+str = msgpack.pack(432.543)
+strmp = mp.pack(432.543)
+msgpack.unpack(strmp)
+print(#str)
+str = msgpack.pack(true)
+strmp = mp.pack(true)
+msgpack.unpack(strmp)
+print(#str)
+str = msgpack.pack('hellp world')
+strmp = mp.pack('hellp world')
+msgpack.unpack(strmp)
+print(#str)
 
 local t = {}
 t.ast = 'fsfa'
 t.brg = 32423
 t[1] = 3445
-msgpack.pack(t)
+str = msgpack.pack(t)
+strmp = mp.pack(t)
+msgpack.unpack(strmp)
+print(#str)
 
-local udata = carray.byte('this is a test')
-msgpack.pack(udata)
-msgpack.pack(udata:pointer())
+--local udata = carray.byte('this is a test')
+--str = msgpack.pack(udata)
+--strmp = mp.pack(udata)
+--msgpack.unpack(strmp)
+--print(#str)
+--str = msgpack.pack(udata:pointer())
+--strmp = mp.pack(udata:pointer())
+--msgpack.unpack(strmp)
+--print(#str)
 
 print('all together\n')
 
-msgpack.pack(432.543,true,'hellp world',t,udata, udata:pointer())
+--str = msgpack.pack(432.543,true,'hellp world',t,udata, udata:pointer())
+--print(#str)
 
 

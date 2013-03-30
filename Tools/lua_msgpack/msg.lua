a8f666d3c51eb73eaf55b9672668d4fba4135c2d
Yida Zhang <hitzhangyd@gmail.com>
2013-03-29 23:59:01 -0400

    find way to read table key and val in c

--- Tools/lua_msgpack/msg.lua
+++ Tools/lua_msgpack/msg.lua
@@ -31,48 +31,50 @@ print(#str)
 print('test array')
 str = msgpack.pack({4,5,6,7})
 strmp = mp.pack({4,5,6,7})
-util.ptable(msgpack.unpack(strmp))
---msgpack.unpack(strmp)
-
+--util.ptable(msgpack.unpack(strmp))
+----msgpack.unpack(strmp)
+--
+print('test array')
 local t = {}
 t.ast = 'fsfa'
 t.brg = 32423
 t[1] = 3445
 t[3] = 34
 str = msgpack.pack(t)
-strmp = mp.pack(t)
-util.ptable(msgpack.unpack(strmp))
-print(#str)
 
-str = msgpack.pack({4,5,6,t})
-strmp = mp.pack({4,5,6,t})
-util.ptable(msgpack.unpack(strmp))
-
---local udata = carray.byte('this is a test')
---str = msgpack.pack(udata)
---strmp = mp.pack(udata)
---msgpack.unpack(strmp)
+--strmp = mp.pack(t)
+--util.ptable(msgpack.unpack(strmp))
 --print(#str)
---str = msgpack.pack(udata:pointer())
---strmp = mp.pack(udata:pointer())
---msgpack.unpack(strmp)
+--
+--str = msgpack.pack({4,5,6,t})
+--strmp = mp.pack({4,5,6,t})
+--util.ptable(msgpack.unpack(strmp))
+--
+----local udata = carray.byte('this is a test')
+----str = msgpack.pack(udata)
+----strmp = mp.pack(udata)
+----msgpack.unpack(strmp)
+----print(#str)
+----str = msgpack.pack(udata:pointer())
+----strmp = mp.pack(udata:pointer())
+----msgpack.unpack(strmp)
+----print(#str)
+--
+--print('all together\n')
+--
+----str = msgpack.pack(432.543,true,'hellp world',t,udata, udata:pointer())
+----print(#str)
+--
+--filename = '../../../bus/data/stateMP-03.29.2013.13.58.27-0'
+--file = io.open(filename, 'r+')
+--str = file:read('*a')
 --print(#str)
-
-print('all together\n')
-
---str = msgpack.pack(432.543,true,'hellp world',t,udata, udata:pointer())
---print(#str)
-
-filename = '../../../bus/data/stateMP-03.29.2013.13.58.27-0'
-file = io.open(filename, 'r+')
-str = file:read('*a')
-print(#str)
-up = msgpack.unpacker(str)
-ret = up:unpack()
-local c = 0
-while ret do
---  util.ptable(ret)
-  c = c + 1
-  ret = up:unpack()
-end
-print(c)
+--up = msgpack.unpacker(str)
+--ret = up:unpack()
+--local c = 0
+--while ret do
+----  util.ptable(ret)
+--  c = c + 1
+--  ret = up:unpack()
+--end
+--print(c)

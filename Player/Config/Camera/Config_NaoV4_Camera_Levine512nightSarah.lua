8e0b7e782bfe593e6e7d8057d0f45174cdf2c302
Yida Zhang <yida@seas.upenn.edu>
2013-03-31 02:24:04 +0000

    making a colortable in levine512 at night

--- Player/Config/Camera/Config_NaoV4_Camera_Levine512nightSarah.lua
+++ Player/Config/Camera/Config_NaoV4_Camera_Levine512nightSarah.lua
@@ -41,5 +41,5 @@ camera.param[8] = {key='Vertical Flip'  , val={1  , 0}};
 -- brightness has to be set seperately from other parameters, and it can only be set to multiple of 4
 camera.brightness = 200;
 
-camera.lut_file = 'lut_TatendaLevine512night.raw';
+camera.lut_file = 'lut_L512night.mat.raw';
 

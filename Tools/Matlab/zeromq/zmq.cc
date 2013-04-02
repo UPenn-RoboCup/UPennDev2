911e8116f58ce2ffe14825ed5f2a384face745e8
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-01 23:08:44 -0400

    mallocing a HUGE area of buffer for receiving data.

--- Tools/Matlab/zeromq/zmq.cc
+++ Tools/Matlab/zeromq/zmq.cc
@@ -5,7 +5,8 @@
 #include <stdint.h>
 #include <string.h>
 #include "mex.h"
-#define BUFLEN 8192
+#define BUFLEN1 8192
+#define BUFLEN2 164000
 #define MAX_SOCKETS 10
 
 char* command;
@@ -17,9 +18,11 @@ uint8_t socket_cnt = 0;
 int result, rc;
 static int initialized = 0;
 mwSize ret_sz[]={1};
-char recv_buffer[BUFLEN];
+//char recv_buffer[BUFLEN1];
+char* recv_buffer;
 
 void cleanup( void ){
+  free( recv_buffer );
   mexPrintf("ZMQMEX: closing sockets and context.\n");
   for(int i=0;i<socket_cnt;i++)
     zmq_close( sockets[i] );
@@ -29,6 +32,7 @@ void cleanup( void ){
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   if (!initialized) {
     mexPrintf("ZMQMEX: creating a 2 thread ZMQ context.\n");
+    recv_buffer = (char*) malloc( BUFLEN2 );
     ctx = zmq_init(2);
     initialized = 1;
     mexAtExit(cleanup);
@@ -126,7 +130,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     int socket = socketid[0];
     if( socket>socket_cnt)
       mexErrMsgTxt("Bad socket id!");
-    int nbytes = zmq_recv(sockets[socket], recv_buffer, BUFLEN, 0);
+    int nbytes = zmq_recv(sockets[socket], recv_buffer, BUFLEN2, 0);
+    //int nbytes = zmq_recv(sockets[socket], recv_buffer, BUFLEN1, 0);
     //int nbytes = zmq_recv(sockets[socket], recv_buffer, BUFLEN, ZMQ_DONTWAIT);
     if(nbytes==-1)
       mexErrMsgTxt("Did not receive anything");
@@ -155,7 +160,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     int r = 0;
     for(int i=0;i<socket_cnt;i++)
       if(poll_items[i].revents){
-        int nbytes = zmq_recv(sockets[i], recv_buffer, BUFLEN, 0);
+        //int nbytes = zmq_recv(sockets[i], recv_buffer, BUFLEN1, 0);
+        int nbytes = zmq_recv(sockets[i], recv_buffer, BUFLEN2, 0);
         idx[r] = i;
         ret_sz[0] = nbytes;
         mxArray* tmp = mxCreateNumericArray(1,ret_sz,mxUINT8_CLASS,mxREAL);

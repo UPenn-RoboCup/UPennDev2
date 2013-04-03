b4f9a4b4eade80b1e9528f399fa7f37df5f92667
Mike Hopkins <michael-hopkins@outlook.com>
2013-04-03 15:30:42 -0400

    add preliminary Manipulation fsm transitions

--- Framework/Motion/FSMs/Attention.lua
+++ Framework/Motion/FSMs/Attention.lua
@@ -1,13 +1,16 @@
 require('look')
 require('Motion_fsm')
+require('Config')
 
 ---------------------------------------------------------
 -- Attention State Machine 
 ---------------------------------------------------------
 
+local joint = Config.joint
+
 Attention = Motion_fsm.new(look)
 
-Attention:set_joint_access(0, 'all')
-Attention:set_joint_access(1, 'head')
+Attention:set_joint_access(0, joint.all)
+Attention:set_joint_access(1, joint.head)
 
 return Attention
--- Framework/Motion/FSMs/Locomotion.lua
+++ Framework/Motion/FSMs/Locomotion.lua
@@ -1,20 +1,23 @@
+require('Config')
+require('Motion_fsm')
 require('idle')
 require('stand')
-require('walk')
 require('walk_stop')
-require('Motion_fsm')
+require('walk')
 
----------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Locomotion State Machine 
----------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local joint = Config.joint
 
 Locomotion = Motion_fsm.new(idle)
 Locomotion:add_state(stand)
 Locomotion:add_state(walk)
 Locomotion:add_state(walk_stop)
 
-Locomotion:set_joint_access(0, 'all')
-Locomotion:set_joint_access(1, 'lowerbody')
+Locomotion:set_joint_access(0, joint.all)
+Locomotion:set_joint_access(1, joint.lowerbody)
 
 Locomotion:set_transition(idle, 'stand', stand)
 Locomotion:set_transition(stand, 'idle', idle)

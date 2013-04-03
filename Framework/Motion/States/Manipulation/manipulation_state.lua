48c09c438feda2f03ecbe534e63e23bc75716c69
jdavis45 <jdavis45@harris.com>
2013-04-03 13:24:27 -0400

    fixed syntax for manipulation_state.lua

--- Framework/Motion/States/Manipulation/manipulation_state.lua
+++ Framework/Motion/States/Manipulation/manipulation_state.lua
@@ -1,6 +1,6 @@
 require('Motion_state')
 require('Config')
-
+require('Transform')
 --------------------------------------------------------------------------
 -- Manipulation Controller
 --------------------------------------------------------------------------
@@ -28,20 +28,19 @@ manipulation_state.parameters = {
 -- config parameters
 manipulation_state:load_parameters()
 
-function manipulation_state:set_joint_goal_enabled(boolean enabled)
+function manipulation_state:set_joint_goal_enabled(enabled)
 	manipulation_controller.THOR_MC_U.JointSpaceGoalEnabled = enabled
 end
 
-function manipulation_state:set_joint_goal_type(boolean is_position_else_velocity)
+function manipulation_state:set_joint_goal_type(is_position_else_velocity)
 	manipulation_controller.THOR_MC_U.JointSpaceGoalType = is_position_else_velocity
 end
 
-function manipulation_state:set_task_goal_enabled(boolean enabled)
+function manipulation_state:set_task_goal_enabled(enabled)
 	manipulation_controller.THOR_MC_U.TaskSpaceGoalEnabled = enabled
 end
 
-function manipulation_state:set_task_goal_type(boolean is_right_position_else_velocity,
-					       boolean is_left_position_else_velocity)
+function manipulation_state:set_task_goal_type(is_right_position_else_velocity,  is_left_position_else_velocity)
 	manipulation_controller.THOR_MC_U.TaskSpaceGoalType = { is_right_position_else_velocity , is_left_position_else_velocity }
 end
 
@@ -63,15 +62,15 @@ function manipulation_state:update()
 	manipulation_controller.THOR_MC_U.JointSpacePositionGoal = { 0, mcm:desired_joint_position({ joint.waist, 
 												joint.r_arm,
 												joint.l_arm } ) }
-	manipulation_controller.THOR_MC_U.TaskSpaceVelocityGoal = { mcm:desired_r_hand_twist, mcm:desired_l_hand_twist }
+	manipulation_controller.THOR_MC_U.TaskSpaceVelocityGoal = { mcm:desired_r_hand_twist(), mcm:desired_l_hand_twist() }
 
-	manipulation_controller.THOR_MC_U.TaskSpacePositionsGoal = { Transform:pose(mcm:desired_r_hand_pose),
-								     Transform:pose(mcm:desired_l_hand_pose)  }
+	manipulation_controller.THOR_MC_U.TaskSpacePositionsGoal = { Transform:pose(mcm:desired_r_hand_pose()),
+								     Transform:pose(mcm:desired_l_hand_pose())  }
 
 	manipulation_controller.THOR_MC_U.JointPositions = { 0, pcm:joint_position({ 	joint.waist, 
 											joint.r_arm,
 											joint.l_arm } ) }
-	manipulation_controller.THOR_MC_U.EndForceTorques = { pcm:r_hand_wrench, pcm:l_hand_wrench }
+	manipulation_controller.THOR_MC_U.EndForceTorques = { pcm:r_hand_wrench(), pcm:l_hand_wrench() }
 
 	-- add manipulation step with time
 	
@@ -81,23 +80,23 @@ function manipulation_state:update()
 	manipulation_controller.THOR_MC_step()
 
 	
-	if (manipulation_controller.THOR_MC_Y.Faulted)
+	if  manipulation_controller.THOR_MC_Y.Faulted == true then
 	--handle faults and pass manipulation_controller.THOR_MC_Y.ErrorCode
 	end
 	
 	--does VT need arm transforms? manipulation_controller.THOR_MC_Y.ArmTransforms
 
 	--can we do both postion and vel or not?
-	dcm.set_joint_velocity( &manipulation_controller.THOR_MC_Y.JointVelocityCmds[1],  { joint.waist, 
+	dcm.set_joint_velocity( manipulation_controller.THOR_MC_Y.JointVelocityCmds[1],  { joint.waist, 
 											    joint.r_arm,
 											    joint.l_arm } )
 
-	dcm.set_joint_position( &manipulation_controller.THOR_MC_Y.JointPositionCmds[1],  { joint.waist, 
+	dcm.set_joint_position( manipulation_controller.THOR_MC_Y.JointPositionCmds[1],  { joint.waist, 
 											    joint.r_arm,
 											    joint.l_arm } )
 											    
 	local step_delta = t-Platform.get_time()
-	print("manipulation state step "+step_delta)	
+	print("manipulation state step "..step_delta)	
 
 end
 

d2ac9cbcbcf93d9d4573719ee85dd4d871ccb395
Yida Zhang <yida@seas.upenn.edu>
2013-04-03 14:08:06 -0400

    add body and vision update loop

--- Run/wbcontroller.lua
+++ Run/wbcontroller.lua
@@ -4,7 +4,19 @@ dofile(cwd.."/Player/include.lua")
 local unix = require 'unix'
 local Body = require 'Body'
 
-print(Body.get_time())
---while true do
---  print(unix.time())
---end
+local last_update = unix.time()
+local update_interval = 0.01
+local last_vision_update = unix.time()
+local vision_update_interval = 0.04; --25fps update
+
+while true do
+  if (unix.time() - last_update) > update_interval then
+    print(unix.time())
+    last_update = unix.time()
+  end
+
+  if (unix.time() - last_vision_update) > vision_update_interval then
+    print('update vision')
+    last_vision_update = unix.time()
+  end
+end

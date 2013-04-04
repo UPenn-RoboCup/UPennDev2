12bf9d7eb0a9bd2880a8fbb366a091ab7796c374
Yida Zhang <yida@seas.upenn.edu>
2013-04-03 22:44:07 -0400

    Working abstract device layer for webots - add stdout:flush

--- Run/wb_listen.lua
+++ Run/wb_listen.lua
@@ -1,9 +1,8 @@
-local cwd = os.getenv('PWD')
 dofile('include.lua')
 local simple_ipc = require 'simple_ipc'
 local camera_channel = simple_ipc.setup_subscriber('camera')
 print('load')
 while true do
     local res = camera_channel:receive()
-    print(res)
+    print(#res)
 end

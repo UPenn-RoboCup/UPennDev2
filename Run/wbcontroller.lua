01960f57b339fc558996883444dba39b76583656
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-03 22:02:09 -0400

    Cleanup code with no unix

--- Run/wbcontroller.lua
+++ Run/wbcontroller.lua
@@ -1,36 +1,39 @@
 local cwd = os.getenv('PWD')
 dofile('Run/include.lua')
-
---local unix = require 'unix'
+local ffi = require'ffi'
 local Body = require 'Body'
 local Camera = require 'Camera'
 local cjpeg = require 'cjpeg'
 local carray = require 'carray'
 local msgpack = require 'msgpack'
-
 local simple_ipc = require 'simple_ipc'
 
+print('Loading the Webots controller script!')
 local camera_channel = simple_ipc.setup_publisher('camera')
-
-print('load')
 local t_last = Body.get_time()
 local debug_fps = 4
 local inv_debug_fps = 1/debug_fps
 while true do
   local t = Body.get_time()
   local t_diff = t - t_last;
+
+  -- Enable debugging
   if (t-t_last)>inv_debug_fps then
-    print(t,t_last)
+    print('Debugging...')
     t_last = t
   end
 
-    local image = carray.byte( Camera.get_image(), Camera.get_width() * Camera.get_height() * 3 )
-    --local img_str = tostring(image)
-    print( type(Camera.get_image()), type(image), type(img_str) )
-    print(Camera.get_width(), Camera.get_height() )
-    local res = camera_channel:send('hello')
-    print(res)
---    local img_str = msgpack.pack(image, Camera.get_width() * Camera.get_height() * 3)
+  -- Grab the image and send it away for processing...
+  local image = carray.byte( Camera.get_image(), Camera.get_width() * Camera.get_height() * 3 )
+  --local jimage = cjpeg.compress( Camera.get_image(), Camera.get_width(), Camera.get_height(), 3)
+  --local img_str = tostring(image)
+  --local img_str = msgpack.pack(image, Camera.get_width() * Camera.get_height() * 3)
+  --print( type(Camera.get_image()), type(image), type(image_str) )
+  print(Camera.get_width(), Camera.get_height() )
+
+  -- Send data on IPC
+  local res = camera_channel:send('hello')
+  -- Update the Webots timestamp and motor commands
   Body.update()
 end
 

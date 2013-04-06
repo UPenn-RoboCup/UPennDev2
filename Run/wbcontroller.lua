75249a1eb5c43aa49e5ee518a18e5135c4349113
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-05 21:27:31 -0400

    Killoff signal and websots version finder

--- Run/wbcontroller.lua
+++ Run/wbcontroller.lua
@@ -1,35 +1,13 @@
 dofile('Run/include.lua')
 local Body = require 'Body'
-local signal = require('signal')
 local Camera = require 'Camera'
 local cjpeg = require 'cjpeg'
 local carray = require 'carray'
 local msgpack = require 'msgpack'
 local simple_ipc = require 'simple_ipc'
 
-local webots_version = io.popen('webots --version', 'r')
-wb_version_str = webots_version:read('*a')
-wb_version_str = wb_version_str:match('%d.%d.%d')
-wb_version = (wb_version_str:byte(1)-48) * 100 + (wb_version_str:byte(3)-48) * 10
-                + wb_version_str:byte(5)-48
-print('Webots Version '..wb_version)
-webots_version:close()
-
-function ShutDownFN()
-  print("Proper shutdown")
-  os.exit(1);
-end
-
-
 print('Loading the Webots controller script!')
 local camera_channel = simple_ipc.new_publisher('camera')
-
-  signal.signal("SIGINT", ShutDownFN);
-  signal.signal("SIGTERM", ShutDownFN);
-  signal.signal("SIGKILL", ShutDownFN);
-  signal.signal("SIGSTOP", ShutDownFN);
-  signal.signal("SIGUSR1", ShutDownFN);
-
 
 local t_last = Body.get_time()
 --print(t_last)
@@ -48,22 +26,15 @@ while true do
 --  -- Grab the image and send it away for processing...
   local image = carray.byte( Camera.get_image(), Camera.get_width() * Camera.get_height() * 3 )
 --  local jimage = cjpeg.compress( Camera.get_image(), Camera.get_width(), Camera.get_height(), 3)
-  local img_str = tostring(image)
+--  local img_str = tostring(image)
 --  --local img_str = msgpack.pack(image, Camera.get_width() * Camera.get_height() * 3)
   print(type(Camera.get_image()), type(image), type(img_str) )
---  print(Camera.get_width(), Camera.get_height() )
+  -- print(Camera.get_width(), Camera.get_height() )
 
   -- Send data on IPC
---  camera_channel:send('hello')
---  local res = camera_channel:send(img_str)
+  -- camera_channel:send('hello')
+  -- local res = camera_channel:send(img_str)
   -- Update the Webots timestamp and motor commands
   Body.update()
-
-  signal.signal("SIGINT", ShutDownFN);
-  signal.signal("SIGTERM", ShutDownFN);
-  signal.signal("SIGKILL", ShutDownFN);
-  signal.signal("SIGSTOP", ShutDownFN);
-  signal.signal("SIGUSR1", ShutDownFN);
-
   io.stdout:flush();
 end

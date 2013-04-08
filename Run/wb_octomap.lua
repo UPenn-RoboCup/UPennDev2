f4390055737c0f53bb1c06c5b202fb9b9dcc8d81
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-07 23:36:10 -0400

    making libSlam smaller

--- Run/wb_octomap.lua
+++ Run/wb_octomap.lua
@@ -4,9 +4,10 @@ dofile('include.lua')
 local simple_ipc = require 'simple_ipc'
 local msgpack = require 'msgpack'
 local carray = require 'carray'
-local Octomap = require'Octomap'
+--local Octomap = require'Octomap'
 local torch = require'torch'
 torch.Tensor = torch.FloatTensor
+local libLaser = require 'libLaser'
 
 -- Global vars
 require 'unix'
@@ -17,86 +18,6 @@ actuator_commands = carray.double(#Params.jointNames);
 for i = 1,#Params.jointNames do
   actuator_commands[i] = 0;
   actuator_positions[i] = 0;
-end
-
--- Prune function
-local LIDAR0 = {}
--- Static configurations
-LIDAR0.resd    = 0.25;
-LIDAR0.res     = LIDAR0.resd/180*math.pi;
-LIDAR0.nRays   = 1081;
-LIDAR0.angles  = torch.range(0,(LIDAR0.nRays-1)*LIDAR0.resd,LIDAR0.resd)
-LIDAR0.angles = (LIDAR0.angles - 135) * math.pi/180
-LIDAR0.cosines = torch.cos(LIDAR0.angles);
-LIDAR0.sines   = torch.sin(LIDAR0.angles);
-LIDAR0.offsetx = 0;
-LIDAR0.offsety = 0;
-LIDAR0.offsetz = 0;--from the body origin (not floor)
-LIDAR0.mask    = torch.Tensor(LIDAR0.angles:size()):fill(1);
--- Take in a CArray
-local X = torch.Tensor(4,LIDAR0.nRays):zero()
-local Y = torch.Tensor(4,LIDAR0.nRays):zero()
-local function ranges2xyz(ranges,pitch,roll)
-  pitch = pitch or 0;
-  roll = roll or 0;
-  local nranges = (#ranges)[1]
-  if nranges~=LIDAR0.nRays then
-    print("BAD RANGE INPUT")
-    return;
-  end
-
-  -- Put lidar readings into relative cartesian coordinate
-  X:resize(4,nranges)
-  local xs = X:select(1,1);
-  local ys = X:select(1,2);
-  xs:copy(ranges):cmul( LIDAR0.cosines )
-  ys:copy(ranges):cmul( LIDAR0.sines )
-
-  -- Accept only ranges that are sufficiently far away
-  -- TODO: Make fast masking!
-  local good_cnt = 0;
-  for i=1,nranges do
-    if ranges[i]>0.25 and LIDAR0.mask==1 then
-      good_cnt = good_cnt+1;
-      xs[good_cnt] = xs[i];
-      ys[good_cnt] = ys[i];
-    end
-  end
-  -- Resize to include just the good readings
-  nranges = good_cnt;
-  if nranges==0 then
-    print('No good readings after initial checks.')
-    return
-  end
-
-  -- Reset the view
-  X:resize(4,nranges)
-  Y:resize(4,nranges)
-  X:select(1,3):fill(0); -- z
-  X:select(1,4):fill(1); -- extra
-
-  -- Apply the transformation given current roll and pitch
-  -- TODO: check if transpose...
-  -- TODO: this is unstable!
-  T = torch.mm(
-  libSlam.roty(pitch),libSlam.rotx(roll)
-  );
-  --T = torch.eye(4);
-  Y:mm(T,X);  --reverse the order because of transpose
-  xs = Y:select(1,1);
-  ys = Y:select(1,2);
-  local zs = Y:select(1,3);
-
-  -- Reset the views
-  X:resize(4,nranges)
-  Y:resize(4,nranges)
-  xs = Y:select(1,1);
-  ys = Y:select(1,2);
-  zs = Y:select(1,3);
-
-  -- Return the data
-  print("Contiguous?",xs:isContiguous(),ys:isContiguous(),xs:isContiguous())
-  return xs,ys,zs;
 end
 
 -- IPC channels
@@ -139,11 +60,14 @@ lidar_channel.callback = function()
   end
   -- Use pointers for "efficiency"
 --[[
-  local ranges_s = torch.FloatStorage( 1081, ranges_t:pointer() )
+  local ranges_s = torch.FloatStorage( 1081, ranges_f:pointer() )
   local ranges = torch.FloatTensor( ranges_s );
-  print( ranges_f[500], ranges[500], ranges_s[500] );
+  print( 'Checking ranges...',ranges_f[500], ranges[500] );
 --]]
-  ranges2xyz(ranges,pitch,roll)
+  
+  local x,y,z = libLaser.ranges2xyz(ranges,actuator_positions[1],0,0)
+  -- TODO: send over to MATLAB for plotting?
+  -- TODO: put into OctoMap for viewing
 
   --print(lidar_ts," Lidar: ", #lidar_ranges)
   -- Change the lidar head to scan

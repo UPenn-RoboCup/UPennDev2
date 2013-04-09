d039bf379160177bc474b310343e5a234f0703ef
Derek Lahr <dlahr@outlook.com>
2013-04-09 01:36:09 -0400

    add COG capture trajectory

--- Run/Test/Motion/print_ahrs.lua
+++ Run/Test/Motion/print_ahrs.lua
@@ -2,10 +2,13 @@ dofile('../../include.lua')
 require('dcm')
 require('unix')
 require('util')
+require('pcm')
 
 local ahrs1={}
 while true do
   unix.sleep(1)
-  ahrs1 = dcm:get_ahrs()
-  print('ahrs', ahrs1[4], ahrs1[5], ahrs1[7], ahrs1[8])
+  --ahrs1 = dcm:get_ahrs()
+  ahrs1 = pcm:get_l_foot_cop_pressure()
+  --print('ahrs', ahrs1[4], ahrs1[5], ahrs1[7], ahrs1[8])
+  print('ahrs', ahrs1[1], ahrs1[3], ahrs1[4], ahrs1[5])
 end

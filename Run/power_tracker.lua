5c24b8be1664d12d366deb4d495dabf5fc7ccefc
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2013-04-10 15:08:50 -0400

    power_tracker: rename 'power' to 'output_power'

--- Run/power_tracker.lua
+++ Run/power_tracker.lua
@@ -34,28 +34,28 @@ local function write_filtered_joint_data_ssv(ssv_file, table)
   ssv_file:write('\n')
 end
 
-local power = {}
-local peak_power = {}
-local total_power = 0
-local peak_total_power = 0
+local output_power = {}
+local peak_output_power = {}
+local total_output_power = 0
+local peak_total_output_power = 0
 local total_energy = 0
-local function update_power(dt)
-  total_power = 0
+local function update_output_power(dt)
+  total_output_power = 0
   for i,joint in ipairs(Config_devices.joint.id) do
     local v = dcm:get_joint_velocity_sensor(joint)
     local f = dcm:get_joint_force_sensor(joint)
-    power[i] = v*f
-    total_power = total_power + math.abs(power[i])
-    if math.abs(power[i]) > math.abs(peak_power[i]) then
-      peak_power[i] = power[i]
+    output_power[i] = v*f
+    total_output_power = total_output_power + math.abs(output_power[i])
+    if math.abs(output_power[i]) > math.abs(peak_output_power[i]) then
+      peak_output_power[i] = output_power[i]
     end
   end
   
-  if total_power > peak_total_power then
-    peak_total_power = total_power
+  if total_output_power > peak_total_output_power then
+    peak_total_output_power = total_output_power
   end
   
-  total_energy = total_energy + total_power*dt
+  total_energy = total_energy + total_output_power*dt
 end
 
 local function draw_screen()
@@ -68,12 +68,12 @@ local function draw_screen()
   curses.printw('---------------------------------------')
   curses.printw('---------------------------------------\n')
   for i,joint in ipairs(Config_devices.joint.id) do
-    curses.printw('%16s         %20f %20f\n', joint, power[i], peak_power[i])
-    total_power = total_power + power[i]
+    curses.printw('%16s         %20f %20f\n', joint, output_power[i], peak_output_power[i])
+    total_output_power = total_output_power + output_power[i]
   end
   curses.printw('---------------------------------------')
   curses.printw('---------------------------------------\n')
-  curses.printw('                         %20f %20f\n', total_power, peak_total_power)
+  curses.printw('                         %20f %20f\n', total_output_power, peak_total_output_power)
   curses.printw('%16s            %38f\n', 'energy (J)', total_energy)
   curses.refresh()
 end
@@ -88,16 +88,16 @@ curses.timeout(1)
 Platform.set_update_rate(500)
 Platform.entry()
 
--- initialize power
+-- initialize output_power
 for i,v in ipairs(Config_devices.joint.id) do
   local walking_frequency = .8
   filters[i] = filter.new_low_pass(1/Platform.get_update_rate(), walking_frequency)
-  power[i] = 0
-  peak_power[i] = 0
+  output_power[i] = 0
+  peak_output_power[i] = 0
 end
 
-local power_filtered_ssv = create_joint_data_ssv('joint_power_filtered.ssv')
-local power_ssv = create_joint_data_ssv('joint_power.ssv')
+local output_power_filtered_ssv = create_joint_data_ssv('joint_output_power_filtered.ssv')
+local output_power_ssv = create_joint_data_ssv('joint_output_power.ssv')
 
 local count = 0
 local last_time = Platform.get_time()
@@ -109,9 +109,9 @@ while true do
   
   local time = Platform.get_time()
   if time > last_time then
-    update_power(time - last_time)
-    write_filtered_joint_data_ssv(power_filtered_ssv, power)
-    write_joint_data_ssv(power_ssv, power)
+    update_output_power(time - last_time)
+    write_filtered_joint_data_ssv(output_power_filtered_ssv, output_power)
+    write_joint_data_ssv(output_power_ssv, output_power)
   end
   last_time = time
   
@@ -128,7 +128,7 @@ while true do
   count = count + 1
 end
 
-power_filtered_ssv:close()
-power_ssv:close()
+output_power_filtered_ssv:close()
+output_power_ssv:close()
 Platform.exit()
 curses.endwin()

7b1e89e1cba6ba204f1a1ee51f16703cd0acc9d9
Michael Hopkins <michael-hopkins@outlook.com>
2013-04-15 13:59:19 -0400

    setup locomotion_slave for end-effector position control, fix function scope bug in Manipulation_state

--- Framework/Motion/States/Manipulation/locomotion_slave.lua
+++ Framework/Motion/States/Manipulation/locomotion_slave.lua
@@ -21,6 +21,20 @@ locomotion_slave:load_parameters()
 
 function locomotion_slave:entry()
 	Manipulation_state.entry(self)
+
+	-- set proper joint gains for position control
+	self.dcm:set_joint_p_gain(0.8, joint.upperbody)
+	self.dcm:set_joint_i_gain(0.1, joint.upperbody)
+	self.dcm:set_joint_d_gain(0.005, joint.upperbody)
+
+	-- enable end-effector position control
+	self:set_joint_goal_enabled(false)
+	self:set_task_goal_enabled(true)
+	self:set_task_goal_type(true, true)
+
+	-- initialize end-effector goals
+	mcm:set_desired_l_hand_pose(pcm:get_l_hand_pose())
+	mcm:set_desired_r_hand_pose(pcm:get_r_hand_pose())
 end
 
 function locomotion_slave:update()

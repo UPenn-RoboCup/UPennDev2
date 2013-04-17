7da3a3cb74dc5936cdc873e4b08222c19227b59c
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-17 01:07:09 -0400

    Separate send and receive in udp so we can test on local machine more easily

--- Tools/Matlab/mex/udp_send.cc
+++ Tools/Matlab/mex/udp_send.cc
@@ -28,14 +28,12 @@ Daniel D. Lee, 6/09 <ddlee@seas.upenn.edu>
 
 const int maxQueueSize = 16;
 static std::deque<std::string> recvQueue;
-static int send_fd, recv_fd;
+static int send_fd;
 
 void mexExit(void)
 {
 	if (send_fd > 0)
 		close(send_fd);
-	if (recv_fd > 0)
-		close(recv_fd);
 }
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
@@ -72,47 +70,10 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 		sizeof(dest_addr)) < 0)
 			mexErrMsgTxt("Could not connect to destination address");
 
-/*
-		recv_fd = socket(AF_INET, SOCK_DGRAM, 0);
-		if (recv_fd < 0)
-			mexErrMsgTxt("Could not open datagram recv socket");
-
-		struct sockaddr_in local_addr;
-		bzero((char *) &local_addr, sizeof(local_addr));
-		local_addr.sin_family = AF_INET;
-		local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		local_addr.sin_port = htons(PORT);
-		if (bind(recv_fd, (struct sockaddr *) &local_addr,
-		sizeof(local_addr)) < 0)
-			mexErrMsgTxt("Could not bind to port");
-
-		// Nonblocking receive:
-		int flags  = fcntl(recv_fd, F_GETFL, 0);
-		if (flags == -1) flags = 0;
-		if (fcntl(recv_fd, F_SETFL, flags | O_NONBLOCK) < 0)
-			mexErrMsgTxt("Could not set nonblocking mode");
-		*/
-		printf("Setting up udp on: %s\n", IP);
+		printf("Setting up udp_send on: %s:%d\n", IP,PORT);
 
 		mexAtExit(mexExit);
 		init = true;
-	}
-
-	// Process incoming messages:
-	socklen_t source_addr_len = sizeof(source_addr);
-	int len = recvfrom(recv_fd, data, MAX_LENGTH, 0,
-	(struct sockaddr *) &source_addr, &source_addr_len);
-	while (len > 0) {
-		std::string msg((const char *) data, len);
-		recvQueue.push_back(msg);
-
-		len = recvfrom(recv_fd, data, MAX_LENGTH, 0,
-		(struct sockaddr *) &source_addr, &source_addr_len);
-	}
-
-	// Remove older messages:
-	while (recvQueue.size() > maxQueueSize) {
-		recvQueue.pop_front();
 	}
 
 	if ((nrhs < 1) || (!mxIsChar(prhs[0])))
@@ -122,31 +83,11 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	if (str == "getQueueSize") {
 		plhs[0] = mxCreateDoubleScalar(recvQueue.size());
 	}
-	else if (str == "receive") {
-		if (recvQueue.empty()) {
-			plhs[0] = mxCreateDoubleMatrix(0, 0, mxREAL);
-			return;
-		}
-
-		int n = recvQueue.front().size();
-		mwSize dims[2];
-		dims[0] = 1;
-		dims[1] = n;
-		plhs[0] = mxCreateNumericArray(2, dims, mxUINT8_CLASS, mxREAL);
-		memcpy(mxGetData(plhs[0]), recvQueue.front().c_str(), n);
-		recvQueue.pop_front();
-	}
 	else if (str == "send") {
 		if (nrhs < 2)
 			mexErrMsgTxt("No input argument");
 		int n = mxGetNumberOfElements(prhs[1])*mxGetElementSize(prhs[1]);
 		int ret = send(send_fd, mxGetData(prhs[1]), n, 0);
 		plhs[0] = mxCreateDoubleScalar(ret);
-
-		// Put it in receive queue as well:
-		/*
-		std::string msg((const char *) mxGetData(prhs[1]), n);
-		recvQueue.push_back(msg);
-		*/
 	}
 }

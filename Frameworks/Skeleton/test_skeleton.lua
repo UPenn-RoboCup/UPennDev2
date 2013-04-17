1917f7d0f35d3869dc04739d0ac81719ce76426f
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-16 22:08:38 -0400

    Access to all joints

--- Frameworks/Skeleton/test_skeleton.lua
+++ Frameworks/Skeleton/test_skeleton.lua
@@ -1,24 +1,23 @@
-local
---typedef enum {
-	NITE_JOINT_HEAD,
-	NITE_JOINT_NECK,
+local	NITE_JOINT_HEAD,
+NITE_JOINT_NECK,
 
-	NITE_JOINT_LEFT_SHOULDER,
-	NITE_JOINT_RIGHT_SHOULDER,
-	NITE_JOINT_LEFT_ELBOW,
-	NITE_JOINT_RIGHT_ELBOW,
-	NITE_JOINT_LEFT_HAND,
-	NITE_JOINT_RIGHT_HAND,
+NITE_JOINT_LEFT_SHOULDER,
+NITE_JOINT_RIGHT_SHOULDER,
+NITE_JOINT_LEFT_ELBOW,
+NITE_JOINT_RIGHT_ELBOW,
+NITE_JOINT_LEFT_HAND,
+NITE_JOINT_RIGHT_HAND,
 
-	NITE_JOINT_TORSO,
+NITE_JOINT_TORSO,
 
-	NITE_JOINT_LEFT_HIP,
-	NITE_JOINT_RIGHT_HIP,
-	NITE_JOINT_LEFT_KNEE,
-	NITE_JOINT_RIGHT_KNEE,
-	NITE_JOINT_LEFT_FOOT,
-	NITE_JOINT_RIGHT_FOOT = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
---} NiteJointType;
+NITE_JOINT_LEFT_HIP,
+NITE_JOINT_RIGHT_HIP,
+NITE_JOINT_LEFT_KNEE,
+NITE_JOINT_RIGHT_KNEE,
+NITE_JOINT_LEFT_FOOT,
+NITE_JOINT_RIGHT_FOOT = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+
+local test_joint = NITE_JOINT_RIGHT_HAND
 
 local skeleton = require 'skeleton'
 -- Get the number of users being tracked
@@ -27,21 +26,27 @@ print( "Number of Skeletons:", n_users )
 
 --for n=1,10 do
 while true do
-  local visible = skeleton.update()
+	local visible = skeleton.update()
 	if visible and type(visible)=="table" and #visible==n_users then
 		for u,v in pairs(visible) do
 			if v then
-				local head = skeleton.joint(u,NITE_JOINT_HEAD);
-				for j,stats in pairs(head)do
-					if type(stats)=="table" then
-						print( j, unpack(stats) )
-					else
-						print( j, stats )
+				local joint = skeleton.joint(u,test_joint);
+				if joint then
+					for j,stats in pairs(joint)do
+						if type(stats)=="table" then
+							print( j, unpack(stats) )
+						else
+							print( j, stats )
+						end
 					end
-				end
-				print()
-			end
-		end
+					print()
+				else
+					print("Bad joint!")
+				end--joint
+			else
+				--print("Invisible user",u)
+			end--v
+		end--uv pairs
 	else
 		print("Bad user tracking!",rc)
 	end

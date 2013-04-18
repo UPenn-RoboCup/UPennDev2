31d2107c92cb8488b4d1f9d54d9ab602137bf9f7
Sarah Dean <xsarah.dean@gmail.com>
2013-04-18 01:35:30 +0000

    made a new colortable for Levine512

--- Player/Config/Config_NaoV4.lua
+++ Player/Config/Config_NaoV4.lua
@@ -28,10 +28,10 @@ game.nPlayers = 4;
 
 param = {}
 param.world = 'World/Config_Nao_World'
-param.walk = 'Walk/Config_NaoV4_Walk_FastStable' 
+param.walk = 'Walk/Config_NaoV4_Steady' 
 param.kick = 'Kick/Config_Nao_Kick'
 param.vision = 'Vision/Config_NaoV4_Vision'
-param.camera = 'Camera/Config_NaoV4_Camera_Levine307'
+param.camera = 'Camera/Config_NaoV4_Camera_GRASPSarah'
 param.fsm = 'FSM/Config_NaoV4_FSM'
 
 loadconfig(param.world)

b3eb20053db5de87284380f747f99bf564f2473a
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-19 13:23:51 -0400

    bug fix

--- Framework/Cognition/libLaser.lua
+++ Framework/Cognition/libLaser.lua
@@ -4,6 +4,7 @@ torch.Tensor = torch.FloatTensor
 local libLaser = {}
 -- Buffers created ONCE for performance
 libLaser.nRays  = 1081
+libLaser.ranges = torch.FloatTensor( libLaser.nRays )
 libLaser.points = torch.Tensor(4,libLaser.nRays):zero()
 libLaser.points_xyz = torch.Tensor(4,libLaser.nRays):zero()
 libLaser.FOV    = 270; -- This many degrees
@@ -25,7 +26,7 @@ end
 libLaser.minRange = 0.25;
 
 -- TODO: Make sure the helper functions are working properly!
-local function rotx(t)
+libLaser.rotx = function(t)
   -- Homogeneous transformation representing a rotation of theta
   -- about the X axis.
   local ct = math.cos(t);
@@ -37,9 +38,8 @@ local function rotx(t)
   r[3][2] = st;
   return r
 end
-libLaser.rotx = rotx
 
-local function roty(t)
+libLaser.roty = function (t)
   -- Homogeneous transformation representing a rotation of theta
   -- about the Y axis.
   local ct = math.cos(t);
@@ -51,9 +51,8 @@ local function roty(t)
   r[3][1] = -1*st;
   return r
 end
-libLaser.roty = roty
 
-local function rotz(t)
+libLaser.rotz = function(t)
   -- Homogeneous transformation representing a rotation of theta
   -- about the Z axis.
   local ct = math.cos(t);
@@ -65,52 +64,62 @@ local function rotz(t)
   r[2][1] = st;
   return r
 end
-libLaser.rotz = rotz
 
-local function trans(v)
+libLaser.trans = function (v)
   local t = torch.eye(4);
   t[1][4] = v[1] or 0;
   t[2][4] = v[2] or 0;
   t[3][4] = v[3] or 0;
   return t;
 end
-libLaser.trans = trans
 
-local function ranges2xyz(ranges,roll,pitch,yaw)
-  local nranges = (#ranges)[1]
-  if nranges~=libLaser.nRays then
-    print("BAD RANGE INPUT",nranges,libLaser.nRays)
-    return;
-  end
+-- Input: carray of float, (optional numbers for rpy)
+libLaser.ranges2xyz = function (ranges_f,roll,pitch,yaw)
+
+	-- Check the input
+	if not ranges_f or #ranges_f~=libLaser.nRays then
+		print("BAD RANGE INPUT")
+		if ranges_f then
+			print('Got',#ranges_f,libLaser.nRays)
+		else
+			print('Got',ranges_f)
+		end
+		return;
+	end
+
+	-- TODO: Use memcpy instead of	silly element-by-element copy
+	for i=1,libLaser.nRays do
+		libLaser.ranges[i] = ranges_f[i]
+	end
+	
   -- easier access to data
   local X = libLaser.points;
 
   -- Put lidar readings into relative cartesian coordinate
-  X:resize(4,nranges)
+  X:resize(4,libLaser.nRays)
   local xs = X:select(1,1);
   local ys = X:select(1,2);
-  xs:copy(ranges):cmul( libLaser.cosines )
-  ys:copy(ranges):cmul( libLaser.sines )
+  xs:copy(libLaser.ranges):cmul( libLaser.cosines )
+  ys:copy(libLaser.ranges):cmul( libLaser.sines )
 
   -- Accept only ranges that are sufficiently far away
   -- TODO: Make fast masking!
   local good_cnt = 0;
-  for i=1,nranges do
-    if ranges[i]>libLaser.minRange and libLaser.mask[i]==1 then
+  for i=1,libLaser.nRays do
+    if libLaser.ranges[i]>libLaser.minRange and libLaser.mask[i]==1 then
       good_cnt = good_cnt+1;
       xs[good_cnt] = xs[i];
       ys[good_cnt] = ys[i];
     end
   end
   -- Resize to include just the good readings
-  nranges = good_cnt;
-  if nranges==0 then
+  if good_cnt==0 then
     print('No good readings after initial checks.')
     return
   end
 
   -- Reset the view
-  X:resize(4,nranges)
+  X:resize(4,good_cnt)
   X:select(1,3):fill(0); -- z
   X:select(1,4):fill(1); -- extra
 
@@ -124,7 +133,7 @@ local function ranges2xyz(ranges,roll,pitch,yaw)
   --T = torch.eye(4);
   -- TODO: Verify that Y:mm(T,X) is not needed
   local Y = libLaser.points_xyz
-  Y:resize(4,nranges)
+  Y:resize(4,good_cnt)
   Y:mm(T,X);  --reverse the order because of transpose
   xs = Y:select(1,1);
   ys = Y:select(1,2);
@@ -136,6 +145,5 @@ local function ranges2xyz(ranges,roll,pitch,yaw)
   -- NOTE: Can also access via libLaser.points_xyz
   --return xs,ys,zs;
 end
-libLaser.ranges2xyz = ranges2xyz
 
 return libLaser

739755816ffc7f952d9454bcb9aa17b204c2d910
Yida Zhang <yida@seas.upenn.edu>
2013-04-21 18:36:21 -0400

    Update legacy comm module - add lua52 support - add debug flag - use copying

--- Frameworks/Comm/lua_UDPComm.cpp
+++ Frameworks/Comm/lua_UDPComm.cpp
@@ -1,3 +1,10 @@
+/* 
+ * Legacy UDP Comm Module
+ * 
+ * Daniel D. Lee <ddlee@seas.upenn.edu>
+ * Yida Zhang <yida@seas.upenn.edu>
+ */
+
 #include <iostream>
 #include <string>
 #include <deque>
@@ -39,16 +46,6 @@ static int PORT = 0;
 static std::deque<std::string> recvQueue;
 static int send_fd, recv_fd;
 
-/*
-void mexExit(void)
-{
-  if (send_fd > 0)
-    close(send_fd);
-  if (recv_fd > 0)
-    close(recv_fd);
-}
-*/
-
 static int lua_comm_init(lua_State *L) {
 	const char *ip = luaL_checkstring(L, 1);
 	int port = luaL_checkint(L,2);
@@ -71,20 +68,17 @@ static int lua_comm_update(lua_State *L) {
   if (!init) {
     struct hostent *hostptr = gethostbyname(IP.c_str());
     if (hostptr == NULL) {
-      printf("Could not get hostname\n");
-      return -1;
+      luaL_error(L, "Could not get hostname\n");
     }
 
     send_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (send_fd < 0) {
-      printf("Could not open datagram send socket\n");
-      return -1;
+      luaL_error(L, "Could not open datagram send socket\n");
     }
 
     int i = 1;
     if (setsockopt(send_fd, SOL_SOCKET, SO_BROADCAST, (const char *) &i, sizeof(i)) < 0) {
-      printf("Could not set broadcast option\n");
-      return -1;
+      luaL_error(L, "Could not set broadcast option\n");
     }
       
     struct sockaddr_in dest_addr;
@@ -94,14 +88,12 @@ static int lua_comm_update(lua_State *L) {
     dest_addr.sin_port = htons(PORT);
 
     if (connect(send_fd, (struct sockaddr *) &dest_addr, sizeof(dest_addr)) < 0) {
-      printf("Could not connect to destination address\n");
-      return -1;
+      luaL_error(L, "Could not connect to destination address\n");
     }
 
     recv_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (recv_fd < 0) {
-      printf("Could not open datagram recv socket\n");
-      return -1;
+      luaL_error(L, "Could not open datagram recv socket\n");
     }
 
     struct sockaddr_in local_addr;
@@ -110,8 +102,7 @@ static int lua_comm_update(lua_State *L) {
     local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     local_addr.sin_port = htons(PORT);
     if (bind(recv_fd, (struct sockaddr *) &local_addr, sizeof(local_addr)) < 0) {
-      printf("Could not bind to port\n");
-      return -1;
+      luaL_error(L, "Could not bind to port\n");
     }
 
     // Nonblocking receive:
@@ -119,8 +110,7 @@ static int lua_comm_update(lua_State *L) {
     if (flags == -1) 
       flags = 0;
     if (fcntl(recv_fd, F_SETFL, flags | O_NONBLOCK) < 0) {
-      printf("Could not set nonblocking mode\n");
-      return -1;
+      luaL_error(L, "Could not set nonblocking mode\n");
     }
 
     // TODO: set at exit
@@ -133,7 +123,9 @@ static int lua_comm_update(lua_State *L) {
   while (len > 0) {
     std::string msg((const char *) data, len);
     recvQueue.push_back(msg);
-//    std::cout << len << ' ' << recvQueue.back().size() << std::endl;
+#ifdef DEBUG
+    std::cout << len << ' ' << recvQueue.back().size() << std::endl;
+#endif
     len = recvfrom(recv_fd, data, MAX_LENGTH, 0, (struct sockaddr *) &source_addr, &source_addr_len);
   }
 
@@ -164,16 +156,6 @@ static int lua_comm_receive(lua_State *L) {
   lua_pushlstring(L, recvQueue.front().c_str(), recvQueue.front().size());
   recvQueue.pop_front();
 
-  /*
-  int n = recvQueue.front().size();
-  mwSize dims[2];
-  dims[0] = 1;
-  dims[1] = n;
-  plhs[0] = mxCreateNumericArray(2, dims, mxUINT8_CLASS, mxREAL);
-  memcpy(mxGetData(plhs[0]), recvQueue.front().c_str(), n);
-  recvQueue.pop_front();
-  */
-
   return 1;
 }
 
@@ -186,12 +168,8 @@ static int lua_comm_send(lua_State *L) {
 
 	std::string header;
   std::string dataStr;
-//	std::string contents(data);
 	std::string contents(data, size);
-//  header.push_back(11);
-//	dataStr = header + contents;
 	dataStr = contents;
-//  std::cout << dataStr.size() << std::endl;
   int ret = send(send_fd, dataStr.c_str(), dataStr.size(), 0);
     
   lua_pushinteger(L, ret);
@@ -211,7 +189,11 @@ static const struct luaL_reg UDPComm_lib [] = {
 extern "C"
 #endif
 int luaopen_UDPComm (lua_State *L) {
+#if LUA_VERSION_NUM == 502
+  luaL_newlib(L, UDPComm_lib);
+#else
   luaL_register(L, "UDPComm", UDPComm_lib);
+#endif
 
   return 1;
 }

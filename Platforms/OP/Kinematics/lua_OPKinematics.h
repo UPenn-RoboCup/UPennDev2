8d0f95cdcdd8ce2965ad2c350f4e28b1ca01524c
Yida Zhang <yida@seas.upenn.edu>
2013-04-21 19:35:42 -0400

    Update generic makerule - merge Makefile.lib to Makefile.inc - update lua header in OP libs

--- Platforms/OP/Kinematics/lua_OPKinematics.h
+++ Platforms/OP/Kinematics/lua_OPKinematics.h
@@ -1,14 +1,7 @@
 #ifndef luaOPKinematics_h_DEFINED
 #define luaOPKinematics_H_DEFINED
 
-extern "C"
-{
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
-}
-
-extern "C"
+#include <lua.hpp>
 int luaopen_OPKinematics(lua_State *L);
 
 #endif

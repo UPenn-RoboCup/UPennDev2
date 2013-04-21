fc2c57f6c433ed80fe95d24d9340662329948b05
Yida Zhang <yida@seas.upenn.edu>
2013-04-21 18:46:34 -0400

    update lidar, dinast, hands - update lidar module to latest - chmod a-x to dinast and hands

--- Frameworks/Hokuyo/run_hokuyo.cc
+++ Frameworks/Hokuyo/run_hokuyo.cc
@@ -1,3 +1,9 @@
+/* 
+(c) 2013 Dan Lee, Alex Kushlyev, Steve McGill, Yida Zhang
+ddlee@seas.upenn.edu, smcgill3@seas.upenn.edu
+University of Pennsylvania
+*/
+
 #include "HokuyoCircularHardware.hh"
 #include "MagicSensorDataTypes.hh"
 #include "MagicStatus.hh"

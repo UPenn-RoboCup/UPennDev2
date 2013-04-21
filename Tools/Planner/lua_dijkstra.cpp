d712f68d0ae6c063b90178c123fb36f7b6b93bc1
Yida Zhang <hitzhangyd@gmail.com>
2013-04-21 03:35:25 -0400

    Add lua dijkstra matrix init

--- Tools/Planner/lua_dijkstra.cpp
+++ Tools/Planner/lua_dijkstra.cpp
@@ -7,7 +7,73 @@
 
 #include <lua.hpp>
 
+#ifdef TORCH
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <luaT.h>
+#include <TH/TH.h>
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// dimensions, sizes
+//template<>
+//{
+//}
+
+static int lua_dijkstra_matrix(lua_State *L) {
+#ifdef TORCH
+  const char *tname = luaT_typename(L, 1);
+  THDoubleTensor *costp = (THDoubleTensor *) luaT_checkudata(L, 1, tname);
+
+  std::cout << "Type Name " << tname << std::endl;
+  std::cout << "Torch Dimension " << costp->nDimension << std::endl;
+
+  vector<double> data;
+  switch (costp->nDimension) {
+    case 1:
+      for (int i = 0; i < costp->size[0]; i++)
+        data.push_back(THTensor_fastGet1d(costp, i));
+      break;
+    case 2:
+      for (int i = 0; i < costp->size[0]; i++)
+        for (int j = 0; j < costp->size[1]; j++)
+          data.push_back(THTensor_fastGet2d(costp, i, j));
+      break;
+    case 3:
+      for (int i = 0; i < costp->size[0]; i++)
+        for (int j = 0; j < costp->size[1]; j++)
+          for (int k = 0; k < costp->size[2]; k++)
+            data.push_back(THTensor_fastGet3d(costp, i, j, k));
+      break;
+    case 4:
+      for (int i = 0; i < costp->size[0]; i++)
+        for (int j = 0; j < costp->size[1]; j++)
+          for (int k = 0; k < costp->size[2]; k++)
+            for (int l = 0; l < costp->size[3]; l++)
+              data.push_back(THTensor_fastGet4d(costp, i, j, k, l));
+      break;
+    default:
+      break;
+  }
+#endif
+  std::cout << data.size() << std::endl;
+  for (int i = 0; i < data.size(); i++)
+    std::cout << data[i] << " ";
+  std::cout << std::endl;
+  return 1;
+}
+
 static const luaL_Reg dijkstra_functions [] = {
+  {"matrix", lua_dijkstra_matrix},
   {NULL, NULL}
 };
 

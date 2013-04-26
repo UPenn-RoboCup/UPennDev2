f475d32985e4437106222fc075ef14a3924c9b8d
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-04-26 18:46:39 -0400

    clean up joystick for no good reason

--- Frameworks/Joystick/lua_xbox360.cpp
+++ Frameworks/Joystick/lua_xbox360.cpp
@@ -26,7 +26,20 @@
  * Lua module for usb joystick interface
  */
 
-#include "luaxbox360.h"
+#include <lua.hpp>
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <libusb.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
 
 // initialized flag
 int init = 0;
@@ -68,164 +81,58 @@ Axes
 7	 Unused
 */
 uint8_t *axes;
-// time of last event (in milliseconds)
+
+/* time of last event (in milliseconds) */
 uint32_t *tbutton; 
 uint32_t *taxis; 
 
-// thread variables
+/* Global Variables */
 static pthread_t xbThread;
+struct libusb_device_handle *devh = NULL;
 
-/////////////////////////////
-
-// For XBOX controller
+/* Constants for the USB device */
 static const int VENDOR_ID = 0x045e;
 static const int PRODUCT_ID = 0x028e;
-
 static const int INTERRUPT_IN_ENDPOINT = 0x81;
 static const int INTERRUPT_OUT_ENDPOINT = 0x02;
-
-// With firmware support, transfers can be > the endpoint's max packet size.
-
 static const int MAX_INTERRUPT_IN_TRANSFER_SIZE = 40;
-static const int MAX_INTERRUPT_OUT_TRANSFER_SIZE = 32; // Seems not to matter how large this is...
-
-// Values for bmRequestType in the Setup transaction's Data packet.
-
-static const int CONTROL_REQUEST_TYPE_IN = LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
-static const int CONTROL_REQUEST_TYPE_OUT = LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
-
-// From the HID spec:
-
+static const int MAX_INTERRUPT_OUT_TRANSFER_SIZE = 32;
+static const int CONTROL_REQUEST_TYPE_IN = LIBUSB_ENDPOINT_IN | 
+	LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
+static const int CONTROL_REQUEST_TYPE_OUT = LIBUSB_ENDPOINT_OUT | 
+	LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
 static const int HID_GET_REPORT = 0x01;
 static const int HID_SET_REPORT = 0x09;
 static const int HID_REPORT_TYPE_INPUT = 0x01;
 static const int HID_REPORT_TYPE_OUTPUT = 0x02;
 static const int HID_REPORT_TYPE_FEATURE = 0x03;
-
-// With firmware support, transfers can be > the endpoint's max packet size.
-
 static const int MAX_CONTROL_IN_TRANSFER_SIZE = 2;
 static const int MAX_CONTROL_OUT_TRANSFER_SIZE = 2;
-
 static const int INTERFACE_NUMBER = 0;
 static const int TIMEOUT_MS = 5000;
 
-// Dev handle
-struct libusb_device_handle *devh = NULL;
-
-int xbox360_thread_init() {
-  if (init) {
-    // a joystick is already open, close it first
-    xbox360_thread_cleanup();
-  } 
-  ///////////////////
-  // INIT
-
-  int result, i;
-
-  result = libusb_init(NULL);
-  if (result < 0)
-  {
-    fprintf(stderr, "Unable to initialize libusb.\n");
-    return -1;
-  }
-  devh = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
-
-  if (devh != NULL)
-  {
-    // The HID has been detected.
-    // Detach the hidusb driver from the HID to enable using libusb.
-
-    libusb_detach_kernel_driver(devh, INTERFACE_NUMBER);
-    result = libusb_claim_interface(devh, INTERFACE_NUMBER);
-    if (result < 0) {
-      fprintf(stderr, "libusb_claim_interface error %d\n", result);
-      return -1;
-    }
-  }
-  else
-  {
-    fprintf(stderr, "Unable to find the device.\n");
-    return -1;
-  }
-  ///////////////
-
-
-
-
-  // allocate array data
-  buttons = (uint8_t *)malloc(nbutton * sizeof(uint8_t));
-  if (!buttons) {
-    fprintf(stderr, "unable to allocate button data array\n");
-    return -1;
-  }
-	for(i=0;i<nbutton;i++) {
-		buttons[i] = 0;
-	}
-  axes = (uint8_t *)malloc(naxis * sizeof(uint8_t));
-  if (!axes) {
-    fprintf(stderr, "unable to allocate axes data array\n");
-    return -1;
-  }
-	for(i=0;i<nbutton;i++) {
-		axes[i] = 0;
-	}
-  tbutton = (uint32_t *)malloc(nbutton * sizeof(uint32_t));
-  if (!tbutton) {
-    fprintf(stderr, "unable to allocate button time array\n");
-    return -1;
-  }
-  taxis = (uint32_t *)malloc(naxis * sizeof(uint32_t));
-  if (!taxis) {
-    fprintf(stderr, "unable to allocate axes time array\n");
-    return -1;
-  }
-
-  // start receiver thread
-  printf("creating xbox360 thread\n");
-  running = 1;
-	int ret;
-  ret = pthread_create(&xbThread, NULL, xbox360_thread_func, NULL);
-  if (ret != 0) {
-    fprintf(stderr, "error creating joystick thread: %d\n", ret);
-    return -1;
-  }
-
-  init = 1;
-
-  return 0;
-}
-
+/* Clean up the Thread when done */
 int xbox360_thread_cleanup() {
-  printf("cleaning up xbox360 thread\n");
   if (init) {
     // set initialized to false
     init = 0;
-
     libusb_release_interface(devh, 0);
     libusb_close(devh);
     libusb_exit(NULL);
-
     // free arrays
     free(buttons);
     free(axes);
     free(tbutton);
     free(taxis);
   }
-
   return 0;
 }
-
-
+/* Thread function for listening to the controller */
 void *xbox360_thread_func(void *) {
-  printf("starting xbox360 thread\n");
-	
 	int bytes_transferred;
 	int result = 0;
 	int i = 0;
-
  	unsigned char data_in[20]; // Reports are 20 bytes for 360
-
   /*
      sigset_t sigs;
      sigfillset(&sigs);
@@ -258,16 +165,92 @@ void *xbox360_thread_func(void *) {
     }
 
   }
-
   xbox360_thread_cleanup();
   running = 0;
 }
 
+int xbox360_thread_init() {
+  if (init) {
+    // a joystick is already open, close it first
+    xbox360_thread_cleanup();
+  } 
+  ///////////////////
+  // INIT
 
+  int result, i;
+
+  result = libusb_init(NULL);
+  if (result < 0) {
+		// TODO: Lua error
+    fprintf(stderr, "Unable to initialize libusb.\n");
+    return -1;
+  }
+  devh = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
+
+  if (devh != NULL)
+  {
+    // The HID has been detected.
+    // Detach the hidusb driver from the HID to enable using libusb.
+
+    libusb_detach_kernel_driver(devh, INTERFACE_NUMBER);
+    result = libusb_claim_interface(devh, INTERFACE_NUMBER);
+    if (result < 0) {
+			// TODO: Lua error
+      fprintf(stderr, "libusb_claim_interface error %d\n", result);
+      return -1;
+    }
+  }
+  else
+  {
+    fprintf(stderr, "Unable to find the device.\n");
+    return -1;
+  }
+  ///////////////
+
+  // allocate array data
+  buttons = (uint8_t *)malloc(nbutton * sizeof(uint8_t));
+  if (!buttons) {
+    fprintf(stderr, "unable to allocate button data array\n");
+    return -1;
+  }
+	for(i=0;i<nbutton;i++) {
+		buttons[i] = 0;
+	}
+  axes = (uint8_t *)malloc(naxis * sizeof(uint8_t));
+  if (!axes) {
+    fprintf(stderr, "unable to allocate axes data array\n");
+    return -1;
+  }
+	for(i=0;i<nbutton;i++) {
+		axes[i] = 0;
+	}
+  tbutton = (uint32_t *)malloc(nbutton * sizeof(uint32_t));
+  if (!tbutton) {
+    fprintf(stderr, "unable to allocate button time array\n");
+    return -1;
+  }
+  taxis = (uint32_t *)malloc(naxis * sizeof(uint32_t));
+  if (!taxis) {
+    fprintf(stderr, "unable to allocate axes time array\n");
+    return -1;
+  }
+
+  // start receiver thread
+  running = 1;
+	int ret;
+  ret = pthread_create(&xbThread, NULL, xbox360_thread_func, NULL);
+  if (ret != 0) {
+    fprintf(stderr, "error creating joystick thread: %d\n", ret);
+    return -1;
+  }
+  init = 1;
+  return 0;
+}
+
+/* Lua accessor functions */
 static int lua_joystick_open(lua_State *L) {
   return xbox360_thread_init();
 }
-
 
 static int lua_joystick_close(lua_State *L) {
   // stop thread
@@ -282,17 +265,13 @@ static int lua_joystick_close(lua_State *L) {
   return 0;
 }
 
-
 static int lua_joystick_num_buttons(lua_State *L) {
   lua_pushinteger(L, nbutton);
-
   return 1;
 }
 
-
 static int lua_joystick_num_axes(lua_State *L) {
   lua_pushinteger(L, naxis);
-
   return 1;
 }
 
@@ -365,7 +344,7 @@ static int lua_joystick_axis_time(lua_State *L) {
 }
 
 
-static const struct luaL_reg xbox360_lib [] = {
+static const struct luaL_Reg xbox360_lib [] = {
   {"open", lua_joystick_open},
   {"close", lua_joystick_close},
   {"button", lua_joystick_button},
--- Frameworks/Joystick/test_360.lua
+++ Frameworks/Joystick/test_360.lua
@@ -1,14 +1,9 @@
 local xbox360 = require 'xbox360'
-local unix = require 'unix'
-
 xbox360.open()
-
 for i=1,100 do
 	local buttons = xbox360.button()
 	local axes = xbox360.axis()
 	print('Buttons:', unpack(buttons) )
 	print('Axes:', unpack(axes) )
-	unix.usleep(1e5)
 end
-
 xbox360.close()

8eadd8673a945952bfd3e22a5f4cfbe211607e2c
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-05-03 19:30:30 -0400

    Memory updates for torch

--- Frameworks/Kalman/libKalman.lua
+++ Frameworks/Kalman/libKalman.lua
@@ -48,36 +48,71 @@ filter.init_position_filter = function( self, nDim )
 	-- Measurement
 	self.R = torch.eye( nDim )*.1
 	self.H = torch.Tensor( nDim, 2*nDim ):zero()
-	self.H:sub(1,nDim,1,nDim):eye(nDim)
+	self.H:sub(1,nDim,1,nDim):eye(nDim):mul(.1)
 	-- Prior
-	self.P_k_minus = torch.eye( 2*nDim )
+	self.P_k_minus = torch.eye( 2*nDim ) -- initial confidence
 	self.x_k_minus = torch.Tensor( 2*nDim ):zero()
 	-- State
-	self.P_k = torch.eye( 2*nDim )
+	self.P_k = torch.eye( 2*nDim ) * .1 -- initial confidence
 	self.x_k = torch.Tensor( 2*nDim ):zero()
-	self.P_k_minus = torch.eye( 2*nDim )
-	self.x_k_minus = torch.Tensor( 2*nDim ):zero()
+	-- Kalman Gain
+	self.K_k = torch.mm(self.P_k_minus, self.H:t()):zero()
+	-- Temporary Variables for memory savings
+	self.tmp_input = torch.mv( self.B, self.u_k ):zero()
+	self.tmp_state = torch.mv( self.A, self.x_k_minus ):zero()
+	self.tmp_covar = torch.mm( self.A, self.P_k_minus ):zero()
+	self.tmp_pcor1 = torch.mm( self.H, self.P_k_minus ):zero()
+	self.tmp_pcor2 = torch.mm( self.tmp_pcor1, self.H:t() ):zero()
+	self.tmp_pcor3 = torch.mm( self.tmp_pcor1, self.H:t() ):zero()
+	self.tmp_pcor4 = torch.mm( self.P_k_minus, self.H:t()):zero()
+	self.K_update  = torch.eye( 2*nDim ):zero()
+	self.tmp_scor  = torch.mv( self.H, self.x_k_minus )
 	return self.x_k, self.P_k
 end
 
 -- Form a state estimate prior based on the process and input
+-- This seems to be working just fine
 filter.predict = function(self, u_k)
 	-- There may be many predictions before a measurement
+	if u_k then
+		self.u_k = u_k
+	end
+	self.tmp_input:mv(self.B,self.u_k)
+	self.tmp_state:mv(self.A,self.x_k_minus)
+	self.x_k_minus:copy( self.tmp_state ):add(self.tmp_input)
+	self.tmp_covar:mm( self.A, self.P_k_minus )
+	self.P_k_minus:mm( self.tmp_covar,self.A:t() ):add(self.Q)
+	--[[
+	-- Simple (i.e. bad memory) way
 	self.x_k_minus = self.A * self.x_k_minus + self.B * self.u_k
 	self.P_k_minus = self.A * self.P_k_minus * self.A:t() + self.Q
+	--]]
 	return self.x_k_minus, self.P_k_minus
 end
 
 -- Correct the state estimate based on the state estimate prior and measurement
 filter.correct = function( self, z_k )
+	self.tmp_pcor1:mm( self.H, self.P_k_minus )
+	self.tmp_pcor2:mm( self.tmp_pcor1, self.H:t() ):add(self.R)
+	torch.inverse( self.tmp_pcor3, self.tmp_pcor2 )
+	self.tmp_pcor4:mm(self.P_k_minus, self.H:t() )	
+	self.K_k:mm( self.tmp_pcor4, self.tmp_pcor3 )
+	self.K_update:mm( self.K_k, self.H ):mul(-1):add(self.I)
+	self.P_k:mm( self.K_update, self.P_k_minus )
+	-- Update state
+	self.tmp_scor:mv( self.H, self.x_k_minus ):mul(-1):add(z_k)
+	self.x_k:mv( self.K_k, self.tmp_scor ):add(self.x_k_minus)
+	--[[
+	-- Simple (i.e. bad memory) way
 	local tmp1 = self.H * self.P_k_minus * self.H:t()
 	local tmp = tmp1 + self.R
 	local K_k = self.P_k_minus * self.H:t() * torch.inverse(tmp)
 	self.P_k = (self.I - K_k * self.H) * self.P_k_minus
 	self.x_k = self.x_k_minus + K_k * (z_k - self.H * self.x_k_minus)
+	--]]
 	-- Prior becomes the corrected state
 	self.x_k_minus, self.P_k_minus = self.x_k, self.P_k
-	return self.x_k, self.P_k, K_k
+	return self.x_k, self.P_k, self.K_k
 end
 
 return filter

5e16ba655c35b5f3e26c66cd8f2c8948d8e0ba43
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-05-07 23:14:59 -0400

    fixing dword bug

--- Frameworks/Dynamixel/test_dynamixel.lua
+++ Frameworks/Dynamixel/test_dynamixel.lua
@@ -3,14 +3,10 @@ local twait = 0.010;
 
 local use_real_device = false
 local test_probe = false
-local test_torque = true
-local test_position = false
-
---[[
-for k,v in pairs(libDynamixel) do
-	print(k,v)
-end
---]]
+local test_torque = false
+local test_position = true
+local show_pairs = true
+local test_crc = false
 
 -- Fake device for testing?
 local dev_name = 'fake'
@@ -19,28 +15,43 @@ if use_real_device then
 end
 local Dynamixel = libDynamixel.open(dev_name)
 
---[[
+if test_crc then
 local DynamixelPacket = require('DynamixelPacket');
 local pkt = string.char(255,255,253,0,7,6,0,3,24,0,0)
 h,l=DynamixelPacket.crc16( pkt )
 print(string.format('crc: %x %x %d',l,h, #pkt) )
---]]
-
---[[
-for k,v in pairs(Dynamixel) do
-	print(k,v)
 end
---]]
+
+if show_pairs then
+	for k,v in pairs(libDynamixel) do
+		print(k,v)
+	end
+	for k,v in pairs(Dynamixel) do
+		print(k,v)
+	end
+end
 
 if test_torque then
 	print('Testing torque enable with MX motors')
-local val = 0
-for id=7,9 do
-	io.write(string.format('ID %d\n', id) )
-	local ret = Dynamixel:set_torque_enable(id,val)
-	io.write( string.format('Sent %d bytes.\n',ret) )
-	print()
+	local val = 0
+	for id=7,9 do
+		io.write(string.format('ID %d\n', id) )
+		local ret = Dynamixel:set_mx_torque_enable(id,val)
+		io.write( string.format('Sent %d bytes.\n',ret) )
+		print()
+	end
 end
+
+if test_position then
+	local goal = 72000 --4096
+	print('Testing position with MX motors. Goal:',goal)
+	local val = 0
+	for id=7,9 do
+		io.write(string.format('ID %d\n', id) )
+		local ret = Dynamixel:set_nx_command_position(id,goal)
+		io.write( string.format('Sent %d bytes.\n',ret) )
+		print()
+	end
 end
 
 if test_probe then

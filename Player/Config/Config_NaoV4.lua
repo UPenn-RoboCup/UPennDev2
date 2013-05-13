20f66fb65250d97abc59a72455deb16a385432a3
Sarah <deans@seas>
2013-05-13 20:26:23 +0000

    fixed typo

--- Player/Config/Config_NaoV4.lua
+++ Player/Config/Config_NaoV4.lua
@@ -47,7 +47,7 @@ dev.ip_wireless_port = 54321
 dev.game_control = 'NaoGameControl';
 dev.team='TeamSPL';
 dev.walk = 'Walk/NaoV4Walk';
-dev.kick = 'walk/BasicKick';
+dev.kick = 'Walk/BasicKick';
 
 --Speak enable
 speakenable = 1;

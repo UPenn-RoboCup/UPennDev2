3a6a748e730578dcd74e556a2d62631a6c3e97be
aqualan <aaquino@seas.upenn.edu>
2013-05-23 20:51:40 +0000

    Slower stable walk for testing.

--- Player/Config/Config_NaoV4.lua
+++ Player/Config/Config_NaoV4.lua
@@ -20,7 +20,7 @@ params.name = {"Walk", "World", "Kick", "Vision", "FSM", "Camera"};
 
 ---Location Specific Camera Parameters--
 params.Camera = "GraspChris"
-params.Walk = "FastStableAlan"
+params.Walk = "SlowSteady"
 params.World = "SPL13Grasp"
 
 util.LoadConfig(params, platform)

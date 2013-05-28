2b9e28e1de1809f3b82524e4767cde0a950830b8
SJ@AW <seungjoon.yi@gmail.com>
2013-05-27 20:42:29 -0400

    Config/Camera/Config_OP_Camera_Grasp.lua

--- Player/Config/Camera/Config_OP_Camera_Grasp.lua
+++ Player/Config/Camera/Config_OP_Camera_Grasp.lua
@@ -54,9 +54,9 @@ camera.param[6] = {key='sharpness',     val={0}};
 camera.param[7] = {key='exposure (absolute)',      val={1005}};
 ]]--
 camera.param = {};
-camera.param[1] = {key='brightness',    val={216}};
-camera.param[2] = {key='contrast',      val={6}};
-camera.param[3] = {key='saturation',    val={43}};
+camera.param[1] = {key='brightness',    val={120}};
+camera.param[2] = {key='contrast',      val={32}};
+camera.param[3] = {key='saturation',    val={85}};
 camera.param[4] = {key='gain',          val={255}};
 camera.param[5] = {key='white balance temperature', val={1000}};
 camera.param[6] = {key='sharpness',     val={0}};
@@ -74,7 +74,8 @@ camera.param[7] = {key='exposure (absolute)',      val={800}};
 --]]
 --
 --camera.lut_file = 'lut_low_contrast_pink_n_green.raw';
-camera.lut_file = 'grasp_low_contrast_1643_samarth.raw';
+--camera.lut_file = 'grasp_low_contrast_1643_samarth.raw';
+camera.lut_file = 'grasp_high_contrast_2030_samarth.raw';
 --camera.lut_file = 'lut_Grasp_Greenonly.raw';
 --camera.lut_file = 'lut_OP_Grasp_GreenOnly.raw';
 --camera.lut_file = '0427_grasp.raw'; --Red ball, not orange 

d2ff31474f0168f9824ad9ae15ef1c792bded5f1
aaquino@seas.upenn.edu <yizhe@seas.upenn.edu>
2013-05-28 00:55:18 +0000

    Updated parameters for night colortable

--- Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
+++ Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
@@ -24,9 +24,9 @@ camera.param[2] = {key='Saturation'     , val={210 , 215}};
 -- Hue will automatically change to 0 if set to a number between -5 and 5, but cannot be set by other numbers
 camera.param[3] = {key='Hue'            , val={0 , 0}};
 
-camera.param[4] = {key='Exposure'       , val={25 , 35}};
+camera.param[4] = {key='Exposure'       , val={25 , 30}};
 -- Gain should be set between 32 and 255
-camera.param[5] = {key='Gain'           , val={130 , 120}};
+camera.param[5] = {key='Gain'           , val={155 , 225}};
 -- Sharpness should be set between 0 and 7
 camera.param[6] = {key='Sharpness'      , val={3  , 3}};
 

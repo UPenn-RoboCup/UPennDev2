97be9aeff555235368e6d6ce49980b3806a14f8e
aaquino@seas.upenn.edu <yizhe@seas.upenn.edu>
2013-05-31 01:30:28 +0000

    fixed

--- Lib/Platform/NaoV4/Body/NaoBody.lua
+++ Lib/Platform/NaoV4/Body/NaoBody.lua
@@ -287,7 +287,7 @@ function get_change_role()
   bumperLeft = get_sensor_bumperLeft();
   bumperRight = get_sensor_bumperRight();
 
-  if (bumperLeft[1]+bumperLeft[2])+
+  if (bumperLeft[1]+bumperLeft[2]+
      bumperRight[1]+bumperRight[2])>0 then
     return 1;
   else

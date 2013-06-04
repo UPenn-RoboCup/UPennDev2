913dc1f59872aa32cef9ac1b019aa87709f2f324
aaquino@seas.upenn.edu <yizhe@seas.upenn.edu>
2013-06-04 18:40:54 +0000

    Camera parameters for the day

--- Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
+++ Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
@@ -18,15 +18,15 @@ camera.focal_base = 640; -- image width used in focal length calculation
 
 camera.param = {};
 -- Contrast should be set between 17 and 64
-camera.param[1] = {key='Contrast'       , val={64 , 45}};
+camera.param[1] = {key='Contrast'       , val={40 , 20}};
 
-camera.param[2] = {key='Saturation'     , val={150 , 200}};
+camera.param[2] = {key='Saturation'     , val={215 , 220}};
 -- Hue will automatically change to 0 if set to a number between -5 and 5, but cannot be set by other numbers
 camera.param[3] = {key='Hue'            , val={0 , 0}};
 
 camera.param[4] = {key='Exposure'       , val={25 , 30}};
 -- Gain should be set between 32 and 255
-camera.param[5] = {key='Gain'           , val={200 , 170}};
+camera.param[5] = {key='Gain'           , val={255 , 225}};
 -- Sharpness should be set between 0 and 7
 camera.param[6] = {key='Sharpness'      , val={3  , 3}};
 

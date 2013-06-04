2e310b9879d8cde8e65ef3bfa2b040f4925fd634
SJ@AW <seungjoon.yi@gmail.com>
2013-06-04 03:55:19 -0400

    approach debug msg added

--- Player/Vision/Vision.lua
+++ Player/Vision/Vision.lua
@@ -241,7 +241,7 @@ function update()
     if (cmd >= 0 and cmd < camera.ncamera) then
       Camera.select_camera(cmd);
     else
-      print('WARNING: attempting to switch to unkown camera select = '..cmd);
+--      print('WARNING: attempting to switch to unkown camera select = '..cmd);
     end
   end
 

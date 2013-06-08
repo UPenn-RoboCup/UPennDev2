b2a352a367ed57c803711c80339e376275ae23ef
aqualan <aaquino@seas.upenn.edu>
2013-06-08 00:16:05 +0000

    Updated color table and camera params

--- Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
+++ Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
@@ -18,15 +18,15 @@ camera.focal_base = 640; -- image width used in focal length calculation
 
 camera.param = {};
 -- Contrast should be set between 17 and 64
-camera.param[1] = {key='Contrast'       , val={40 , 25}};
+camera.param[1] = {key='Contrast'       , val={30 , 25}};
 
-camera.param[2] = {key='Saturation'     , val={140 , 150}};
+camera.param[2] = {key='Saturation'     , val={140 , 140}};
 -- Hue will automatically change to 0 if set to a number between -5 and 5, but cannot be set by other numbers
 camera.param[3] = {key='Hue'            , val={0 , 0}};
 
-camera.param[4] = {key='Exposure'       , val={30 , 30}};
+camera.param[4] = {key='Exposure'       , val={40 , 35}};
 -- Gain should be set between 32 and 255
-camera.param[5] = {key='Gain'           , val={240 , 200}};
+camera.param[5] = {key='Gain'           , val={225 , 220}};
 -- Sharpness should be set between 0 and 7
 camera.param[6] = {key='Sharpness'      , val={3  , 3}};
 

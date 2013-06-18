cf91b6518656e2fa7b697ba9142c348832c48262
aaquino@seas.upenn.edu <yizhe@seas.upenn.edu>
2013-06-18 19:30:37 +0000

    Updated camera params for lut_nightgrasp.raw

--- Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
+++ Player/Config/Camera/Config_NaoV4_Camera_GraspChris.lua
@@ -18,15 +18,15 @@ camera.focal_base = 640; -- image width used in focal length calculation
 
 camera.param = {};
 -- Contrast should be set between 17 and 64
-camera.param[1] = {key='Contrast'       , val={30 , 16}};
+camera.param[1] = {key='Contrast'       , val={35 , 35}};
 
-camera.param[2] = {key='Saturation'     , val={180 , 170}};
+camera.param[2] = {key='Saturation'     , val={150 , 150}};
 -- Hue will automatically change to 0 if set to a number between -5 and 5, but cannot be set by other numbers
 camera.param[3] = {key='Hue'            , val={0 , 0}};
 
-camera.param[4] = {key='Exposure'       , val={32 , 30}};
+camera.param[4] = {key='Exposure'       , val={30 , 25}};
 -- Gain should be set between 32 and 255
-camera.param[5] = {key='Gain'           , val={140 , 140}};
+camera.param[5] = {key='Gain'           , val={150 , 120}};
 -- Sharpness should be set between 0 and 7
 camera.param[6] = {key='Sharpness'      , val={3  , 3}};
 
@@ -41,5 +41,5 @@ camera.param[8] = {key='Vertical Flip'  , val={1  , 0}};
 -- brightness has to be set seperately from other parameters, and it can only be set to multiple of 4
 camera.brightness = 200;
 
-camera.lut_file = 'lut_graspchris.raw';
+camera.lut_file = 'lut_nightgrasp.raw';
 

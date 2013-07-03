7348ef9de7ef35552f3f0969a57f8170c1a92a5f
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-07-03 21:40:47 +0200

    new unlogger and removing some unnecessary stuff

--- Config/Config_Stretcher.lua
+++
@@ -1,9 +0,0 @@
-module(..., package.seeall);
-local util = require('util')
-local vector = require('vector')
-local unix = require 'unix'
--- Stretcher Parameters
-
-stretcher = {};
-stretcher.primesense = true;
-

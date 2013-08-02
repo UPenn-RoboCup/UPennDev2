41024c41f089d846a6627b14bdb306fbccda40e7
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-08-01 20:45:06 -0400

    Add some of Yida's UKF functions into a quaternion library and extra libTrig functions

--- Util/quaternion.lua
+++ Util/quaternion.lua
@@ -1,27 +1,40 @@
+local vector=require'vector'
 local quaternion = {}
 
 local mt = {}
 
-local function norm(v1)
-  local s = 0
-  for i = 1, #v1 do s = s + v1[i] * v1[i] end
-  return math.sqrt(s)
-end
-
 -- New quaternion from a 3 or 4 element table
-function new(t,alpha)
+function quaternion.new(t,alpha)
   if not t then return setmetatable({1,0,0,0}, mt) end
   if #t==4 then return setmetatable(t, mt) end
   -- Rotation vector to a quaternion
   assert(#t==3,'Must give a 3 or 4 element table to make a quaternion')
-  local mag = quaternion.norm(t)
-  alpha = alpha or mag
+  local q = {1,0,0,0}
+  -- Grab the norm of the rotation vector
+  local wNorm = vector.norm(t)
+  -- Avoid small norms
+  if wNorm < 1e-6 then return setmetatable(q, mt) end
+  -- If given, use alpha as the amount of rotation about the axis vector t
+  alpha = alpha or wNorm
   local scale = math.sin(alpha/2) / mag
-  t = { math.cos(alpha/2), scale*t[1], scale*t[2], scale*t[3] }
-  return setmetatable(t, mt)
+  q[1] = math.cos(alpha/2)
+  q[2] = scale*t[1]
+  q[3] = scale*t[2]
+  q[4] = scale*t[3]
+  return setmetatable(q, mt)
 end
 
-function conjugate(v1, istart, iend)
+-- Return the Roll/Pitch/Yaw of this quaternion
+-- Modified from Yida's UKF
+function quaternion.rpy( q )
+  local rpy = {}
+  rpy[1] = math.atan2(2*(q[1]*q[2]+q[3]*q[4]), 1-2*(q[2]*q[2]+q[3]*q[3]))
+  rpy[2] = math.asin(2*(q[1]*q[3]-q[4]*q[2]))
+  rpy[3] = math.atan2(2*(q[1]*q[4]+q[2]*q[3]), 1-2*(q[3]*q[3]+q[4]*q[4]))
+  return rpy
+end
+
+function quaternion.conjugate(v1, istart, iend)
   local v = {}
   iend = iend or #v1
   for i = 1,iend-istart+1 do
@@ -30,45 +43,67 @@ function conjugate(v1, istart, iend)
   return setmetatable(v, mt)
 end
 
-function add(v1, v2)
-  local v = {}
-  for i = 1, #v1 do v[i] = v1[i] + v2[i] end
-  return setmetatable(v, mt)
-end
-
-function sub(v1, v2)
-  local v = {}
-  for i = 1, #v1 do
-    v[i] = v1[i] - v2[i]
+-- Return a rotation vector
+-- From Yida
+function quaternion.vector(q)
+  assert( math.abs(q[1])<=1, 'Bad quaternion' )
+  local alphaW = 2*math.acos(q[1])
+  local v = vector.new({0,0,0})
+  -- Avoid the divide by zero scenario
+  if alphaW > 1e-6 then
+    v[1] = q[2] / math.sin(alphaW/2) * alphaW
+    v[2] = q[3] / math.sin(alphaW/2) * alphaW
+    v[3] = q[4] / math.sin(alphaW/2) * alphaW
   end
-  return setmetatable(v, mt)
+  return v
 end
 
-function divnum(v1, a)
-  local v = {}
-  for i = 1, #v1 do
-    v[i] = v1[i]/a
-  end
-  return setmetatable(v, mt)
+-- Take the average of a set of quaternions
+-- TODO: Remove the torch dependence
+-- NOTE: without torch, this may be slow
+function quaternion.mean( QMax, qInit )
+  local torch = require'torch'
+  local qIter = torch.DoubleTensor(4):copy(qInit)
+  local e = torch.DoubleTensor(3, QMax:size(2)):fill(0)
+  local iter = 0
+  local diff = 0
+  repeat
+    iter = iter + 1
+    for i = 1, QMax:size(2) do
+      local ei = e:narrow(2, i, 1):fill(0)
+      local qi = QMax:narrow(2, i, 1)
+      local eQ = QuatMul(qi, QuatInv(qIter))
+      ei:copy(Quat2Vector(eQ))
+    end
+    local eMean = quaternion.new( torch.mean(e,2) )
+    
+    local qIterNext = eMean * qIter
+    -- Compare the roation vectors
+    local diff = vector.norm(quaternion.vector(qIterNext)-quaternion.vector(e2))
+    qIter = qIterNext
+  until diff < 1e-6
+  return qIter, e
 end
 
-function mul(v1, v2)
-  local s = 0
-  for i = 1, #v1 do
-    s = s + v1[i] * v2[i]
-  end
-  return s
+-- Metatable methods are local
+local function add(q1, q2)
+  local q = {}
+  for i,v in ipairs(q1) do q[i] = v + q2[i] end
+  return setmetatable(q, mt)
 end
 
-function div(v1, v2)
-  if type(v2) == "number" then
-    return divnum(v1, v2)
-  else
-    return nil
-  end
+local function mul(q1, q2)
+  local a1,b1,c1,d1 = unpack(q1)
+  local a2,b2,c2,d2 = unpack(q2)
+  local q = {}
+  q[1] = a1*a2 - b1*b2 - c1*c2 - d1*d2
+  q[2] = a1*b2 + b1*a2 + c1*d2 - d1*c2
+  q[3] = a1*c2 - b1*d2 + c1*a2 + d1*b2
+  q[4] = a1*d2 + b1*c2 - c1*b2 + d1*a2
+  return q
 end
 
-
+-- Same tostring as a vector
 local function tostring(v1, formatstr)
   formatstr = formatstr or "%g"
   local str = "{"..string.format(formatstr, v1[1])
@@ -79,10 +114,9 @@ local function tostring(v1, formatstr)
   return str
 end
 
+-- Set the metatable values
 mt.__add = add
-mt.__sub = sub
 mt.__mul = mul
-mt.__div = div
-mt.__unm = unm
 mt.__tostring = tostring
 
+return quaternion

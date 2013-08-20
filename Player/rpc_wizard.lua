482ff83c57f3a4d32723d981273422102ff9c11b
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-08-20 00:02:20 -0400

    a bit better

--- Player/rpc_wizard.lua
+++ Player/rpc_wizard.lua
@@ -2,19 +2,39 @@ dofile'include.lua'
 local mp = require'msgpack'
 local simple_ipc = require'simple_ipc'
 local rep = simple_ipc.new_replier'test'
+local util = require'util'
 
 -- TODO: Require all necessary modules
 require'vcm'
--- TODO: perform each request inside of a coroutine, so that the main thread does not die
 
 while true do
 	repeat
 		request, has_more = rep:receive()
-		--print('Request: ', request, has_more)
-    reply = assert(loadstring(request))()
+    local rpc_tbl = mp.unpack(request)
+		
+    --util.ptable(rpc_tbl)
+    
+    method = rpc_tbl.call..'_'..rpc_tbl.segment..'_'..rpc_tbl.key
+    func = _G[rpc_tbl.memory][method]
+    
+    --[[
+    print('method',method,func)
+    if type(rpc_tbl.val)=='table' then
+      util.ptable(rpc_tbl.val)
+    else
+      print(rpc_tbl.val)
+    end
+    --]]
+    
+    -- Use a protected call so that we do not error out on a bad RPC
+    status, reply = pcall(func,rpc_tbl.val)
+    -- TODO: if set ensure rpc_tbl.val, and other checks
+    
+    --print('RPC status',status)
+    
 	until not has_more
 
-	print('Replying',reply)
+	print( util.color('RPC:','yellow'), util.color(method,'green'), reply )
 	local ret = rep:send( mp.pack(reply) )
 	--print('Return',ret)
 end

044191bc1bb9f15fbba735a642a25a27cd89fc21
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-08-26 21:08:25 -0400

    Nicer looking state machine wizard

--- Player/state_wizard.lua
+++ Player/state_wizard.lua
@@ -14,15 +14,6 @@ local function set_broadcast()
   needs_broadcast = true
 end
 
-local function broadcast_states(name)
-  for _,my_fsm in pairs(state_machines) do
-    local cur_st = my_fsm.sm:get_current_state()
-    status[my_fsm._NAME] = cur_st._NAME
-  end
-  -- Broadcast over UDP/TCP/IPC
-  local ret = state_pub_ch:send( mp.pack(status) )
-end
-
 -- TODO: Make coroutines for each FSM
 -- TODO: Or other way of handling state machine failure
 -- Maybe a reset() function in each fsm?
@@ -36,43 +27,51 @@ for _,sm in ipairs(unix.readdir(CWD)) do
   end
 end
 
--- Start the webots routine
+-- Update rate (if not webots)
+local fps = 120
+local us_sleep = 1e6 / fps
+
+-- Start the state machines
 local t0 = Body.get_time()
 local t_debug = t0
 
--- Update rate
-local fps = 100
-local us_sleep = 1e6 / fps
-
 -- Perform inialization
 Body.entry()
-for _,sm in pairs(state_machines) do sm.entry() end
+for _,my_fsm in pairs(state_machines) do
+  my_fsm.entry()
+  local cur_st = my_fsm.sm:get_current_state()
+  status[my_fsm._NAME] = cur_st._NAME
+end
 while true do
   local t = Body.get_time()
   local t_diff = t-t_debug
   
   -- Update each state machine
-  for _,sm in pairs(state_machines) do sm.update() end
+  for _,my_fsm in pairs(state_machines) do
+    local event  = my_fsm.update()
+    local cur_st = my_fsm.sm:get_current_state()
+    local s = {cur_st._NAME,event}
+    status[my_fsm._NAME] = s
+    if event then print(unpack(s)) end
+  end
 
   if needs_broadcast then
     needs_broadcast = false
-    broadcast_states()
-    print()
-    print( util.color('FSM Status','blue') )
-    util.ptable(status)
-    print()
+    -- Broadcast over UDP/TCP/IPC
+    local ret = state_pub_ch:send( mp.pack(status) )
   end
 
-  if t_diff>1 then
-	  print( string.format('Running Time: %d seconds',t-t0) )
-    t_debug = t
-  end
-  -- NOTE: Body is the main update, so it must not fail
+  -- Update the body (mostly needed for webots)
 	Body.update()
   
   -- Sleep a bit if not webots
   if not IS_WEBOTS then unix.usleep(us_sleep) end
   
 end
-
 Body.exit()
+for _,my_fsm in pairs(state_machines) do
+  my_fsm.exit()
+  local cur_st = my_fsm.sm:get_current_state()
+  status[my_fsm._NAME] = cur_st._NAME
+  local ret = state_pub_ch:send( mp.pack(status) )
+end

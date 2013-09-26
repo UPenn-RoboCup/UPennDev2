c29cf58d140143ef16b85d87f9dcd4cfaf2b7783
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-09-26 19:10:10 -0400

    local write func

--- Util/libMicrostrain.lua
+++ Util/libMicrostrain.lua
@@ -6,211 +6,11 @@ local libMicrostrain = {}
 --local MicrostrainPacket = require'MicrostrainPacket'
 local stty = require'stty'
 local unix = require'unix'
-local tcp = require'tcp'
-
---------------------
--- libMicrostrain constants
-local HOKUYO_SCAN_REGULAR = 0
-local HOKUYO_2DIGITS = 2
-local HOKUYO_3DIGITS = 3
 
 -- Reading properties
-local N_SCAN_BYTES = 3372
 local TIMEOUT = 0.05 -- (uses select)
 
--------------------------
--- Write a command to a microstrain
-local write_command = function(fd, cmd, expected_response)
-	-- Should get one of these... '00', '02', '03', '04'
-	expected_response = expected_response or '00'
-  local response_timeout = 2
-  
-	--------------------
-	-- Write the command
-	local response = ''
-	local ret = unix.write(fd, cmd)
-  local t_write = unix.time()
-  
-  -- Attempt to find the correct response
-  local iCmd = nil
-	while not iCmd do
-    
-		-- Read from the Microstrain
-		-- Wait for the response
-    local status, ready = unix.select( {fd}, TIMEOUT )
-    -- Check the timeout
-    if not status then return nil end
-    if status>0 then
-      local response_buf = unix.read(fd)
-      response = response..response_buf
-      iCmd = response:find(cmd)
-    end
-    
-		-- Timeout if not finding the response in time
-		local t_diff = unix.time() - t_write
-    if t_diff>response_timeout then
-      -- TODO: DEBUG should be error or print
-  		--DEBUG( string.format('Response timeout for command (%s)',cmd) )
-      return nil
-    end
-    
-    end -- not iCmd
-	--------------------
-
-	--------------------
-	-- Check the correctness of the response
-	-- Just checking the last byte for now
-	-- Filter to only the response
-	response = response:sub(iCmd+#cmd)
-	-- Last byte is 10 (\n)
-	if response:byte(#response)~=10 then
-		--DEBUG(string.format('Bad end of response!'))
-		return nil
-	end
-	
-	--------------------
-
-	--------------------
-	-- Check the response
-  if response:sub(1,2)~=expected_response then
-    --DEBUG(string.format('Cmd (%s)Expected response (%s) but got (%s)', 
-    --cmd, expected_response, response:sub(1,2) ))
-    return nil
-  end
-	--------------------
-
-	return response
-end
-
--------------------------
--- Form the stream command
--- TODO range checks and more types support
-local create_scan_request = 
-function(scan_start, scan_end, scan_skip, encoding, scan_type, num_scans)
-local request = nil
-local base_request = string.format(
-"%04d%04d%02x0%02d\n", scan_start, scan_end, scan_skip, num_scans
-);
-if num_scans==0 then
-  if scan_type == HOKUYO_SCAN_REGULAR then
-    if encoding == HOKUYO_3DIGITS then
-      request = 'MD'..base_request
-      elseif encoding == HOKUYO_2DIGITS then
-        request = 'MS'..base_request
-      end
-    end
-  end
-  assert(request,'Invalid character encoding')
-  return request
-end
-
----------------------------
--- Grab the scan from the buffer
-local get_scan = function(self)
-	local scan_str = ''
-	while true do
-		local status, ready = unix.select({self.fd},TIMEOUT)
-    if not status then return nil end
-		local scan_buf = unix.read( self.fd, N_SCAN_BYTES )
-		if scan_buf then
-			scan_str = scan_str..scan_buf
-      if #scan_str==0 then
-        local idx = scan_buf:find('99b')
-        if idx then scan_str = scan_buf:sub(idx) end
-      else
-      	scan_str = scan_str..scan_buf -- Append it to the scan string
-      end
-      if #scan_str>=N_SCAN_BYTES then
-				break
-			end
-		end
-	end
-	return MicrostrainPacket.parse(scan_str)
-end
-
-
-
--------------------------
--- Turn off data streaming
-local stream_off = function(self)
-	local ntries = 3
-	local stop_delay = 5e4
-	for i=1,ntries do
-		local resp = write_command( self.fd, 'QT\n' )
-		if resp then
-			return true
-		end
-		-- TODO: Yield the delay time between stop attempts
-		unix.usleep(stop_delay)
-	end
-  return false
-end
-
--------------------------
--- Turn on data streaming
-local stream_on = function(self)
-
-	-- Ensure that we stream_off before we stream_on again
-	stream_off(self)
-
-	local ret = write_command( self.fd, 'BM\n' )
-
-	-- scan_start, scan_end, scan_skip, encoding, scan_type, num_scans 
-	-- TODO: Check encoding types
-	local scan_req = create_scan_request(0, 1080, 1, 3, 0, 0);
-	-- TODO: Does the scan request expect a return, 
-	-- or just the actual streaming data?
-	ret = write_command( self.fd, scan_req )
-
-end
-
--------------------------
--- Get sensor parameters from the microstrain
-local get_sensor_params = function(self)
-	local res = write_command( self.fd, 'PP\n' )
-
-	local sensor_params = {}
-	local params = MicrostrainPacket.parse_info(res, 8)
-  if not params then return nil end
-
-	sensor_params.model = params[1]
-	sensor_params.min_distance = tonumber(params[2])
-	sensor_params.max_distance = tonumber(params[3])
-	sensor_params.angular_resolution = tonumber(params[4])
-	sensor_params.min_angle_count = tonumber(params[5])
-	sensor_params.max_angle_count = tonumber(params[6])
-	sensor_params.frint_angle_count = tonumber(params[7])
-	sensor_params.scan_rate = tonumber(params[8])
-
-	return sensor_params
-end
-
--------------------------
--- Get Microstrain sensor information
-local get_sensor_info = function(self)
-	local cmd = 'VV\n'
-	local res = write_command( self.fd, cmd )
-
-	local sensor_info = {}
-	local info = MicrostrainPacket.parse_info(res, 5)
-  
-	if not info then return nil end
-  
-	sensor_info.vender = info[1]
-	sensor_info.product = info[2]
-	sensor_info.firmware = info[3]
-	sensor_info.protocol = info[4]
-	sensor_info.serial_number = info[5]
-	return sensor_info
-end
-
----------------------------
--- Set the Microstrain baud rate
-local set_baudrate = function(self, baud)
-	local cmd = 'SS'..string.format("%6d", baud)..'\n';
-	local res = write_command(self.fd, cmd, '04')
-end
-
+-- Checksum formation (slow, but speed is unneeded)
 local function generate_packet(byte_array)
   local checksum_byte1, checksum_byte2 = 0, 0
   for _,val in ipairs(byte_array) do
@@ -220,6 +20,21 @@ local function generate_packet(byte_array)
   table.insert(byte_array,checksum_byte1)
   table.insert(byte_array,checksum_byte2)
   return string.char( unpack(byte_array) )
+end
+
+-------------------------
+-- Write a command to a microstrain
+local function write_command(fd,cmd)
+  local str = generate_packet(cmd)
+  local ret = unix.write(fd,str)
+  assert(ret==#str,'Bad save write!')
+  local fd_id = unix.select( {fd}, TIMEOUT )
+  assert(fd_id==1,'Timeout!')
+  local res = unix.read(fd)
+  assert(res,'No data!')
+  local response = {res:byte(1,-1)}
+  --for i,b in ipairs(response) do print( string.format('%d: %02X',i,b) ) end
+  return response
 end
 
 ---------------------------
@@ -335,8 +150,6 @@ libMicrostrain.new_microstrain = function(ttyname, ttybaud )
 
   -- TODO: Use NAV or not?? This is the EKF filtered stuff... quaternion format...
 
-
-
 	-----------
 	-- Begin the Microstrain object
 	local obj = {}
@@ -352,29 +165,16 @@ libMicrostrain.new_microstrain = function(ttyname, ttybaud )
 	end
   obj.ahrs_on = function(self)
     -- Turn on the ahrs stream
-    local stream_enable = { 0x75, 0x65, 0x0C, 0x05, 0x05, 0x11, 0x01, 0x01, 0x01 }
-    local stream_enable_cmd = generate_packet(stream_enable)
-    ret = unix.write(self.fd,stream_enable_cmd)
-    assert(ret==#stream_enable_cmd,'Bad save write!')
-    fd_id = unix.select( {self.fd}, TIMEOUT )
-    assert(fd_id==1,'Timeout!')
-    res = unix.read(fd)
-    assert(res,'No data!')
-    response = {res:byte(1,-1)}
-    for i,b in ipairs(response) do print( string.format('%d: %02X',i,b) ) end
+    local response = write_command(fd,{
+      0x75, 0x65, 0x0C, 0x05, 0x05, 0x11, 0x01, 0x01, 0x01
+    })
   end
   obj.ahrs_off = function(self)
     -- Turn off the ahrs stream
-    local stream_disable = { 0x75, 0x65, 0x0C, 0x05, 0x05, 0x11, 0x01, 0x01, 0x00 }
-    local stream_disable_cmd = generate_packet(stream_disable)
-    ret = unix.write(self.fd,stream_disable_cmd)
-    assert(ret==#stream_disable_cmd,'Bad save write!')
-    fd_id = unix.select( {self.fd}, TIMEOUT )
-    assert(fd_id==1,'Timeout!')
-    res = unix.read(fd)
-    assert(res,'No data!')
-    response = {res:byte(1,-1)}
-    for i,b in ipairs(response) do print( string.format('%d: %02X',i,b) ) end
+    local response = write_command(fd,{
+      0x75, 0x65, 0x0C, 0x05, 0x05, 0x11, 0x01, 0x01, 0x00
+    })
+    --for i,b in ipairs(response) do print( string.format('%d: %02X',i,b) ) end
   end
 
 	-----------

c94c67fcbfc5e949a232312ce26d515c186115e4
Stephen McGill <smcgill3@seas.upenn.edu>
2013-12-15 16:52:13 -0500

    make the audio much smaller

--- Player/audio_wizard.lua
+++ Player/audio_wizard.lua
@@ -8,13 +8,12 @@ local audio_tcp_ch = simple_ipc.new_publisher(Config.net.audio,false,'*')
 while true do
 
 if(hcm.get_audio_request()==1) then
---  os.execute('sh get_audio.sh')
-  os.execute('arecord -f S16_LE -c2 -d 10 -D hw:1,0 -t raw | lame -r -s 16 - /tmp/robot.mp3')
---arecord -f S16_LE -c2 -d 10 -D hw:1,0 /tmp/robot.wav')
+  --os.execute'arecord -f S16_LE -c2 -d 10 -D hw:1,0 -t raw | lame -r -s 16 -b 8 -h - /tmp/robot.mp3'
+  os.execute'arecord -f S16_LE -c2 -d 7 -D hw:1,0 -t raw | lame -r -s 16 -b 8 -h - /tmp/robot.mp3'
   local f = io.open('/tmp/robot.mp3')
   local audio = f:read('*all')
   local ret =audio_tcp_ch:send({mp.pack{t=0},audio})
-print('ret',ret,#audio)
+  print('Send',#audio,'bytes')
   -- Suppress
   hcm.set_audio_request(0)
 end

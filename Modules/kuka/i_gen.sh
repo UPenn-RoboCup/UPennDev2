2986eb1e03a63341c9cae175559b28ba3c4b1bc0
Stephen McGill <smcgill3@seas.upenn.edu>
2014-01-13 12:04:52 +0100

    bugfix kuka

--- Modules/kuka/i_gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for name in `ls /home/youbot/youbot_driver/youbot/*.hpp`
-do
-	echo '#include "youbot/$name"' >> kuka.i
-done
--- Modules/kuka/lua_kuka.cpp
+++ Modules/kuka/lua_kuka.cpp
@@ -2,6 +2,11 @@
 #include "youbot/YouBotBase.hpp"
 #include "youbot/YouBotManipulator.hpp"
 
+#define YOUBOT_CONFIGURATIONS_DIR "/home/youbot/youbot_driver/config"
+
+// Namespace
+using namespace youbot;
+
 // Important globals
 YouBotBase* ybBase = NULL;
 YouBotManipulator* ybArm = NULL;
@@ -10,15 +15,15 @@ YouBotManipulator* ybArm = NULL;
 static int lua_init_base(lua_State *L) {
 	try {
 		ybBase = new YouBotBase("youbot-base", YOUBOT_CONFIGURATIONS_DIR);
-    // Why the commutation?
+		// Why the commutation?
 		ybBase->doJointCommutation();
 	} catch (std::exception& e) {
 		return luaL_error(L, e.what() );
 	}
-  
-  // Return true
-  lua_pushboolean(L,1);
-  return 1;
+
+	// Return true
+	lua_pushboolean(L,1);
+	return 1;
 }
 
 // Initialize the arm module
@@ -26,20 +31,20 @@ static int lua_init_arm(lua_State *L) {
 	try {
 		ybArm = new YouBotManipulator("youbot-manipulator", YOUBOT_CONFIGURATIONS_DIR);
 		ybArm->doJointCommutation();
-    //ybArm->calibrateManipulator();
+		//ybArm->calibrateManipulator();
 	} catch (std::exception& e) {
 		return luaL_error(L, e.what() );
 	}
-  
-  // Return true
-  lua_pushboolean(L,1);
-  return 1;
+
+	// Return true
+	lua_pushboolean(L,1);
+	return 1;
 }
 
 static const struct luaL_reg kuka_lib [] = {
-  {"init_base", lua_init_base},
-  {"init_arm", lua_init_arm},
-  {NULL, NULL}
+	{"init_base", lua_init_base},
+	{"init_arm", lua_init_arm},
+	{NULL, NULL}
 };
 
 #ifdef __cplusplus
@@ -49,7 +54,7 @@ int luaopen_kuka (lua_State *L) {
 #if LUA_VERSION_NUM == 502
 	luaL_newlib(L, kuka_lib);
 #else
-  luaL_register(L, "kuka", kuka_lib);
+	luaL_register(L, "kuka", kuka_lib);
 #endif
-  return 1;
+	return 1;
 }

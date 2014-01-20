5ba1baf446e2062810e01c2e4ac9bddd9a8d1686
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-01-20 15:31:11 +0100

    Somewhat cleaner

--- test_kuka.lua
+++ test_kuka.lua
@@ -1,45 +1,39 @@
 dofile'fiddle.lua'
 local K = Body.Kinematics
---local T = require'Transform'
 local T = require'libTransform'
 local getch = require'getch'
 
-
 -- Translate the end effector
+local ds = 0.005
 local trans_arm = {
-  u = T.trans(0,0,.01),
-  m = T.trans(0,0,-.01),
-  i = T.trans(.01,0,0),
-  [','] = T.trans(-.01,0,0),
-  j = T.trans(0,.01,0),
-  l = T.trans(0,-.01,0),
+  u = T.trans(0,0,ds),
+  m = T.trans(0,0,-ds),
+  i = T.trans(ds,0,0),
+  [','] = T.trans(-ds,0,0),
+  j = T.trans(0,ds,0),
+  l = T.trans(0,-ds,0),
 }
 
 -- Rotate (locally) the end effector
+local dr = 0.05
 local rot_arm = {
-  [']'] = T.rotY(.1),
-  ['['] = T.rotY(-.1),
-  ["'"] = T.rotZ(.1),
-  [';'] = T.rotZ(-.1),
+  [']'] = T.rotY(dr),
+  ['['] = T.rotY(-dr),
+  ["'"] = T.rotZ(dr),
+  [';'] = T.rotZ(-dr),
 }
 
+local dv = 0.01
 local move_base = {
-  w = vector.new{.01,0,0},
-  x = vector.new{-.01,0,0},
-  a = vector.new{0,.01,0},
-  d = vector.new{0,-.01,0},
+  w = vector.new{dv,0,0},
+  x = vector.new{-dv,0,0},
+  a = vector.new{0,dv,0},
+  d = vector.new{0,-dv,0},
   q = vector.new{0,0,.01},
   e = vector.new{0,0,-.01},
 }
 
-local direct_arm = {
-  [1] = vector.new{.01,0,0,0,0},
-  [2] = vector.new{.01,0,0,0,0},
-  [3] = vector.new{.01,0,0,0,0},
-  [4] = vector.new{.01,0,0,0,0},
-  [5] = vector.new{.01,0,0,0,0},
-}
-
+local dq = 0.05
 local delta_q = vector.zeros(5)
 local function process_keycode(keycode,t_diff)
   local char = string.char(keycode)
@@ -89,7 +83,7 @@ local function process_keycode(keycode,t_diff)
   local num = tonumber(char)
   if num then
     delta_q = vector.zeros(5)
-    delta_q[num] = .1
+    delta_q[num] = dq
     return
   elseif char=='=' then
     local qArm = Body.get_position()
@@ -120,21 +114,16 @@ local function process_keycode(keycode,t_diff)
   
 end
 
-------------
 -- Start processing
 io.flush()
 local t0 = unix.time()
 while true do
-  
   -- Grab the keyboard character
   local keycode = getch.block()
-  
   -- Measure the timing
   local t = unix.time()
   local t_diff = t - t0
   t0 = t
-  
   -- Process the character
   process_keycode(keycode,t_diff)
-    
 end

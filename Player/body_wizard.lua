e204c3a5e4997f79a5d25f208b30d3fe04da19bf
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-01-22 22:09:07 +0100

    External state wizard based on the pulse of the body. Now we can restart state_wizard whenever. Must just add set_state

--- Player/body_wizard.lua
+++ Player/body_wizard.lua
@@ -5,14 +5,25 @@
 ---------------------------------
 dofile'include.lua'
 local Body = require'Body'
+local simple_ipc = require'simple_ipc'
+local mp = require'msgpack'
 require'unix'
 
-Body.entry()
+-- Send a Body pulse after each update
+local pulse_ch = simple_ipc.new_publisher'pulse'
 
+-- Get the timing
 local t_wait = Body.update_cycle
 local t_wait_us = t_wait*1e6
+
+Body.entry()
 local t_last = Body.get_time()
 
+-- Send the entry pulse
+local pulse_tbl = {mode='entry',t=t_last}
+pulse_ch:send(mp.pack(pulse_tbl))
+
+pulse_tbl.mode='update'
 while true do
   local t = Body.get_time()
   local t_diff = t-t_last
@@ -22,11 +33,18 @@ while true do
     Body.update()
     -- Webots must always update...
     if not IS_WEBOTS then unix.usleep(t_wait_us) end
+		-- Send a pulse after each update, so that the state wizard may proceed
+		pulse_tbl.t = t
+		pulse_ch:send(mp.pack(pulse_tbl))
   else
     if not IS_WEBOTS then print('NOP CYCLE') end
     Body.nop()
+		-- No pulse sent, since no update
   end
 
 end
 
 Body.exit()
+pulse_tbl.t = Body.get_time()
+pulse_tbl.mode='exit'
+pulse_ch:send(mp.pack(pulse_tbl))
--- Player/state_wizard.lua
+++ Player/state_wizard.lua
@@ -17,7 +17,6 @@ local state_pub_ch
 if broadcast_en then
   state_pub_ch = simple_ipc.new_publisher(Config.net.state)
 end
---local pulse_ch = simple_ipc.new_publisher'pulse'
 
 require'gcm'
 require'wcm'
@@ -49,10 +48,6 @@ for _,sm in ipairs(Config.fsm.enabled) do
   print( util.color('FSM | Loaded','yellow'),sm)
 end
 
--- Update rate (if not webots)
-local fps = 120
-local us_sleep = 1e6 / fps
-
 -- Start the state machines
 local t0 = Body.get_time()
 local t_debug = t0
@@ -60,7 +55,6 @@ local t_debug = t0
 --------------------
 -- Clean Shutdown function
 function shutdown()
-  Body.exit()
   print'Shutting down the state machines...'
   for _,my_fsm in pairs(state_machines) do
     my_fsm.exit()
@@ -73,28 +67,34 @@ signal.signal("SIGINT", shutdown)
 signal.signal("SIGTERM", shutdown)
 
 -- Perform inialization
-Body.entry()
-for _,my_fsm in pairs(state_machines) do
-  my_fsm.entry()
-  local cur_state = my_fsm.sm:get_current_state()
-  local cur_state_name = cur_state._NAME
-  local s = {cur_state_name,nil}
-  status[my_fsm._NAME] = s
-  if broadcast_en then
-    local ret = state_pub_ch:send( mp.pack(status) )
-  end
+local entry = function()
+	for _,my_fsm in pairs(state_machines) do
+		my_fsm.entry()
+		local cur_state = my_fsm.sm:get_current_state()
+		local cur_state_name = cur_state._NAME
+		local s = {cur_state_name,nil}
+		status[my_fsm._NAME] = s
+		if broadcast_en then
+			local ret = state_pub_ch:send( mp.pack(status) )
+		end
+	end
 end
 
--- Joint reading... seems that we are always reading????
---local all_joint_read = vector.ones(#jcm.get_read_position())
+local exit = function()
+	for _,my_fsm in pairs(state_machines) do
+		my_fsm.exit()
+		local cur_state = my_fsm.sm:get_current_state()
+		local cur_state_name = cur_state._NAME
+		local s = {cur_state_name,nil}
+		status[my_fsm._NAME] = s
+		local ret = state_pub_ch:send( mp.pack(status) )
+	end
+end
 
--- Pulse
-local pulse_tbl = {t=0}
-
-while true do
-  local t = Body.get_time()
-  
-  -- Update each state machine
+local update = function(pulse)
+	local t = Body.get_time()
+	print('State Update from Body pulse',pulse.t,t)
+	-- Update each state machine
   for _,my_fsm in pairs(state_machines) do local event = my_fsm.update() end
 
   -- Broadcast state changes... why...? Need the stage as well?
@@ -103,36 +103,33 @@ while true do
     -- Broadcast over UDP/TCP/IPC
     local ret = state_pub_ch:send( mp.pack(status) )
   end
-  
-  -- Always read from all the motors
-  --jcm.set_read_position(all_joint_read)
-  
-  -- Update the body (mostly needed for webots)
-	Body.update()
-  
-  -- Send the tick tock at the end of a cycle
-  pulse_tbl.t = t
-  --pulse_ch:send(mp.pack(pulse_tbl))
-  
-  -- Sleep a bit if not webots
-  if not IS_WEBOTS then
-    local t_loop = unix.time()-t
-    local t_sleep = us_sleep-t_loop*1e6
-    if t_sleep>0 then unix.usleep(t_sleep) end
---    print((1/t_loop))
-
-  end
-
-
-  local gyro = Body.get_sensor_gyro()
-  
 end
-Body.exit()
-for _,my_fsm in pairs(state_machines) do
-  my_fsm.exit()
-  local cur_state = my_fsm.sm:get_current_state()
-  local cur_state_name = cur_state._NAME
-  local s = {cur_state_name,nil}
-  status[my_fsm._NAME] = s
-  local ret = state_pub_ch:send( mp.pack(status) )
+
+-- Listen for pulses
+local wait_channels = {}
+
+local pulse_ch = simple_ipc.new_subscriber'pulse'
+print'Receiving Body pulse'
+pulse_ch.callback = function(sh)
+	local ch = wait_channels.lut[sh]
+	local ekg, has_more
+	repeat
+		-- Do not block
+    data, has_more = ch:receive(true)
+		--print('Pulse Data',type(data))
+		if data then ekg = mp.unpack(data) end
+	until not data
+	-- Call the update
+	update(ekg)
 end
+
+-- Make the poller
+table.insert(wait_channels, pulse_ch)
+local channel_timeout = 2 * Body.update_cycle * 1e3
+local channel_poll = simple_ipc.wait_on_channels( wait_channels );
+
+entry()
+while true do
+  local npoll = channel_poll:poll(channel_timeout)
+end
+exit()

96e3274242d6cf8ab1131d94fc29790240407c33
Stephen McGill <smcgill3@seas.upenn.edu>
2014-01-22 14:12:29 +0100

    Fix bug on linux

--- fiddle.py
+++ fiddle.py
@@ -1,3 +1,11 @@
+# Very important for linux, via:
+# http://stackoverflow.com/questions/8361437/linker-error-lunatic-python-lua-requiresocket-undefined-symbol-lua-getme
+import sys
+
+if sys.platform != "darwin":
+	import DLFCN
+	sys.setdlopenflags(DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL)
+
 # Import luantic-python
 import lua
 # Have all the framework files/Modules available

513aa8fdf5378f90de53f834c8d228f016210040
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-01-30 12:08:27 +0100

    Somewhat nicer looking

--- Player/BodyFSM/bodyGrab.lua
+++ Player/BodyFSM/bodyGrab.lua
@@ -9,32 +9,54 @@ local vector = require'vector'
 --local timeout = 10.0
 local t_entry, t_update, t_exit
 
+
+
+-- Min/Switch pitch/Max range
+local min_r, mid_r, max_r = .25, .3, .39
+--local object_coord = vector.new{.275,.275,-.175} --max
+--local object_coord = vector.new{.18,.18,-.175}
+local object_coord = vector.new{.27,.27,-.175}
+--local object_coord = vector.new{.25,0,-.175}
+local object_r = math.sqrt(object_coord[1]^2+object_coord[2]^2)
+assert(object_r>min_r and object_r<max_r,"Cannot reach!")
+
 local stages = {}
 local function init_stages()
 	-- Open on the right side
 	table.insert(stages,
-		{tr=T.rotZ(-math.pi/2)*T.trans(.2,0,0)*T.rotY(math.pi/1.01),
+		{tr=T.rotZ(math.pi/1.01)*T.trans(.2,0,0)*T.rotY(math.pi/1.01),
 			dt=2,gr=0.0115})
 	-- To the right side
 	table.insert(stages,
-		{tr=T.rotZ(-math.pi/2)*T.trans(.2,0,0)*T.rotY(math.pi/1.01),dt=2})
+		{tr=T.rotZ(math.pi/1.01)*T.trans(.2,0,0)*T.rotY(math.pi/1.01),dt=2})
+
+
 	-- Left side back up
 	table.insert(stages,
-		{tr=T.rotZ(math.pi/2)*T.trans(.2,0,0)*T.rotY(math.pi/1.01),dt=2})
+		{tr=T.trans(object_coord[1],object_coord[2],-.1)
+			*T.rotY(object_r<mid_r and math.pi/1.01 or math.pi/1.3),
+			dt=2})
+
 	-- Left side down w/ grip
 	table.insert(stages,
-		{tr=T.rotZ(math.pi/2)*T.trans(.3,0,-.2)*T.rotY(math.pi/1.01),
+		{tr=T.trans(unpack(object_coord))
+			*T.rotY(object_r<mid_r and math.pi/1.01 or math.pi/1.3),
 			dt=2,gr=0.0025})
 	-- Left side down
 	table.insert(stages,
-		{tr=T.rotZ(math.pi/2)*T.trans(.3,0,-.2)*T.rotY(math.pi/1.01),dt=2})
+		{tr=T.trans(unpack(object_coord))
+			*T.rotY(object_r<mid_r and math.pi/1.01 or math.pi/1.3),
+			dt=2})
+
 	-- Left side
 	table.insert(stages,
-		{tr=T.rotZ(math.pi/2)*T.trans(.3,0,.2)*T.rotY(math.pi/2),
+		{tr=T.trans(object_coord[1],object_coord[2],.2)*T.rotY(math.pi/2),
 			dt=2,gr=0.0115})
+	--[[
 	-- Low
 	table.insert(stages,
 		{tr=T.trans(.3,0,.2)*T.rotY(math.pi/2),dt=2})
+	--]]
 	-- High
 	table.insert(stages,
 		{tr=T.trans(.3,0,.125)*T.rotY(math.pi/2),dt=2})
@@ -67,7 +89,7 @@ function state.entry()
 end
 
 function state.update()
-  print(state._NAME..' Update', id_stage )
+  --print(state._NAME..' Update' )
   -- Get the time of update
   local t  = Body.get_time()
   local dt = t - t_update

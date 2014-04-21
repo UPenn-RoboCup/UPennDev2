64cde5de5eea24b1b5fa9c320658b5f6a432a892
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-04-21 18:09:10 +0200

    Get to a point to run Radon

--- Test/test_line_detect.lua
+++ Test/test_line_detect.lua
@@ -14,6 +14,7 @@ local meta, yuyv
 for i,m,r in d do
 	meta = m
 	yuyv = r
+	if i>#metadata/2 then break end
 end
 
 -- Extract the Y-plane
@@ -30,6 +31,33 @@ for i=1,n do
 	yi = yi+2
 end
 
+-- Downsampled
+local y_img2 = torch.IntTensor(meta.w/2,meta.h/2)
+local y_img_b2 = torch.ByteTensor(meta.w/2,meta.h/2)
+local y_plane2 = torch.IntTensor(y_img)
+local y_img_plane2 = torch.ByteTensor(y_img_b2)
+local n2 = y_img2:nElement()
+y_plane2:resizeAs(torch.IntTensor(n2))
+y_img_plane2:resizeAs(torch.ByteTensor(n2))
+local yi = 0
+local stride = meta.w * 4
+local col = 1
+for i=1,n2 do
+	local y0 = yuyv[yi]
+	local u  = yuyv[yi+1]
+	local y1 = yuyv[yi+2]
+	local v  = yuyv[yi+3]
+	y_plane2[i] = y0
+	y_img_plane2[i] = y0
+	yi = yi+4
+	if col==meta.w then
+		yi = yi + stride
+		col = 1
+	else
+		col = col + 1
+	end
+end
+
 -- Attempt conv2
 k = torch.IntTensor({
 	{0, 0, 1, 0, 0,},
@@ -42,12 +70,10 @@ c = torch.conv2(y_img,k,'V')
 print("Conv2",c:size(1),c:size(2))
 
 -- Now let's save this to a JPEG for viewing
-local y_img_byte = torch.ByteTensor(meta.w,meta.h)
-y_img_byte:copy(y_img)
 local jpeg = require'jpeg'
 c_gray = jpeg.compressor('gray')
-local str = c_gray:compress(y_img_byte)
-local f_y = io.open('y.jpg','w')
+local str = c_gray:compress(y_img_b2)
+local f_y = io.open('y.jpeg','w')
 f_y:write(str)
 f_y:close()
 
@@ -56,6 +82,11 @@ f_y.binary(f_y)
 f_y:writeInt(y_img:storage())
 f_y:close()
 
+print('subdim',y_img2:size(1),y_img2:size(2))
+f_y = torch.DiskFile('y2.raw', 'w')
+f_y.binary(f_y)
+f_y:writeInt(y_img2:storage())
+f_y:close()
 
 f_y = torch.DiskFile('c.raw', 'w')
 f_y.binary(f_y)

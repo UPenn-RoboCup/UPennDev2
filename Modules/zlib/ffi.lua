034717ffbe048c86bc1aba3bb5cb02272452587f
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-04-24 08:32:03 -0400

    another function

--- Modules/zlib/ffi.lua
+++ Modules/zlib/ffi.lua
@@ -29,4 +29,17 @@ function zlib.compress(txt)
   return ffi.string(buf, buflen[0])
 end
 
+function zlib.compress_cdata (ptr, len)
+  local n = z.compressBound(len)
+  if n>nBound then
+    nBound = n
+    buf = ffi.new("uint8_t[?]", nBound)
+    buflen = ffi.new("unsigned long[1]", n)
+  end
+  -- Just make fast, so use 1 for level
+  local res = z.compress2(buf, buflen, ffi.cast('uint8_t*',ptr), len, 1)
+  assert(res == 0)
+  return ffi.string(buf, buflen[0])
+end
+
 return zlib

6d0547be4a046e8885de805be73fcc08a7b7aa90
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-04-24 11:53:31 -0400

    Show 2 cameras at once

--- Tools/Matlab/show_labelA.m
+++ Tools/Matlab/show_labelA.m
@@ -1,52 +1,63 @@
 clear all;
-% raw labelA
-%fid = fopen('Data/labelA.raw');A = fread(fid,Inf,'*uint8');fclose(fid);
-%A = reshape(A,[320,240])'; figure(1); imagesc(A);
-% zlib labelA
-fid = fopen('Data/labelA_z.raw');Az = fread(fid,Inf,'*uint8');fclose(fid);
-Az = reshape(zlibUncompress(Az),[320,240])';
-figure(2); i_az = image(Az);
+% Colormap for the labeled image
 cbk=[0 0 0];cr=[1 0 0];cg=[0 1 0];cb=[0 0 1];cy=[1 1 0];cw=[1 1 1];
-cmap=[cbk;cr;cy;cy;cb;cb;cb;cb;cg;cg;cg;cg;cg;cg;cg;cg;cw];
+cmap = [cbk;cr;cy;cy;cb;cb;cb;cb;cg;cg;cg;cg;cg;cg;cg;cg;cw];
+figure(1);
+% LabelA
+subplot(2,2,1);
+i_az_t = image(zeros(240,320));
 colormap(cmap);
-% JPEG yuyv
-fid = fopen('Data/labelA.jpeg');
-jimg = fread(fid,Inf,'*uint8');fclose(fid);
-jimg = djpeg(jimg);
-figure(3); imagesc(jimg);
+% yuyv
+subplot(2,2,2);
+i_j_t = image(zeros(480,640));
+% LabelA (bottom)
+subplot(2,2,3);
+i_az_b = image(zeros(120,160));
+colormap(cmap);
+% yuyv (bottom)
+subplot(2,2,4);
+i_j_b = image(zeros(240,320));
+%
 drawnow;
 
-%{
-% Grab the YUYV logged information
-fid = fopen('Data/yuyv_r_09.17.2009.01.48.45.log');
-yuyvMontage = fread(fid,Inf,'*uint32');
-fclose(fid);
-% Metadata
-fid = fopen('Data/yuyv_m_09.17.2009.01.48.45.log');
-yuyvMeta = fread(fid,Inf,'*uint8');
-fclose(fid);
-yuyvMeta = msgpack('unpacker',yuyvMeta,'uint8');
-% Setup the colortable item
-n_img = numel(yuyvMontage) / yuyvMeta{1}.w / yuyvMeta{1}.h * 2;
-yuyvMontage = reshape(yuyvMontage,[yuyvMeta{1}.w/2,yuyvMeta{1}.h,1,n_img]);
-save('test.mat','yuyvMontage');
-%}
-
 % Add the monitor
-head_zlib_fd  = udp_recv('new', 54321);
-s_head_zlib = zmq( 'fd', head_zlib_fd );
+top_fd  = udp_recv('new', 33333);
+s_top = zmq( 'fd', top_fd );
+bottom_fd  = udp_recv('new', 33334);
+s_bottom = zmq( 'fd', bottom_fd );
+% Cameras
+cams = {};
+cams{1} = {};
+cams{1}.labelA = i_az_t;
+cams{1}.yuyv = i_j_t;
+cams{1}.fd = top_fd;
+%
+cams{2} = {};
+cams{2}.labelA = i_az_b;
+cams{2}.yuyv = i_j_b;
+cams{2}.fd = bottom_fd;
+
 while 1
     % 1 second timeout
     idx = zmq('poll',1000);
-    if numel(idx)>0
-        [s_data,has_more] = zmq( 'receive', s_head_zlib );
-        while udp_recv('getQueueSize',head_zlib_fd) > 0
-            udp_data = udp_recv('receive',head_zlib_fd);
+    for s=1:numel(idx)
+        s_idx = idx(s);
+        s_idx_m = s_idx + 1;
+        cam = cams{s_idx_m};
+        [s_data, has_more] = zmq( 'receive', s_idx );
+        while udp_recv('getQueueSize',cam.fd) > 0
+            udp_data = udp_recv('receive',cam.fd);
         end
         [metadata,offset] = msgpack('unpack',udp_data);
-        lA_z = udp_data(offset+1:end); % This must be uint8
-        Az = reshape(zlibUncompress(lA_z),[metadata.w,metadata.h])';
-        set(i_az,'Cdata', Az);
+        raw = udp_data(offset+1:end); % This must be uint8
+        compr_t = char(metadata.c);
+        if strcmp(compr_t,'jpeg')
+            jimg = djpeg(raw);
+            set(cam.yuyv,'Cdata', jimg);
+        elseif strcmp(compr_t,'zlib')
+            Az = reshape(zlibUncompress(raw),[metadata.w,metadata.h])';
+            set(cam.labelA,'Cdata', Az);
+        end
         drawnow;
     end
 end
---
+++ Tools/Matlab/show_labelA_bak.m
@@ -0,0 +1,59 @@
+clear all;
+% raw labelA
+%fid = fopen('Data/labelA.raw');A = fread(fid,Inf,'*uint8');fclose(fid);
+%A = reshape(A,[320,240])'; figure(1); imagesc(A);
+% zlib labelA
+fid = fopen('Data/labelA_z.raw');Az = fread(fid,Inf,'*uint8');fclose(fid);
+Az = reshape(zlibUncompress(Az),[320,240])';
+figure(2); i_az = image(Az);
+cbk=[0 0 0];cr=[1 0 0];cg=[0 1 0];cb=[0 0 1];cy=[1 1 0];cw=[1 1 1];
+cmap=[cbk;cr;cy;cy;cb;cb;cb;cb;cg;cg;cg;cg;cg;cg;cg;cg;cw];
+colormap(cmap);
+% JPEG yuyv
+fid = fopen('Data/labelA.jpeg');
+jimg = fread(fid,Inf,'*uint8');fclose(fid);
+jimg = djpeg(jimg);
+figure(3);
+i_j = image(jimg);
+drawnow;
+
+%{
+% Grab the YUYV logged information
+fid = fopen('Data/yuyv_r_09.17.2009.01.48.45.log');
+yuyvMontage = fread(fid,Inf,'*uint32');
+fclose(fid);
+% Metadata
+fid = fopen('Data/yuyv_m_09.17.2009.01.48.45.log');
+yuyvMeta = fread(fid,Inf,'*uint8');
+fclose(fid);
+yuyvMeta = msgpack('unpacker',yuyvMeta,'uint8');
+% Setup the colortable item
+n_img = numel(yuyvMontage) / yuyvMeta{1}.w / yuyvMeta{1}.h * 2;
+yuyvMontage = reshape(yuyvMontage,[yuyvMeta{1}.w/2,yuyvMeta{1}.h,1,n_img]);
+save('test.mat','yuyvMontage');
+%}
+
+% Add the monitor
+head_zlib_fd  = udp_recv('new', 33333);
+s_head_zlib = zmq( 'fd', head_zlib_fd );
+while 1
+    % 1 second timeout
+    idx = zmq('poll',1000);
+    if numel(idx)>0
+        [s_data,has_more] = zmq( 'receive', s_head_zlib );
+        while udp_recv('getQueueSize',head_zlib_fd) > 0
+            udp_data = udp_recv('receive',head_zlib_fd);
+        end
+        [metadata,offset] = msgpack('unpack',udp_data);
+        raw = udp_data(offset+1:end); % This must be uint8
+        compr_t = char(metadata.c);
+        if strcmp(compr_t,'jpeg')
+            jimg = djpeg(raw);
+            set(i_j,'Cdata', jimg);
+        elseif strcmp(compr_t,'zlib')
+            Az = reshape(zlibUncompress(raw),[metadata.w,metadata.h])';
+            set(i_az,'Cdata', Az);
+        end
+        drawnow;
+    end
+end

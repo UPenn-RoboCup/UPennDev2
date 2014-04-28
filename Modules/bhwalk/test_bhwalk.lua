7dce884b679fd65b35dca9606f53ffe54dbff117
Stephen McGill <smcgill3@seas.upenn.edu>
2014-04-28 16:01:12 +0000

    Add sensor feedback

--- Modules/bhwalk/test_bhwalk.lua
+++ Modules/bhwalk/test_bhwalk.lua
@@ -1,10 +1,21 @@
 bhwalk = require'bhwalk'
 
 for k, v in pairs(bhwalk) do
-  if type(v)=='function' then
-    print(k,v())
-  else
-    print(k,v)
-  end
+  print(k,v)
 end
 
+-- Try to walk
+local nJoint = 22
+local jAngles, jCurrents = {}, {}
+for i=1,nJoint do
+  jAngles[i] = 0
+  jCurrents[i] = 0
+end
+bhwalk.walk_request(10,10,0)
+for i=1,5 do
+  bhwalk.set_sensor_angles(jAngles)
+  bhwalk.set_sensor_currents(jCurrents)
+  bhwalk.update()
+  print("Joints", unpack(bhwalk.get_joint_angles()) )
+  print("Odometry", unpack(bhwalk.get_odometry()) )
+end

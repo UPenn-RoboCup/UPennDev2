c3621c5a5389c58078358e8e84c076227b9ba0eb
Stephen McGill <smcgill3@seas.upenn.edu>
2014-04-28 19:16:14 +0000

    More advanced a bit

--- Modules/bhwalk/test_bhwalk.lua
+++ Modules/bhwalk/test_bhwalk.lua
@@ -1,4 +1,5 @@
 bhwalk = require'bhwalk'
+require'unix'
 
 for k, v in pairs(bhwalk) do
   print(k,v)
@@ -11,11 +12,27 @@ for i=1,nJoint do
   jAngles[i] = 0
   jCurrents[i] = 0
 end
+print('Walk request',bhwalk.get_motion_request())
+
+bhwalk.stand_request()
+print('Walk request',bhwalk.get_motion_request())
 for i=1,5 do
+  bhwalk.update()
+  bhwalk.set_sensor_angles(jAngles)
+  bhwalk.set_sensor_currents(jCurrents)
+  print("Joints", unpack(bhwalk.get_joint_angles()) )
+  print("Odometry", unpack(bhwalk.get_odometry()) )
+  -- 100Hz
+  unix.usleep(1e4)
+end
+
+for i=1,100 do
   bhwalk.walk_request(10,10,0)
   bhwalk.set_sensor_angles(jAngles)
   bhwalk.set_sensor_currents(jCurrents)
+  print('Walk request',bhwalk.get_motion_request())
   bhwalk.update()
   print("Joints", unpack(bhwalk.get_joint_angles()) )
   print("Odometry", unpack(bhwalk.get_odometry()) )
+  unix.usleep(1e4)
 end

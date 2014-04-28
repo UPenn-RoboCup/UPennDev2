3fb19c1a23cd7cc6e2035e3cc9494c642f33960e
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-04-28 09:22:12 -0400

    BHuman Walk

---
+++ Modules/bhwalk/lua_bhwalk.cpp
@@ -0,0 +1,63 @@
+/*
+ Lua Octomap Wrapper
+(c) 2013 Stephen McGill
+*/
+#include <lua.hpp>
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+#include "WalkingEngine.h"
+
+using namespace std;
+
+// NOTE: This variable could cause segfaults, see OpenNI for how to use C++ objs
+static WalkingEngine walkingEngine;
+
+const float INITIAL_BODY_POSE_ANGLES[] =
+{
+        1.57f, 0.18f, -1.56f, -0.18f,
+        0.0f, 0.0f, -0.39f, 0.76f, -0.37f, 0.0f,
+        0.0f, 0.0f, -0.39f, 0.76f, -0.37f, 0.0f,
+        1.57f, -0.18f, 1.43f, 0.23f
+};
+
+inline bool hasLargerMagnitude(float x, float y) {
+    if (y > 0.0f)
+        return x > y;
+    if (y < 0.0f)
+        return x < y;
+    return true; // considers values of 0.0f as always smaller in magnitude than anything
+}
+
+/* return true if p1 has "passed" p2 (has components values that either have a magnitude
+ larger than the corresponding magnitude p2 within the same sign)
+*/
+bool hasPassed(const Pose2D& p1, const Pose2D& p2) {
+
+    return (hasLargerMagnitude(p1.rotation, p2.rotation) &&
+            hasLargerMagnitude(p1.translation.x, p2.translation.x) &&
+            hasLargerMagnitude(p1.translation.y, p2.translation.y));
+}
+
+static int lua_is_standing( lua_State *L ) {
+  
+  lua_pushboolean(
+    L, walkingEngine.theMotionRequest.motion == MotionRequest::stand);
+  
+	return 1;
+}
+
+static const struct luaL_Reg bhwalk_lib [] = {
+	{"is_standing", lua_is_standing},
+	{NULL, NULL}
+};
+
+extern "C" int luaopen_bhwalk(lua_State *L) {
+#if LUA_VERSION_NUM == 502
+	luaL_newlib(L, bhwalk_lib);
+#else
+	luaL_register(L, "bhwalk", bhwalk_lib);
+#endif
+	return 1;
+}

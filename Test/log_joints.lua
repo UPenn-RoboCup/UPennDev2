f615570ca852e55e2c129a968d67a3037cd675e4
Stephen McGill <smcgill3@seas.upenn.edu>
2014-06-04 13:47:40 -0400

    fixing

--- Test/log_joints.lua
+++ Test/log_joints.lua
@@ -1,6 +1,7 @@
 dofile'../include.lua'
 local signal = require'signal'
 local libLog = require'libLog'
+local Body = require(Config.dev.body)
 local logger = libLog.new'joint'
 
 function shutdown()
@@ -12,9 +13,10 @@ signal.signal("SIGTERM", shutdown)
 
 local sample_hz = 125
 
-local get_time = unix.time
+local get_time, usleep, max = unix.time, unix.usleep, math.max
 local t0, t_sleep = get_time(), 1 / sample_hz
-local count, t, t_last = 0, t0, t0
+local count, t, t_last, t_debug = 0, t0, t0, t0
+print('Begin logging joints...')
 while true do
   count = count + 1
   t_last = t
@@ -32,7 +34,13 @@ while true do
     mag = Body.get_magnetometer(),
   }
   logger:record(entry)
+	--
+	if t - t_debug>1 then
+		t_debug = t
+		print('Joint Logger', count)
+	end
   --
+	collectgarbage('step')
   t_diff = get_time() - t
   usleep(1e6 * max(t_sleep - t_diff, 0))
 end

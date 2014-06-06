121070ed6c59c2a52fa91f74fb4d783603073c1d
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-06-06 10:21:37 -0400

    plot goalpost

--- Tools/Matlab/monitor/process_libVision_msg.m
+++ Tools/Matlab/monitor/process_libVision_msg.m
@@ -9,8 +9,12 @@ function [needs_draw] = process_libVision_msg(metadata, raw, cam)
         if isfield(metadata,'ball')
             % Show our ball on the YUYV image plot
             ball_c = metadata.ball.centroid * cam.scale;
-            set(cam.p_ball,'Xdata', ball_c(1));
-            set(cam.p_ball,'Ydata', ball_c(2));
+            ball_radius = (metadata.ball.axisMajor / 2) * cam.scale;
+            ball_box = [ball_c(1)-ball_radius ball_c(2)-ball_radius...
+                2*ball_radius 2*ball_radius];
+            set(cam.p_ball, 'Xdata', ball_c(1));
+            set(cam.p_ball, 'Ydata', ball_c(2));
+            set(cam.r_ball, 'Position', ball_box);
         else
             % Remove from the plot
             set(cam.p_ball,'Xdata', []);
@@ -18,7 +22,20 @@ function [needs_draw] = process_libVision_msg(metadata, raw, cam)
         end
         if isfield(metadata,'posts')
             % Show on the plot
-            %metadata.posts
+            postStats = metadata.posts{1};
+            post_c = postStats.centroid' * cam.scale;
+            x0 = post_c(1);
+            y0 = post_c(2);
+            w0 = postStats.axisMajor * cam.scale / 2;
+            h0 = postStats.axisMinor * cam.scale / 2;
+            post_o = postStats.orientation;
+            rot = [cos(post_o) sin(post_o); -sin(post_o) cos(post_o)];
+            x11 = [x0 y0] + (rot*[w0 h0]')';
+            x12 = [x0 y0] + (rot*[-w0 h0]')';
+            x21 = [x0 y0] + (rot*[w0 -h0]')';
+            x22 = [x0 y0] + (rot*[-w0 -h0]')';
+            post_box = [x11; x12; x22; x21; x11];
+            set(cam.p_post1, 'XData', post_box(:,1), 'YData', post_box(:,2));
         else
             % Remove from the plot
         end

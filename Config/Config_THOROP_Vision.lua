5af9adf8eccd9b9f67aaee9b36ae9109175d7743
Stephen McGill <smcgill3@seas.upenn.edu>
2014-07-10 18:42:48 -0400

    test

--- Config/Config_THOROP_Vision.lua
+++ Config/Config_THOROP_Vision.lua
@@ -52,7 +52,7 @@ vision.ball = {
   diameter = 0.22,
   th_min_bbox_area = 40, --50
   th_min_area = 20, --10,
-  th_min_fill_rate = 0.45, --0.35,
+  th_min_fill_rate = 0.35,
 --  max_height  = 0.7,
 
   max_height0 = 0.4,    --Max height = max_height0 + dist*max_height1

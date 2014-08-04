3220c1a48929e2df53d521bbe767a82072f819b5
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-08-04 15:51:32 -0400

    seems to work in webots, for chasing, but the robot falls after a kick

--- Config/Config_THOROP_Robot.lua
+++ Config/Config_THOROP_Robot.lua
@@ -389,10 +389,10 @@ end
 if IS_WEBOTS then
   -- Webots overrides tested in Webots 7.2.3, with ShortNewHand
   servo.direction = vector.new({
-    -1,-1, -- Head
+   	1,1, -- Head
     1,-1,-1,  1,  -1,-1,-1, --LArm
-    --[[Yaw/Roll:]] -1, 1, --[[3 Pitch:]] 1,1, 1, 1, --LLeg
-    --[[Yaw/Roll:]] -1, 1, --[[3 Pitch:]] 1,1, 1, 1, --RLeg
+    --[[Yaw/Roll:]] 1, 1, --[[3 Pitch:]] 1,1, 1, 1, --LLeg
+    --[[Yaw/Roll:]] 1, 1, --[[3 Pitch:]] 1,1, 1, 1, --RLeg
     1,-1,-1,  -1,  -1,-1,-1, --RArm
     -- TODO: Check the gripper
     -1,1, -- Waist

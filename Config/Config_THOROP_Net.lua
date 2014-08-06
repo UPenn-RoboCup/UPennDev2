b57d512a87b27ad3cca47f989eae3bc9b6ffa524
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-08-05 20:35:04 -0400

    Finish up the Config net

--- Config/Config_THOROP_Net.lua
+++ Config/Config_THOROP_Net.lua
@@ -1,5 +1,4 @@
 assert(Config, 'Need a pre-existing Config table!')
-local vector = require'vector'
 
 -- Network settings --
 local net = {
@@ -41,119 +40,47 @@ net.rpc = {
 	udp = 55556,
 	uds = 'rpc',
 }
---[[
-net.audio = 55557
---
-net.team           = 44444
-net.state          = 44445
---
-net.camera = {}
-net.camera.head     = 33333
-net.camera.forehead = 33335
-net.camera.forehead2 = 33337
-net.detect = 33331
---
-net.reliable_camera = {}
-net.reliable_camera.head     = 33334
-net.reliable_camera.forehead = 33336
-net.reliable_camera.forehead2 = 33338
---
-net.mesh           = 33344
-net.reliable_mesh  = 33345
-net.rgbd_depth     = 33346
-net.rgbd_color     = 33347
---
-net.omap           = 22222
-net.hmap           = 22223
 
-net.feedback       = 54329
-
-
-mesh = {
-	name : 'mesh', // reliable name
-	ws : 9001,
-	udp: 33344,
+local streams = {}
+net.streams = streams
+streams.mesh = {
+	ws = 9001,
+	udp= 33344,
+}
+streams.camera0 = {
+  ws = 9003,
+  udp= 33333,
+	sub = 'camera0',
+}
+streams.camera1 = {
+  ws = 9004,
+  udp= 33334,
+	sub = 'camera1',
+}
+streams.lidar0 = {
+  ws = 9015,
+	sub = 'lidar0',
+}
+streams.kinect_depth = {
+  ws = 9010,
+	udp= 33346,
+}
+streams.kinect_color = {
+  ws = 9011,
+	udp= 33347,
+}
+streams.feedback = {
+  ws = 9013,
+	udp= 54329,
+}
+streams.audio = {
+  ws = 9014,
+	tcp= 55557,
 }
 
-bridges.push({
-  name : 'camera0',
-  ws : 9003,
-  udp: 33333,
-  //tcp : 33334,
-	sub : 'camera0',
-  clients : []
-});
-
-bridges.push({
-  name : 'camera1',
-  ws : 9004,
-  udp: 33335,
-  //tcp: 33336,
-  clients : []
-});
-
-bridges.push({
-	name : 'feedback',
-	ws : 9013,
-	udp: 54329,
-	clients : []
-});
-
-bridges.push({
-	name : 'audio',
-	ws : 9014,
-  //tcp: 55557,
-	clients : []
-});
-
-bridges.push({
-	name : 'lidar0',
-	ws : 9015,
-	sub: 'lidar0',
-	clients : []
-});
-
-bridges.push({
-	name : 'touch',
-	ws : 9064,
-	pub: 'touch',
-	sub: 'bbox', // listen for the bounding box
-	/*tcp: '55588',*/
-	clients : []
-});
-
-bridges.push({
-	name : 'wire',
-	ws : 9065,
-	sub: 'wire',
-	tcp: '55589',
-	clients : []
-});
-
-/*
-bridges.push({
-	name : 'rgbd_depth',
-	ws : 9010,
-	udp: 33346,
-	clients : []
-});
-
-bridges.push({
-  name : 'rgbd_color',
-  ws : 9011,
-  udp: 33347,
-  clients : []
-});
-
-bridges.push({
-	name : 'spacemouse',
-	ws : 9012,
-	sub: 'spacemouse',
-	clients : []
-});
-*/
---]]
-
+-- TODO: Eliminate these
+net.team           = 44444
+net.state          = 44445
 
 -- Export
 Config.net = net

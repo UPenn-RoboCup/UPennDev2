b50efa4745dd3141c6704a023f7309d0b5f133c6
Stephen McGill <smcgill3@seas.upenn.edu>
2014-08-27 18:49:39 -0400

    Don't use rpy just yet

--- Test/log_joints.lua
+++ Test/log_joints.lua
@@ -1,29 +1,30 @@
 dofile'../include.lua'
-local signal = require'signal'
+local signal = require'signal'.signal
+local running = true
+
+local function shutdown()
+	running = false
+end
+signal("SIGINT", shutdown)
+signal("SIGTERM", shutdown)
+
+
 local si = require'simple_ipc'
 local libLog = require'libLog'
-local Body = require(Config.dev.body)
+local Body = require('Body')
 local logger = libLog.new'joint'
-local body_ch = si.new_publisher'body!'
-
-function shutdown()
-  logger:stop()
-  os.exit()
-end
-signal.signal("SIGINT", shutdown)
-signal.signal("SIGTERM", shutdown)
-
 local sample_hz = 125
 
 local get_time, usleep, max = unix.time, unix.usleep, math.max
 local t0, t_sleep = get_time(), 1 / sample_hz
 local count, t, t_last, t_debug = 0, t0, t0, t0
 print('Begin logging joints...')
-while true do
+local entry
+while running do
   count = count + 1
   t_last = t
   t = get_time()
-  local entry = {
+  entry = {
     n = count,
     t = t,
     cp = Body.get_command_position(),
@@ -32,15 +33,11 @@ while true do
     ft_r = Body.get_rfoot(),
     gyro = Body.get_gyro(),
     acc = Body.get_accelerometer(),
-    rpy = Body.get_rpy(),
-    mag = Body.get_magnetometer(),
   }
-	-- Ask for ft readings
-	body_ch:send'ft'
 	-- Write the log
   logger:record(entry)
-	--
-	if t - t_debug>1 then
+	-- Status message
+	if t - t_debug > 1 then
 		t_debug = t
 		print('Joint Logger', count)
 	end
@@ -49,3 +46,5 @@ while true do
   t_diff = get_time() - t
   usleep(1e6 * max(t_sleep - t_diff, 0))
 end
+
+logger:stop()

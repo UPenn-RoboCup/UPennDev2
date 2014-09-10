07a9ec18da815aa336adc2bd6adc92012c049239
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-09-10 11:41:32 -0400

    motion cleaning

--- Player/MotionFSM/Teach/motionIdle.lua
+++ Player/MotionFSM/Teach/motionIdle.lua
@@ -12,28 +12,16 @@ local t_entry, t_update
 local qLLeg, qRLeg, qWaist
 
 function state.entry()
-  print(state._NAME..' Entry' ) 
-  Body.enable_read'lleg'
-  Body.enable_read'rleg'
- 
-  wcm.set_robot_initdone(0)
+  print(state._NAME..' Entry')
 
   -- Update the time of entry
   local t_entry_prev = t_entry -- When entry was previously called
   t_entry = Body.get_time()
   t_update = t_entry
+  
+  Body.enable_read'lleg'
+  Body.enable_read'rleg'
 
-  -- Torque OFF the motors
-  --[[
-  Body.set_waist_torque_enable(0)
-  Body.set_lleg_torque_enable(0)
-  Body.set_rleg_torque_enable(0)
-  --]]
-
-  -- Commanded at first
-  qLLeg = Body.get_lleg_command_position()
-  qRLeg = Body.get_rleg_command_position()
-  qWaist = Body.get_waist_command_position()
   if Config.walk.legBias then
     mcm.set_leg_bias(Config.walk.legBias)
     print("BIAS SET:",unpack(Config.walk.legBias))
@@ -63,16 +51,7 @@ function state.update()
   local t_diff = t - t_update
   -- Save this at the last update time
   t_update = t
-  --if t - t_entry > timeout then return'timeout' end
-
-  -- Grab our position if available
-  local updatedL, updatedR, updatedW
-  qLLeg, updatedL = Body.get_lleg_position()
-  qRLeg, updatedR = Body.get_rleg_position()
-  qWaist, updatedW = Body.get_waist_position()
-
-  Body.set_lleg_command_position(qLLeg)
-  Body.set_rleg_command_position(qRLeg)
+  if t - t_entry > timeout then return'timeout' end
 
 end
 
@@ -80,7 +59,6 @@ function state.exit()
   print(state._NAME..' Exit' ) 
   -- Torque on the motors
   Body.set_waist_torque_enable(1)
-
 
   if Config.torque_legs then
     Body.set_lleg_torque_enable(1)

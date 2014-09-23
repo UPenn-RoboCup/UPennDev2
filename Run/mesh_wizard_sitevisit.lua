f5251fd49ee3465600f4c6aae113478b421a1b6d
Qin He <heqin@seas.upenn.edu>
2014-09-23 11:12:47 -0700

    mesh wizard for site visit

--- Run/mesh_wizard_sitevisit.lua
+++ Run/mesh_wizard_sitevisit.lua
@@ -12,6 +12,7 @@ local p_compress = require'png'.compress
 local j_compress = require'jpeg'.compressor'gray'
 local vector = require'vector'
 require'vcm'
+require'Body'
 
 -- Shared with LidarFSM
 -- t_sweep: Time (seconds) to fulfill scan angles in one sweep
@@ -20,11 +21,6 @@ require'vcm'
 local t_sweep, mag_sweep, ranges_fov
 -- NOTE: The following is LIDAR dependent
 local t_scan = 1 / 40 -- Time to gather returns
--- Setup tensors for a lidar mesh
-local mesh, mesh_byte, mesh_adj, scan_angles, offset_idx
-local n_scanlines
--- Save the current scanline and current direction
-local scanline, direction
 
 -- Open up channels to send/receive data
 local operator
@@ -37,12 +33,82 @@ local stream = Config.net.streams['mesh']
 local mesh_tcp_ch = stream.tcp and si.new_publisher(stream.tcp, operator)
 local mesh_udp_ch = stream.udp and si.new_sender(operator, stream.udp)
 local mesh_ch = stream.sub and si.new_publisher(stream.sub)
-print("OPERATOR", operator, stream.sub)
+print("OPERATOR", operator, stream.udp)
 
 local metadata = {
-	id = 'mesh0',
+	name = 'mesh0',
 	t = 0,
 }
+
+local scan_angles, scan_x, scan_y, scan_a, scan_p, scan_angles
+-- Setup tensors for a lidar mesh
+local mesh, mesh_byte, mesh_adj, offset_idx
+local n_scanlines
+local function setup_mesh(meta)
+	local n, res = meta.n, meta.res
+	local fov = n * res
+	-- Check that we can access enough FOV
+	local min_view, max_view = unpack(ranges_fov)
+	assert(fov > max_view-min_view, 'Not enough FOV available')
+	-- Find the offset for copying lidar readings into the mesh
+  -- if fov is from -fov/2 to fov/2 degrees, then offset_idx is zero
+  -- if fov is from 0 to fov/2 degrees, then offset_idx is sensor_width/2
+  local fov_offset = min_view / res + n / 2
+	-- Round the offset (0 based offset)
+  offset_idx = math.floor(fov_offset + 0.5)
+	-- Round to get the number of returns for each scanline
+	n_returns = math.floor((max_view - min_view) / res + 0.5)
+	print("n_returns", n_returns, max_view, min_view, res)
+	-- Check the number of scanlines in each mesh
+	-- Indexed by the actuator angle
+	-- Depends on the speed we use
+	n_scanlines = math.floor(t_sweep / t_scan + 0.5)
+  -- In-memory mesh
+  mesh = torch.FloatTensor(n_scanlines, n_returns):zero()
+  -- Mesh buffers for compressing and sending to the user
+	mesh_adj  = torch.FloatTensor(n_scanlines, n_returns):zero()
+  mesh_byte = torch.ByteTensor(n_scanlines, n_returns):zero()
+  -- Data for each scanline
+	if TORCH_SCANLINE_INFO then
+		scan_angles = torch.DoubleTensor(n_scanlines):zero()
+		scan_x = scan_angles:clone()
+		scan_y = scan_angles:clone()
+		scan_a = scan_angles:clone()
+    scan_pitch = scan_angles:clone()
+    scan_roll = scan_angles:clone()
+    scan_pose = torch.DoubleTensor(n_scanlines,3):zero()
+	else
+		scan_angles = vector.zeros(n_scanlines)
+		scan_x = vector.zeros(n_scanlines)
+		scan_y = vector.zeros(n_scanlines)
+		scan_a = vector.zeros(n_scanlines)
+    scan_pitch = vector.zeros(n_scanlines)
+    scan_roll = vector.zeros(n_scanlines)
+    scan_pose = {}
+	end
+	-- Metadata for the mesh
+	metadata.rfov = ranges_fov
+	metadata.sfov = {-mag_sweep / 2, mag_sweep / 2}
+	metadata.a = scan_angles
+	metadata.px = scan_x
+	metadata.py = scan_y
+	metadata.pa = scan_a
+	-- Add Orientation for pitch and roll
+  metadata.pitch = scan_pitch
+  metadata.roll = scan_roll
+  -- Odometry
+  metadata.pose = scan_pose
+  -- Add the dimensions (useful for raw)
+  metadata.dims = {n_scanlines, n_returns}
+end
+
+
+
+
+
+
+
+
 
 local function postprocess()
   -- Let's cheat, since we know we are the chest mesh
@@ -81,63 +147,12 @@ local function postprocess()
   --print('ANGLES FOR MESH',mag_sweep, unpack(ranges_fov))
   --print('POST PROCESS MESH TRANSFORM', first, last)
   -- TODO: Just write this to file... (However you wish)
-  
-  
 end
 
 
-local function setup_mesh(meta)
-	local n, res = meta.n, meta.res
-	local fov = n * res
-	-- Check that we can access enough FOV
-	local min_view, max_view = unpack(ranges_fov)
-	assert(fov > max_view-min_view, 'Not enough FOV available')
-	-- Find the offset for copying lidar readings into the mesh
-  -- if fov is from -fov/2 to fov/2 degrees, then offset_idx is zero
-  -- if fov is from 0 to fov/2 degrees, then offset_idx is sensor_width/2
-  local fov_offset = min_view / res + n / 2
-	-- Round the offset (0 based offset)
-  offset_idx = math.floor(fov_offset + 0.5)
-	-- Round to get the number of returns for each scanline
-	n_returns = math.floor((max_view - min_view) / res + 0.5)
-	print("n_returns", n_returns, max_view, min_view, res)
-	-- Check the number of scanlines in each mesh
-	-- Indexed by the actuator angle
-	-- Depends on the speed we use
-	n_scanlines = math.floor(t_sweep / t_scan + 0.5)
-  -- In-memory mesh
-  mesh = torch.FloatTensor(n_scanlines, n_returns):zero()
-  -- Mesh buffers for compressing and sending to the user
-	mesh_adj  = torch.FloatTensor(n_scanlines, n_returns)
-  mesh_byte = torch.ByteTensor(n_scanlines, n_returns)
-  -- Data for each scanline
-	if TORCH_SCANLINE_INFO then
-		scan_angles = torch.DoubleTensor(n_scanlines):zero()
-		scan_x = torch.DoubleTensor(n_scanlines):zero()
-		scan_y = torch.DoubleTensor(n_scanlines):zero()
-		scan_a = torch.DoubleTensor(n_scanlines):zero()
-	else
-		scan_angles = vector.zeros(n_scanlines)
-		scan_x = vector.zeros(n_scanlines)
-		scan_y = vector.zeros(n_scanlines)
-		scan_a = vector.zeros(n_scanlines)
-	end
-	-- Metadata for the mesh
-	metadata.rfov = ranges_fov
-	metadata.sfov = {-mag_sweep / 2, mag_sweep / 2}
-	metadata.a = scan_angles
-	metadata.px = scan_x
-	metadata.py = scan_y
-	metadata.pa = scan_a
-  metadata.n_scanlines = n_scanlines
-  metadata.n_returns = n_returns
-  metadata.lidarZ = 0.1
-	-- TODO: Add IMU for body orientation
-  metadata.bodyPitch = 3*DEG_TO_RAD
-  metadata.bodyRoll = 0
-end
 
 -- Convert a pan angle to a column of the chest mesh image
+local scanline, direction
 local function angle_to_scanlines(rad)
 	-- Find the scanline
   local ratio = (rad + mag_sweep / 2) / mag_sweep
@@ -185,29 +200,22 @@ local function send_mesh(destination, compression, dynrange)
   mesh_byte:copy(mesh_adj)
   -- Compression
   local c_mesh
-  
-  --HARD CODE
-  -- compression = 'jpeg'
   compression = 'raw'
-  
   if compression=='jpeg' then
-    print('JPEG FOR NOW...')
 		c_mesh = j_compress:compress(mesh_byte)
   elseif compression=='png' then
     c_mesh = p_compress(mesh_byte)
   else
+    -- Raw
     print('compressing RAW...')
-    
     c_mesh = ffi.string(mesh:data(), mesh:nElement() * ffi.sizeof'float')
   end
-  
 	-- Update relevant metadata
 	metadata.c = compression
 	metadata.dr = dynrange
 	-- Send away
-    
 	if type(destination)=='string' then
-		local f_img = io.open(destination..'.'..metadata.c, 'w')
+		local f_img = io.open(destination..'.'..compression, 'w')
 		local f_meta = io.open(destination..'.meta', 'w')
 		f_img:write(c_mesh)
 		f_img:close()
@@ -224,19 +232,26 @@ local function send_mesh(destination, compression, dynrange)
 		local ret, err = mesh_udp_ch:send(mpack(metadata)..c_mesh)
 		print('Mesh | Sent UDP', err or 'successfully')
 	end
+  
+  -- postprocess()
+  
 end
+
+local compression = {
+  [0] = 'jpeg',
+  [1] = 'png',
+  [2] = 'raw'
+}
 
 local function check_send_mesh()
 	local net = vcm.get_mesh_net()
-	local request, destination, compression = unpack(net)
+	local request, destination, comp = unpack(net)
 	if request==0 then return end
 	local dynrange = vcm.get_mesh_dynrange()
-	send_mesh(destination==1, compression==1 and 'png' or 'jpeg', dynrange)
+	send_mesh(destination==1, compression[comp], dynrange)
 	-- Reset the request
 	net[1] = 0
 	vcm.set_mesh_net(net)
-  -- Post process
-  postprocess()
 end
 
 local function update(meta, ranges)
@@ -253,12 +268,17 @@ local function update(meta, ranges)
 		setup_mesh(meta)
 		print('Mesh | Updated containers')
 	end
+  -- Metadata
+  -- NOTE: Orientation should include the joint positions as well!
+  local roll, pitch, yaw = unpack(meta.rpy)
+  -- Body pose
+  local pose = meta.pose
+  
   -- Find the scanline indices
 	local rad_angle = meta.angle
   local scanlines = angle_to_scanlines(rad_angle)
 	local byte_sz = mesh:size(2) * ffi.sizeof'float'
 	local float_ranges = ffi.cast('float*', ranges)
-
 	local dest
   for _,line in ipairs(scanlines) do
 		if line >= 1 and line<=n_scanlines then
@@ -267,6 +287,10 @@ local function update(meta, ranges)
 			-- Save the pan angle
 			scan_angles[line] = rad_angle
 			-- TODO: Save the pose
+      scan_pose[line] = vector.new(pose)
+      -- Save the orientation
+      scan_pitch[line] = pitch
+      scan_roll[line] = roll
 		end
   end
 	-- Check for sending out on the wire
@@ -284,7 +308,18 @@ function lidar_ch.callback(skt)
 	local mdata, ranges = unpack(skt:recv_all())
 	local meta = munpack(mdata)
 	update(meta, ranges)
-	print("hey")
 end
 
 si.wait_on_channels({lidar_ch}):start()
+
+
+
+
+
+
+
+
+
+
+
+

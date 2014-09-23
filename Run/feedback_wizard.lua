e0721364045676bd132e5f7278b8f8b54756c24b
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-09-22 18:37:42 -0700

    Add back the feedback wizard

--- Run/feedback_wizard.lua
+++ Run/feedback_wizard.lua
@@ -2,27 +2,41 @@
 -- (c) 2013, 2014 Stephen McGill, Seung-Joon Yi
 dofile'../include.lua'
 local si = require'simple_ipc'
+local mpack = require'msgpack.MessagePack'.pack
+local Body = require'Body'
 
 -- 2 Hz feedback
 local t_sleep = 1e6 / 2
-
-local running = true
 local feedback_udp_ch =
-si.new_sender(Config.net.operator.wired, Config.net.streams.feedback.tcp)
+si.new_sender(Config.net.operator.wired, Config.net.streams.feedback.udp)
 
-local data, ret, err
-local function send_status_feedback()
-	data = {
-		pose = wcm.get_robot_pose(),
-		battery = Body.get_sensor_battery(),
-		rpy = Body.get_sensor_rpy(),
-		t   = unix.time(),
-	}
-	ret, err = feedback_udp_ch:send( mp.pack(data) )
+local ret, err
+local feedback = {}
+local function update()
+  feedback.t = Body.get_time()
+  feedback.joints = Body.get_position()
+	feedback.pose = wcm.get_robot_pose()
+	feedback.rpy = Body.get_rpy()
+  feedback.battery = Body.get_battery()
+	ret, err = feedback_udp_ch:send(mpack(feedback))
 	if err then print('Feedback UDP error',err) end
 end
 
+-- If required from Webots, return the table
+if ... and type(...)=='string' then
+	return {entry=nil, update=update, exit=nil}
+end
+
+local running = true
+local function shutdown()
+  running = false
+end
+local signal = require'signal'.signal
+signal("SIGINT", shutdown)
+signal("SIGTERM", shutdown)
+
+local unix = require'unix'
 while running do
-	send_status_feedback()
+	update()
 	unix.usleep(t_sleep)
 end

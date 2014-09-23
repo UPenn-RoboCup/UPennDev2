0ae8b9c0537a67a021da7cd660136e91f9854b52
Stephen McGill <smcgill3@seas.upenn.edu>
2014-09-22 22:38:52 -0400

    head scan FSM for site visit

--- Config/THOROP/Config_THOROP_World_DRCTrials.lua
+++ Config/THOROP/Config_THOROP_World_DRCTrials.lua
@@ -5,6 +5,10 @@ local world = {}
 world.odomScale = {1,1,1} -- For now IMU not in use
 world.use_imu_yaw = true
 
+-- "Ball"
+world.ballDiameter = 0.22
+
+
 -- Associate with the table
 Config.world = world
 

2a5aa6f444b8279f9f92a2ec06257515acf3c82d
ASUS_ROG <seungjoon.yi@gmail.com>
2014-10-03 02:13:35 -0700

    some more testing

--- Player/BodyFSM/DRCNew/bodyStepUp1.lua
+++ Player/BodyFSM/DRCNew/bodyStepUp1.lua
@@ -35,6 +35,10 @@ local sh1,sh2 = 0.22, 0.19
 local step1,step2 = 0.30, 0.27
 
 
+--touchdown sensing test
+local sh1,sh2 = 0.23, 0
+local step1,step2 = 0.31, 0.27
+
 --[[
 local step_queues={
     --put first step up
@@ -62,7 +66,7 @@ local step_queues={
 }
 --]]
 
---
+--[[
 --ZMP modulation testing
 
 step_queues={
@@ -99,28 +103,59 @@ step_queues={
 }
 --]]
 
---body height change testing
+
 --[[
 step_queues={
    {
     {{0,0,0},   2,  0.1, 1, 0.1,    {0,0},  {0, 0, 0}},
-    {{step1,0,0},0,  1, 3, 0.5,   {0,-0.04}, {0,sh1,sh2}   ,  {-step1/2*0.8  ,Config.walk.footY*0.4}},   --LS
-    {{0,0,0},   2,  1, 1, 0.1,      {-step1/2*0.8,Config.walk.footY*0.4},  {0, 0, 0},{-step1/2*0.8,Config.walk.footY*0.4}},
-   },
+--    {{step1,0,0},0,  1, 3, 0.5,   {0,-0.04}, {0,sh1,sh2}   ,  {-step1/2*0.8  ,Config.walk.footY*0.4}},   --LS
 
-  {
-    {{0,0,0},   2,  4, 1, 0.1,     {step1/2,-Config.walk.footY*0.8},  {0, 0, 0}, {step1/2,-Config.walk.footY*0.8}},
+    {{step1,0,0},0,  1, 6, 0.5,   {0,-0.04}, {0,sh1,sh2}   ,  {-step1/2*0.8  ,Config.walk.footY*0.4}},   --LS    
+
+    {{0,0,0},   2,  1, 1, 0.1,      {-step1/2*0.8,Config.walk.footY*0.4},  {0, 0, 0},{-step1/2*0.8,Config.walk.footY*0.4}},
+    {{0,0,0},   2,  4, 1, 0.1,     {step1/2,-Config.walk.footY*0.8},  {0, 0, 0}, {step1/2,-Config.walk.footY*0.8}},    
    },
 
   { --put second step up
---    {{step1,0,0},1,  0.5,   4, 1,  {0.0,0},  {0,0.20,sh2}},    --RS    
-
-    {{step1,0,0},1,  4,   12, 4,  {0.0,0},  {0,0.20,sh2}},    --RS    
-
+    {{step1,0,0},1,  1,   6, 1,  {0.0,0},  {0,sh1,sh2}},    --RS    
     {{0,0,0},   2,  0.1, 1, 0.1,   {0,0},  {0, 0, 0}},
   },
+
+
+  {  --landing
+    {{step2,0,0} ,0, 2,   6,  1,  {-0.0,-0.00,0},  {sh2,sh1,0.0},{-step1/2,Config.walk.footY*0.8}},--LS
+    {{0,0,0},   2,  1, 1, 0.1,      {-step1/2,Config.walk.footY*0.8},  {0, 0, 0},{-step1/2,Config.walk.footY*0.8}},
+  },
+
+  {
+    {{0,0,0},   2,  2, 1, 0.1,    {-0.00,0,0},  {0, 0, 0}},
+  },
+
+  {
+    {{step2,0,0}, 1,  1,  6, 1,   {0,0.00,0},  {sh2,sh1,0.0}},--RS
+    {{0,0,0,},  2,   0.1, 2, 1,     {0,0.0,0},  {0, 0, 0}},                  --DS
+  }
+
 }
 --]]
+
+step_queues={
+   {
+    {{0,0,0},   2,  0.1, 1, 0.1,    {0,0},  {0, 0, 0}},
+    {{step1,0,0},0,  1, 6, 0.5,   {0,-0.04}, {0,sh1,sh2}   ,  {-step1/2*0.8  ,Config.walk.footY*0.4}},   --LS    
+    {{0,0,0},   2,    1, 0.1, 0.1,      {-step1/2*0.8,Config.walk.footY*0.4},  {0, 0, 0},{-step1/2*0.8,Config.walk.footY*0.4}},
+    {{0,0,0},   2,    1, 0.1, 0.1,     {step1/2,-Config.walk.footY*0.8},  {0, 0, 0}, {step1/2,-Config.walk.footY*0.8}},    
+    {{step1,0,0},1,   1, 6, 1,  {0.0,0},  {0,sh1,sh2}},    --RS    
+    {{0,0,0},   2,    0.1, 1, 0.1,   {0,0},  {0, 0, 0}},
+  
+    {{step2,0,0} ,0,   2, 6,  1,  {-0.0,-0.00,0},  {sh2,sh1,0.0},{-step1/2,Config.walk.footY*0.8}},--LS
+    {{0,0,0},   2,    1, 0.1, 0.1,      {-step1/2,Config.walk.footY*0.8},  {0, 0, 0},{-step1/2,Config.walk.footY*0.8}},
+    {{0,0,0},   2,    1, 0.1, 0.1,    {-0.00,0,0},  {0, 0, 0}},
+    {{step2,0,0}, 1,  1,  6, 1,   {0,0.00,0},  {sh2,sh1,0.0}},--RS
+    {{0,0,0,},  2,    0.1, 2, 1,     {0,0.0,0},  {0, 0, 0}},                  --DS
+  }
+
+}
 
 
 

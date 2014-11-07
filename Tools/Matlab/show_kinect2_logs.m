1b4251c11f1be1f344f0b94df32e769957a8d2e0
Stephen G. McGill <smcgill3@seas.upenn.edu>
2014-11-07 15:06:49 -0500

    Show IR as well

--- Tools/Matlab/show_kinect2_logs.m
+++ Tools/Matlab/show_kinect2_logs.m
@@ -19,59 +19,69 @@ DEPTH_W = 512;
 DEPTH_H = 424;
 DEPTH_MAX = 2000;%8000;
 DEPTH_MIN = 200;
+figure(1);
+h_depth = imagesc(zeros(DEPTH_H, DEPTH_W));
+caxis([DEPTH_MIN DEPTH_MAX]);
+%
+figure(3);
+h_ir = imagesc(zeros(DEPTH_H, DEPTH_W));
+%caxis([DEPTH_MIN DEPTH_MAX]);
 %
 RGB_W = 1920;
 RGB_H = 1080;
 rgb_img = uint8(zeros([RGB_H, RGB_W, 3]));
+figure(2);
+h_rgb = image(rgb_img);
 
 n = numel(log_files);
 for idx=1:n
     datestamp = log_files(idx).name(nprefix+1:end-4);
     fprintf('Timestamp: %s (Index %d of %d)\n', datestamp, idx, n);
-    % Metadata
+    % Depth
     fid = fopen(sprintf('Data/k2_depth_m_%s.log',datestamp));
     depthMeta = fread(fid,Inf,'*uint8');
     fclose(fid);
     depthMeta = msgpack('unpacker', depthMeta, 'uint8');
-    % Metadata
+    % RGB
     fid = fopen(sprintf('Data/k2_rgb_m_%s.log',datestamp));
     rgbMeta = fread(fid,Inf,'*uint8');
     fclose(fid);
     rgbMeta = msgpack('unpacker',rgbMeta,'uint8');
+    % RGB
+    fid = fopen(sprintf('Data/k2_ir_m_%s.log',datestamp));
+    irMeta = fread(fid,Inf,'*uint8');
+    fclose(fid);
+    irMeta = msgpack('unpacker',irMeta,'uint8');
     
     % Grab the Depth logged information
-    f_raw = fopen(sprintf('Data/k2_depth_r_%s.log',datestamp));
-    f_rgb = fopen(sprintf('Data/k2_rgb_r_%s.log',datestamp));
+    f_depth = fopen(sprintf('Data/k2_depth_r_%s.log', datestamp));
+    f_rgb = fopen(sprintf('Data/k2_rgb_r_%s.log', datestamp));
+    f_ir = fopen(sprintf('Data/k2_ir_r_%s.log', datestamp));
     
     % Individual reading
     nlog = numel(depthMeta);
     for ilog=1:nlog
         meta = depthMeta{ilog};
         fprintf('Log %d of %d\n', ilog, nlog);
+        %
         if DEPTH_W * DEPTH_H * 4 ~= meta.rsz, disp('Bad metadata!'); end
-        depthRaw = fread(f_raw, [DEPTH_W, DEPTH_H], '*single');
+        depthRaw = fread(f_depth, [DEPTH_W, DEPTH_H], '*single');
+        set(h_depth, 'CData', depthRaw');
+        %
         meta = rgbMeta{ilog};
         rgbJPEG = fread(f_rgb, meta.rsz, '*uint8');
-        %{
-    rgbRaw = fread(f_rgb, [3 RGB_W * RGB_H], '*uint8');
-    rgbRaw = permute(reshape(rgbRaw, [3, RGB_W, RGB_H]), [3,2,1]);
-    % Color channels are BGR, not RGB...
-    rgb_img(:,:,1) = rgbRaw(:,:,3);
-    rgb_img(:,:,2) = rgbRaw(:,:,2);
-    rgb_img(:,:,3) = rgbRaw(:,:,1);
-        %}
-        figure(1);
-        imagesc(depthRaw');
-        caxis([DEPTH_MIN DEPTH_MAX]);
-        figure(2);
         rgb_img0 = djpeg(rgbJPEG);
         rgb_img(:,:,1) = rgb_img0(:,:,3);
         rgb_img(:,:,2) = rgb_img0(:,:,2);
         rgb_img(:,:,3) = rgb_img0(:,:,1);
-        image(rgb_img);
-        pause;
+        set(h_rgb, 'CData', rgb_img);
+        %
+        irRaw = fread(f_ir, [DEPTH_W, DEPTH_H], '*single');
+        set(h_ir, 'CData', irRaw');
+        %
+        pause(0.5);
     end
-    fclose(f_raw);
+    fclose(f_depth);
     fclose(f_rgb);
 end
 

6e6eaea528618d2734730e003efd089d63e39b80
Stephen McGill <smcgill3@seas.upenn.edu>
2014-12-22 15:29:55 -0500

    testing

--- Run/kinect2_wizard.lua
+++ Run/kinect2_wizard.lua
@@ -11,44 +11,63 @@ local color_ch = require'simple_ipc'.new_publisher'kinect2_color'
 local c_rgb = require'jpeg'.compressor('rgb')
 
 local cfg = Config.kinect
+local Body = require'Body'
+require'mcm'
 
-local has_detection, detection = pcall(require, cfg.detection)
-if type(detection)=='string' then
-	print(detection)
+--local has_detection, detection = pcall(require, cfg.detection)
+
+local ENABLE_LOG = true
+local libLog, logger
+if ENABLE_LOG then
+  libLog = require'libLog'
+  log_rgb = libLog.new('k2_rgb', true)
+  log_depth = libLog.new('k2_depth', true)
 end
 
-local cnt = 0
+local get_time = Body.get_time
+local cnt, t, t_send = 0, 0, -2
 local function update(rgb, depth)
+  t = get_time()
   cnt = cnt + 1
-	-- Process
+	-- Process and send
 	if has_detection then
 		detection.update(rgb, depth)
+		for _,v in ipairs(detection.send()) do color_ch:send({mp.pack(v[1]), v[2]}) end
 	end
 	-- Send debug
-  if cnt % 5 == 0 then
+  if t - t_send > 1 then
+    t_send = t
     local q, rpy, bh = Body.get_position(), Body.get_rpy(), mcm.get_walk_bodyHeight()
 	  -- Send color
-	  local j_rgb = c_rgb:compress(rgb.data, rgb.width, rgb.height)
-	  rgb.data = nil
-	  rgb.sz = #j_rgb
+    rgb.t = t
 		rgb.c = 'jpeg'
     rgb.q = q
     rgb.bh = bh
     rgb.rpy = rpy
     rgb.id = 'k2_rgb'
-    color_ch:send({mpack(rgb), j_rgb})
+    local j_rgb = c_rgb:compress(rgb.data, rgb.width, rgb.height)
+    rgb.data = nil
+    rgb.sz = #j_rgb
+    rgb.rsz = #j_rgb
+    local m_rgb = mpack(rgb)
+    color_ch:send({m_rgb, j_rgb})
 	  -- Send depth (TODO: zlib)
-	  local ranges = depth.data
-	  depth.data = nil
-	  depth.sz = #ranges
     depth.c = 'raw'
     depth.q = q
     depth.bh = bh
     depth.rpy = rpy
     depth.id = 'k2_depth'
-    depth_ch:send({mpack(depth), ranges})
-		-- Send extra color processing
-		for _,v in ipairs(detection.send()) do color_ch:send({mp.pack(v[1]), v[2]}) end
+	  local ranges = depth.data
+	  depth.data = nil
+	  depth.sz = #ranges
+    depth.rsz = #ranges
+    local m_depth = mpack(depth)
+    depth_ch:send({m_depth, ranges})
+    -- Log
+    if ENABLE_LOG then
+      log_rgb:record(m_rgb, j_rgb)
+      log_depth:record(m_depth, ranges)
+    end
   end
   return t
 end
@@ -68,6 +87,10 @@ end
 local function exit()
   freenect2.shutdown()
 	if has_detection then detection.exit() end
+  if ENABLE_LOG then
+    log_rgb:stop()
+    log_depth:stop()
+  end
 end
 
 -- Cleanly exit on Ctrl-C

1155770541bdea2a185aad9cdb14007f94f53484
ASUS_ROG <seungjoon.yi@gmail.com>
2015-01-12 10:20:16 -0800

    testing

--- Player/MotionFSM/RoboCup/motionStance.lua
+++ Player/MotionFSM/RoboCup/motionStance.lua
@@ -14,6 +14,7 @@ local util   = require'util'
 local moveleg = require'moveleg'
 local libStep = require'libStep'
 require'mcm'
+require'hcm'
 
 -- Keep track of important times
 local t_entry, t_update, t_last_step
@@ -31,6 +32,9 @@ local aShiftTargetY = vector.new{0.0,0.0}
 
 local angleShift = vector.new{0,0,0,0}
 
+
+local virtual_torso_angle = {0,0}
+
 ---------------------------
 -- State machine methods --
 ---------------------------
@@ -47,6 +51,15 @@ function state.entry()
   mcm.set_walk_bipedal(1)
   mcm.set_walk_steprequest(0)
   mcm.set_motion_state(2)
+
+
+  local uLeft = mcm.get_status_uLeft()
+  local uRight = mcm.get_status_uRight()
+  local uTorso = mcm.get_status_uTorso()  
+  hcm.set_legdebug_left({uLeft[1],uLeft[2],uLeft[3],0})
+  hcm.set_legdebug_right({uRight[1],uRight[2],uRight[3],0})
+  hcm.set_legdebug_torso({uTorso[1],uTorso[2]})
+  hcm.set_legdebug_torso_angle({0,0})
 end
 
 function state.update()
@@ -80,6 +93,27 @@ function state.update()
   local delta_legs
   delta_legs, angleShift = moveleg.get_leg_compensation_simple(supportLeg,0,gyro_rpy, angleShift)
 
+
+------------------------------------------------
+--External control
+  local uLeftTarget = hcm.get_legdebug_left() 
+  local uRightTarget = hcm.get_legdebug_right()  
+  local uTorsoTarget = hcm.get_legdebug_torso()  
+
+  uLeft[1],uLeft[2],uLeft[3]=
+    uLeftTarget[1],uLeftTarget[2],uLeftTarget[3]
+  uRight[1],uRight[2],uRight[3]=
+    uRightTarget[1],uRightTarget[2],uRightTarget[3]
+  uTorso[1],uTorso[2]=
+    uTorsoTarget[1],uTorsoTarget[2]
+------------------------------------------------
+
+
+
+
+
+
+
 --Compensation for arm / objects
   local uTorsoComp = mcm.get_stance_uTorsoComp()
   local uTorsoCompensated = util.pose_global(
@@ -90,18 +124,32 @@ function state.update()
 
 
 
+
+
+
+
+
+
 ----------------------------------------------------------------------------------------------
 -- FT testing
 ----------------------------------------------------------------------------------------------
-ft_test = false
 
-if ft_test then
-
+if hcm.get_legdebug_enable_balance()>0 then
 
   local l_ft, r_ft = Body.get_lfoot(), Body.get_rfoot()
   local lf_z,rf_z = l_ft[3],r_ft[3]
   local lt_y,rt_y = l_ft[5],r_ft[5] 
-  if IS_WEBOTS then  lt_y = l_ft[4] end--FOR WEBOTS, ft sensor is rotated
+  if IS_WEBOTS then  
+    --FOR WEBOTS, ft sensor is rotated
+    --FOR webots, torque readings are inverted too    
+
+    lt_y,rt_y = -l_ft[4],-r_ft[5]      
+  end
+
+  --Toe push up: negative y torque readings!
+
+  
+
 
   print(string.format("Fz: %d %d  Ty: %d %d", lf_z,rf_z,lt_y,rt_y))
 
@@ -113,6 +161,8 @@ if ft_test then
   local z_shift_max = 0.03
   local z_shift_db = 0.005
   local z_vel_max = 0.03 --max 5cm per sec
+
+--[[
 
   zShiftTarget[1] = (lf_z-z_force_zero)*k_const_z + zvShift[1]*r_const_z 
   zShiftTarget[2] = (rf_z-z_force_zero)*k_const_z + zvShift[2]*r_const_z
@@ -131,8 +181,44 @@ if ft_test then
 
   zShift[1] = zShift[1]+zvShift[1]*t_diff
   zShift[2] = zShift[2]+zvShift[2]*t_diff
+--]]
+
+
+  local z_shift_max = 0.05
+  local z_shift_db = 0.01
+  local z_vel_max = 0.1 --max 10cm per sec
+
+  local zShiftTargetDiff = (lf_z-rf_z)*k_const_z
+
+
+  zShiftTarget[1] = math.min(z_shift_max,math.max(-z_shift_max,zShiftTargetDiff))
+
+  if math.abs(zShiftTarget[1])<z_shift_db then zShiftTarget[1]=0 end
+
+  zShiftTarget[2] = -zShiftTarget[1]
+
+  zvShift[1] =  (zShiftTarget[1]-zShift[1])/t_diff
+  zvShift[2] =  (zShiftTarget[2]-zShift[2])/t_diff
+
+  zvShift[1]=math.min(z_vel_max,math.max(-z_vel_max,zvShift[1]))
+  zvShift[2]=math.min(z_vel_max,math.max(-z_vel_max,zvShift[2]))
+
+  zShift[1] = zShift[1]+zvShift[1]*t_diff
+  zShift[2] = zShift[2]+zvShift[2]*t_diff
+
+
+
+
+--zShift[1],zShift[2]=0,0
 
   print(string.format("Zshift: %.2f %.2f cm",zShift[1]*100,zShift[2]*100))
+
+
+
+
+
+
+
 
 
 ------------------------------------------------------------------------------------------------
@@ -149,22 +235,22 @@ if ft_test then
 
 
 
-  local k_balancing_y = 10 / (math.pi/180) --10Nm per 1 deg error
+  local k_balancing_y = -10 / (math.pi/180) --10Nm per 1 deg error
 
- k_balancing_y = 100 / (math.pi/180) --10Nm per 1 deg error
+ k_balancing_y = -100 / (math.pi/180) --10Nm per 1 deg error
 
   local balancing_db = 1*math.pi/180
 
   local y_err = rpy[2]-y_angle_zero  
   local balancing_torque = y_err * k_balancing_y
 
-  print("Y angle err:",y_err*180/math.pi)
+  print(string.format("Y angle err: %.2f",y_err*180/math.pi))
   if math.abs(y_err)<balancing_db then balancing_torque = 0 end
 
 
   --make it conform to external torque
 
-  local k_const_ty =  -1 *   math.pi/180   --Y angular spring constant: 10 deg / 5 Nm
+  local k_const_ty =  1 *   math.pi/180   --Y angular spring constant: 10 deg / 5 Nm
 
   local r_const_ty =   0 --zero damping for now
 
@@ -176,13 +262,13 @@ if ft_test then
 
 --balancing_torque = 0
 
-lt_y,rt_y = 0,0 --zero sensed torque
+--lt_y,rt_y = 0,0 --zero sensed torque
 
 --  aShiftTargetY[1] = (-balancing_torque+lt_y)*k_const_ty + avShiftY[1]*r_const_ty
 --  aShiftTargetY[2] = (-balancing_torque+rt_y)*k_const_ty + avShiftY[2]*r_const_ty
 
   aShiftTargetY[1] = aShiftY[1]+(-balancing_torque+lt_y)*k_const_ty + avShiftY[1]*r_const_ty
-  aShiftTargetY[2] = aShiftY[1]+(-balancing_torque+rt_y)*k_const_ty + avShiftY[2]*r_const_ty
+  aShiftTargetY[2] = aShiftY[2]+(-balancing_torque+rt_y)*k_const_ty + avShiftY[2]*r_const_ty
 
 
 
@@ -199,7 +285,7 @@ lt_y,rt_y = 0,0 --zero sensed torque
   avShiftY[2]=math.min(ay_vel_max,math.max(-ay_vel_max,avShiftY[2]))
 
   aShiftY[1] = aShiftY[1]+avShiftY[1]*t_diff
-  aShiftY[2] = aShiftY[1]+avShiftY[1]*t_diff
+  aShiftY[2] = aShiftY[2]+avShiftY[2]*t_diff
 
   print(string.format("Ashift: %.2f %.2f deg",aShiftY[1]*180/math.pi,aShiftY[2]*180/math.pi))
 
@@ -207,11 +293,20 @@ lt_y,rt_y = 0,0 --zero sensed torque
 
 end
 
+
   delta_legs[5]=delta_legs[5]+aShiftY[1]
   delta_legs[11]=delta_legs[11]+aShiftY[2]
 
-  moveleg.set_leg_positions(uTorsoCompensated,uLeft,uRight,  
-    zShift[1],zShift[2],delta_legs)
+
+virtual_torso_angle = hcm.get_legdebug_torso_angle()
+
+
+--  moveleg.set_leg_positions(uTorsoCompensated,uLeft,uRight,  zShift[1],zShift[2],delta_legs)
+  moveleg.set_leg_positions_torsoflex(    
+    uTorsoCompensated,uLeft,uRight,  
+    zShift[1],zShift[2],delta_legs,
+    virtual_torso_angle
+    )
   mcm.set_status_uTorsoVel({0,0,0})
 
   local steprequest = mcm.get_walk_steprequest()    

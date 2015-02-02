e74163437903e3904176d963b7f4a031a14f20bd
Stephen McGill <smcgill3@seas.upenn.edu>
2015-02-02 15:09:42 -0500

    old robots

--- Robots/Nao/Camera/test.lua
+++
@@ -1,1 +0,0 @@
-local NaoCam = require 'NaoCam'
--- Robots/OP/lua_OPKinematics.h
+++
@@ -1,7 +0,0 @@
-#ifndef luaOPKinematics_h_DEFINED
-#define luaOPKinematics_H_DEFINED
-
-#include <lua.hpp>
-int luaopen_OPKinematics(lua_State *L);
-
-#endif

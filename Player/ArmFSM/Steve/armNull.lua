1ffde8e9de6c188919eab88b5ec13a70e3a633d2
Stephen McGill <smcgill3@seas.upenn.edu>
2015-02-05 15:26:24 -0500

    almost working with IK

--- Player/ArmFSM/Steve/armNull.lua
+++ Player/ArmFSM/Steve/armNull.lua
@@ -88,12 +88,12 @@ function state.update()
       local s = torch.Tensor(sample_cur)
       cur0 = s:mean()
       -- Assign the threshold
-      cur_std = s:std()
+      cur_std = math.max(s:std(), 3)
     end
     -- We are moving if outside twice our std dev
-		if math.abs(cur-cur0) < 2*cur_std then return end
+		if math.abs(cur-cur0) < 3*cur_std then return end
 		moving = true
-    print('Moving the arm!')
+    print('Moving the arm!', cur, cur0, cur_std)
 	end
 
 	local dCur = cur - cur0
@@ -139,25 +139,28 @@ function state.update()
 	-- Must only use our initial direction to react to the human
 	-- Can just re-enter the state quickly on direction change
 	if dirCur~=dir0 then
-    print('outlier dir')
+    --print('outlier dir')
     return
   end
 
   -- Calculate the new position to use for the arm
 	local dqFromCur = qCurGain * dCur
   local q1 = q0 - dqFromCur
-  io.write('dCur', dCur, 'dir0', dir0, 'dirCur', dirCur, '\n')
-	io.write('dq*', dqFromCur*RAD_TO_DEG, 'dq', dq*RAD_TO_DEG, '\n')
-  io.write('q0', q0*RAD_TO_DEG, '\n')
-	io.write('q1', q1*RAD_TO_DEG, 'q', q*RAD_TO_DEG, '\n')
+  io.write('dCur ', dCur, '\tdir0 ', dir0, '\tdirCur ', dirCur, '\n')
+	io.write('dq* ', dqFromCur*RAD_TO_DEG, '\tdq ', dq*RAD_TO_DEG, '\n')
+  io.write('q0 ', q0*RAD_TO_DEG, '\n')
+	io.write('q1 ', q1*RAD_TO_DEG, '\tq ', q*RAD_TO_DEG, '\n')
   io.write('\n')
 
   -- Set on the robot
   -- NOTE: THIS CAN BE DANGEROUS!
+	----[[
 	q0 = q1
 	Body.set_rarm_command_position(q0, dof)
+	--]]
 
   --[[
+	q0 = q1
   local trArm = K.forward_rarm(qArm)
   -- Get the inverse using the new free dof parameter (shoulderYaw)
   local iqArm = K.inverse_rarm(trArm, qArm, q0)

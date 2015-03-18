ca69fe9e8b632fbea21849b53ec2dac2a4bdb9fd
Stephen McGill <smcgill3@seas.upenn.edu>
2015-03-18 18:03:44 -0400

    More modular code use

--- Player/ArmFSM/Steve/armTeleop.lua
+++ Player/ArmFSM/Steve/armTeleop.lua
@@ -2,80 +2,16 @@ local state = {}
 state._NAME = ...
 local Body = require'Body'
 local movearm = require'movearm'
--- Use SJ's kinematics for the mass properties
-local K = Body.Kinematics
--- Use Steve's kinematics for arm kinematics
-local K2 = require'K_ffi'
-local T = require'Transform'
 
 local USE_COMPENSATION = true
-
---[[
-SJ's arm compensation:
-calculate_com_pos -> get_torso_compensation -> get_next_movement -> plan_unified -> plan_arm_sequence -> armTeleop
---]]
 
 local t_entry, t_update, t_finish
 local timeout = 10.0
 local lPathIter, rPathIter
 local qLGoal, qRGoal
 local qLGoalFiltered, qRGoalFiltered
-local qcLArm, qcRArm, qcWaist, qLArm, qRArm, qWaist
 local qLD, qRD
 local uTorso0, uTorsoComp
-
--- Compensation items
-local torsoX = Config.walk.torsoX
-local torso0 = vector.new({-torsoX,0,0})
-
-local function get_compensation()
-	-- Legs are a bit different, since we are working in IK space
-	local bH = mcm.get_stance_bodyHeight()
-	local bT = mcm.get_stance_bodyTilt()
-	local uTorso = mcm.get_status_uTorso()
-	local aShiftX = mcm.get_walk_aShiftX()
-  local aShiftY = mcm.get_walk_aShiftY()
-	-- Current Foot Positions
-	local uLeft = mcm.get_status_uLeft()
-  local uRight = mcm.get_status_uRight()
-	local zLeg = mcm.get_status_zLeg()
-  local zSag = mcm.get_walk_zSag()
-	local zLegComp = mcm.get_status_zLegComp()
-	local zLeft,zRight = unpack(zLeg + zSag + zLegComp)
-	local pLLeg = vector.new({uLeft[1],uLeft[2],zLeft,0,0,uLeft[3]})
-  local pRLeg = vector.new({uRight[1],uRight[2],zRight,0,0,uRight[3]})
-	--
-
-	-- Initial guess is torso0, our default position.
-	-- See how far this guess is from our current torso position
-	local uTorsoAdapt = util.pose_global(torso0, uTorso)
-	local adapt_factor = 1.0
-	for i=1,4 do
-		-- Form the torso position now in the 6D space
-		local pTorso = vector.new{uTorsoAdapt[1], uTorsoAdapt[2], bH, 0, bT, uTorsoAdapt[3]}
-		local qLegs = K.inverse_legs(pLLeg, pRLeg, pTorso, aShiftX, aShiftY)
-		-- Grab the intended leg positions from this shift
-		local qLLeg = vector.slice(qLegs,1,6)
-		local qRLeg = vector.slice(qLegs,7,12)
-		-- Calculate the COM position
-		local com = K.calculate_com_pos(qcWaist, qcLArm, qcRArm, qLLeg, qRLeg, 0, 0, 0)
-		local uCOM = util.pose_global(
-			vector.new({com[1]/com[4], com[2]/com[4],0}),
-			uTorsoAdapt
-		)
-		uTorsoAdapt = uTorsoAdapt + adapt_factor * (uTorso - uCOM)
-	end
-	return uTorsoAdapt, uTorso
-end
-
-local function compensate_arms(qLGoal, qRGoal, uTorsoComp)
-	local fkL = K2.forward_larm(qLGoal)
-	local fkR = K2.forward_rarm(qRGoal)
-	local trComp = T.trans(-uTorsoComp[1],-uTorsoComp[2], 0)
-	local fkLComp = trComp * fkL
-	local fkRComp = trComp * fkR
-	return movearm.goto_tr_via_q(fkLComp, fkRComp, {qLGoal[3]}, {qRGoal[3]})
-end
 
 function state.entry()
   print(state._NAME..' Entry' )
@@ -84,8 +20,8 @@ function state.entry()
   t_entry = Body.get_time()
   t_update = t_entry
   -- Reset the human position
-	qcLArm = Body.get_larm_command_position()
-	qcRArm = Body.get_rarm_command_position()
+	local qcLArm = Body.get_larm_command_position()
+	local qcRArm = Body.get_rarm_command_position()
   hcm.set_teleop_larm(qcLArm)
   hcm.set_teleop_rarm(qcRArm)
 	-- Make sure to reset
@@ -103,12 +39,10 @@ function state.update()
   --if t-t_entry > timeout then return'timeout' end
 
 	-- Update our measurements availabl in the state
-	qcLArm = Body.get_larm_command_position()
-	qcRArm = Body.get_rarm_command_position()
-	qcWaist = Body.get_waist_command_position()
-	qLArm = Body.get_larm_position()
-	qRArm = Body.get_rarm_position()
-	qWaist = Body.get_waist_position()
+	local qcLArm = Body.get_larm_command_position()
+	local qcRArm = Body.get_rarm_command_position()
+	local qLArm = Body.get_larm_position()
+	local qRArm = Body.get_rarm_position()
 
 	-- Check reset conditions
 	if hcm.get_teleop_larm()~=qLGoal then lPathIter = nil end
@@ -122,13 +56,12 @@ function state.update()
 
 		if USE_COMPENSATION then
 			-- Grab the torso compensation
-			local uTorsoAdapt, uTorso = get_compensation()
+			local uTorsoAdapt, uTorso = movearm.get_compensation()
 			uTorso0 = uTorso
 			uTorsoComp = util.pose_relative(uTorsoAdapt, uTorso0)
 			-- Form the iterator
 			lPathIter, rPathIter, qLGoalFiltered, qRGoalFiltered, qLD, qRD =
-				compensate_arms(qLGoal, qRGoal, uTorsoComp)
-			--mcm.set_stance_uTorsoComp(uTorsoComp)
+				movearm.apply_compensation(qLGoal, qRGoal, uTorsoComp)
 		else
 			-- #nofilter
 			qLGoalFiltered, qRGoalFiltered = qLGoal, qRGoal
--- Player/ArmLib/Steve/movearm.lua
+++ Player/ArmLib/Steve/movearm.lua
@@ -4,11 +4,20 @@ local util = require'util'
 local vector = require'vector'
 local T = require'Transform'
 local P = require'libPlan'
+-- Use Steve's kinematics for arm kinematics
 local K = require'K_ffi'
+-- Use SJ's kinematics for the mass properties
+local K0 = Body.Kinematics
 local sanitize = K.sanitize
 
 local degreesPerSecond = vector.new{15,10,20, 15, 20,20,20}
 local radiansPerSecond = degreesPerSecond * DEG_TO_RAD
+-- TODO: Add dt into the joint iterator
+local dqLimit = DEG_TO_RAD / 3
+
+-- Compensation items
+local torsoX = Config.walk.torsoX
+local torso0 = vector.new({-torsoX,0,0})
 
 local lPlanner = P.new_planner(
 	vector.slice(Config.servo.min_rad, Config.parts.LArm[1], Config.parts.LArm[#Config.parts.LArm]),
@@ -21,9 +30,6 @@ local rPlanner = P.new_planner(
 	vector.slice(Config.servo.max_rad, Config.parts.RArm[1], Config.parts.RArm[#Config.parts.RArm]),
 	radiansPerSecond
 ):set_chain(K.forward_rarm, K.inverse_rarm)
-
--- TODO: Add dt into the joint iterator
-local dqLimit = DEG_TO_RAD / 3
 
 -- Take a desired joint configuration and move linearly in each joint towards it
 function movearm.goto_q(lwrist, rwrist, safe)
@@ -84,6 +90,62 @@ function movearm.goto_wrists(lwrist, rwrist)
 		rPathIter = rPlanner:joint_iter(qRGoal, qRArm, dqLimit)
 	end
 	return lPathIter, rPathIter
+end
+
+--[[
+SJ's arm compensation:
+calculate_com_pos -> get_torso_compensation -> get_next_movement -> plan_unified -> plan_arm_sequence -> armTeleop
+--]]
+function movearm.get_compensation()
+	-- Legs are a bit different, since we are working in IK space
+	local bH = mcm.get_stance_bodyHeight()
+	local bT = mcm.get_stance_bodyTilt()
+	local uTorso = mcm.get_status_uTorso()
+	local aShiftX = mcm.get_walk_aShiftX()
+  local aShiftY = mcm.get_walk_aShiftY()
+	-- Current Foot Positions
+	local uLeft = mcm.get_status_uLeft()
+  local uRight = mcm.get_status_uRight()
+	local zLeg = mcm.get_status_zLeg()
+  local zSag = mcm.get_walk_zSag()
+	local zLegComp = mcm.get_status_zLegComp()
+	local zLeft,zRight = unpack(zLeg + zSag + zLegComp)
+	local pLLeg = vector.new({uLeft[1],uLeft[2],zLeft,0,0,uLeft[3]})
+  local pRLeg = vector.new({uRight[1],uRight[2],zRight,0,0,uRight[3]})
+	--
+	local qcLArm = Body.get_larm_command_position()
+	local qcRArm = Body.get_rarm_command_position()
+	local qcWaist = Body.get_waist_command_position()
+
+	-- Initial guess is torso0, our default position.
+	-- See how far this guess is from our current torso position
+	local uTorsoAdapt = util.pose_global(torso0, uTorso)
+	local adapt_factor = 1.0
+	for i=1,4 do
+		-- Form the torso position now in the 6D space
+		local pTorso = vector.new{uTorsoAdapt[1], uTorsoAdapt[2], bH, 0, bT, uTorsoAdapt[3]}
+		local qLegs = K0.inverse_legs(pLLeg, pRLeg, pTorso, aShiftX, aShiftY)
+		-- Grab the intended leg positions from this shift
+		local qLLeg = vector.slice(qLegs,1,6)
+		local qRLeg = vector.slice(qLegs,7,12)
+		-- Calculate the COM position
+		local com = K0.calculate_com_pos(qcWaist, qcLArm, qcRArm, qLLeg, qRLeg, 0, 0, 0)
+		local uCOM = util.pose_global(
+			vector.new({com[1]/com[4], com[2]/com[4],0}),
+			uTorsoAdapt
+		)
+		uTorsoAdapt = uTorsoAdapt + adapt_factor * (uTorso - uCOM)
+	end
+	return uTorsoAdapt, uTorso
+end
+
+function movearm.apply_compensation(qLGoal, qRGoal, uTorsoComp)
+	local fkL = K.forward_larm(qLGoal)
+	local fkR = K.forward_rarm(qRGoal)
+	local trComp = T.trans(-uTorsoComp[1],-uTorsoComp[2], 0)
+	local fkLComp = trComp * fkL
+	local fkRComp = trComp * fkR
+	return movearm.goto_tr_via_q(fkLComp, fkRComp, {qLGoal[3]}, {qRGoal[3]})
 end
 
 return movearm

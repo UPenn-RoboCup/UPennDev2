348a3042beef2404fae7aeabad2fc377856e78c5
leebhoram <bhoram.lee@gmail.com>
2015-03-25 11:25:08 -0400

    plane detection for lidar log data

--- Tools/Matlab/depth_proc/detectPlaneInstances_lidar_v1.m
+++ Tools/Matlab/depth_proc/detectPlaneInstances_lidar_v1.m
@@ -2,14 +2,21 @@ function [ Planes ] = detectPlaneInstances_lidar_v1( meshRaw, visflag, resetPara
 
 persistent ONESCAN_         % single scan resolution 
 persistent NUMSCAN_      % number of scans (in horizontal direction)
-persistent v_angles;
-persistent s_angles;
+persistent v_angles
+persistent s_angles
 persistent Ccb_prev
 persistent Tcb_prev
 
+% parameters 
+persistent normalComp_param 
+persistent thre_svalue 
+persistent thre_clusterSize 
+persistent thre_memberSize 
+persistent param_meanShiftResol 
+persistent param_meanShiftWeights 
 
 if resetParam.flag 
-    loadPersistentVariablesL_0113;
+    loadPersistentVariablesL_0324;
 end
 
 if isempty(ONESCAN_),
@@ -30,15 +37,16 @@ end
 
 % if params == 0
 Planes = [];
+Points3D = [];
 PlaneID = 0;
     
 % parameters 
-normalComp_param = [2 4]; %  (w^2 + 1) half-window size
-thre_svalue = 0.02; % The smaller it is, the flatter the plane fit is 
-thre_clusterSize = 50; % number of clusters
-thre_memberSize = 30; % number of connected members (in the image domain)
-ms_resol = 0.5;% 0.6;         % mean shift resolution
-ms_weights = [0 1]; %[0.2 1];   % mean shift weights (1:image distance, 2:angular distance in normal space) 
+normalComp_param = [3 1]; %  (w^2 + 1) half-window size
+thre_svalue = 0.1; % The smaller it is, the flatter the plane fit is 
+thre_clusterSize = 500; % number of clusters
+thre_memberSize = 100; % number of connected members (in the image domain)
+param_meanShiftResol = 0.5;% 0.6;         % mean shift resolution
+param_meanShiftWeights = [0 1]; %[0.2 1];   % mean shift weights (1:image distance, 2:angular distance in normal space) 
 
 %%
 % meshRaw = reshape(typecast(meshRaw,'single'), [ONESCAN_ NUMSCAN_]);
@@ -47,7 +55,7 @@ meshRaw(meshRaw<0.5) = 0;
 [mesh_, s_, v_] = scan2DepthImg_spherical( meshRaw, s_angles, v_angles); % remove repeated measure   
 NUMS_ = size(mesh_,1);
 NUMV_ = size(mesh_,2);
-Xind = kron([1:NUMS_]',ones(1,NUMV_)); 
+Xind = repmat([1:NUMS_]',1,NUMV_); 
 Yind = repmat(1:NUMV_,NUMS_,1); % index in 1D array    
 validInd = find(mesh_>0);   
 mask = zeros(size(mesh_));
@@ -62,10 +70,13 @@ X0 = cs_.*cv_.*mesh_;
 Y0 = ss_.*cv_.*mesh_; 
 Z0  = -sv_.*mesh_ ;
 
+figure(visflag), hold off;
+showPointCloud(X0(:),Y0(:),Z0(:),[0.5 0.5 0.5],'VerticalAxis', 'Z', 'VerticalAxisDir', 'Up','MarkerSize',2);
+hold on;
 %% Normal Computation
-[N, S] = computeNormal_lidar(X0, Y0, Z0, mask, normalComp_param(1));
+[N, S] = computeNormal_lidarB(X0, Y0, Z0, mask, normalComp_param(1), normalComp_param(2));
 validNormal = (find( sum(S,1) > 0)); 
-validNormal = validNormal(find(S(4,validNormal)<0.03));
+validNormal = validNormal(find(S(4,validNormal)<thre_svalue));
 
 % figure(5), scatter3(N(1,validNormal),N(2,validNormal),N(3,validNormal),5,[0.5 0.5 0.5], 'filled'); hold on;
 
@@ -73,7 +84,7 @@ validNormal = validNormal(find(S(4,validNormal)<0.03));
 data = [  Xind(validNormal) ; Yind(validNormal)];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % generate initial mean information HERE for better starting 
-[finalMean,clusterXYcell,nMembers] = sphericalMeanShiftxyB(data,N(1:3,validNormal),ms_resol,ms_weights);
+[finalMean,clusterXYcell,nMembers] = sphericalMeanShiftxyB(data,N(1:3,validNormal),param_meanShiftResol,param_meanShiftWeights);
 
 % for each cluster
 blankConnImg = zeros(floor(NUMS_/normalComp_param(2)),NUMV_);
@@ -95,7 +106,7 @@ for tt = 1: size(finalMean,2)
             % offsets = []; % for merging?          
             % c0 = [];
             for t = 1: length(count_)                 
-                if count_(t) > thre_memberSize % if the connected bloc is big enough 
+                if count_(t) > (thre_memberSize / normalComp_param(2)) % if the connected bloc is big enough 
                     [dummy,whichcell] = intersect(index_ , indices{ids(t)});    
                     if ~isempty(whichcell)   
                      %% Find center, bbox, boundary
@@ -140,7 +151,13 @@ for tt = 1: size(finalMean,2)
                             end
  
                             PlaneID = PlaneID + 1;
-                            Planes{PlaneID} = struct('Center', Center, 'Normal', n_ , 'Points', [Pts Bbox],'Size',numel(ins));                             
+                            Planes{PlaneID} = struct('Center', Center,...
+                                                     'Normal', n_ ,...
+                                                     'Points', [Pts Bbox],...
+                                                     'Size',numel(ins));      
+                                                 
+                            
+                            Points3D{PlaneID} = [ X0(index(whichcell)); Y0(index(whichcell)); Z0(index(whichcell)) ];    
                         end
                     end
                 end
@@ -150,28 +167,7 @@ for tt = 1: size(finalMean,2)
     end % end of for each cluster
 end
     
-     % visualization
-    if visflag
-       % disp('vis');
-       % Tcb
-        Ztemp = Z0(validInd);
-        Ytemp = Y0(validInd);
-        Xtemp = X0(validInd);
-        Xvis = Ccb*[Xtemp(:)'; Ytemp(:)'; Ztemp(:)'] + repmat(Tcb,1,length(Ztemp(:)));
-        figure(visflag), [az,el] = view; hold off; 
-        scatter3( Xvis(1,:),Xvis(2,:), Xvis(3,:), 2 ,[0.5 0.5 0.5] ,'filled'); axis equal; hold on;
-        set(gca,'XDir','reverse');
-        xlabel('y');
-        ylabel('x');
-        zlabel('z','Rotation',0);        
-        % axis([0 1.5 -1 1 0 1.5]);
-        view(az,el);
-
-        figure(visflag + 1), hold off;
-        imagesc(mesh_); % axis equal;
-
-    end
-    
+       
  % Coordinate Transformation
 if 1 %~isempty(params)
     for t = 1:PlaneID  
@@ -179,14 +175,16 @@ if 1 %~isempty(params)
         Planes{t}.Center = Ccb*Planes{t}.Center + Tcb;
         Planes{t}.Points = Ccb*Planes{t}.Points + repmat(Tcb,1,size(Planes{t}.Points,2)) ;
         Planes{t}.Normal = Ccb*Planes{t}.Normal;
-
+        
+        ALL = Ccb*Points3D{t} + repmat(Tcb,1,length(Points3D{t}));
         if visflag
             randcolor = rand(1,3); % 0.5*(finalMean(3:5,tt)+1);   
             figure(visflag), 
-            scatter3(Planes{t}.Points(1,:), Planes{t}.Points(2,:), Planes{t}.Points(3,:),15,randcolor,'filled');
-            nvec = [Planes{t}.Center  Planes{t}.Center+Planes{t}.Normal*0.2];
+            showPointCloud(ALL(1,:), ALL(2,:),ALL(3,:),...
+                  randcolor,'VerticalAxis', 'Z', 'VerticalAxisDir', 'Up','MarkerSize',5);
+            nvec = [Planes{t}.Center  Planes{t}.Center+Planes{t}.Normal*0.15];
             figure(visflag),
-            plot3(nvec(1,:), nvec(2,:), nvec(3,:),'-', 'Color', randcolor, 'LineWidth',2);
+            plot3(nvec(1,:), nvec(2,:), nvec(3,:),'-', 'Color', [0 0 0], 'LineWidth',2);
         end
     end
 end

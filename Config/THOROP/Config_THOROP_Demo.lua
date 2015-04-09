4bdaa26d22e1f3c9b8dc3754aaf039fe7f07b66a
Stephen McGill <smcgill3@seas.upenn.edu>
2015-04-09 12:10:25 -0400

    Largebox

--- Config/THOROP/Config_THOROP_Demo.lua
+++ Config/THOROP/Config_THOROP_Demo.lua
@@ -8,26 +8,33 @@ local vector = require'vector'
 local demo = {}
 demo.waypoints = {
 	['deans_reception'] = {
-       vector.pose{1, 0, 0*DEG_TO_RAD},
-        vector.pose{2, 0, 0*DEG_TO_RAD},
-        vector.pose{3, 0, 0*DEG_TO_RAD},
-        vector.pose{4, 0, 0*DEG_TO_RAD},
-        vector.pose{5, 0, 0*DEG_TO_RAD},
---		vector.pose{1, 0, 0*DEG_TO_RAD},
---		vector.pose{1, 1, 90*DEG_TO_RAD},
---		vector.pose{2, 1, 0*DEG_TO_RAD},
---		vector.pose{10, 0, 0*DEG_TO_RAD},
+		vector.pose{1, 0, 0*DEG_TO_RAD},
+		vector.pose{2, 0, 0*DEG_TO_RAD},
+		vector.pose{3, 0, 0*DEG_TO_RAD},
+		vector.pose{4, 0, 0*DEG_TO_RAD},
+		vector.pose{5, 0, 0*DEG_TO_RAD},
 	},
 }
+
+local smallbox = {
+	--trLArm =
+	--trRArm =
+	qLArm = vector.new{106.287, 56.4949, -0.0387251, -113.479, -47.9627, -30.4474, 0.854519}*DEG_TO_RAD,
+	qRArm = vector.new{76.7008, -42.5073, -32.2451, -89.8207, 48.285, 69.9745, -11.2936}*DEG_TO_RAD,
+	qLGrip = vector.new{-12.3047, -49.1309, 6.76758}*DEG_TO_RAD,
+	qRGrip = vector.new{-25.9668, -123.154, -60.9961}*DEG_TO_RAD,
+}
+local largebox = {
+	--trLArm =
+	--trRArm =
+	qLArm = vector.new{99.4202, 46.1582, 0.027251, -82.8667, -44.5019, -69.7399, 18.4759}*DEG_TO_RAD,
+	qRArm = vector.new{78.4585, -47.0316, -40.9898, -76.3207, 56.7093, 75.1538, -30.3858}*DEG_TO_RAD,
+	qLGrip = vector.new{-32.5195, -20.127, 29.707}*DEG_TO_RAD,
+	qRGrip = vector.new{-6.63086, -116.123, -48.3398}*DEG_TO_RAD,
+}
+
 demo.arms = {
-	['dean'] = {
-		--trLArm =
-		--trRArm =
-		qLArm = vector.new{106.287, 56.4949, -0.0387251, -113.479, -47.9627, -30.4474, 0.854519}*DEG_TO_RAD,
-		qRArm = vector.new{76.7008, -42.5073, -32.2451, -89.8207, 48.285, 69.9745, -11.2936}*DEG_TO_RAD,
-		qLGrip = vector.new{-12.3047, -49.1309, 6.76758}*DEG_TO_RAD,
-		qRGrip = vector.new{-25.9668, -123.154, -60.9961}*DEG_TO_RAD,
-	},
+	['dean'] = largebox
 }
 
 --[[
@@ -44,6 +51,19 @@ demo.arms = {
 
 -- Left grip: 3rd is spport
 -- Right grip: 2nd is support
+
+-- Large box
+--[[
+>  =Body.get_rarm_command_position()*RAD_TO_DEG
+[1] {78.4585, -47.0316, -40.9898, -76.3207, 56.7093, 75.1538, -30.3858}
+>  =Body.get_larm_command_position()*RAD_TO_DEG
+[1] {99.4202, 46.1582, 0.027251, -82.8667, -44.5019, -69.7399, 18.4759}
+>  =Body.get_rgrip_position()*RAD_TO_DEG
+[1] {-6.63086, -116.123, -48.3398}
+>  =Body.get_lgrip_position()*RAD_TO_DEG
+[1] {-32.5195, -20.127, 29.707}
+--]]
+
 
 Config.demo = demo
 

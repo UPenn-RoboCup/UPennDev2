bfe829942f71c807f85eb1ae5233adde4f6019e4
Stephen McGill <smcgill3@seas.upenn.edu>
2015-04-09 11:28:03 -0400

    New arm config for the small box

--- Config/THOROP/Config_THOROP_Demo.lua
+++ Config/THOROP/Config_THOROP_Demo.lua
@@ -23,23 +23,23 @@ demo.arms = {
 	['dean'] = {
 		--trLArm =
 		--trRArm =
-		qLArm = vector.new{101, 35.0032, 0.00215139, -113.999, -21.8187, -33.9864, -6.35141}*DEG_TO_RAD,
-		qRArm = vector.new{70, -53, -36, -90, 60, 70, -11}*DEG_TO_RAD,
-		qLGrip = vector.new{-6, -50, 13}*DEG_TO_RAD,
-		qRGrip = vector.new{5, -100, -64}*DEG_TO_RAD
+		qLArm = vector.new{106.287, 56.4949, -0.0387251, -113.479, -47.9627, -30.4474, 0.854519}*DEG_TO_RAD,
+		qRArm = vector.new{76.7008, -42.5073, -32.2451, -89.8207, 48.285, 69.9745, -11.2936}*DEG_TO_RAD,
+		qLGrip = vector.new{-12.3047, -49.1309, 6.76758}*DEG_TO_RAD,
+		qRGrip = vector.new{-25.9668, -123.154, -60.9961}*DEG_TO_RAD
 	},
 }
 
 --[[
 
->  return Body.get_larm_position()*RAD_TO_DEG
-[1] {100.157, 23.0945, -14.154, -71.2635, -37.0116, -70.9179, 38.688}
->  return Body.get_rarm_position()*RAD_TO_DEG
-[1] {85.4061, -26.2384, -20.0216, -60.7991, 207.627, -76.7064, -37.1212}
+>  =Body.get_larm_position()*RAD_TO_DEG
+[1] {106.289, 56.4949, -0.0372908, -113.477, -47.9627, -30.4474, 0.854519}
+>  =Body.get_rarm_position()*RAD_TO_DEG
+[1] {76.7001, -42.5087, -32.2444, -89.82, 48.2874, 69.9721, -11.296}
 >  return Body.get_lgrip_position()*RAD_TO_DEG
-[1] {-5.97656, -50.4492, 13.2715}
+[1] {-12.3047, -49.1309, 6.76758}
 >  return Body.get_rgrip_position()*RAD_TO_DEG
-[1] {-76.2402, -27.5293, -39.2871}
+[1] {-25.9668, -123.154, -60.9961}
 
 --]]
 Config.demo = demo

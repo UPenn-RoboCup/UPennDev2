68265598906e8f5143400df9d25eedc19c05d6b3
Stephen McGill <smcgill3@seas.upenn.edu>
2015-04-15 15:30:55 -0400

    NO LCP issues

--- Player/BodyFSM/Steve/bodyApproach.lua
+++ Player/BodyFSM/Steve/bodyApproach.lua
@@ -10,14 +10,16 @@ local t_entry, t_update, t_exit
 local wp_thread
 local waypoints = {
 	vector.pose{1, 0, 0*DEG_TO_RAD},
-	vector.pose{2, 0, 0*DEG_TO_RAD},
-	vector.pose{3, 0, 0*DEG_TO_RAD},
-	vector.pose{4, 0, 0*DEG_TO_RAD},
-	vector.pose{5, 0, 0*DEG_TO_RAD},
+	vector.pose{1, 0, -45*DEG_TO_RAD},
+	vector.pose{2, -1, -45*DEG_TO_RAD},
+	vector.pose{3, -2, -45*DEG_TO_RAD},
+	vector.pose{3, -2, 45*DEG_TO_RAD},
+	vector.pose{4, -1, 45*DEG_TO_RAD},
+	vector.pose{5, 0, 45*DEG_TO_RAD},
 }
 local dist_threshold = 0.05
 local angle_threshold = 5 * DEG_TO_RAD
-local maxStep = 0.06
+local maxStep = 0.07
 local maxTurn = 0.10
 
 local sqrt = math.sqrt
@@ -53,32 +55,27 @@ function state.entry()
   t_entry = Body.get_time()
   t_update = t_entry
 
-	-- Make our coroutine
+	-- Start walking
+	motion_ch:send'hybridwalk'
+
+	-- Make our waypoint follower
 	wp_thread = coroutine.create(function(waypoints)
-			util.ptable(waypoints)
-			local pose, pBias = coroutine.yield()
+			local pose, pBias = coroutine.yield({0,0,0})
 			for i, wp in ipairs(waypoints) do
 				print('bodyApproach | Waypoint', wp)
-				local betweenWP = true
-				while betweenWP do
+				while true do
 					local pOffset = util.pose_global(pBias, {0,0,pose[3]})
 					local wp_adjusted = util.pose_relative(pOffset, wp)
 					local vel, dR, dA = robocup_approach(USE_ADJUSTMENT and wp_adjusted or wp, pose)
-					if dR<dist_threshold and dA<angle_threshold then
-						betweenWP = false
-						pose, pBias = coroutine.yield({0,0,0})
-					else
-						pose, pBias = coroutine.yield(vel)
-					end
+					if dR<dist_threshold and dA<angle_threshold then break end
+					pose, pBias = coroutine.yield(vel)
 				end
+				pose, pBias = coroutine.yield({0,0,0})
 			end
 			return {0,0,0}
 		end)
 	-- set the waypoints
 	coroutine.resume(wp_thread, waypoints)
-
-	-- Start walking
-	motion_ch:send'hybridwalk'
 	
 end
 
@@ -94,16 +91,11 @@ function state.update()
 	local pose = vector.pose(wcm.get_robot_pose())
 	local status, velocity = coroutine.resume(wp_thread, pose, pBias)
 	
+	-- Exit if the coroutine has no more waypoints left to follow
 	if not status then return'done' end
 	
+	-- Set the walking velocity
 	mcm.set_walk_vel(velocity)
-	
-	--[[
-	print('pose', pose)
-	print('target_pose', target_pose)
-	print('dist', dist)
-	print('vel', vel)
-	--]]
 
 end
 

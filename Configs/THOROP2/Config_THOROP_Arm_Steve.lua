eab5b9a7f525aadd37440f5dfe24123b58a79848
Stephen McGill <smcgill3@seas.upenn.edu>
2015-04-29 10:43:54 -0700

    THOROP2 Configs

---
+++ Configs/THOROP2/Config_THOROP_Arm_Steve.lua
@@ -0,0 +1,50 @@
+assert(Config, 'Need a pre-existing Config table!')
+local vector = require'vector'
+local T = require'Transform'
+local tr6D = require'Transform'.transform6D
+
+------------------------------------
+-- For the arm FSM
+local arm = {}
+
+-- Default init position
+arm.trLArm0 = {0.05, 0.35, -0.25,0,0,0}
+arm.trRArm0 = {0.05, -0.35, -0.25,0,0,0}
+
+-- Stages: L, R (Transforms or joints), type of motion, left shoulder search weights, right weights
+-- Planner: usage, diff, tight
+arm.readyFromInitStages = {
+	{
+		tr6D{0.25, 0.3, -0.1,  0,0,0}, tr6D{0.25, -0.3, -0.1, 0,0,0},
+		'goto_tr_via_q', {0,1,0}, {0,1,0}
+	},
+	{
+		tr6D{0.25, 0.3, 0,  0,0,-45*DEG_TO_RAD}, tr6D{0.25, -0.3, 0, 0,0,45*DEG_TO_RAD},
+		'goto_tr_via_q', {1,1,0}, {1,1,0}
+	},
+	{
+		tr6D{0.28, 0.25, 0.2,  0,0,-45*DEG_TO_RAD}, tr6D{0.28, -0.25, 0.2, 0,0,45*DEG_TO_RAD},
+		'goto_tr_via_q', {1,0,1}, {1,0,1}
+	},
+}
+
+
+--Gripper end position offsets (Y is inside)
+arm.handoffset = {}
+--0.130 + 0.60+0.50
+--arm.handoffset.gripper = {0.241,0,0} --Default gripper
+arm.handoffset.gripper = {0.23,0,0} --Default gripper (VT)
+--0.130+0.139+0.80-0.10
+arm.handoffset.outerhook = {0.339,0,0.060} --Single hook (for door)
+--0.130 + 0.140+0.80-0.10
+--arm.handoffset.chopstick = {0.340,0,0} --Two rod (for valve)
+--FROM EMPIRICAL DATA
+arm.handoffset.chopstick = {0.440,0,0} --Two rod (for valve)
+--New 3 finger gripper
+arm.handoffset.gripper3 = {0.28,-0.05,0}
+
+
+-- Export
+Config.arm = arm
+
+return Config
---
+++ Configs/THOROP2/Config_THOROP_FSM_DRCFinal.lua
@@ -0,0 +1,134 @@
+assert(Config, 'Need a pre-existing Config table!')
+
+local fsm = {}
+
+-- Do we disable FSMs?
+fsm.disabled = false
+
+-- Do we disable Kick?
+--fsm.disable_kick = true
+fsm.disable_kick = false
+
+Config.torque_legs = true
+
+-- Update rate in Hz
+fsm.update_rate = 100
+
+-- Which FSMs should be enabled?
+fsm.enabled = {
+  Arm = true,
+  Body = true,
+  Head = true,
+  Motion = true,
+  Lidar = true,
+	Gripper = false
+}
+
+--SJ: now we can have multiple FSM options
+fsm.select = {
+  Arm = 'DRCFinal',
+  Head = 'DRCFinal',
+  Body = 'DRCFinal',
+  Motion = 'DRCFinal'
+}
+
+--SJ: custom library selection moved to HERE
+fsm.libraries = {
+  ArmLib = 'DRCFinal',
+  MotionLib = 'DRCFinal',
+  World = 'DRCFinal'
+}
+
+
+
+fsm.Lidar = {
+  {'lidarIdle', 'pan', 'lidarPan'},
+  {'lidarPan', 'switch', 'lidarPan'},
+  {'lidarPan', 'stop', 'lidarIdle'},
+}
+
+fsm.Head = {
+  {'headIdle', 'teleop', 'headTeleop'},
+}
+
+fsm.Body = {
+  {'bodyIdle', 'init', 'bodyInit'},
+  {'bodyInit', 'done', 'bodyStop'},
+
+  {'bodyStop', 'uninit', 'bodyUnInit'},
+  {'bodyUnInit', 'done', 'bodyIdle'},
+
+  {'bodyStop', 'approach', 'bodyApproach'},
+  {'bodyApproach', 'done', 'bodyStop'},  
+
+--[[
+  {'bodyStop', 'stepinplace', 'bodyStepPlace'},
+  {'bodyStepPlace',   'done', 'bodyStop'},
+
+  {'bodyStop', 'stepover1', 'bodyStep'},
+  {'bodyStep', 'nextstep', 'bodyStep'},
+  {'bodyStep', 'done', 'bodyStop'},
+--]]
+}
+
+
+--Cut down arm FSM for now
+
+fsm.Arm = {
+  {'armIdle', 'timeout', 'armIdle'},
+  {'armIdle', 'init', 'armInit'},
+  {'armInit', 'done', 'armPose1'},
+
+  {'armPose1', 'teleop', 'armTeleop'},
+--  {'armPose1', 'toolgrab', 'armToolGrip'},
+--  {'armPose1', 'pushdoorgrab', 'armPushDoorSideGrip'},
+--  {'armPose1', 'doorgrab', 'armPullDoorSideGrip'},
+  
+  {'armToolGrip', 'done', 'armPose1'},
+--  {'armToolGrip', 'hold', 'armToolHold'},
+  {'armTeleop', 'done', 'armPose1'},
+
+
+  {'armPose1', 'uninit', 'armUnInit'},
+  {'armToolGrip', 'uninit', 'armUnInit'},
+  {'armTeleop', 'uninit', 'armUnInit'},
+
+  {'armUnInit', 'done', 'armIdle'},
+}
+
+
+
+fsm.Motion = {
+  {'motionIdle', 'timeout', 'motionIdle'},
+  {'motionIdle', 'stand', 'motionInit'},
+  {'motionInit', 'done', 'motionStance'},
+
+  {'motionIdle', 'bias', 'motionBiasInit'},
+  {'motionStance', 'bias', 'motionBiasInit'},
+  {'motionBiasInit', 'done', 'motionBiasIdle'},
+  {'motionBiasIdle', 'stand', 'motionInit'},
+
+
+  {'motionStance', 'preview', 'motionStepPreview'},
+  {'motionStepPreview', 'done', 'motionStance'},
+
+  {'motionStance', 'stair', 'motionStepPreviewStair'},
+  {'motionStepPreviewStair', 'done', 'motionStance'},
+
+  {'motionStance', 'hybridwalk', 'motionHybridWalkInit'},
+  {'motionHybridWalkInit', 'done', 'motionHybridWalk'},
+  {'motionHybridWalk', 'done', 'motionHybridWalkEnd'},
+  {'motionHybridWalkEnd', 'done', 'motionStance'},
+
+  {'motionStance', 'uninit', 'motionUnInit'},
+  {'motionUnInit', 'done', 'motionIdle'},
+
+}
+
+fsm.dqNeckLimit = {
+  60 * DEG_TO_RAD, 60 * DEG_TO_RAD
+}
+
+Config.fsm = fsm
+
+return Config

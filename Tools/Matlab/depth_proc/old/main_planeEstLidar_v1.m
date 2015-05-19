4176ff110a17eba230f331fa470c6e377e2947bb
leebhoram <bhoram.lee@gmail.com>
2015-05-19 17:18:55 -0400

    old scripts

---
+++ Tools/Matlab/depth_proc/old/main_planeEstLidar_v1.m
@@ -0,0 +1,43 @@
+% Plane Instance Detection Example
+% by Bhoram Lee
+%
+% Input: Lidar scans 
+% Output: 
+% (1) surface normals, 
+% (2) some boundary points 
+% All in camera centered coordinate
+clear all;
+close all;
+
+% foldername = '/home/leebhoram/Data/mesh_logs/Unpacked/';
+% datestamp = '03.11.2015.15.30.41l'; % Testbed: walls (near valve)
+% datestamp = '03.11.2015.15.45.07l';
+
+foldername = '/home/leebhoram/Data/LOGS_Lab_0324_2/Unpacked/';
+datestamp = '03.25.2015.16.36.42l'; 
+
+[ fileSequence] = getMatFilesFromFolder( strcat(foldername,datestamp));
+ 
+ts = 0;
+prevts = 0;
+for ilog=1:length(fileSequence)
+    ilog
+    metal = [];
+    load(fileSequence{ilog}); 
+       
+    if 1   
+  
+    metal.flag = 1;
+    [ Planes ] = detectPlaneInstances_lidar_v1( meshRaw', 3, metal);
+    % [res, meta] = detectPlanes5(depthRaw, metad, ui);
+    
+    ilog
+     
+
+    end
+   % prevts = metad.t;   
+  %  F(iLog) = getframe(gcf);    
+end 
+
+
+
---
+++ Tools/Matlab/depth_proc/old/main_planeEstLidar_v3.m
@@ -0,0 +1,45 @@
+% Plane Instance Detection Example
+% by Bhoram Lee
+%
+% Input: Lidar scans 
+% Output: 
+% (1) surface normals, 
+% (2) some boundary points 
+% All in camera centered coordinate for now
+clear all;
+close all;
+
+foldername = '/home/leebhoram/Data/Webots_log/Unpacked/';
+datestamp = '04.21.2015.15.07.32l'
+%datestamp = '04.22.2015.15.59.32l';
+
+% foldername = '/home/leebhoram/Data/mesh_logs/Unpacked/';
+% datestamp = '03.11.2015.15.30.41l'; % Testbed: walls (near valve)
+% datestamp = '03.11.2015.15.45.07l';
+
+% foldername = '/home/leebhoram/Data/LOGS_Lab_0325_3/Unpacked/';
+% datestamp = '03.25.2015.16.36.42l'; 
+% datestamp = '03.25.2015.16.40.20l';
+
+[ fileSequence] = getMatFilesFromFolder( strcat(foldername,datestamp));
+ 
+ts = 0;
+prevts = 0;
+for ilog=4:length(fileSequence)
+    ilog
+    metal = [];
+    load(fileSequence{ilog}); 
+    metal.dims = metal.dim;   
+    
+    if 1   
+  
+        metal.flag = 1;
+        [ Planes ] = detectPlaneInstances_lidar_v3( meshRaw', 3, metal);
+     
+    end 
+  
+  pause(0.01);
+end 
+
+
+

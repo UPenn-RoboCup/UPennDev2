4f54b391563a3a76adc974cef22eaa34dbbc7948
Stephen McGill <smcgill3@seas.upenn.edu>
2015-05-19 19:04:08 -0700

    Remove some old items

--- Run/mesh_wizard.lua
+++ Run/mesh_wizard.lua
@@ -4,284 +4,217 @@ local ENABLE_LOG = false
 -- Accumulate lidar readings into an image for mesh viewing
 -- (c) Stephen McGill, Seung Joon Yi, 2013, 2014
 dofile'../include.lua'
-local ffi = require'ffi'
-local torch = require'torch'
+local libMesh = require'libMesh'
 local si = require'simple_ipc'
 local mpack = require'msgpack.MessagePack'.pack
 local munpack = require('msgpack.MessagePack')['unpack']
-local p_compress = require'png'.compress
-local j_compress = require'jpeg'.compressor'gray'
-local vector = require'vector'
 local Body = require'Body'
-
 require'vcm'
 require'hcm'
 
--- Shared with LidarFSM
--- t_sweep: Time (seconds) to fulfill scan angles in one sweep
--- mag_sweep: How much will we sweep over
--- ranges_fov: In a single scan, which ranges to use
-local t_sweep, mag_sweep, ranges_fov
--- NOTE: The following is LIDAR dependent
-local t_scan = 1 / 40 -- Time to gather returns
+local mesh0_udp_ch, mesh1_udp_ch
+local mesh0_ch, mesh1_ch
+local t_send_mesh0 = -math.huge
+local t_send_mesh1 = -math.huge
+local libLog, logger
+local mesh0, mesh1
+local mag_sweep0, t_sweep0, ranges_fov0
+local mag_sweep1, t_sweep1, ranges_fov1
 
--- Open up channels to send/receive data
-local operator
-if Config.net.use_wireless then
-	operator = Config.net.operator.wireless
-else
-	operator = Config.net.operator.wired
-end
-local stream = Config.net.streams.mesh
-local mesh_udp_ch
-local mesh_tcp_ch = stream.tcp and si.new_publisher(stream.tcp)
-local mesh_ch = stream.sub and si.new_publisher(stream.sub)
--- Need UDP when competing
-if Config.IS_COMPETING then
-	print("UDP", operator, stream.udp)
-	mesh_udp_ch = stream.udp and si.new_sender(operator, stream.udp)
-end
-
-local libLog, logger, nlog
-if ENABLE_LOG then
-	libLog = require'libLog'
-	logger = libLog.new('mesh', true)
-	nlog = 0
-end
-
-local metadata = {
-	id = 'mesh0',
-	t = 0,
-}
-
-local scan_pose, scan_angles, scan_local, scan_global
--- Setup tensors for a lidar mesh
-local mesh, mesh_byte, mesh_adj, offset_idx
-local n_scanlines
-local function setup_mesh(meta)
-	local n, res = meta.n, meta.res
-	local fov = n * res
-	-- Check that we can access enough FOV
-	local min_view, max_view = unpack(ranges_fov)
-	assert(fov > max_view-min_view, 'Not enough FOV available')
-	-- Find the offset for copying lidar readings into the mesh
-	-- if fov is from -fov/2 to fov/2 degrees, then offset_idx is zero
-	-- if fov is from 0 to fov/2 degrees, then offset_idx is sensor_width/2
-	local fov_offset = min_view / res + n / 2
-	-- Round the offset (0 based offset)
-	offset_idx = math.floor(fov_offset + 0.5)
-	-- Round to get the number of returns for each scanline
-	n_returns = math.floor((max_view - min_view) / res + 0.5)
-	--print("n_returns", n_returns, max_view, min_view, res)
-	-- Check the number of scanlines in each mesh
-	-- Indexed by the actuator angle
-	-- Depends on the speed we use
-	n_scanlines = math.floor(t_sweep / t_scan + 0.5)
-	-- In-memory mesh
-	mesh = torch.FloatTensor(n_scanlines, n_returns):zero()
-	-- Mesh buffers for compressing and sending to the user
-	mesh_adj  = torch.FloatTensor(n_scanlines, n_returns):zero()
-	mesh_byte = torch.ByteTensor(n_scanlines, n_returns):zero()
-	-- Data for each scanline
-	if TORCH_SCANLINE_INFO then
-		scan_angles = torch.DoubleTensor(n_scanlines):zero()
-		scan_local = torch.DoubleTensor(n_scanlines,6):zero()
-		scan_global = torch.DoubleTensor(n_scanlines,6):zero()
-	else
-		scan_angles = vector.zeros(n_scanlines)
-		scan_local = vector.zeros(n_scanlines)
-		scan_global = vector.zeros(n_scanlines)
-	end
-	-- Metadata for the mesh
-	metadata.rfov = ranges_fov
-	metadata.sfov = {-mag_sweep / 2, mag_sweep / 2}
-	metadata.a = scan_angles
-	metadata.tfL6 = scan_local
-	metadata.tfG6 = scan_global
-	-- Add the dimensions (useful for raw)
-	metadata.dims = {n_scanlines, n_returns}
-end
-
--- Convert a pan angle to a column of the chest mesh image
-local scanline, direction
-local function angle_to_scanlines(rad)
-	-- Find the scanline
-	local ratio = (rad + mag_sweep / 2) / mag_sweep
-	local scanline_now = math.floor(ratio*n_scanlines+.5)
-	scanline_now = math.max(math.min(scanline_now, n_scanlines), 1)
-	local prev_scanline = scanline or scanline_now
-	scanline = scanline_now
-	-- Find the direction
-	local direction_now = vcm.get_mesh_state()
-	local prev_direction = direction or direction_now
-	direction = direction_now
-	-- If unknown, then only populate a single scanline
-	if direction == 0 then return {scanline} end
-	-- Find the set of scanlines for copying the lidar reading
-	local scanlines = {}
-	-- No direction change
-	if direction==prev_direction then
-		-- Fill all lines between previous and now
-		for s=prev_scanline+direction,scanline,direction do table.insert(scanlines,s) end
-		return scanlines
-	end
-	-- Changed directions, so populate the borders, too
-	if direction > 0 then
-		local fill_line = math.max(prev_scanline - 1, scanline)
-		for s=1,fill_line do table.insert(scanlines, s) end
-	else
-		local fill_line = math.min(prev_scanline + 1, scanline)
-		for s=fill_line,n_scanlines do table.insert(scanlines, s) end
-	end
-	return scanlines
-end
-
-local compression = {
-	[0] = 'jpeg',
-	[1] = 'png',
-	[2] = 'raw'
-}
-
-local function send_mesh(compression, dynrange)
-	local near, far = unpack(dynrange)
-	if near>far then
-		print('Near greater than far...')
-		return
-	end
-	-- Enhance the dynamic range of the mesh image
-	mesh_adj:copy(mesh):add(-near)
-	mesh_adj:mul(255/(far-near))
-	-- Ensure that we are between 0 and 255
-	mesh_adj[torch.lt(mesh_adj,0)] = 0
-	mesh_adj[torch.gt(mesh_adj,255)] = 255
-	mesh_byte:copy(mesh_adj)
-	-- Update relevant metadata
-	metadata.c = compression
-	metadata.dr = dynrange
-	metadata.t = Body.get_time()
-	-- Compression
-	local c_mesh
-	if compression=='jpeg' then
-		c_mesh = j_compress:compress(mesh_byte)
-	elseif compression=='png' then
-		c_mesh = p_compress(mesh_byte)
-	else
-		-- Raw
-		c_mesh = ffi.string(mesh:data(), mesh:nElement() * ffi.sizeof'float')
-	end
-	-- Send away
-	mesh_ch:send{mpack(metadata), c_mesh}
-	mesh_tcp_ch:send{mpack(metadata), c_mesh}
-	if mesh_udp_ch then
-		local ret, err = mesh_udp_ch:send(mpack(metadata)..c_mesh)
-		print('Mesh | Sent UDP', err or 'successfully')
-	end
-end
-local t_send_mesh = -math.huge
 local function check_send_mesh()
-	local net = vcm.get_mesh_net()
-	local request, comp = unpack(net)
-	local t_check = Body.get_time()
+	local request = false
+	local t = Body.get_time()
 	local n_open = hcm.get_network_open()
 	local t_open
 	if n_open==1 then
 		t_open = hcm.get_network_topen()
-		if t_open - t_send_mesh > 0.5 then request = 1 end
+		if t_open - t_send_mesh > 0.5 then
+			request = true
+		end
 	end
-	if request==0 then return end
-	local dynrange = vcm.get_mesh_dynrange()
-	send_mesh(compression[comp], dynrange)
-	t_send_mesh = t_check
-	
-	-- Reset the request
-	net[1] = 0
-	vcm.set_mesh_net(net)
+	if mesh0 and (t-t_send_mesh0>t_sweep0 or request) then
+		t_send_mesh0 = t
+		local metadata = mesh0.metadata
+		metadata.t = t
 
+		mesh0:dynamic_range(vcm.get_mesh0_dynrange())
+		local c_mesh = mesh0:get_png_string2()
+
+		-- Send away
+		if mesh0_ch then
+			metadata.c = 'raw'
+			mesh0_ch:send{mpack(metadata), mesh0:get_raw_string()}
+			--metadata.c = 'png'
+			--mesh0_ch:send{mpack(metadata), c_mesh}
+		end
+		if mesh0_udp_ch then
+			metadata.c = 'png'
+			local meta = mpack(metadata)
+			local ret, err = mesh0_udp_ch:send(meta..c_mesh)
+			--print('Mesh0 | Sent UDP', unpack(ret))
+		end
+		io.write('Mesh0 | Sending\n')
+	end
+
+	if mesh1 and (t-t_send_mesh1>t_sweep1 or request) then
+		t_send_mesh1 = t
+		local metadata = mesh1.metadata
+		metadata.t = t
+		mesh1:dynamic_range(vcm.get_mesh1_dynrange())
+		local c_mesh = mesh1:get_png_string2()
+
+		-- Send away
+		if mesh1_ch then
+			metadata.c = 'raw'
+			mesh1_ch:send{mpack(metadata), mesh1:get_raw_string()}
+			--metadata.c = 'png'
+			--mesh1_ch:send{mpack(metadata), c_mesh}
+		end
+		if mesh1_udp_ch then
+			metadata.c = 'png'
+			local meta = mpack(metadata)
+			local ret, err = mesh1_udp_ch:send(meta..c_mesh)
+			--print('Mesh1 | Sent UDP', unpack(ret))
+		end
+		print('Mesh1')
+	end
 
 	-- Log
-	-- Do the logging if we wish
-	if ENABLE_LOG then
-		metadata.rsz = mesh:nElement() * ffi.sizeof'float'
-		logger:record(metadata, mesh:data(), metadata.rsz)
-		nlog = nlog + 1
-		print(request, "# mesh logs: "..nlog, metadata.rsz)
-		if nlog % 100 == 0 then
-			logger:stop()
-			logger = libLog.new('mesh', true)
-			print('Open new log!')
+	if logger0 and mesh0 then
+		local raw_str = mesh0:get_raw_string()
+		mesh0.metadata.rsz = #raw_str
+		logger0:record(mesh0.metadata, raw_str)
+		if logger0.n % 10 == 0 then
+			logger0:stop()
+			logger0 = libLog.new('mesh0', true)
+			print('Mesh0 | Open new log!')
+		end
+	end
+
+	-- Log
+	if logger1 and mesh1 then
+		local raw_str = mesh1:get_raw_string()
+		mesh1.metadata.rsz = #raw_str
+		logger1:record(mesh1.metadata, raw_str)
+		if logger1.n % 10 == 0 then
+			logger1:stop()
+			logger1 = libLog.new('mesh1', true)
+			print('Mesh1 | Open new log!')
 		end
 	end
 
 end
 
-local function update(meta, ranges)
-	-- Check shared parameters
-	local mag_sweep0, t_sweep0 = unpack(vcm.get_mesh_sweep())
-	local ranges_fov0 = vcm.get_mesh_fov()
-	mag_sweep0 = math.min(math.max(mag_sweep0, 10 * DEG_TO_RAD), math.pi)
-	t_sweep0 = math.min(math.max(t_sweep0, 1), 20)
-	-- Check if updated parameters
-	if not mesh or mag_sweep~=mag_sweep0 or t_sweep~=t_sweep0 or ranges_fov~=ranges_fov0 then
-		mag_sweep = mag_sweep0
-		t_sweep = t_sweep0
-		ranges_fov = ranges_fov0
-		setup_mesh(meta)
-		print('Mesh | Updated containers')
+local function entry()
+	local stream0 = Config.net.streams.mesh0
+	local stream1 = Config.net.streams.mesh1
+	local operator = Config.net.use_wireless and Config.net.operator.wireless or Config.net.operator.wired
+	mesh0_udp_ch = stream0.udp and si.new_sender(operator, stream0.udp)
+require'util'.ptable(stream0)
+print('operator', operator, stream0.udp)
+	mesh0_ch = stream0.sub and si.new_publisher(stream0.sub)
+	mesh1_udp_ch = stream1.udp and si.new_sender(operator, stream1.udp)
+	mesh1_ch = stream1.sub and si.new_publisher(stream1.sub)
+	if ENABLE_LOG then
+		libLog = require'libLog'
+		logger0 = libLog.new('mesh0', true)
+		logger1 = libLog.new('mesh1', true)
 	end
-	-- Metadata
-	local pose = vector.pose(meta.pose)
-	local tfL6 = vector.new(meta.tfL6)
-	local tfG6 = vector.new(meta.tfG6)
+end
 
-	-- Find the scanline indices
-	local rad_angle = meta.angle
-	local scanlines = angle_to_scanlines(rad_angle)
-	local byte_sz = mesh:size(2) * ffi.sizeof'float'
-	local float_ranges = ffi.cast('float*', ranges)
-	local dest
-	for _,line in ipairs(scanlines) do
-		if line >= 1 and line<=n_scanlines then
-			dest = mesh:select(1, line) -- NOTE: must be contiguous
-			ffi.copy(dest:data(), float_ranges + offset_idx, byte_sz)
-			-- Save the pan angle
-			scan_angles[line] = rad_angle
-			-- Save the torso compensation
-			scan_local[line] = tfL6
-			scan_global[line] = tfG6
+
+local function update(meta, ranges)
+--require'util'.ptable(meta)
+	-- Check shared parameters
+	if meta.id=='lidar0' then
+		local mag_sweep, t_sweep = unpack(vcm.get_mesh0_sweep())
+		mag_sweep = math.min(math.max(mag_sweep, 10 * DEG_TO_RAD), math.pi)
+		t_sweep = math.min(math.max(t_sweep, 1), 20)
+		local ranges_fov = vcm.get_mesh0_fov()
+		-- Check if updated parameters
+		if not mesh0 or mag_sweep~=mag_sweep0 or t_sweep~=t_sweep0 or ranges_fov~=ranges_fov0 then
+			-- lidar is 40Hz
+			local t_scan = 1 / 40
+			mag_sweep0 = mag_sweep
+			t_sweep0 = t_sweep
+			ranges_fov0 = ranges_fov
+			mesh0 = libMesh.new('mesh0', {
+				n_lidar_returns = meta.n,
+				lidar_resolution = meta.res,
+				rfov = ranges_fov0,
+				sfov = {-mag_sweep / 2, mag_sweep / 2},
+				n_scanlines = math.floor(t_sweep / t_scan + 0.5),
+			})
+			print('Mesh0 | Updated containers')
 		end
+		mesh0:add_scan(meta.angle, ranges, meta)
+	elseif meta.id=='lidar1' then
+		local mag_sweep, t_sweep = unpack(vcm.get_mesh1_sweep())
+		mag_sweep = math.min(math.max(mag_sweep, 10 * DEG_TO_RAD), math.pi)
+		t_sweep = math.min(math.max(t_sweep, 1), 20)
+		local ranges_fov = vcm.get_mesh1_fov()
+		-- Check if updated parameters
+		if not mesh1 or mag_sweep~=mag_sweep1 or t_sweep~=t_sweep1 or ranges_fov~=ranges_fov1 then
+			-- lidar is 40Hz
+			local t_scan = 1 / 40
+			mag_sweep1 = mag_sweep
+			t_sweep1 = t_sweep
+			ranges_fov1 = ranges_fov
+			mesh1 = libMesh.new('mesh1', {
+				n_lidar_returns = meta.n,
+				lidar_resolution = meta.res,
+				rfov = ranges_fov1,
+				sfov = {0, mag_sweep},
+				n_scanlines = math.floor(t_sweep / t_scan + 0.5),
+			})
+			print('Mesh1 | Updated containers')
+		end
+		mesh1:add_scan(meta.angle[2], ranges, meta)
 	end
+
 	-- Check for sending out on the wire
 	-- TODO: This *should* be from another ZeroMQ event, in case the lidar dies
 	check_send_mesh()
 
 end
 
--- If required from Webots, return the table
-if ... and type(...)=='string' then
-	return {entry=nil, update=update, exit=nil}
+local function exit()
+if ENABLE_LOG then
+	logger0:stop()
+	logger1:stop()
+end
 end
 
-local lidar_ch = si.new_subscriber'lidar0'
-function lidar_ch.callback(skt)
+-- If required from Webots, return the table
+if ... and type(...)=='string' then
+	return {entry=entry, update=update, exit=exit}
+end
+
+local poller
+local function cb(skt)
+	local idx = poller.lut[skt]
 	local mdata, ranges = unpack(skt:recv_all())
 	local meta = munpack(mdata)
 	update(meta, ranges)
 end
-local poller = si.wait_on_channels({lidar_ch})
+
+local lidar0_ch = si.new_subscriber'lidar0'
+local lidar1_ch = si.new_subscriber'lidar1'
+
+lidar0_ch.callback = cb
+lidar1_ch.callback = cb
+poller = si.wait_on_channels({lidar0_ch, lidar1_ch})
 
 -- Cleanly exit on Ctrl-C
+local signal = require'signal'.signal
 local running = true
 local function shutdown()
-	print('Shutdown!')
+	io.write('Shutdown!\n')
 	poller:stop()
 end
-
-local signal = require'signal'.signal
 signal("SIGINT", shutdown)
 signal("SIGTERM", shutdown)
 
+entry()
 poller:start()
-
-if ENABLE_LOG then
-	logger:stop()
-end
+exit()

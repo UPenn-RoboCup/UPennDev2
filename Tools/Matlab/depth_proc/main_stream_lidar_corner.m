1756a7854b70db7166484f17c0e244237e277335
SJ@NUC <seungjoon.yi@gmail.com>
2015-05-21 01:31:04 -0400

    move or change name of function

--- Tools/Matlab/depth_proc/main_stream_lidar_corner.m
+++ Tools/Matlab/depth_proc/main_stream_lidar_corner.m
@@ -1,14 +1,11 @@
 clear all;
 close all;
 
-datetime.setDefaultFormats('defaultdate','yyyy-MM-dd');
-
 % 1 second timeout
 %s_depth = zmq('subscribe', 'tcp', '192.168.123.246', 43346);
 %s_color = zmq('subscribe', 'tcp', '192.168.123.246', 43347);
 %s_mesh = zmq('subscribe', 'tcp', '192.168.123.246', 43344);
 s_mesh = zmq( 'subscribe', 'ipc', 'mesh0' );
-addpath('./util');
 
 count = 0;
 while 1
@@ -24,7 +21,7 @@ while 1
         [metadata,offset] = msgpack('unpack', data);
         if has_more, [raw, has_more] = zmq('receive', s_idx); end
         if strcmp(char(metadata.id), 'mesh0')          
-            if (mod(count,2) == 0) 
+            if (mod(count,1) == 0) 
                 
                 metadata.dims = metadata.dim;
                 metadata.flag = 1;
@@ -32,7 +29,22 @@ while 1
 
                 [ Planes ] = detectPlaneInstances_lidar_loc( raw, 4, metadata);    
                 pose = localizeCorner_v4L(Planes,metadata)
-               
+                 if (pose.isValid1 + pose.isValid2) > 0
+                    if pose.isValid1 == 1 && pose.isValid2 == 0
+                        distance = pose.x;
+                    else % both 1
+                        if pose.theta_body > 0
+                            distance = [pose.x pose.y];
+                        else
+                            distance = [pose.y pose.x];                            
+                        end
+                    end
+                    yaw = pi-pose.theta_body;
+                    
+                    data = struct('dist',distance, 'yaw',yaw); % yaw degree
+                    packed_data=msgpack('pack',data);                    
+                    zmq('send',s_field,packed_data);
+                 end
             end
            count = count + 1;
         end

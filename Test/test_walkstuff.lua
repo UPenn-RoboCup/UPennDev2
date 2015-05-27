eb7f720032d88950ee2fc07f2fae154d334baa6e
NUC 2 <NUC 2>
2015-05-26 20:53:51 -0400

    tested stair a bit

--- Test/test_walkstuff.lua
+++ Test/test_walkstuff.lua
@@ -45,6 +45,11 @@ local function update(key_code)
 	local ltorso = hcm.get_legdebug_torso()
 
 	local torsoangle = hcm.get_legdebug_torso_angle()
+
+
+	local stairh = 0.22
+
+
 	 if key_char_lower==("i") then      targetvel_new[1]=targetvel[1]+0.02;
     elseif key_char_lower==("j") then  targetvel_new[3]=targetvel[3]+0.1;
     elseif key_char_lower==("k") then  targetvel_new[1],targetvel_new[2],targetvel_new[3]=0,0,0;
@@ -60,44 +65,28 @@ local function update(key_code)
 
 elseif key_char_lower==("3") then      
 		hcm.set_step_supportLeg(1)		
---		hcm.set_step_relpos({0.35,0,0})
-		--hcm.set_step_relpos({0.0,0,0})
-		
-		hcm.set_step_zpr({0,0,0})
 		hcm.set_step_relpos({0.30,0,0})
-
-		
-		hcm.set_step_zpr({0.225,0,0}) --stair
-		
+		hcm.set_step_zpr({stairh,0,0}) --stair
 		body_ch:send'stepover1'		
 
 elseif key_char_lower==("4") then      
 		hcm.set_step_supportLeg(0)
-		
---		hcm.set_step_relpos({0.35,0,0})
-		--hcm.set_step_relpos({0.0,0,0})
-		hcm.set_step_zpr({0.00,0,0})
-
 		hcm.set_step_relpos({0.30,0,0})
-
-		hcm.set_step_zpr({0.225,0,0}) --stair
-		
+		hcm.set_step_zpr({stairh,0,0}) --stair
 		body_ch:send'stepover1'		
 
 
 	elseif key_char_lower==("5") then      
-		hcm.set_step_supportLeg(1)
-		hcm.set_step_relpos({0.16,0,0})
---		hcm.set_step_relpos({-0.28,0,0})
-		hcm.set_step_zpr({0.00,0,0})
-		body_ch:send'stepflat'		
+		hcm.set_step_supportLeg(1)		
+		hcm.set_step_relpos({-0.30,0,0})
+		hcm.set_step_zpr({-stairh,0,0}) --stair
+		body_ch:send'stepover1'		
 
 	elseif key_char_lower==("6") then      
 		hcm.set_step_supportLeg(0)
-		hcm.set_step_relpos({0.16,0,0})
-		hcm.set_step_zpr({0.00,0,0})
-		body_ch:send'stepflat'
-
+		hcm.set_step_relpos({-0.30,0,0})
+		hcm.set_step_zpr({-stairh,0,0}) --stair
+		body_ch:send'stepover1'		
 
 
 

88e3151a6007b0c0d62d3e4eb37b7975484a85d5
teddy2 <teddy2_NUC>
2015-06-02 11:55:41 -0700

    audio in start all

--- Run/audio_wizard.lua
+++ Run/audio_wizard.lua
@@ -5,30 +5,36 @@ dofile'../include.lua'
 require'hcm'
 
 local cmdstr = {
-	'arecord -f S16_LE -c1 -D hw:2,0 -t raw | ./vumeter',
-	'arecord -f S16_LE -c2 -D hw:3,0 -t raw | ./vumeter',
-	-- flip
-	'arecord -f S16_LE -c2 -D hw:2,0 -t raw | ./vumeter',
-	'arecord -f S16_LE -c1 -D hw:3,0 -t raw | ./vumeter'
+	'arecord -f S16_LE -r16000 -c1 -D hw:2,0 -t raw | ./vumeter',
+	'arecord -f S16_LE -r16000 -c2 -D hw:3,0 -t raw | ./vumeter',
 }
 
-hcm.set_audio_volume(percent)
-hcm.set_audio_rawvolume(raw)
+print('arg[1]', arg[1])
+local idx = tonumber(arg[1]) or 1
+local cmd = cmdstr[idx]
 
 -- TODO: Reliably opening these, regardless of the dev number
-local f = assert(io.popen(cmdstr[arg[1]]or cmdstr[1]))
+local f = assert(io.popen(cmd))
 f:setvbuf'no'
+
+local volume_ptr = hcm.audioPtr.volume
+local rawvolume_ptr = hcm.audioPtr.rawvolume
 
 local level
 repeat
 	level = f:read('*line')
+	print('vumeter', level)
 	local raw, percent = level:match("(%d+) (%d+)")
 	raw = tonumber(raw)
 	percent = tonumber(percent)
-	print('from vumeter', level)
-	print('Volume', raw, percent)
-	hcm.set_audio_volume(percent)
-	hcm.set_audio_rawvolume(raw)
+	if percent then
+		print('Volume', percent, idx-1)
+		volume_ptr[idx-1] = percent
+	end
+	if raw then
+		print('Raw', raw, idx-1)
+		--rawvolume_ptr[idx-1] = raw
+	end
 until not level
 print('done reading!')
 

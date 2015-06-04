26f76f77d684560210cb4d7a12b9e94f60dd9611
Stephen McGill <smcgill3@seas.upenn.edu>
2015-06-04 12:14:27 -0700

    ok

--- Test/test_drive.lua
+++ Test/test_drive.lua
@@ -12,11 +12,12 @@ if not ok then dofile'riddle.lua' end
 local targetvel = {0,0,0}
 local targetvel_new = {0,0,0}
 local WAS_REQUIRED
+local get_time = require'unix'.time
+
 
 local t_last = get_time()
 local tDelay = 0.005*1E6
 
-local get_time = require'unix'.time
 
 local throttle = 0
 local steering = 0
@@ -40,36 +41,36 @@ local function update(key_code)
   elseif key_char_lower==("j") then      
     steering = steering - 45*math.pi/180
     print("Steering angle:",steering*180/math.pi)
-    hcm.set_teleop_steering(steering)
+    hcm.set_teleop_steering{steering}
 
   elseif key_char_lower==("k") then      
     steering = 0
     print("Steering angle:",steering*180/math.pi)
-    hcm.set_teleop_steering(steering)
+    hcm.set_teleop_steering{steering}
 
   elseif key_char_lower==("l") then      
     steering = steering + 45*math.pi/180
     print("Steering angle:",steering*180/math.pi)
-    hcm.set_teleop_steering(steering)
+    hcm.set_teleop_steering{steering}
 
   elseif key_char_lower==("w") then
     throttle = math.min(throttle + 10*math.pi/180,30*math.pi/180)
     print("Throttle:  ",throttle*RAD_TO_DEG)
-    hcm.set_teleop_throttle(throttle)
+    hcm.set_teleop_throttle{throttle}
 
   elseif key_char_lower==("s") then
     throttle = 0
     print("Throttle: ",throttle*RAD_TO_DEG)
-    hcm.set_teleop_throttle(throttle)
+    hcm.set_teleop_throttle{throttle}
 
   elseif key_char_lower==("x") then
     throttle = math.max(throttle - 10*math.pi/180,0*math.pi/180)
     print("Throttle:  ",throttle*RAD_TO_DEG)
-    hcm.set_teleop_throttle(throttle)
+    hcm.set_teleop_throttle{throttle}
 
   elseif key_char_lower==(" ") then
     print("2 sec burst")
-    hcm.set_teleop_throttle_duration(2)
+    hcm.set_teleop_throttle_duration{2}
   end
 	-- end
 end

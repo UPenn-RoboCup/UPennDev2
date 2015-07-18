c2c4a65d50674c7370e9600d2aab2e818cd3941f
Stephen McGill <smcgill3@seas.upenn.edu>
2015-07-18 05:21:02 -0400

    gc update jinwook

--- Run/gc_wizard.lua
+++ Run/gc_wizard.lua
@@ -22,8 +22,8 @@ if not IS_WEBOTS then
 end
 
 local ENABLE_COACH = false
-local teamId = 36
-local gc = libGC.init(teamId, 1, '192.168.100.1')
+local teamId = 44  --36
+local gc = libGC.init(teamId, 1, '192.168.44.1')
 
 local OUR_GAME
 local ret, msg
@@ -37,12 +37,12 @@ gcm.set_game_state(Config.default_state or 5) --Pre-init
 
 --To stop the head movement for tester role
 local head_ch = require'simple_ipc'.new_publisher('HeadFSM!')
-
 local function process_switch(t)
 	if t-t_switch < 1 then return end
 	t_switch = t
 	led_count = led_count +1
-	local current_lidar_deg = Body.get_lidar_position()[1] * RAD_TO_DEG  
+--	local current_lidar_deg = Body.get_lidar_position()[1] * RAD_TO_DEG  
+	local current_lidar_deg = 90
 	local cur_position 
 	if current_lidar_deg>45 then
 		cur_position = 1
@@ -79,6 +79,7 @@ local function process_switch(t)
 		else
 			rgb = {255,0,255}
 		end
+	local intensity = 1.0
 		Body.set_head_led_red(rgb[1]*intensity)
     Body.set_head_led_blue(rgb[3]*intensity)
     Body.set_head_led_green(rgb[3]*intensity)
@@ -92,27 +93,36 @@ local function process_switch(t)
 end
 
 local function process_packet(pkt, t)
-	if not pkt then
-		if OUR_GAME then
-			local secs = gcm.get_game_secsremaining()
-			secs = secs - (t - t_timeupdate)
-			t_timeupdate = t
-			gcm.set_game_secsremaining(secs)
-		end
-		return
-	end
+	if not pkt then return end
 	local teams = pkt.teams
 	OUR_GAME = teams[0].teamNumber==teamId or teams[1].teamNumber==teamId
+	--[[
+	print("temaId:"..teamId)
+	print("teams[0]:"..teams[0].teamNumber)
+	print("teams[1]:"..teams[1].teamNumber)
+	if OUR_GAME == true then
+		print("OUR_GAME")
+	end
+	if OUR_GAME == false then
+		print("Not OUR_GAME")
+	end
+	--]]
+
+	--print("temaNumber:"..teamNumber)
+	
 	-- If not our game, then return
 	if not OUR_GAME then return end
 	-- Set things
 	gcm.set_game_gctime(t) -- GC time of received packet
 	gcm.set_game_state(pkt.state)
-	gcm.set_game_secsremaining(pkt.secsRemaining)
+--	gcm.set_game_secsremaining(pkt.secsRemaining)
+	gcm.set_game_timeleft(pkt.secsRemaining)
+
 	t_timeupdate = t
 	-- Save the packet number
 	last_pkt = cur_pkt
-	cur_pkt = gc_pkt.packetNumber
+	cur_pkt = pkt.packetNumber
+
 	-- Send a return to the game controller
 	ret, msg = gc:send_return()
 	if msg then print("GC | Return error", msg) end
@@ -123,20 +133,37 @@ local function process_packet(pkt, t)
 	-- Print debug
 	local state_name = libGC.state_to_name[pkt.state] or "UNKNOWN"
 	local debug_str = table.concat({
-		color("State: "..state_name 'yellow'),
+		color("State: "..state_name, 'yellow'),
 		color('Packet '..cur_pkt, 'green'),
-		color(gc_pkt.secsRemaining..' seconds left', 'red'),
+		color(pkt.secsRemaining..' seconds left', 'red'),
 		''
 	}, '\n')
 	print(debug_str)
+	print(role)
 end
 
 while running do
   collectgarbage('step')
 	pkt = gc:wait_for_gc()
   t = get_time()
-	process_packet(pkt)
-	process_switch()
+	if pkt then
+		process_packet(pkt, t)
+	end
+	--print(pkt)
+--	if pkt then
+--		print("secsRemaing:"..pkt.secsRemaining)
+--		if (pkt.state == 2) then
+--		print("state:".."set")
+--		end
+--		if (pkt.state == 3) then
+--		print("state:".."play")
+--		end
+--		if (pkt.state == 4) then
+--		print("state:".."finish")
+--	 	end
+
+--	end
+	--process_switch(t)
   if ENABLE_COACH and t - t_send > SEND_RATE then
     t_send = t
 		ret, msg = gc:send_coach("go team!")
@@ -144,6 +171,6 @@ while running do
     send_count = send_count + 1
   end
 	if t - t_pkt > 2 then
-		print("GC | TIMEOUT")
+		--print("GC | TIMEOUT")
 	end
 end

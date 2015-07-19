a7c883b282c3bc10b0e0f3168b797bd5527ca883
Stephen McGill <smcgill3@seas.upenn.edu>
2015-07-19 14:45:45 +0800

    game check

---
+++ check_game.lua
@@ -0,0 +1,46 @@
+#!/usr/bin/env luajit
+-- (c) 2014 Stephen McGill
+dofile'include.lua'
+local util = require'util'
+
+local processes = {
+  'run_dcm',
+  'run_imu',
+  'state_wizard',
+  'camera_wizard',
+  'vision_wizard',
+  'world_wizard',
+  'gc_wizard',
+}
+local is_running = {}
+for _, v in ipairs(processes) do
+  is_running[v] = false
+end
+
+local pgrep = io.popen("pgrep -fl luajit")
+
+for l in pgrep:lines() do
+  local m = l:match("luajit%s+(.+)")
+  for _, p in ipairs(processes) do
+    if m and m:find(p) then
+      is_running[p] = true
+    end
+  end
+end
+
+print(util.color("==== CHECKING STATUS ===",'yellow'))
+
+local is_done = true
+for k, v in pairs(is_running) do
+  if not v then
+    is_done = false
+    print("NOT RUNNING", util.color(k, 'red'))
+  else
+    print(util.color(k, 'green'))
+  end
+end
+
+if not is_done then
+  print("You are not ready for a game!")
+  os.exit(1)
+end

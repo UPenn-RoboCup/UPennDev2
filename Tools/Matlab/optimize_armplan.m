de5e1e25e22a1d1dd50d61f099ac324f08e0856c
Stephen McGill <smcgill3@seas.upenn.edu>
2015-09-01 10:11:48 -0400

    Got the optimization to run

--- Tools/Matlab/optimize_armplan.m
+++ Tools/Matlab/optimize_armplan.m
@@ -11,23 +11,38 @@ load plan;
 
 % Acceleration matrix
 nq = 7;
-A = zeros(nq, nq*3);
+n = numel(bigQ);
+np = n / nq;
+A0 = zeros(nq, nq*3);
 a = zeros(1, nq*3);
 a(1) = -1;
 a(nq+1) = 2;
 a(2*nq+1) = -1;
 for i=1:nq
-    A(i,:) = circshift(a, i-1, 2);
+    A0(i,:) = circshift(a, i-1, 2);
 end
+A = zeros(size(bigNulls));
+for i=1:np-2
+    A( (i-1)*nq+1 : i*nq, (i-1)*nq+1 : (i+2)*nq) = A0;
+end
+A( (np-2)*nq+1 : (np-1)*nq, (np-2)*nq+1 : n ) = A0(:,1:2*nq);
+A( (np-1)*nq+1 : n, (np-1)*nq+1 : n ) = A0(:,1:nq);
 
-P0 = bigNulls' * bigNulls;
-P1 = 
+NTN = bigNulls' * bigNulls;
+ATA = A'*A;
+epsilon = deg2rad(5);
+%
+P0 = NTN + ATA;
+q0 = -2 * bigQstar' * NTN;
+r0 = bigQstar' * NTN * bigQstar;
+%
+q1 = -2 * bigQ';
+r1 = bigQ' * bigQ - epsilon;
 
-P1 = randn(n); P1 = P1'*P1;
-P2 = randn(n); P2 = P2'*P2;
-P3 = randn(n); P3 = P3'*P3;
-q0 = randn(n,1); q1 = randn(n,1); q2 = randn(n,1); q3 = randn(n,1);
-r0 = randn(1); r1 = randn(1); r2 = randn(1); r3 = randn(1);
+% Flip dimensions...
+P0 = P0';
+q0 = q0';
+q1 = q1';
 
 fprintf(1,'Computing the optimal value of the QCQP and its dual... ');
 
@@ -35,15 +50,20 @@ cvx_begin
     variable q(n)
     dual variables lam1 lam2 lam3
     minimize( 0.5*quad_form(q,P0) + q0'*q + r0 )
-    lam1: 0.5*quad_form(q,P1) + q1'*x + r1 <= 0;
-    lam2: 0.5*quad_form(q,P2) + q2'*x + r2 <= 0;
-    lam3: 0.5*quad_form(q,P3) + q3'*x + r3 <= 0;
+    lam1: 0.5*norm(q) + q1'*q + r1 <= 0;
+    %lam1: 0.5*quad_form(q,P1) + q1'*x + r1 <= 0;
+    %lam2: 0.5*quad_form(q,P2) + q2'*x + r2 <= 0;
+    %lam3: 0.5*quad_form(q,P3) + q3'*x + r3 <= 0;
 cvx_end
 
 obj1 = cvx_optval;
-P_lam = P0 + lam1*P1 + lam2*P2 + lam3*P3;
-q_lam = q0 + lam1*q1 + lam2*q2 + lam3*q3;
-r_lam = r0 + lam1*r1 + lam2*r2 + lam3*r3;
+P_lam = P0 + lam1;
+q_lam = q0 + lam1*q1;
+r_lam = r0 + lam1*r1;
+
+% P_lam = P0 + lam1*P1 + lam2*P2 + lam3*P3;
+% q_lam = q0 + lam1*q1 + lam2*q2 + lam3*q3;
+% r_lam = r0 + lam1*r1 + lam2*r2 + lam3*r3;
 obj2 = -0.5*q_lam'*inv(P_lam)*q_lam + r_lam;
 
 fprintf(1,'Done! \n');

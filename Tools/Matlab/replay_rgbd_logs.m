66d4a1e2477ade026df55fd6561475800988b5c8
Stephen McGill <smcgill3@seas.upenn.edu>
2015-12-08 21:34:23 -0500

    add color

--- Tools/Matlab/replay_rgbd_logs.m
+++ Tools/Matlab/replay_rgbd_logs.m
@@ -9,7 +9,7 @@ h=240;
 
 h_f = figure(1);
 h_a = gca;
-h_img = imagesc(img);
+h_img = imagesc;
 
 for i=1:numel(d)
     fname = d(i).name;
@@ -39,18 +39,33 @@ prefix = 'k_rgb';
 suffix = '.log';
 d = dir(strcat('Data/', prefix, '_m_*', suffix));
 
+w=320;
+h=240;
+
+h_f = figure(2);
+h_a = gca;
+h_img = imagesc;
+
 for i=1:numel(d)
-    fname = d(1).name;
+    fname = d(i).name;
     fid = fopen(fname);
     msg = fread(fid, inf, '*uchar');
     fclose(fid);
     objs = msgpack('unpacker', msg);
     clear msg fid;
-    %
     timestamp = fname(numel(prefix)+4:end-numel(suffix));
     fname = strcat(logdir, prefix, '_r_', timestamp, suffix);
     fid = fopen(fname);
-    %jimg = fread(fid,objs{i}.sz,'*uchar');
+    for o=1:numel(objs)
+        metadata = objs{o};
+        img = fread(fid, w*h*3, '*uint8');
+        img = reshape(img, [3, w, h]);
+        img = permute(img, [3,2,1]);
+        img = flipud(img);
+        set(h_img,'cdata', img);
+        title(h_a,sprintf('Log %d | %d',i,o));
+        %log_img = djpeg(jimg);
+        pause(1/30);
+    end
     fclose(fid);
-    %log_img = djpeg(jimg);
 end

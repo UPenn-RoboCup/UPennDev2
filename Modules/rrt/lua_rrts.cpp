ab9306ec4d936b9fd8c3d026f58a61fec2bd2c95
Stephen McGill <smcgill3@seas.upenn.edu>
2016-04-14 21:59:25 -0400

    Table based for using with other plans

--- Modules/rrt/lua_rrts.cpp
+++ Modules/rrt/lua_rrts.cpp
@@ -25,201 +25,227 @@ typedef Planner<State,Trajectory,System> planner_t;
 typedef Vertex<State,Trajectory,System> vertex_t;
 
 static int lua_rrts_plan(lua_State *L) {
-    
-  // This parameter should be larger than 1.5 for asymptotic 
-  //   optimality. Larger values will weigh on optimization 
-  //   rather than exploration in the RRT* algorithm. Lower 
-  //   values, such as 0.1, should recover the RRT.
-  const double gamma = luaL_optnumber(L, 6, 1.5);
-  const int nIterations = luaL_optint(L, 7, 10000);
-  
-  // Initialize the planner
-  planner_t rrts;
-  
-  // Create the dynamical system that checks obstacles and closeness
-  System system;
-  
-  
-  // Initial pose
-  luaL_checktype(L, 1, LUA_TTABLE);
+
+	// This parameter should be larger than 1.5 for asymptotic 
+	//   optimality. Larger values will weigh on optimization 
+	//   rather than exploration in the RRT* algorithm. Lower 
+	//   values, such as 0.1, should recover the RRT.
+	const double gamma = luaL_optnumber(L, 3, 1.5);
+	const int nIterations = luaL_optint(L, 4, 10000);
+
+	// Initialize the planner
+	planner_t rrts;
+
+	// Create the dynamical system that checks obstacles and closeness
+	System system;
+
+	// fk pose
+	lua_pushliteral(L, "trGoal");
+	lua_rawget(L, 1);
+	luaL_checktype(L, -1, LUA_TTABLE);
 #if LUA_VERSION_NUM == 502
-		int nDim = lua_rawlen(L, 1);
+	if(6 != lua_rawlen(L, -1)){ return luaL_error(L, "Bad fk dimensions"); }
 #else
-		int nDim = lua_objlen(L, 1);
+	if(6 != lua_objlen(L, -1)){ return luaL_error(L, "Bad fk dimensions"); }
 #endif
-    system.setNumDimensions(nDim);
-    
-    // fk pose
-    luaL_checktype(L, 2, LUA_TTABLE);
-  #if LUA_VERSION_NUM == 502
-  		if(6 != lua_rawlen(L, 2)){ return luaL_error(L, "Bad fk dimensions"); }
-  #else
-      if(6 != lua_objlen(L, 2)){ return luaL_error(L, "Bad fk dimensions"); }
-  #endif
-      for(int i = 0;i<6;i++){
-        lua_rawgeti(L, 2, i+1);
-        xyzG[i] = lua_tonumber(L, -1);
-        lua_pop(L, 1);
-      }
-    
-    // TODO: This should be optional for goal biasing...
-  // Guessed pose
-  luaL_checktype(L, 3, LUA_TTABLE);
+	for(int i = 0;i<6;i++){
+		lua_rawgeti(L, -1, i+1);
+		xyzG[i] = luaL_checknumber(L, -1);
+		lua_pop(L, 1);
+#ifdef DEBUG
+    printf("TR GOAL: %f\n", xyzG[i]);
+#endif
+	}
+  
+	// Grab the initial pose
+	lua_pushliteral(L, "qArm0");
+	lua_rawget(L, 1);
+	luaL_checktype(L, -1, LUA_TTABLE);
 #if LUA_VERSION_NUM == 502
-		if(nDim != lua_rawlen(L, 3)){ return luaL_error(L, "Bad goal dimensions"); }
+	int nDim = lua_rawlen(L, -1);
 #else
-    if(nDim != lua_objlen(L, 3)){ return luaL_error(L, "Bad goal dimensions"); }
+	int nDim = lua_objlen(L, -1);
 #endif
-  // Define the goal region
-  system.regionGoal.setNumDimensions(nDim);  
+	system.setNumDimensions(nDim);
+
+	// Define the region dimensions
+  system.regionGoal.setNumDimensions(nDim);
+	system.regionOperating.setNumDimensions(nDim);
+
+	// Guessed pose (Option for goal biasing)
+	lua_pushliteral(L, "qArmGuess");
+	lua_rawget(L, 1);
+	luaL_checktype(L, -1, LUA_TTABLE);
+#if LUA_VERSION_NUM == 502
+	if(nDim != lua_rawlen(L, -1)){ return luaL_error(L, "Bad goal dimensions"); }
+#else
+	if(nDim != lua_objlen(L, -1)){ return luaL_error(L, "Bad goal dimensions"); }
+#endif
 	for (int i = 0; i < nDim; i++) {
-    lua_rawgeti(L, 3, i+1);
-		system.regionGoal.center[i] = lua_tonumber(L, -1);
-    system.regionGoal.size[i] = 2 * DEGREE_TOLERANCE * M_PI / 180;
+		lua_rawgeti(L, -1, i+1);
+		system.regionGoal.center[i] = luaL_checknumber(L, -1);
+    lua_pop(L, 1);
+		system.regionGoal.size[i] = 2 * DEGREE_TOLERANCE * M_PI / 180;
+		
+#ifdef DEBUG
+    printf("GUESS: %f\n", system.regionGoal.center[i]);
+#endif
+
+	}
+
+	// qMid
+	lua_pushliteral(L, "qMid");
+	lua_rawget(L, 2);
+	luaL_checktype(L, -1, LUA_TTABLE);
+#if LUA_VERSION_NUM == 502
+	if(nDim != lua_rawlen(L, -1)){ return luaL_error(L, "Bad mid dimensions"); }
+#else
+	if(nDim != lua_objlen(L, -1)){ return luaL_error(L, "Bad mid dimensions"); }
+#endif
+	for(int i = 0;i<nDim;i++){
+		lua_rawgeti(L, -1, i+1);
+		system.regionOperating.center[i] = luaL_checknumber(L, -1);
 		lua_pop(L, 1);
 	}
-    
-    
-  // qMid
-  luaL_checktype(L, 4, LUA_TTABLE);
-  // qRange
-  luaL_checktype(L, 5, LUA_TTABLE);
-  #if LUA_VERSION_NUM == 502
-  		if(nDim != lua_rawlen(L, 4)){ return luaL_error(L, "Bad mid dimensions"); }
-      if(nDim != lua_rawlen(L, 5)){ return luaL_error(L, "Bad range dimensions"); }
-  #else
-      if(nDim != lua_objlen(L, 4)){ return luaL_error(L, "Bad mid dimensions"); }
-      if(nDim != lua_objlen(L, 5)){ return luaL_error(L, "Bad range dimensions"); }
-  #endif
-
-  // Define the operating region (Mid, Size)
-  system.regionOperating.setNumDimensions(nDim);
-  for(int i = 0;i<nDim;i++){
-    lua_rawgeti(L, 4, i+1);
-    system.regionOperating.center[i] = lua_tonumber(L, -1);
-    lua_pop(L, 1);
-    lua_rawgeti(L, 5, i+1);
-    system.regionOperating.size[i] = lua_tonumber(L, -1);
-    lua_pop(L, 1);
-  }
-    
-  // Add the system to the planner
-  rrts.setSystem (system);
   
-  // Set up the root vertex
-  vertex_t &root = rrts.getRootVertex();  
-  State &rootState = root.getState();
-  
-  // Get the initial state
-	for (int i = 0; i < nDim; i++) {
-    lua_rawgeti(L, 1, i+1);
-		rootState[i] = lua_tonumber(L, -1);
+	// qRange
+	lua_pushliteral(L, "qRange");
+	lua_rawget(L, 2);
+	luaL_checktype(L, -1, LUA_TTABLE);
+#if LUA_VERSION_NUM == 502
+	if(nDim != lua_rawlen(L, -1)){ return luaL_error(L, "Bad range dimensions"); }
+#else
+	if(nDim != lua_objlen(L, -1)){ return luaL_error(L, "Bad range dimensions"); }
+#endif
+	for(int i = 0;i<nDim;i++){
+		lua_rawgeti(L, -1, i+1);
+		system.regionOperating.size[i] = luaL_checknumber(L, -1);
 		lua_pop(L, 1);
 	}
-    
-  // Initialize the planner
-  rrts.initialize();
-  rrts.setGamma(gamma);
 
-    double* ostate = NULL;
-    double* pstate = NULL;
-    
-#ifdef CSV
-    FILE * pFile;
-    pFile = fopen ("/tmp/samples.csv","w");
-    if(pFile != NULL){
-      ostate = new double[nDim];
-      pstate = new double[nDim];
-    }
-#endif
-    
-#ifdef TIMING
-    clock_t start = clock();
-#endif
-    
-    // Run the algorithm for 10000 iteartions
-    for (int i = 0; i < nIterations; i++){
-#ifdef TIMING
-      if(i%1000 == 0){
-        clock_t now = clock();
-        printf("Completion: %4.2f, %5.2f sec\n", i/(double)nIterations, ((double)(now-start))/CLOCKS_PER_SEC);
-      }
-#endif
+	// Add the system to the planner
+	rrts.setSystem (system);
 
-      rrts.iteration(ostate, pstate);
+	// Set up the root vertex
+	vertex_t &root = rrts.getRootVertex();  
+	State &rootState = root.getState();
+
+	// Get the initial state
+	// Grab the initial pose
+	lua_pushliteral(L, "qArm0");
+	lua_rawget(L, 1);
+	for (int i = 0; i < nDim; i++) {
+		lua_rawgeti(L, -1, i+1);
+		rootState[i] = luaL_checknumber(L, -1);
+		lua_pop(L, 1);
+#ifdef DEBUG
+    printf("qArm0: %f\n", rootState[i]);
+#endif
+	}
+
+	// Initialize the planner
+	rrts.initialize();
+	rrts.setGamma(gamma);
+
+	double* ostate = NULL;
+	double* pstate = NULL;
 
 #ifdef CSV
-      rrts.iteration(ostate, pstate);
-      if (pFile!=NULL){
-        for(int i=0;i<nDim;i++){
-          fprintf(pFile, "%.2f ", pstate[i]);
-        }
-        for(int i=0;i<nDim;i++){
-          fprintf(pFile, "%.2f ", ostate[i]);
-        }
-        fprintf(pFile, "\n");
-      }
+	FILE * pFile;
+	pFile = fopen ("/tmp/samples.csv","w");
+	if(pFile != NULL){
+		ostate = new double[nDim];
+		pstate = new double[nDim];
+	}
 #endif
-      
-    }
 
 #ifdef TIMING
-    clock_t finish = clock();
-    printf("TimeL %.2f\n", ((double)(finish-start))/CLOCKS_PER_SEC);
-    fflush(stdout);
+	clock_t start = clock();
+#endif
+
+	// Run the algorithm for 10000 iteartions
+	for (int i = 0; i < nIterations; i++){
+#ifdef TIMING
+		if(i%1000 == 0){
+			clock_t now = clock();
+			printf("Completion: %4.2f, %5.2f sec\n", i/(double)nIterations, ((double)(now-start))/CLOCKS_PER_SEC);
+		}
+#endif
+
+		rrts.iteration(ostate, pstate);
+
+#ifdef CSV
+		rrts.iteration(ostate, pstate);
+		if (pFile!=NULL){
+			for(int i=0;i<nDim;i++){
+				fprintf(pFile, "%.2f ", pstate[i]);
+			}
+			for(int i=0;i<nDim;i++){
+				fprintf(pFile, "%.2f ", ostate[i]);
+			}
+			fprintf(pFile, "\n");
+		}
+#endif
+
+	}
+
+#ifdef TIMING
+	clock_t finish = clock();
+	printf("Time: %.2f\n", ((double)(finish-start))/CLOCKS_PER_SEC);
+	fflush(stdout);
 #endif
 
 #ifdef CSV
-    if (pFile!=NULL)
-    {
-      fclose (pFile);
-    }
+	if (pFile!=NULL)
+	{
+		fclose (pFile);
+	}
 #endif
 
-  std::list<double*> finalTraj;
-  int ret = rrts.getBestTrajectory(finalTraj);
+	std::list<double*> finalTraj;
+	int ret = rrts.getBestTrajectory(finalTraj);
 
-  // No path found
-  if(!ret){ return 0; }
-  
-  lua_createtable(L, finalTraj.size(), 0);
-  //printf("Num path: %d\n", finalTraj.size());
-  int j = 0;
-  for (std::list<double*>::iterator it = finalTraj.begin(); it != finalTraj.end(); it++){
-    double* coord = *it;
-    lua_createtable(L, nDim, 0);
-    for (int i = 0; i < nDim; i++) {
-      lua_pushnumber(L, coord[i]);
-      lua_rawseti(L, -2, i+1);
-    }
-    lua_rawseti(L, -2, ++j);
-  }
-  
-  return 1;
+	// No path found
+	if(!ret){ return 0; }
+
+	lua_createtable(L, finalTraj.size(), 0);
+	//printf("Num path: %d\n", finalTraj.size());
+	int j = 0;
+	for (std::list<double*>::iterator it = finalTraj.begin(); it != finalTraj.end(); it++){
+		double* coord = *it;
+		lua_createtable(L, nDim, 0);
+		for (int i = 0; i < nDim; i++) {
+			lua_pushnumber(L, coord[i]);
+			lua_rawseti(L, -2, i+1);
+		}
+		lua_rawseti(L, -2, ++j);
+	}
+
+	return 1;
 }
 
 static int lua_rrts_smooth(lua_State *L) {
-  return 0;
+	return 0;
 }
 
 static const struct luaL_Reg rrts_lib [] = {
-  {"plan", lua_rrts_plan},
-  {"smooth", lua_rrts_smooth},
-  {NULL, NULL}
+	{"plan", lua_rrts_plan},
+	{"smooth", lua_rrts_smooth},
+	{NULL, NULL}
 };
 
 extern "C" int luaopen_rrts(lua_State *L) {
 #if LUA_VERSION_NUM == 502
-  luaL_newlib(L, rrts_lib);
+	luaL_newlib(L, rrts_lib);
 #else
-  luaL_register(L, "rrts", rrts_lib);
+	luaL_register(L, "rrts", rrts_lib);
 #endif
-  
-  // Seed the random bits
-  srand (time(NULL));
-  //srand(123);
-  
-  return 1;
+
+	// Seed the random bits
+	srand (time(NULL));
+	//srand(123);
+
+	return 1;
 }
 
 
--- Modules/rrt/test_rrts.lua
+++ Modules/rrt/test_rrts.lua
@@ -2,13 +2,23 @@
 require'rrts'
 
 local gamma = 0.75
+local nIter = 1e4
 
 print("Solving with gamma=", gamma)
-local plan
+local plan = {
+  qArm0 = {1.91986, 0, 0, -2.7, 0, 0.785398, 0},
+  trGoal = {0.46, 0.27, 0.3, 0, 0, -math.pi/3},
+  qArmGuess = {-0.349, 1.047, -1.57, -2.094, 0, -0.785, 0},
+}
+local planner = {
+  qMid = {0.785398, 0.759218, 0.392699, -1.39626, 0, 0, 0},
+  qRange = {4.71239, 1.51844, 3.92699, 2.79253, 6.28319, 3.03687, 6.28319}
+}
 
 for i=1,5 do
-
-  plan = rrts.plan(
+  path = rrts.plan(plan, planner, gamma, nIter)
+  --[[
+  path = rrts.plan(
     {1.91986, 0, 0, -2.7, 0, 0.785398, 0}, -- start
     {0.46, 0.27, 0.3, 0, 0, -math.pi/3}, -- fk target
     {-0.349, 1.047, -1.57, -2.094, 0, -0.785, 0}, -- goal
@@ -17,13 +27,14 @@ for i=1,5 do
     gamma,
     1e4
   )
-  if plan then break end
+--]]
+  if path then break end
   
 end
 
-if type(plan)=='table' then
-  print("Plan", plan, #plan)
-  for i, v in ipairs(plan)
+if type(path)=='table' then
+  print("path", path, #path)
+  for i, v in ipairs(path)
   do
     if type(v)=='table' then
       io.write(i, ': {', table.concat(v, ', '), '}', '\n')
